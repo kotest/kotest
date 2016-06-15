2bcd9073069a50ea1b954942872fa5658eb02d71
Christian Helmbold <christian@helmbold.de>
2016-06-15 22:16:54 +0200

    fix EventuallyTest (change to Duration)

--- src/test/kotlin/io/kotlintest/EventuallyTest.kt
+++ src/test/kotlin/io/kotlintest/EventuallyTest.kt
@@ -1,27 +1,28 @@
 package io.kotlintest
 
 import io.kotlintest.specs.WordSpec
-import java.util.concurrent.TimeUnit
+import io.kotlintest.Duration.Companion.days
+import io.kotlintest.Duration.Companion.seconds
 
 class EventuallyTest : WordSpec(), Eventually {
 
   init {
     "eventually" should {
       "pass working tests"  {
-        eventually(5, TimeUnit.DAYS) {
+        eventually(5.days) {
           System.currentTimeMillis()
         }
       }
       "pass tests that completed within the time allowed"  {
         val end = System.currentTimeMillis() + 2000
-        eventually(5, TimeUnit.DAYS) {
+        eventually(5.days) {
           if (System.currentTimeMillis() < end)
             throw RuntimeException("foo")
         }
       }
       "fail tests that do not complete within the time allowed"  {
-        expecting(TestFailedException::class) {
-          eventually(2, TimeUnit.SECONDS) {
+        shouldThrow<TestFailedException> {
+          eventually(2.seconds) {
             throw RuntimeException("foo")
           }
         }
