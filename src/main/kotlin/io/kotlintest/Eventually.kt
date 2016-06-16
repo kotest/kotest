24c6485601434af126c017e1cd845987e0dc116a
Christian Helmbold <christian.helmbold@yahoo.de>
2016-06-17 00:24:15 +0200

    reintroduce overload for eventually to keep backwards compatibility (#34)

--- src/main/kotlin/io/kotlintest/Eventually.kt
+++ src/main/kotlin/io/kotlintest/Eventually.kt
@@ -1,5 +1,7 @@
 package io.kotlintest
 
+import java.util.concurrent.TimeUnit
+
 interface Eventually {
 
   fun eventually(duration: Duration, f: () -> Unit): Unit {
@@ -15,5 +17,20 @@ interface Eventually {
       times++
     }
     throw TestFailedException("Test failed after ${duration.amount} ${duration.timeUnit}; attempted $times times")
+  }
+
+  @Deprecated("use the overload with Duration instead", replaceWith = ReplaceWith("eventually(duration, f)"))
+  fun eventually(duration: Long, unit: TimeUnit, f: () -> Unit): Unit {
+    val end = System.nanoTime() + unit.toNanos(duration)
+    var times = 0
+    while (System.nanoTime() < end) {
+      try {
+        f()
+        return
+      } catch (e: Exception) {
+      }
+      times++
+    }
+    throw TestFailedException("Test failed after $duration $unit; attempted $times times")
   }
 }
