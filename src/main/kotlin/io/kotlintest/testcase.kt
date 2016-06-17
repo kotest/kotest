ae40e951af75c2628e572e5fb7d8ebdc93fe8b44
Christian Helmbold <christian@helmbold.de>
2016-06-17 22:38:58 +0200

    reintroduce overload for String.config, TestConfig constructor, and TestCase.config to keep backwards compatibility

--- src/main/kotlin/io/kotlintest/testcase.kt
+++ src/main/kotlin/io/kotlintest/testcase.kt
@@ -1,5 +1,7 @@
 package io.kotlintest
 
+import java.util.concurrent.TimeUnit
+
 data class TestSuite(val name: String, val nestedSuites: MutableList<TestSuite>, val cases: MutableList<io.kotlintest.TestCase>) {
   companion object {
     fun empty(name: String) = TestSuite(name, mutableListOf<TestSuite>(), mutableListOf<io.kotlintest.TestCase>())
@@ -10,7 +12,15 @@ data class TestConfig(var ignored: Boolean = false,
                       var invocations: Int = 1,
                       var timeout: Duration = Duration.unlimited,
                       var threads: Int = 1,
-                      var tags: List<String> = listOf())
+                      var tags: List<String> = listOf()) {
+  constructor(
+          ignored: Boolean,
+          invocations: Int,
+          timeout: Long = 0,
+          timeoutUnit: TimeUnit = TimeUnit.MILLISECONDS,
+          threads: Int,
+          tags: List<String>): this(ignored, invocations, Duration(timeout, timeoutUnit), threads, tags)
+}
 
 data class TestCase(val suite: TestSuite,
                     val name: String,
@@ -26,6 +36,25 @@ data class TestCase(val suite: TestSuite,
     config.invocations = invocations
     config.ignored = ignored
     config.timeout = timeout
+    config.threads = threads
+    config.tags = tags
+    if (tag != null)
+      config.tags = config.tags.plus(tag)
+  }
+
+  @Deprecated(
+          message = "use overload instead",
+          replaceWith = ReplaceWith("config(invocations, ignored, timeout, threads, tag, tags)"))
+  fun config(invocations: Int = 1,
+             ignored: Boolean = false,
+             timeout: Long = 0,
+             timeoutUnit: TimeUnit = TimeUnit.MILLISECONDS,
+             threads: Int = 1,
+             tag: String? = null,
+             tags: List<String> = listOf()): Unit {
+    config.invocations = invocations
+    config.ignored = ignored
+    config.timeout = Duration(timeout, timeoutUnit)
     config.threads = threads
     config.tags = tags
     if (tag != null)
