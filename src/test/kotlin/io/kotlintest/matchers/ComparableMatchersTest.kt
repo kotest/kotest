41854941f095e95fcc7f2bb92dc665260fe8d4a8
Aleix Morgadas <aleix.morgadas@gmail.com>
2017-02-26 18:05:18 +0100

    FIX Merge Removed Deprecated methods

--- src/test/kotlin/io/kotlintest/matchers/ComparableMatchersTest.kt
+++ src/test/kotlin/io/kotlintest/matchers/ComparableMatchersTest.kt
@@ -1,5 +1,6 @@
 package io.kotlintest.matchers
 
+import io.kotlintest.forAll
 import io.kotlintest.specs.FreeSpec
 
 class ComparableMatchersTest : FreeSpec() {
@@ -28,7 +29,6 @@ class ComparableMatchersTest : FreeSpec() {
           forAll(arrayOf(Pair(cn, cz), Pair(cz, cp))) {
             it.first shouldBe lt(it.second)
             it.first should beLessThan(it.second)
-            it.first should be lt it.second // Deprecated infix method
           }
         }
 
@@ -36,7 +36,6 @@ class ComparableMatchersTest : FreeSpec() {
           forAll(arrayOf(cn, cz, cp)) {
             shouldThrow<AssertionError> { it shouldBe lt(it) }
             shouldThrow<AssertionError> { it should beLessThan(it) }
-            shouldThrow<AssertionError> { it should be lt it } // Deprecated infix method
           }
         }
 
@@ -44,7 +43,6 @@ class ComparableMatchersTest : FreeSpec() {
           forAll(arrayOf(Pair(cp, cz), Pair(cz, cn))) {
             shouldThrow<AssertionError> { it.first shouldBe lt(it.second) }
             shouldThrow<AssertionError> { it.first should beLessThan(it.second) }
-            shouldThrow<AssertionError> { it.first should be lt it.second } // Deprecated infix method
           }
         }
 
@@ -56,7 +54,6 @@ class ComparableMatchersTest : FreeSpec() {
           forAll(arrayOf(Pair(cn, cn), Pair(cn, cz), Pair(cz, cz), Pair(cz, cp), Pair(cp, cp))) {
             it.first shouldBe lte(it.second)
             it.first should beLessThanOrEqualTo(it.second)
-            it.first should be lte it.second // Deprecated infix method
           }
         }
 
@@ -64,7 +61,6 @@ class ComparableMatchersTest : FreeSpec() {
           forAll(arrayOf(Pair(cp, cz), Pair(cz, cn))) {
             shouldThrow<AssertionError> { it.first shouldBe lte(it.second) }
             shouldThrow<AssertionError> { it.first should beLessThanOrEqualTo(it.second) }
-            shouldThrow<AssertionError> { it.first should be lte it.second } // Deprecated infix method
           }
         }
 
@@ -76,7 +72,6 @@ class ComparableMatchersTest : FreeSpec() {
           forAll(arrayOf(Pair(cp, cz), Pair(cz, cn))) {
             it.first shouldBe gt(it.second)
             it.first should beGreaterThan(it.second)
-            it.first should be gt it.second // Deprecated infix method
           }
         }
 
@@ -84,7 +79,6 @@ class ComparableMatchersTest : FreeSpec() {
           forAll(arrayOf(cn, cz, cp)) {
             shouldThrow<AssertionError> { it shouldBe gt(it) }
             shouldThrow<AssertionError> { it should beGreaterThan(it) }
-            shouldThrow<AssertionError> { it should be gt it } // Deprecated infix method
           }
         }
 
@@ -92,7 +86,6 @@ class ComparableMatchersTest : FreeSpec() {
           forAll(arrayOf(Pair(cn, cz), Pair(cz, cp))) {
             shouldThrow<AssertionError> { it.first shouldBe gt(it.second) }
             shouldThrow<AssertionError> { it.first should beGreaterThan(it.second) }
-            shouldThrow<AssertionError> { it.first should be gt it.second } // Deprecated infix method
           }
         }
 
@@ -104,7 +97,6 @@ class ComparableMatchersTest : FreeSpec() {
           forAll(arrayOf(Pair(cp, cp), Pair(cp, cz), Pair(cz, cz), Pair(cz, cn), Pair(cn, cn))) {
             it.first shouldBe gte(it.second)
             it.first should beGreaterThanOrEqualTo(it.second)
-            it.first should be gte it.second // Deprecated infix method
           }
         }
 
@@ -112,7 +104,6 @@ class ComparableMatchersTest : FreeSpec() {
           forAll(arrayOf(Pair(cn, cz), Pair(cz, cp))) {
             shouldThrow<AssertionError> { it.first shouldBe gte(it.second) }
             shouldThrow<AssertionError> { it.first should beGreaterThanOrEqualTo(it.second) }
-            shouldThrow<AssertionError> { it.first should be gte it.second } // Deprecated infix method
           }
         }
 
