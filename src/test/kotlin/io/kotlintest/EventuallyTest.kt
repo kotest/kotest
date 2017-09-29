ddcf703ca94791ab69100b302c8ae0ce12c5dabd
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2017-09-29 14:54:38 +0100

    Improve "eventually" syntax to allow passing custom Exception type
    This is useful when it is know that the block of code can fail with "AssertionError" for some time.
    AssertionError is not of type Exception so old implementation is limiting in this regard.

--- src/test/kotlin/io/kotlintest/EventuallyTest.kt
+++ src/test/kotlin/io/kotlintest/EventuallyTest.kt
@@ -3,6 +3,7 @@ package io.kotlintest
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.WordSpec
 import io.kotlintest.matchers.shouldThrow
+import java.io.IOException
 
 class EventuallyTest : WordSpec() {
 
@@ -32,6 +33,20 @@ class EventuallyTest : WordSpec() {
           1
         }
         result shouldBe 1
+      }
+      "pass tests that completed within the time allowed, custom exception"  {
+        val end = System.currentTimeMillis() + 2000
+        eventually(5.days, AssertionError::class.java) {
+          if (System.currentTimeMillis() < end)
+            assert(false)
+        }
+      }
+      "fail tests throw unexpected exception type"  {
+        shouldThrow<KotlinNullPointerException> {
+          eventually(2.seconds, IOException::class.java) {
+            (null as String?)!!.length
+          }
+        }
       }
     }
   }
