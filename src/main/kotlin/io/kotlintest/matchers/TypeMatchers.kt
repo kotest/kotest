e9ad11b5e22438b1d4fbd3ddcab2dc5bc2c26f89
sksamuel <sam@sksamuel.com>
2017-10-04 05:40:58 +0100

    Change exception.cause shouldBe beInstanceOf #185

--- src/main/kotlin/io/kotlintest/matchers/TypeMatchers.kt
+++ src/main/kotlin/io/kotlintest/matchers/TypeMatchers.kt
@@ -2,6 +2,9 @@ package io.kotlintest.matchers
 
 import kotlin.reflect.KClass
 
+// alias for beInstanceOf
+fun instanceOf(expected: KClass<*>): Matcher<Any> = beInstanceOf(expected)
+
 fun beInstanceOf(expected: KClass<*>): Matcher<Any> = object : Matcher<Any> {
   override fun test(value: Any): Result =
       Result(
