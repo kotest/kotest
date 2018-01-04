6b1ccd18565ac1c753f7285e77b094c863a0a58f
Kerooker <leonardo.colman98@gmail.com>
2018-01-04 02:01:17 -0200

    Added double between matcher and the respective test

--- src/main/kotlin/io/kotlintest/matchers/DoubleMatchers.kt
+++ src/main/kotlin/io/kotlintest/matchers/DoubleMatchers.kt
@@ -6,12 +6,27 @@ fun exactly(d: Double): Matcher<Double> = object : Matcher<Double> {
   override fun test(value: Double) = Result(value == d, "$value is not equal to expected value $d")
 }
 
+fun between(a: Double, b: Double, tolerance: Double ): Matcher<Double> = object : Matcher<Double> {
+    override fun test(value: Double): Result {
+        val differenceToMinimum = value - a
+        val differenceToMaximum = b - value
+
+        if (differenceToMinimum < 0 && differenceToMinimum > tolerance) {
+            return Result(false, "$value should be bigger than $a")
+        }
+        if (differenceToMaximum < 0 && differenceToMaximum > tolerance) {
+            return Result(false, "$value should be smaller than $b")
+        }
+        return Result(true, "")
+    }
+}
+
 class ToleranceMatcher(val expected: Double, val tolerance: Double) : Matcher<Double> {
 
   override fun test(value: Double): Result {
     return if (Double.NaN.equals(expected) && Double.NaN.equals(value)) {
       println("[WARN] By design, Double.Nan != Double.Nan; see https://stackoverflow.com/questions/8819738/why-does-double-nan-double-nan-return-false/8819776#8819776")
-      Result(false, "By design, Double.Nan != Double.Nan; see https://stackoverflow.com/questions/8819738/why-does-double-nan-double-nan-return-false/8819776#8819776")
+      Result(false, "By design, Double.Nan != Double.Nan; see htps://stackoverflow.com/questions/8819738/why-does-double-nan-double-nan-return-false/8819776#8819776")
     } else {
       if (tolerance == 0.0)
         println("[WARN] When comparing doubles consider using tolerance, eg: a shouldBe b plusOrMinus c")
