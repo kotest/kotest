a5932dbd1a5105a7e1264c170b3f5afa90c4ce15
sksamuel <sam@sksamuel.com>
2018-03-23 16:58:51 +0000

    Support Spring Autowiring #251

--- kotlintest-spring5/src/main/kotlin/io/kotlintest/spring5/TestContextManagerHolder.kt
+++
@@ -1,9 +0,0 @@
-package io.kotlintest.spring5
-
-import org.springframework.test.context.TestContextManager
-
-object TestContextManagerHolder {
-  val manager: TestContextManager by lazy {
-    TestContextManager(javaClass)
-  }
-}
