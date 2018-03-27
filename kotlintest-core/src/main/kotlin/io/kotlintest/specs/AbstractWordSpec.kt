2d9d3ce374347eb23d68ae363a15a594d9671177
sksamuel <sam@sksamuel.com>
2018-03-27 22:54:13 +0100

    Removed some debug code

--- kotlintest-core/src/main/kotlin/io/kotlintest/specs/AbstractWordSpec.kt
+++ kotlintest-core/src/main/kotlin/io/kotlintest/specs/AbstractWordSpec.kt
@@ -5,7 +5,6 @@ import io.kotlintest.TestCase
 import io.kotlintest.TestContainer
 import io.kotlintest.TestContext
 import io.kotlintest.lineNumber
-import kotlin.reflect.full.memberExtensionFunctions
 
 /**
  * Example:
@@ -26,14 +25,11 @@ abstract class AbstractWordSpec(body: AbstractWordSpec.() -> Unit = {}) : Abstra
   final override fun isInstancePerTest(): Boolean = false
 
   infix fun String.should(init: WordContext.() -> Unit) {
-    val func = this@AbstractWordSpec::class.memberExtensionFunctions.find { it.name == "should" }!!
-    println("Annotations=" + func.annotations)
     rootScopes.add(TestContainer(rootDescription().append(this), this@AbstractWordSpec, { WordContext(it).init() }))
   }
 
   inner class WordContext(val context: TestContext) {
     infix operator fun String.invoke(test: TestContext.() -> Unit): TestCase {
-      println("Annotations=" + this::class.annotations)
       val tc = TestCase(context.currentScope().description().append("should " + this), this@AbstractWordSpec, test, lineNumber(), defaultTestCaseConfig)
       context.addScope(tc)
       return tc
