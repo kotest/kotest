72505b4302d6ad9ab8638af0c84da5de8e28b93f
sksamuel <sam@sksamuel.com>
2018-03-29 14:34:13 +0100

    Added spring sample

---
+++ kotlintest-extensions/kotlintest-extensions-spring/src/test/kotlin/io/kotlintest/spring/UserServiceTest.kt
@@ -0,0 +1,25 @@
+package io.kotlintest.spring
+
+import io.kotlintest.samples.spring.Components
+import io.kotlintest.samples.spring.UserService
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.WordSpec
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+
+@ContextConfiguration(classes = [(Components::class)])
+class UserServiceTest : WordSpec() {
+
+  override fun listeners() = listOf(SpringListener)
+
+  @Autowired
+  var service: UserService? = null
+
+  init {
+    "SpringListener" should {
+      "have autowired the service" {
+        service!!.repository.findUser().name shouldBe "system_user"
+      }
+    }
+  }
+}
