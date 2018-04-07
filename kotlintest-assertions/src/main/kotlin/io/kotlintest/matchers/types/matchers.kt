bebb699297097d43bd569a6432d59383f2950747
sksamuel <sam@sksamuel.com>
2018-04-07 12:22:51 +0100

    Reverted annotation change

--- kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/types/matchers.kt
+++ kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/types/matchers.kt
@@ -3,7 +3,7 @@ package io.kotlintest.matchers.types
 import io.kotlintest.Matcher
 import io.kotlintest.Result
 
-inline fun <A, reified T : Annotation> haveAnnotation() = object : Matcher<Class<A>> {
+inline fun <A, reified T : Annotation> haveAnnotation(klass: Class<T>) = object : Matcher<Class<A>> {
   val className = T::class.qualifiedName
   override fun test(value: Class<A>): Result {
     val passed = value.annotations.any { it.javaClass.canonicalName == className }
