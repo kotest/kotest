73469d9016d21ce1009f0d3d70d2de7c34c0597a
sksamuel <sam@sksamuel.com>
2018-04-11 02:06:48 +0100

    Type matchers now support extension style

--- kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/types/matchers.kt
+++ kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/types/matchers.kt
@@ -2,9 +2,37 @@ package io.kotlintest.matchers.types
 
 import io.kotlintest.Matcher
 import io.kotlintest.Result
+import io.kotlintest.matchers.beInstanceOf
+import io.kotlintest.matchers.beOfType
+import io.kotlintest.matchers.beTheSameInstanceAs
 import io.kotlintest.should
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
 
-inline fun <reified T : Annotation> Class<Any>.shouldHaveAnnotation(klass: Class<T>) = this should haveAnnotation(klass)
+inline fun <reified T : Any> Any.shouldBeInstanceOf() {
+  val matcher = beInstanceOf<T>()
+  this shouldBe matcher
+}
+
+inline fun <reified T : Any> Any.shouldNotBeInstanceOf() {
+  val matcher = beInstanceOf<T>()
+  this shouldNotBe matcher
+}
+
+inline fun <reified T : Any> Any.shouldBeTypeOf() {
+  val matcher = beOfType<T>()
+  this shouldBe matcher
+}
+
+inline fun <reified T : Any> Any.shouldNotBeTypeOf() {
+  val matcher = beOfType<T>()
+  this shouldNotBe matcher
+}
+
+fun Any.shouldBeSameInstanceAs(ref: Any) = this should beTheSameInstanceAs(ref)
+fun Any.shouldNotBeSameInstanceAs(ref: Any) = this shouldNotBe beTheSameInstanceAs(ref)
+
+inline fun <A, reified T : Annotation> Class<A>.shouldHaveAnnotation(klass: Class<T>) = this should haveAnnotation<A, T>(klass)
 
 @Suppress("UNUSED_PARAMETER")
 inline fun <A, reified T : Annotation> haveAnnotation(klass: Class<T>) = object : Matcher<Class<A>> {
--- kotlintest-tests/src/test/kotlin/com/sksamuel/kotlintest/tests/matchers/types/TypeMatchersTest.kt
+++ kotlintest-tests/src/test/kotlin/com/sksamuel/kotlintest/tests/matchers/types/TypeMatchersTest.kt
@@ -4,6 +4,12 @@ import io.kotlintest.matchers.beInstanceOf
 import io.kotlintest.matchers.beOfType
 import io.kotlintest.matchers.beTheSameInstanceAs
 import io.kotlintest.matchers.types.haveAnnotation
+import io.kotlintest.matchers.types.shouldBeInstanceOf
+import io.kotlintest.matchers.types.shouldBeSameInstanceAs
+import io.kotlintest.matchers.types.shouldBeTypeOf
+import io.kotlintest.matchers.types.shouldHaveAnnotation
+import io.kotlintest.matchers.types.shouldNotBeInstanceOf
+import io.kotlintest.matchers.types.shouldNotBeTypeOf
 import io.kotlintest.should
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.WordSpec
@@ -20,9 +26,18 @@ class TypeMatchersTest : WordSpec() {
 
   init {
 
+    "typeOf" should {
+      "test for exact type" {
+        val arrayList: List<Int> = arrayListOf(1, 2, 3)
+        arrayList.shouldBeTypeOf<ArrayList<*>>()
+        arrayList.shouldNotBeTypeOf<List<*>>()
+      }
+    }
+
     "haveAnnotation(annotation)" should {
       "test for the presence of an annotation" {
         Wibble::class.java should haveAnnotation(Vod::class.java)
+        Wibble::class.java.shouldHaveAnnotation(Vod::class.java)
       }
     }
 
@@ -31,11 +46,18 @@ class TypeMatchersTest : WordSpec() {
         val arrayList: List<Int> = arrayListOf(1, 2, 3)
 
         arrayList should beInstanceOf(ArrayList::class)
+        arrayList.shouldBeInstanceOf<ArrayList<*>>()
 
         arrayList should beInstanceOf(List::class)
 
         shouldThrow<AssertionError> {
           arrayList should beInstanceOf(LinkedList::class)
+        }
+
+        arrayList.shouldNotBeInstanceOf<LinkedList<*>>()
+
+        shouldThrow<AssertionError> {
+          arrayList.shouldNotBeInstanceOf<ArrayList<*>>()
         }
       }
     }
@@ -63,9 +85,14 @@ class TypeMatchersTest : WordSpec() {
         val c = listOf(1, 2, 3)
 
         a should beTheSameInstanceAs(b)
+        a.shouldBeSameInstanceAs(b)
 
         shouldThrow<AssertionError> {
           a should beTheSameInstanceAs(c)
+        }
+
+        shouldThrow<AssertionError> {
+          a.shouldBeSameInstanceAs(c)
         }
       }
     }
