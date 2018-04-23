6137b3d8c5c6115edd3f97ae2d250d1279e85564
sksamuel <sam@sksamuel.com>
2018-04-23 20:16:57 +0100

    Restored deprecated support for shouldBe/shouldNotBe for Any? #301

--- kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/migration.kt
+++ kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/migration.kt
@@ -8,8 +8,8 @@ import io.kotlintest.shouldNot
 import io.kotlintest.shouldNotBe
 import io.kotlintest.shouldThrow
 
-infix fun <T> T.shouldBe(matcher: Matcher<T>) = shouldBe(matcher)
-infix fun <T> T.shouldNotBe(matcher: Matcher<T>) = shouldNotBe(matcher)
+infix fun <T, U : T> T.shouldBe(any: U?) = shouldBe(any)
+infix fun <T> T.shouldNotBe(any: Any?) = shouldNotBe(any)
 infix fun <T> T.shouldNot(matcher: Matcher<T>) = shouldNot(matcher)
 infix fun <T> T.shouldHave(matcher: Matcher<T>) = shouldHave(matcher)
 infix fun <T> T.should(matcher: Matcher<T>) = should(matcher)
