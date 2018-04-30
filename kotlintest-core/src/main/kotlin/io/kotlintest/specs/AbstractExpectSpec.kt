6087f7702e62128fc131a7333cb248368fa829a3
sksamuel <sam@sksamuel.com>
2018-04-30 18:13:10 +0100

    Updated expect and should spec to work with nested contexts

--- kotlintest-core/src/main/kotlin/io/kotlintest/specs/AbstractExpectSpec.kt
+++ kotlintest-core/src/main/kotlin/io/kotlintest/specs/AbstractExpectSpec.kt
@@ -1,7 +1,11 @@
 package io.kotlintest.specs
 
 import io.kotlintest.AbstractSpec
+import io.kotlintest.Tag
+import io.kotlintest.TestCaseConfig
 import io.kotlintest.TestContext
+import io.kotlintest.extensions.TestCaseExtension
+import java.time.Duration
 
 abstract class AbstractExpectSpec(body: AbstractExpectSpec.() -> Unit = {}) : AbstractSpec() {
 
@@ -11,9 +15,41 @@ abstract class AbstractExpectSpec(body: AbstractExpectSpec.() -> Unit = {}) : Ab
 
   final override fun isInstancePerTest(): Boolean = false
 
-  fun context(name: String, test: TestContext.() -> Unit) =
-      addTestCase("Context $name", test, defaultTestCaseConfig)
 
-  fun expect(name: String, test: TestContext.() -> Unit) =
-      addTestCase("Expect $name", test, defaultTestCaseConfig)
+  inner class TestBuilder(val context: TestContext, val name: String) {
+
+    fun config(
+        invocations: Int? = null,
+        enabled: Boolean? = null,
+        timeout: Duration? = null,
+        threads: Int? = null,
+        tags: Set<Tag>? = null,
+        extensions: List<TestCaseExtension>? = null,
+        test: TestContext.() -> Unit) {
+      val config = TestCaseConfig(
+          enabled ?: defaultTestCaseConfig.enabled,
+          invocations ?: defaultTestCaseConfig.invocations,
+          timeout ?: defaultTestCaseConfig.timeout,
+          threads ?: defaultTestCaseConfig.threads,
+          tags ?: defaultTestCaseConfig.tags,
+          extensions ?: defaultTestCaseConfig.extensions)
+      context.registerTestScope(name, this@AbstractExpectSpec, test, config)
+    }
+  }
+
+  inner class ExpectContext(val context: TestContext) {
+
+    fun context(name: String, test: ExpectContext.() -> Unit) =
+        context.registerTestScope("Context: $name", this@AbstractExpectSpec, { ExpectContext(this).test() }, defaultTestCaseConfig)
+
+    fun expect(name: String, test: TestContext.() -> Unit) =
+        context.registerTestScope("Expect: $name", this@AbstractExpectSpec, test, defaultTestCaseConfig)
+
+    fun expect(name: String) = TestBuilder(context, "Expect: $name")
+  }
+
+  fun context(name: String, test: ExpectContext.() -> Unit) =
+      addTestCase("Context: $name", { ExpectContext(this).test() }, defaultTestCaseConfig)
+
+
 }
