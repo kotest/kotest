f4e228dcc3df332af2dcf8164823d6c86cc9bfe0
sksamuel <sam@sksamuel.com>
2018-05-07 20:46:22 +0100

    Updated futures test

--- kotlintest-core/src/main/kotlin/io/kotlintest/futures.kt
+++ kotlintest-core/src/main/kotlin/io/kotlintest/futures.kt
@@ -6,14 +6,7 @@ import java.util.concurrent.CompletableFuture
  * Allows callers to queue up futures with a callback that will be executed
  * when the future has completed.
  */
-fun <T> TestContext.whenReady(f: CompletableFuture<T>, test: (T) -> Unit) {
-  f.whenComplete { value, throwable ->
-    try {
-      if (throwable != null)
-        throw throwable
-      test(value)
-    } catch (t: Throwable) {
-    } finally {
-    }
-  }
+fun <A> whenReady(f: CompletableFuture<A>, test: (A) -> Unit) {
+  val a = f.get()
+  test(a)
 }
