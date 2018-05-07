659c306a51aed2bb611fdba2198d43d38edf8749
sksamuel <sam@sksamuel.com>
2018-05-07 13:29:33 +0100

    Spec should fail if test case name is duplicated #319

--- kotlintest-runner/kotlintest-runner-jvm/src/main/kotlin/io/kotlintest/runner/jvm/InstancePerTestSpecExecutor.kt
+++ kotlintest-runner/kotlintest-runner-jvm/src/main/kotlin/io/kotlintest/runner/jvm/InstancePerTestSpecExecutor.kt
@@ -10,10 +10,23 @@ class InstancePerTestSpecExecutor(listener: TestEngineListener) : SpecExecutor(l
 
   data class ExecutionContext(val spec: Spec, val target: Description)
 
+  private val executed = HashSet<Description>()
+
   override fun execute(spec: Spec) {
     spec.testCases().forEach { locateAndExecute(spec::class, it.description) }
   }
 
+  /**
+   * When a new [TestScope] is registered, we require a new instance of the [Spec] class.
+   * A new instance is created, and then the root test that matches the required test scope
+   * is located. This may be the test itself, or a parent of the test.
+   *
+   * If it is the test itself then we simply execute it with another context that will
+   * call this method.
+   *
+   * If it's a parent, then we invoke the scope with a context that will keep executing
+   * the correct child scope until the actual test is found.
+   */
   private fun locateAndExecute(klass: KClass<out Spec>, target: Description) {
     val spec = createSpecInstance(klass)
     spec.testCases().forEach {
@@ -23,16 +36,24 @@ class InstancePerTestSpecExecutor(listener: TestEngineListener) : SpecExecutor(l
   }
 
   private fun execute(spec: Spec, current: TestScope, target: Description) {
-    current.test.invoke(object : TestContext() {
+
+    val nestedContext = object : TestContext() {
       override fun description(): Description = current.description
       override fun registerTestScope(scope: TestScope) {
         if (scope.description == target) {
+
+          if (executed.contains(scope.description))
+            throw IllegalStateException("Cannot add duplicate test name ${scope.name}")
+          executed.add(scope.description)
+
           TestScopeExecutor(listener, scope, targetContext(spec, target)).execute()
         } else if (scope.description.isAncestorOf(target)) {
           execute(spec, scope, target)
         }
       }
-    })
+    }
+
+    current.test.invoke(nestedContext)
   }
 
   private fun targetContext(spec: Spec, target: Description) = object : TestContext() {
--- kotlintest-runner/kotlintest-runner-jvm/src/main/kotlin/io/kotlintest/runner/jvm/SharedInstanceSpecExecutor.kt
+++ kotlintest-runner/kotlintest-runner-jvm/src/main/kotlin/io/kotlintest/runner/jvm/SharedInstanceSpecExecutor.kt
@@ -5,7 +5,7 @@ import io.kotlintest.Spec
 import io.kotlintest.TestContext
 import io.kotlintest.TestScope
 
-class SingleInstanceSpecExecutor(listener: TestEngineListener) : SpecExecutor(listener) {
+class SharedInstanceSpecExecutor(listener: TestEngineListener) : SpecExecutor(listener) {
 
   override fun execute(spec: Spec) {
     interceptSpec(spec, {
@@ -16,8 +16,16 @@ class SingleInstanceSpecExecutor(listener: TestEngineListener) : SpecExecutor(li
   }
 
   private fun callingThreadContext(description: Description): TestContext = object : TestContext() {
+
+    private val seen = HashMap<String, Int>()
+
     override fun description(): Description = description
     override fun registerTestScope(scope: TestScope) {
+      // if we have a test with this name already, but the line number is different
+      // then it's a duplicate test name, so boom
+      if (seen.containsKey(scope.name) && seen[scope.name] != scope.line)
+        throw IllegalStateException("Cannot add duplicate test name ${scope.name}")
+      seen[scope.name] = scope.line
       TestScopeExecutor(listener, scope, callingThreadContext(scope.description)).execute()
     }
   }
