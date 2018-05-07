42402dcfc517c7ea38c4026d74e522c5f606b7bc
sksamuel <sam@sksamuel.com>
2018-05-07 14:22:34 +0100

    Merged with master

--- kotlintest-runner/kotlintest-runner-junit4/src/main/kotlin/io/kotlintest/runner/junit4/KotlinTestRunner.kt
+++ kotlintest-runner/kotlintest-runner-junit4/src/main/kotlin/io/kotlintest/runner/junit4/KotlinTestRunner.kt
@@ -1,38 +1,20 @@
 package io.kotlintest.runner.junit4
 
 import io.kotlintest.Spec
-import io.kotlintest.Scope
-import io.kotlintest.TestContainer
-import io.kotlintest.runner.jvm.TestRunner
+import io.kotlintest.runner.jvm.TestEngine
 import io.kotlintest.runner.jvm.createSpecInstance
-import io.kotlintest.runner.jvm.AsynchronousTestContext
 import org.junit.runner.Description
 import org.junit.runner.Runner
-import org.junit.runners.ParentRunner
 import org.junit.runner.notification.RunNotifier
 
 class KotlinTestRunner(private val testClass: Class<out Spec>) : Runner() {
 
   override fun run(notifier: RunNotifier) {
     val listener = JUnitTestRunnerListener(testClass.kotlin, notifier)
-    val runner = TestRunner(listOf(testClass.kotlin), listener)
+    val runner = TestEngine(listOf(testClass.kotlin), listener)
     runner.execute()
   }
 
   override fun getDescription(): Description =
-      describeScope(createSpecInstance(testClass.kotlin).root())
-
-  /*override fun runChild(child: Scope, notifier: RunNotifier) {
-      notifier.fireTestStarted(describeScope(child))
-  }
-
-  override fun getChildren(): List<Scope> {
-	  val spec = createSpecInstance(testClass.kotlin)
-	  val context = AsynchronousTestContext(spec.root())
-	  spec.root().closure(context)
-	  return context.scopes()
-  }
-
-  override fun describeChild(child: Scope): Description =
-	  describeScope(child)*/
+      describeScope(createSpecInstance(testClass.kotlin))
 }
