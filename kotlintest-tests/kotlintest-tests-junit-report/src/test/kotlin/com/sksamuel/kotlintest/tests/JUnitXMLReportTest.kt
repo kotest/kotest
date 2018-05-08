358518abf98542e93fcbcf795d4c8c904be1a05b
sksamuel <sam@sksamuel.com>
2018-05-08 14:37:58 +0100

    Updated junit test url for travis

--- kotlintest-tests/kotlintest-tests-junit-report/src/test/kotlin/com/sksamuel/kotlintest/tests/JUnitXMLReportTest.kt
+++ kotlintest-tests/kotlintest-tests-junit-report/src/test/kotlin/com/sksamuel/kotlintest/tests/JUnitXMLReportTest.kt
@@ -10,7 +10,11 @@ class JUnitXMLReportTest : WordSpec() {
     // we test the output from the earlier test of tests in
     // kotlintest-tests/kotlintest-tests-core
     "JUnit XML Output" should {
-      val file = File("./kotlintest-tests/kotlintest-tests-core/build/test-results/test/TEST-com.sksamuel.kotlintest.tests.specs.WordSpecTest.xml")
+      val file = if (System.getenv("TRAVIS") == "true") {
+        File("/home/travis/build/kotlintest/kotlintest/kotlintest-tests/kotlintest-tests-core/build/test-results/test/TEST-com.sksamuel.kotlintest.tests.specs.WordSpecTest.xml")
+      } else {
+        File("./kotlintest-tests/kotlintest-tests-core/build/test-results/test/TEST-com.sksamuel.kotlintest.tests.specs.WordSpecTest.xml")
+      }
       val builder = SAXBuilder()
       val doc = builder.build(file)
       val root = doc.rootElement
