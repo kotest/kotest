da751b25945ab12e9d8465959599861bc5810b0a
Hannes Thaller <hannes.thaller.at@gmail.com>
2018-05-17 11:43:51 +0200

    Implemented the inspector and extractor inspectors and added tests for them.

--- kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/InspectorsTest.kt
+++ kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/InspectorsTest.kt
@@ -237,7 +237,77 @@ The following elements failed:
 3 => expected: 33 but was: 3
 4 => expected: 33 but was: 4
 5 => expected: 33 but was: 5"""
-      }
+            }
+        }
+
+
+        data class Person(val name: String, val age: Int, val friends: List<Person>)
+
+        val p1 = Person("John Doe", 20, emptyList())
+        val p2 = Person("Samantha Rose", 19, listOf(p1))
+        val persons = listOf(p1, p2)
+
+        "inspecting" should {
+            "expose properties"{
+                inspecting(p1) {
+                    name shouldBe "John Doe"
+                    age shouldBe 20
+                }
+            }
+
+            "be usable within other inspectors"{
+                forOne(persons) {
+                    inspecting(it) {
+                        name shouldBe "John Doe"
+                        age shouldBe 20
+                    }
+                }
+            }
+
+            "be nestable"{
+                inspecting(p2) {
+                    name shouldBe "Samantha Rose"
+                    age shouldBe 19
+                    inspecting(friends.first()) {
+                        name shouldBe "John Doe"
+                        age shouldBe 20
+                    }
+                }
+            }
+            "should fail if the matchers fail"{
+                shouldThrowAny{
+                    inspecting(p2) {
+                        name shouldBe "Samantha Rose"
+                        age shouldBe 19
+                        inspecting(friends.first()) {
+                            name shouldBe "<Some name that is wrong>"
+                            age shouldBe 19
+                        }
+                    }
+                }.message shouldBe "expected:<[<Some name that is wrong>]> but was:<[John Doe]>"
+            }
+        }
+
+        "extracting" should {
+            "extract simple properties"{
+                extracting(persons) { name }
+                        .shouldContainAll("John Doe", "Samantha Rose")
+            }
+
+            "extract complex properties"{
+                extracting(persons) { Pair(name, age) }
+                        .shouldContainAll(
+                                Pair("John Doe", 20),
+                                Pair("Samantha Rose", 19)
+                        )
+            }
+            "fail if the matcher fails"{
+                shouldThrowAny {
+                    extracting(persons) { name }
+                            .shouldContainAll("<Some name that is wrong>")
+                }.message shouldBe  "Collection should contain all of <Some name that is wrong>"
+            }
+
+        }
     }
-  }
 }
