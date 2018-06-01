2394f65d36be0425553366c22154655f357d4fac
AJ Alt <aj@alt>
2018-05-31 20:29:56 -0700

    Add extension functions for table assertions.
    These extensions simplify the common case of creating a table and
    immediately asserting on its contents. They let you write code like
    this:
    
    ```kotlin
    table(...).forAll { a, b -> ...}
    ```
    
    instead of needing an intermediate variable:
    
    ```kotlin
    val myTable = table(...)
    forAll(myTable) { a, b -> ... }
    ```
    
    I deliberately did not change any documentation in this PR. If it's
    accepted, documentation can be updated in another commit.

--- kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/tables/TableTestingTest.kt
+++ kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/tables/TableTestingTest.kt
@@ -2,10 +2,7 @@ package com.sksamuel.kotlintest.tables
 
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
-import io.kotlintest.tables.forAll
-import io.kotlintest.tables.headers
-import io.kotlintest.tables.row
-import io.kotlintest.tables.table
+import io.kotlintest.tables.*
 
 class TableTestingTest : StringSpec() {
   init {
@@ -34,7 +31,29 @@ class TableTestingTest : StringSpec() {
       )
 
       forAll(table2) { a, b ->
-        a + b == 10
+        a + b shouldBe 10
+      }
+    }
+
+    "numbers should add up to ten using extension function" {
+
+      table(headers("a", "b"),
+              row(5, 5),
+              row(4, 6),
+              row(3, 7)
+      ).forAll { a, b ->
+        a + b shouldBe 10
+      }
+    }
+
+    "numbers all be different using extension function" {
+
+      table(headers("a", "b"),
+              row(1, 2),
+              row(3, 4),
+              row(5, 6)
+      ).forNone { a, b ->
+        a shouldBe b
       }
     }
 
