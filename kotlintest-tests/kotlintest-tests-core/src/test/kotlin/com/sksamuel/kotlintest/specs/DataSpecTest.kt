f1681976a1e6064cb1058ebd92f27776c4399fb1
sksamuel <sam@sksamuel.com>
2018-06-02 22:32:00 +0100

    Added more row options to data spec

--- kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/specs/DataSpecTest.kt
+++ kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/specs/DataSpecTest.kt
@@ -6,6 +6,15 @@ import io.kotlintest.tables.row
 
 class DataSpecTest : DataSpec({
 
+  "square roots"(
+      row(2, 4),
+      row(3, 9),
+      row(4, 16),
+      row(5, 25)
+  ) { a, b ->
+    a * a shouldBe b
+  }
+
   "maximum of two numbers"(
       row(1, 5, 5),
       row(5, 1, 5),
@@ -17,4 +26,11 @@ class DataSpecTest : DataSpec({
     Math.max(a, b) shouldBe c
   }
 
+  "string concat"(
+      row("a", "b", "c", "abc"),
+      row("hel", "lo wo", "rld", "hello world"),
+      row("", "z", "", "z")
+  ) { a, b, c, d ->
+    a + b + c shouldBe d
+  }
 })
