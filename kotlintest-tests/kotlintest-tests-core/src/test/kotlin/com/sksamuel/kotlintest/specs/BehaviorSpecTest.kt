0df655240623c530d78c84f6c7e3eba7edbe5d53
sksamuel <sam@sksamuel.com>
2018-06-09 15:44:36 +0100

    Move tables and data packages into kotlintest-assertions package #354

--- kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/specs/BehaviorSpecTest.kt
+++ kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/specs/BehaviorSpecTest.kt
@@ -1,6 +1,7 @@
 package com.sksamuel.kotlintest.specs
 
 import io.kotlintest.matchers.numerics.shouldBeLessThan
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.BehaviorSpec
 
 class BehaviorSpecTest : BehaviorSpec() {
@@ -9,6 +10,21 @@ class BehaviorSpecTest : BehaviorSpec() {
       `when`("b") {
         then("c") {
           1.shouldBeLessThan(2)
+        }
+      }
+    }
+  }
+}
+
+fun boom() = 1 / 0
+
+class TestBoom: BehaviorSpec() {
+  init {
+    Given("Test") {
+      boom()
+      When("running here a code that potentially throws an exception") {
+        Then("no info about that exception") {
+          1 shouldBe 1
         }
       }
     }
