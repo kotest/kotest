bfee94ddbcd8f6294c9efe0987fc9fc721491f21
sksamuel <sam@sksamuel.com>
2018-07-18 09:22:04 +0100

    Updated some internal docs

--- kotlintest-runner/kotlintest-runner-jvm/src/main/kotlin/io/kotlintest/runner/jvm/InstancePerTestSpecRunner.kt
+++ kotlintest-runner/kotlintest-runner-jvm/src/main/kotlin/io/kotlintest/runner/jvm/InstancePerTestSpecRunner.kt
@@ -13,11 +13,14 @@ class InstancePerTestSpecRunner(listener: TestEngineListener) : SpecRunner(liste
   private val executed = HashSet<Description>()
 
   override fun execute(spec: Spec) {
+    // we start by executing each top level test in the spec, which in turn will lead
+    // to further test discoveries
     topLevelTests(spec).forEach { locateAndExecute(spec::class, it.description) }
   }
 
   /**
    * When a new [TestCase] is registered, we require a new instance of the [Spec] class.
+   *
    * A new instance is created, and then the root test that matches the required test scope
    * is located. This may be the test itself, or a parent of the test.
    *
@@ -26,6 +29,8 @@ class InstancePerTestSpecRunner(listener: TestEngineListener) : SpecRunner(liste
    *
    * If it's a parent, then we invoke the scope with a context that will keep executing
    * the correct child scope until the actual test is found.
+   *
+   * @param target the test we are looking to execute
    */
   private fun locateAndExecute(klass: KClass<out Spec>, target: Description) {
     instantiateSpec(klass).let {
@@ -33,9 +38,17 @@ class InstancePerTestSpecRunner(listener: TestEngineListener) : SpecRunner(liste
         is Failure -> throw it.exception
         is Success -> {
           val spec = it.value
+          // for each of the top level tests we check if it is the one we are
+          // looking to execute. If it is then we execute it immediately using a context
+          // which will invoke any nested tests in turn.
+          // If the test is not the one we are looking for, but is infact an ancestor
+          // then we need to execute the closure to ensure state is correct. We do this
+          // with a context that will perform the same logic as this code, but on the
+          // nested test itself.
           spec.testCases().forEach {
+
             if (it.description == target) interceptSpec(spec) {
-              io.kotlintest.runner.jvm.TestCaseExecutor(listener, it, targetContext(spec, target)).execute()
+              TestCaseExecutor(listener, it, targetContext(spec, target)).execute()
             } else if (it.description.isAncestorOf(target)) {
               interceptSpec(spec) {
                 execute(spec, it, target)
