cb82314ae7dfe0039f87c49698b3ba4bb008d516
sksamuel <sam@sksamuel.com>
2018-07-18 15:09:01 +0100

    Updated examples to KotlinTest 3.1.8

--- kotlintest-samples/kotlintest-samples-maven/src/test/kotlin/io/kotlintest/samples/maven/StringTest.kt
+++ kotlintest-samples/kotlintest-samples-maven/src/test/kotlin/io/kotlintest/samples/maven/StringTest.kt
@@ -1,18 +1,17 @@
 package io.kotlintest.samples.maven
 
-import io.kotlintest.matchers.haveLength
-import io.kotlintest.matchers.include
-import io.kotlintest.should
+import io.kotlintest.matchers.string.shouldContain
+import io.kotlintest.matchers.string.shouldHaveLength
 import io.kotlintest.specs.WordSpec
 
 class StringTest : WordSpec() {
   init {
     "A String" should {
       "Report correct length" {
-        "wibble" should haveLength(6)
+        "wibble".shouldHaveLength(6)
       }
       "include substring" {
-        "wibble" should include("wib")
+        "wibble".shouldContain("wib")
       }
     }
   }
