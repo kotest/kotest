4ae402fe12b6109d717a84cb63f12129851bef84
AJ Alt <ajalt@users.noreply.github.com>
2018-08-27 05:52:20 -0700

    Fix shouldBe on arrays without static types (#397)

--- kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/matchers/ArrayEqualsTest.kt
+++ kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/matchers/ArrayEqualsTest.kt
@@ -6,27 +6,27 @@ import io.kotlintest.shouldBe
 class ArrayEqualsTest : StringSpec() {
   init {
     "shouldBe should support int arrays" {
-      val array = intArrayOf(1, 2, 3)
+      val array: Any = intArrayOf(1, 2, 3)
       array shouldBe intArrayOf(1, 2, 3)
     }
     "shouldBe should support long arrays" {
-      val array = longArrayOf(1L, 2L, 3L)
+      val array: Any = longArrayOf(1L, 2L, 3L)
       array shouldBe longArrayOf(1L, 2L, 3L)
     }
     "shouldBe should support double arrays" {
-      val array = doubleArrayOf(1.0, 2.0, 3.0)
+      val array: Any = doubleArrayOf(1.0, 2.0, 3.0)
       array shouldBe doubleArrayOf(1.0, 2.0, 3.0)
     }
     "shouldBe should support boolean arrays" {
-      val array = booleanArrayOf(true, false)
+      val array: Any = booleanArrayOf(true, false)
       array shouldBe booleanArrayOf(true, false)
     }
     "shouldBe should support generic arrays" {
-      val array = arrayOf("hello", "welcome")
+      val array: Any = arrayOf("hello", "welcome")
       array shouldBe arrayOf("hello", "welcome")
     }
     "shouldBe should support nullable boolean arrays" {
-      val someBooleans = arrayOf(false, true, false) as Array<Boolean?>
+      val someBooleans: Array<Boolean?> = arrayOf(false, true, false)
       someBooleans shouldBe arrayOf<Boolean?>(false, true, false)
     }
   }
