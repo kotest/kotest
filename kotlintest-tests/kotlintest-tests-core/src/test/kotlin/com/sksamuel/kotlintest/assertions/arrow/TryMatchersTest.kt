69ee5dfcf1d963683b04d28854d82ec45163747e
Riley O'Shea <RileyOShea@fairisaac.com>
2018-10-03 12:12:28 -0700

    Add arrow-assertion Failure matcher that checks underlying throwable equality (https://github.com/kotlintest/kotlintest/issues/422)

--- kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/assertions/arrow/TryMatchersTest.kt
+++ kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/assertions/arrow/TryMatchersTest.kt
@@ -2,11 +2,14 @@ package com.sksamuel.kotlintest.assertions.arrow
 
 import arrow.core.Try
 import io.kotlintest.assertions.arrow.`try`.beFailure
+import io.kotlintest.assertions.arrow.`try`.beFailureWithThrowable
 import io.kotlintest.assertions.arrow.`try`.beSuccess
 import io.kotlintest.should
 import io.kotlintest.shouldBe
+import io.kotlintest.shouldNot
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.WordSpec
+import java.io.IOException
 
 class TryMatchersTest : WordSpec() {
 
@@ -35,6 +38,21 @@ class TryMatchersTest : WordSpec() {
 
         Try.Failure<Nothing>(RuntimeException()) should beFailure()
       }
+
+      "test that a try is a Failure with a given throwable" {
+        shouldThrow<AssertionError> {
+          Try.Success("foo") should beFailureWithThrowable<RuntimeException>()
+        }.message shouldBe "Try should be a Failure but was Success(foo)"
+
+        shouldThrow<AssertionError> {
+          Try.Failure<Nothing>(RuntimeException()) should beFailureWithThrowable<IOException>()
+        }.message shouldBe "Try should be a Failure(${IOException::class}), but was Failure(${RuntimeException::class})"
+
+        Try.Failure<Nothing>(RuntimeException()) should beFailureWithThrowable<RuntimeException>()
+        Try.Failure<Nothing>(RuntimeException()) should beFailureWithThrowable<Exception>()
+        Try.Failure<Nothing>(Exception()) shouldNot beFailureWithThrowable<java.lang.RuntimeException>()
+      }
     }
   }
 }
+
