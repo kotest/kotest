2109b686c9d0ddcc8b629ce85b6545812abe2b4a
Kerooker <leonardo.colman98@gmail.com>
2018-10-03 02:39:50 -0300

    Adds boolean matchers documentation
    Boolean matchers, although trivial, were missing some documentations.
    
    Solves https://github.com/kotlintest/kotlintest/issues/424

--- kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/boolean/BooleanMatchers.kt
+++ kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/boolean/BooleanMatchers.kt
@@ -2,6 +2,35 @@ package io.kotlintest.matchers.boolean
 
 import io.kotlintest.shouldBe
 
+/**
+ * Asserts that this [Boolean] is true
+ *
+ * Verifies that this [Boolean] or boolean expression is true.
+ * Opposite of [Boolean.shouldBeFalse]
+ *
+ * ```
+ * true.shouldBeTrue    // Assertion passes
+ * false.shouldBeTrue   // Assertion fails
+ *
+ * (3 + 3 == 6).shouldBeTrue    // Assertion passes
+ * (3 + 3 == 42).shouldBeTrue   // Assertion fails
+ * ```
+ */
 fun Boolean.shouldBeTrue() = this shouldBe true
 
+
+/**
+ * Asserts that this [Boolean] is false
+ *
+ * Verifies that this [Boolean] or boolean expression is false.
+ * Opposite of [Boolean.shouldBeTrue]
+ *
+ * ```
+ * false.shouldBeFalse  // Assertion passes
+ * true.shouldBeFalse   // Assertion fails
+ *
+ * (3 + 3 == 42).shouldBeFalse  // Assertion passes
+ * (3 + 3 == 6).shouldBeFalse   // Assertion fails
+ * ```
+ */
 fun Boolean.shouldBeFalse() = this shouldBe false
