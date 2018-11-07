2a2cf22cc6ded49e616f72fc7018c45027a2d719
sksamuel <sam@sksamuel.com>
2018-11-07 06:54:57 +0000

    Fix failures class now that we are using multiple threads.

--- kotlintest-assertions/src/main/kotlin/io/kotlintest/Failures.kt
+++ kotlintest-assertions/src/main/kotlin/io/kotlintest/Failures.kt
@@ -1,6 +1,7 @@
 package io.kotlintest
 
 object Failures {
+
   /**
    * Whether KotlinTest-related frames will be removed from the stack traces of thrown [AssertionError]s.
    *
@@ -13,7 +14,7 @@ object Failures {
    * -Dkotlintest.failures.stacktrace.clean=false
    * ```
    */
-  var shouldRemoveKotlintestElementsFromStacktrace: Boolean = readSystemProperty()
+  val shouldRemoveKotlintestElementsFromStacktrace: Boolean = readSystemProperty()
 
   /**
    * Creates an [AssertionError] with the given [message] and [cause]
@@ -21,13 +22,11 @@ object Failures {
    * If [shouldRemoveKotlintestElementsFromStacktrace] is `true`, [removeKotlintestElementsFromStacktrace] will be
    * called on the returned error.
    */
-  fun failure(message: String, cause: Throwable?=null): AssertionError = AssertionError(message).apply {
+  fun failure(message: String, cause: Throwable? = null): AssertionError = AssertionError(message).apply {
     if (shouldRemoveKotlintestElementsFromStacktrace) {
       removeKotlintestElementsFromStacktrace(this)
     }
-    if (cause != null) {
-      initCause(cause)
-    }
+    this.initCause(cause)
   }
 
   /**
@@ -37,10 +36,12 @@ object Failures {
    */
   fun removeKotlintestElementsFromStacktrace(throwable: Throwable) {
     val stackTrace = throwable.stackTrace
-    val lastKotlintestIndex = stackTrace.indexOfLast {
-      it.className.startsWith("io.kotlintest") && ! it.className.startsWith("io.kotlintest.runner")
-    }
-    throwable.stackTrace = stackTrace.drop(lastKotlintestIndex + 1).toTypedArray()
+    // we are looking for the first stack trace element that is not an internal kotlintest class.
+    throwable.stackTrace = stackTrace.dropWhile {
+      it.className.startsWith("io.kotlintest") ||
+          it.className.startsWith("sun.reflect") ||
+          it.className.startsWith("java.")
+    }.toTypedArray()
   }
 
   private fun readSystemProperty(): Boolean {
