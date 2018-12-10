c7f16a161f3ee10ba8dc79e8fe16b1fecbb5649a
sksamuel <sam@sksamuel.com>
2018-12-10 20:16:27 +0000

    BehaviorSpec doesn't allow config #495

--- kotlintest-core/src/main/kotlin/io/kotlintest/specs/AbstractBehaviorSpec.kt
+++ kotlintest-core/src/main/kotlin/io/kotlintest/specs/AbstractBehaviorSpec.kt
@@ -1,8 +1,12 @@
 package io.kotlintest.specs
 
 import io.kotlintest.AbstractSpec
+import io.kotlintest.Tag
+import io.kotlintest.TestCaseConfig
 import io.kotlintest.TestContext
 import io.kotlintest.TestType
+import io.kotlintest.extensions.TestCaseExtension
+import java.time.Duration
 
 @Suppress("FunctionName")
 abstract class AbstractBehaviorSpec(body: AbstractBehaviorSpec.() -> Unit = {}) : AbstractSpec() {
@@ -26,6 +30,30 @@ abstract class AbstractBehaviorSpec(body: AbstractBehaviorSpec.() -> Unit = {}) 
     fun Then(name: String, test: TestContext.() -> Unit) = then(name, test)
     fun then(name: String, test: TestContext.() -> Unit) =
         context.registerTestCase("Then: $name", this@AbstractBehaviorSpec, test, this@AbstractBehaviorSpec.defaultTestCaseConfig, TestType.Test)
+
+    fun then(name: String): ThenScope = ThenScope(name, context)
+  }
+
+  @KotlinTestDsl
+  inner class ThenScope(val name: String, val context: TestContext) {
+
+    fun config(
+        invocations: Int? = null,
+        enabled: Boolean? = null,
+        timeout: Duration? = null,
+        threads: Int? = null,
+        tags: Set<Tag>? = null,
+        extensions: List<TestCaseExtension>? = null,
+        test: TestContext.() -> Unit) {
+      val config = TestCaseConfig(
+          enabled ?: this@AbstractBehaviorSpec.defaultTestCaseConfig.enabled,
+          invocations ?: this@AbstractBehaviorSpec.defaultTestCaseConfig.invocations,
+          timeout ?: this@AbstractBehaviorSpec.defaultTestCaseConfig.timeout,
+          threads ?: this@AbstractBehaviorSpec.defaultTestCaseConfig.threads,
+          tags ?: this@AbstractBehaviorSpec.defaultTestCaseConfig.tags,
+          extensions ?: this@AbstractBehaviorSpec.defaultTestCaseConfig.extensions)
+      context.registerTestCase(name, this@AbstractBehaviorSpec, { test.invoke(this) }, config, TestType.Test)
+    }
   }
 
   fun Given(name: String, test: GivenContext.() -> Unit) = given(name, test)
--- kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/specs/behavior/BehaviorSpecTest.kt
+++ kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/specs/behavior/BehaviorSpecTest.kt
@@ -1,7 +1,9 @@
 package com.sksamuel.kotlintest.specs.behavior
 
 import io.kotlintest.matchers.numerics.shouldBeLessThan
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.BehaviorSpec
+import java.util.concurrent.atomic.AtomicInteger
 
 class BehaviorSpecTest : BehaviorSpec() {
   init {
@@ -10,6 +12,11 @@ class BehaviorSpecTest : BehaviorSpec() {
         then("c") {
           1.shouldBeLessThan(2)
         }
+        val counter = AtomicInteger(0)
+        then("with config").config(invocations = 3) {
+          counter.incrementAndGet()
+        }
+        counter.get() shouldBe 3
       }
     }
   }
