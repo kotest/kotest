344732371179c18c019e1f714effba3742eb2e88
sksamuel <sam@sksamuel.com>
2019-01-24 22:52:24 +0000

    Timezone / Locale Extension #587

---
+++ kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/extensions/LocaleExtensionTest.kt
@@ -0,0 +1,31 @@
+package com.sksamuel.kotlintest.extensions
+
+import io.kotlintest.Spec
+import io.kotlintest.TestCase
+import io.kotlintest.TestResult
+import io.kotlintest.extensions.LocaleExtension
+import io.kotlintest.extensions.TopLevelTest
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.FunSpec
+import java.util.*
+
+class LocaleExtensionTest : FunSpec() {
+
+  override fun extensions() = listOf(LocaleExtension(Locale.FRANCE))
+
+  private var deflocale: Locale? = null
+
+  override fun beforeSpecClass(spec: Spec, tests: List<TopLevelTest>) {
+    deflocale = Locale.getDefault()
+  }
+
+  override fun afterSpecClass(spec: Spec, results: Map<TestCase, TestResult>) {
+    Locale.getDefault() shouldBe deflocale
+  }
+
+  init {
+    test("locale default should be set, and then restored after") {
+      Locale.getDefault() shouldBe Locale.FRANCE
+    }
+  }
+}
