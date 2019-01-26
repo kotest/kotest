caf6b5fe9241e39a061d1608ca4ae2ccf3ed40dd
sksamuel <sam@sksamuel.com>
2019-01-26 12:32:31 +0000

    Tweaking tty setting

--- snapshot_deploy.sh
+++ snapshot_deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    export GPG_TTY=tty
+    GPG_TTY=$(tty)
+    export GPG_TTY
   ./gradlew uploadArchives -PossrhUsername=${OSSRH_USERNAME} -PossrhPassword=${OSSRH_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE}
 fi
