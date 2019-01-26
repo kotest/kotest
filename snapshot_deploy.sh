2f5b148ffff73ec3f89e5ce4d47209d05566f645
sksamuel <sam@sksamuel.com>
2019-01-26 12:25:17 +0000

    export GPG_TTY=tty

--- snapshot_deploy.sh
+++ snapshot_deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    export GPG_TTY=tty
   ./gradlew uploadArchives -PossrhUsername=${OSSRH_USERNAME} -PossrhPassword=${OSSRH_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE}
 fi
