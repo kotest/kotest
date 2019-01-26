baaf202a954e77f2498ab209ff20156306df5d76
sksamuel <sam@sksamuel.com>
2019-01-26 11:54:46 +0000

    Tweaking gradle script

--- snapshot_deploy.sh
+++ snapshot_deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K $encrypted_1aa081e50934_key -iv $encrypted_1aa081e50934_iv -in sksamuel.gpg.enc -out sksamuel.gpg -d
+  openssl aes-256-cbc -K $encrypted_1aa081e50934_key -iv $encrypted_1aa081e50934_iv -in ./sksamuel.gpg.enc -out /.sksamuel.gpg -d
+  echo ./sksamuel.gpg
   ./gradlew uploadArchives -PossrhUsername=${OSSRH_USERNAME} -PossrhPassword=${OSSRH_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=./sksamuel.gpg
 fi
