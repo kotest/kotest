c3a9b4edab19b5d980ca3330c53287997cdfd323
sksamuel <sam@sksamuel.com>
2019-01-28 11:30:40 +0000

    Updated deploy command to use publish

--- snapshot_deploy.sh
+++ snapshot_deploy.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    GPG_TTY=$(tty)
-    export GPG_TTY
-  ./gradlew uploadArchives -PossrhUsername=${OSSRH_USERNAME} -PossrhPassword=${OSSRH_PASSWORD}
+  ./gradlew publish
 fi
