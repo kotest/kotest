c64029c53d2af498833c5c75250367225854e65a
sksamuel <sam@sksamuel.com>
2019-02-12 17:28:39 +0000

    Added should test example to gradle examples

---
+++ kotlintest-samples/kotlintest-samples-gradle/src/test/kotlin/io/kotlintest/samples/gradle/ShouldSpecExampleTest.kt
@@ -0,0 +1,30 @@
+package io.kotlintest.samples.gradle
+
+import io.kotlintest.specs.ShouldSpec
+
+class ShouldSpecExampleTest : ShouldSpec() {
+
+  init {
+    should("top level test") {
+      // test here
+    }
+    should("top level test with config").config(enabled = true) {
+      // test here
+    }
+    "a context" {
+      should("do a test") {
+        // test here
+      }
+      should("have config").config(enabled = true) {
+        // test here
+      }
+    }
+    "another context" {
+      "a nested context" {
+        should("do a test") {
+          // test here
+        }
+      }
+    }
+  }
+}
