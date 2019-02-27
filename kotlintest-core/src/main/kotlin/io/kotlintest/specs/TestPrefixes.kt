2bed43ddc510a7a5ff9f7d9f7df30a1f21f4bd9b
Leonardo Colman Lopes <leonardo.colman98@gmail.com>
2019-02-27 20:24:39 -0300

    Allows focusing tests with prefix (#700)
    Before this, and with commit 31d524513970947442385e849e8fca304dea1681, any focused test that contains a prefix wouldn't be correctly focused, because the focus wouldn't be brought behind the prefix.
    
    As of the forementioned commit already delegated test name creation to `createTestName`, this commit just adapt that behavior to include focus.
    Fixes #699

--- kotlintest-core/src/main/kotlin/io/kotlintest/specs/TestPrefixes.kt
+++ kotlintest-core/src/main/kotlin/io/kotlintest/specs/TestPrefixes.kt
@@ -1,5 +1,9 @@
 package io.kotlintest.specs
 
-internal fun createTestName(prefix: String, name: String): String {
-  return if(name.startsWith("!")) "!$prefix${name.drop(1)}" else "$prefix$name"
+fun createTestName(prefix: String, name: String): String {
+  return when {
+    name.startsWith("!") -> "!$prefix${name.drop(1)}"
+    name.startsWith("f:") -> "f:$prefix${name.drop(2)}"
+    else -> "$prefix$name"
+  }
 }
