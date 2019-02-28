026eb2dead0c5c96d9f3e64371cb55116eb7ac3b
sksamuel <sam@sksamuel.com>
2019-02-28 22:52:23 +0000

    Added test for bang intention

--- src/test/kotlin/io/kotlintest/plugin/intellij/intentions/BangIntentionTest.kt
+++ src/test/kotlin/io/kotlintest/plugin/intellij/intentions/BangIntentionTest.kt
@@ -2,6 +2,7 @@ package io.kotlintest.plugin.intellij.intentions
 
 import com.intellij.openapi.command.CommandProcessor
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
+import io.kotlintest.matchers.collections.shouldBeEmpty
 import io.kotlintest.shouldBe
 import org.jetbrains.kotlin.idea.core.moveCaret
 import org.jetbrains.kotlin.idea.util.application.runWriteAction
@@ -34,5 +35,12 @@ class BangIntentionTest : LightCodeInsightFixtureTestCase() {
       }
     }
     file.findElementAt(270)?.text shouldBe "another test"
+  }
+
+  fun testIntentionShouldOnlyBeAvailableOnStrings() {
+
+    myFixture.configureByFile("/behaviorspec.kt")
+    editor.moveCaret(313)
+    myFixture.filterAvailableIntentions("Add/Remove bang to test name").shouldBeEmpty()
   }
 }
