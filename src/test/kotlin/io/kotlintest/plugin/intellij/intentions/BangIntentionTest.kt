e63133d0fde8b0fad3c2c300767d5ca44368b120
sksamuel <sam@sksamuel.com>
2019-02-28 22:50:44 +0000

    Added test for bang intention

---
+++ src/test/kotlin/io/kotlintest/plugin/intellij/intentions/BangIntentionTest.kt
@@ -0,0 +1,38 @@
+package io.kotlintest.plugin.intellij.intentions
+
+import com.intellij.openapi.command.CommandProcessor
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
+import io.kotlintest.shouldBe
+import org.jetbrains.kotlin.idea.core.moveCaret
+import org.jetbrains.kotlin.idea.util.application.runWriteAction
+import java.nio.file.Paths
+
+class BangIntentionTest : LightCodeInsightFixtureTestCase() {
+
+  override fun getTestDataPath(): String {
+    val path = Paths.get("./src/test/resources/").toAbsolutePath()
+    return path.toString()
+  }
+
+  fun testIntention() {
+
+    myFixture.configureByFile("/behaviorspec.kt")
+    editor.moveCaret(270)
+
+    val intention = myFixture.findSingleIntention("Add/Remove bang to test name")
+    intention.familyName shouldBe "Add/Remove bang to test name"
+    CommandProcessor.getInstance().runUndoTransparentAction {
+      runWriteAction {
+        intention.invoke(project, editor, file)
+      }
+    }
+    file.findElementAt(270)?.text shouldBe "!another test"
+
+    CommandProcessor.getInstance().runUndoTransparentAction {
+      runWriteAction {
+        intention.invoke(project, editor, file)
+      }
+    }
+    file.findElementAt(270)?.text shouldBe "another test"
+  }
+}
