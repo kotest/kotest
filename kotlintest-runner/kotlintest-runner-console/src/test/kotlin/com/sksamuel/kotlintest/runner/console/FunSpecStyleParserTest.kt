0dc5b111ae726514a9fd7bab7aba83b14febae96
sksamuel <sam@sksamuel.com>
2019-02-28 23:41:01 +0000

    Fixed failing test

--- kotlintest-runner/kotlintest-runner-console/src/test/kotlin/com/sksamuel/kotlintest/runner/console/FunSpecStyleParserTest.kt
+++ kotlintest-runner/kotlintest-runner-console/src/test/kotlin/com/sksamuel/kotlintest/runner/console/FunSpecStyleParserTest.kt
@@ -1,7 +1,7 @@
 package com.sksamuel.kotlintest.runner.console
 
 import io.kotlintest.Description
-import io.kotlintest.runner.console.FunSpecStyleParser
+import io.kotlintest.runner.console.DelimitedTestPathParser
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.FunSpec
 
@@ -10,18 +10,18 @@ class FunSpecStyleParserTest : FunSpec() {
   init {
 
     test("should parse tests") {
-      FunSpecStyleParser.parse(Description.spec("myspec"), "should testa") shouldBe
+      DelimitedTestPathParser.parse(Description.spec("myspec"), "should testa") shouldBe
           Description.spec("myspec").append("should testa")
 
-      FunSpecStyleParser.parse(Description.spec("myspec"), "should testatesta--") shouldBe
+      DelimitedTestPathParser.parse(Description.spec("myspec"), "should testatesta--") shouldBe
           Description.spec("myspec").append("should testatesta--")
     }
 
     test("should parse tests with a context") {
-      FunSpecStyleParser.parse(Description.spec("myspec"), "some context -- some other context -- test") shouldBe
+      DelimitedTestPathParser.parse(Description.spec("myspec"), "some context -- some other context -- test") shouldBe
           Description.spec("myspec").append("some context").append("some other context").append("test")
 
-      FunSpecStyleParser.parse(Description.spec("myspec"), "some context -- test") shouldBe
+      DelimitedTestPathParser.parse(Description.spec("myspec"), "some context -- test") shouldBe
           Description.spec("myspec").append("some context").append("test")
 
     }
