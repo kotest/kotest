782a813e234826a23835ef77f7f9aa65197904f5
Kerooker <leonardo.colman98@gmail.com>
2019-03-07 11:03:40 -0300

    Adds Javadocs and negated version of matchers
    Part of #711

--- kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/equality/reflection.kt
+++ kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/equality/reflection.kt
@@ -3,13 +3,87 @@ package io.kotlintest.matchers.equality
 import io.kotlintest.Matcher
 import io.kotlintest.Result
 import io.kotlintest.should
+import io.kotlintest.shouldNot
 import kotlin.reflect.KProperty
 import kotlin.reflect.full.memberProperties
 
-fun <T> T.shouldBeEqualToUsingFields(other: T, vararg properties: KProperty<*>) =
+/**
+ * Asserts that this is equal to [other] using specific fields
+ *
+ * Verifies that [this] instance is equal to [other] using only some specific fields. This is useful for matching
+ * on objects that contain unknown values, such as a database Entity that contains an ID (you don't know this ID, and it
+ * doesn't matter for you, for example)
+ *
+ * Opposite of [shouldNotBeEqualToUsingFields]
+ *
+ * Example:
+ * ```
+ * data class Foo(val id: Int, val description: String)
+ *
+ * val firstFoo = Foo(1, "Bar!")
+ * val secondFoo = Foo(2, "Bar!")
+ *
+ * firstFoo.shouldBeEqualUsingFields(secondFoo, Foo::description) // Assertion passes
+ *
+ * firstFoo shouldBe secondFoo // Assertion fails, `equals` is false!
+ * ```
+ *
+ */
+fun <T : Any> T.shouldBeEqualToUsingFields(other: T, vararg properties: KProperty<*>) =
     this should beEqualToUsingFields(other, *properties)
 
-fun <T> beEqualToUsingFields(other: T, vararg fields: KProperty<*>): Matcher<T> = object : Matcher<T> {
+/**
+ * Asserts that this is NOT equal to [other] using specific fields
+ *
+ * Verifies that [this] instance is not equal to [other] using only some specific fields. This is useful for matching
+ * on objects that contain unknown values, such as a database Entity that contains an ID (you don't know this ID, and it
+ * doesn't matter for you, for example)
+ *
+ * Opposite of [shouldBeEqualToUsingFields]
+ *
+ * Example:
+ * ```
+ * data class Foo(val id: Int, val description: String)
+ *
+ * val firstFoo = Foo(1, "Bar!")
+ * val secondFoo = Foo(2, "BAT")
+ *
+ * firstFoo.shouldNotBeEqualToUsingFields(secondFoo, Foo::description) // Assertion passes
+ *
+ * ```
+ *
+ * @see [beEqualToUsingFields]
+ * @see [shouldNotBeEqualToIgnoringFields]
+ *
+ */
+fun <T : Any> T.shouldNotBeEqualToUsingFields(other: T, vararg properties: KProperty<*>) =
+    this shouldNot beEqualToUsingFields(other, *properties)
+
+/**
+ * Matcher that compares values using specific fields
+ *
+ * Verifies that two instances not equal using only some specific fields. This is useful for matching
+ * on objects that contain unknown values, such as a database Entity that contains an ID (you don't know this ID, and it
+ * doesn't matter for you, for example)
+ *
+ *
+ * Example:
+ * ```
+ * data class Foo(val id: Int, val description: String)
+ *
+ * val firstFoo = Foo(1, "Bar!")
+ * val secondFoo = Foo(2, "Bar!")
+ *
+ * firstFoo should beEqualToUsingFields(secondFoo, Foo::description) // Assertion passes
+ *
+ * ```
+ *
+ * @see [shouldBeEqualToUsingFields]
+ * @see [shouldNotBeEqualToUsingFields]
+ * @see [beEqualToIgnoringFields]
+ *
+ */
+fun <T : Any> beEqualToUsingFields(other: T, vararg fields: KProperty<*>): Matcher<T> = object : Matcher<T> {
   override fun test(value: T): Result {
 
     val failed = fields.mapNotNull {
@@ -30,9 +104,78 @@ fun <T> beEqualToUsingFields(other: T, vararg fields: KProperty<*>): Matcher<T> 
   }
 }
 
+/**
+ * Asserts that this is equal to [other] without using specific fields
+ *
+ * Verifies that [this] instance is equal to [other] without using some specific fields. This is useful for matching
+ * on objects that contain unknown values, such as a database Entity that contains an ID (you don't know this ID, and it
+ * doesn't matter for you, for example)
+ *
+ * Opposite of [shouldNotBeEqualToIgnoringFields]
+ *
+ * Example:
+ * ```
+ * data class Foo(val id: Int, val description: String)
+ *
+ * val firstFoo = Foo(1, "Bar!")
+ * val secondFoo = Foo(2, "Bar!")
+ *
+ * firstFoo.shouldBeEqualToIgnoringFields(secondFoo, Foo::id) // Assertion passes
+ *
+ * firstFoo shouldBe secondFoo // Assertion fails, `equals` is false!
+ * ```
+ *
+ */
 fun <T : Any> T.shouldBeEqualToIgnoringFields(other: T, vararg properties: KProperty<*>) =
     this should beEqualToIgnoringFields(other, *properties)
 
+/**
+ * Asserts that this is not equal to [other] without using specific fields
+ *
+ * Verifies that [this] instance is not equal to [other] without using some specific fields. This is useful for matching
+ * on objects that contain unknown values, such as a database Entity that contains an ID (you don't know this ID, and it
+ * doesn't matter for you, for example)
+ *
+ * Opposite of [shouldBeEqualToIgnoringFields]
+ *
+ * Example:
+ * ```
+ * data class Foo(val id: Int, val description: String)
+ *
+ * val firstFoo = Foo(1, "Bar!")
+ * val secondFoo = Foo(2, "BAT!")
+ *
+ * firstFoo.shouldNotBeEqualToIgnoringFields(secondFoo, Foo::id) // Assertion passes
+ * ```
+ *
+ */
+fun <T : Any> T.shouldNotBeEqualToIgnoringFields(other: T, vararg properties: KProperty<*>) =
+    this shouldNot beEqualToIgnoringFields(other, *properties)
+
+/**
+ * Matcher that compares values without using specific fields
+ *
+ * Verifies that two instances are equal by not using only some specific fields. This is useful for matching
+ * on objects that contain unknown values, such as a database Entity that contains an ID (you don't know this ID, and it
+ * doesn't matter for you, for example)
+ *
+ *
+ * Example:
+ * ```
+ * data class Foo(val id: Int, val description: String)
+ *
+ * val firstFoo = Foo(1, "Bar!")
+ * val secondFoo = Foo(2, "Bar!")
+ *
+ * firstFoo should beEqualToIgnoringFields(secondFoo, Foo::id) // Assertion passes
+ *
+ * ```
+ *
+ * @see [beEqualToUsingFields]
+ * @see [shouldBeEqualToIgnoringFields]
+ * @see [shouldNotBeEqualToIgnoringFields]
+ *
+ */
 fun <T : Any> beEqualToIgnoringFields(other: T,
                                       vararg fields: KProperty<*>): Matcher<T> = object : Matcher<T> {
   override fun test(value: T): Result {
