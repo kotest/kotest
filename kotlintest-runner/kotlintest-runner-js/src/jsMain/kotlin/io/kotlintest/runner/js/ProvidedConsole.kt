0ecb7c071d1d47f24c53eb094cecd0b9647e15f2
sksamuel <sam@sksamuel.com>
2019-03-23 18:04:37 +0000

    Added js runner with stub console

---
+++ kotlintest-runner/kotlintest-runner-js/src/jsMain/kotlin/io/kotlintest/runner/js/ProvidedConsole.kt
@@ -0,0 +1,21 @@
+package io.kotlintest.runner.js
+
+import kotlin.js.Console
+
+class ProvidedConsole : Console {
+  override fun dir(o: Any) {
+  }
+
+  override fun error(vararg o: Any?) {
+  }
+
+  override fun info(vararg o: Any?) {
+  }
+
+  override fun log(vararg o: Any?) {
+  }
+
+  override fun warn(vararg o: Any?) {
+  }
+
+}
