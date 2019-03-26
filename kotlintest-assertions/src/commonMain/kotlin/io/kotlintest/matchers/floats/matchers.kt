394eaf7b267c9b2929c3f00d5196042302fa8a27
Leonardo Colman Lopes <leonardo.colman98@gmail.com>
2019-03-26 17:24:28 -0300

    Move Float matchers to common (#730)

--- kotlintest-assertions/src/commonMain/kotlin/io/kotlintest/matchers/floats/matchers.kt
+++ kotlintest-assertions/src/commonMain/kotlin/io/kotlintest/matchers/floats/matchers.kt
@@ -33,14 +33,14 @@ fun beGreaterThanOrEqualTo(x: Float) = object : Matcher<Float> {
       "$value should not be >= $x")
 }
 
-infix fun Float.shouldBeLessThan(x: Float) = this shouldBe io.kotlintest.matchers.lt(x)
-infix fun Float.shouldNotBeLessThan(x: Float) = this shouldNotBe io.kotlintest.matchers.lt(x)
+infix fun Float.shouldBeLessThan(x: Float) = this shouldBe lt(x)
+infix fun Float.shouldNotBeLessThan(x: Float) = this shouldNotBe lt(x)
 
-infix fun Float.shouldBeLessThanOrEqual(x: Float) = this shouldBe io.kotlintest.matchers.lte(x)
-infix fun Float.shouldNotBeLessThanOrEqual(x: Float) = this shouldNotBe io.kotlintest.matchers.lte(x)
+infix fun Float.shouldBeLessThanOrEqual(x: Float) = this shouldBe lte(x)
+infix fun Float.shouldNotBeLessThanOrEqual(x: Float) = this shouldNotBe lte(x)
 
-infix fun Float.shouldBeGreaterThan(x: Float) = this shouldBe io.kotlintest.matchers.gt(x)
-infix fun Float.shouldNotBeGreaterThan(x: Float) = this shouldNotBe io.kotlintest.matchers.gt(x)
+infix fun Float.shouldBeGreaterThan(x: Float) = this shouldBe gt(x)
+infix fun Float.shouldNotBeGreaterThan(x: Float) = this shouldNotBe gt(x)
 
-infix fun Float.shouldBeGreaterThanOrEqual(x: Float) = this shouldBe io.kotlintest.matchers.gte(x)
-infix fun Float.shouldNotBeGreaterThanOrEqual(x: Float) = this shouldNotBe io.kotlintest.matchers.gte(x)
+infix fun Float.shouldBeGreaterThanOrEqual(x: Float) = this shouldBe gte(x)
+infix fun Float.shouldNotBeGreaterThanOrEqual(x: Float) = this shouldNotBe gte(x)
