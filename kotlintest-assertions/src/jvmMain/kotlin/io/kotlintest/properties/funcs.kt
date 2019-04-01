9fdcac33f3d6391236d7b05d1f99dacd56c809a5
Kerooker <leonardo.colman98@gmail.com>
2019-04-01 15:09:58 -0300

    Move Inspectors to Common

--- kotlintest-assertions/src/jvmMain/kotlin/io/kotlintest/properties/funcs.kt
+++ kotlintest-assertions/src/jvmMain/kotlin/io/kotlintest/properties/funcs.kt
@@ -1,14 +1,5 @@
 import io.kotlintest.properties.PropertyContext
 
-fun exceptionToMessage(t: Throwable): String =
-  when (t) {
-    is AssertionError -> when (t.message) {
-      null -> t.toString()
-      else -> t.message!!
-    }
-    else -> t.toString()
-  }
-
 fun outputClassifications(context: PropertyContext) {
   context.classificationCounts().entries.sortedByDescending { it.value }.forEach {
     val percentage = (it.value / context.attempts().toDouble() * 100)
