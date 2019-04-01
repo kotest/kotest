6695d1bec91da58ece2cc1774094a465d4797466
Leonardo Colman Lopes <leonardo.colman98@gmail.com>
2019-04-01 15:34:40 -0300

    Move collection matchers to common (#743)

--- kotlintest-assertions/src/commonMain/kotlin/io/kotlintest/matchers/collections/matchers.kt
+++ kotlintest-assertions/src/commonMain/kotlin/io/kotlintest/matchers/collections/matchers.kt
@@ -2,12 +2,6 @@ package io.kotlintest.matchers.collections
 
 import io.kotlintest.Matcher
 import io.kotlintest.Result
-import io.kotlintest.matchers.beEmpty
-import io.kotlintest.matchers.beSorted
-import io.kotlintest.matchers.containAll
-import io.kotlintest.matchers.containsInOrder
-import io.kotlintest.matchers.haveSize
-import io.kotlintest.matchers.singleElement
 import io.kotlintest.neverNullMatcher
 import io.kotlintest.should
 import io.kotlintest.shouldHave
@@ -373,3 +367,76 @@ private fun throwEmptyCollectionError(): Nothing {
   throw AssertionError("Asserting content on empty collection. Use Collection.shouldBeEmpty() instead.")
 }
 
+fun <T> haveSizeMatcher(size: Int) = object : Matcher<Collection<T>> {
+  override fun test(value: Collection<T>) =
+          Result(
+                  value.size == size,
+                  "Collection should have size $size but has size ${value.size}",
+                  "Collection should not have size $size"
+          )
+}
+
+
+fun <T> beEmpty(): Matcher<Collection<T>> = object : Matcher<Collection<T>> {
+  override fun test(value: Collection<T>): Result = Result(
+          value.isEmpty(),
+          "Collection should be empty but contained ${stringRepr(value)}",
+          "Collection should not be empty"
+  )
+}
+
+
+fun <T> containAll(vararg ts: T) = containAll(ts.asList())
+fun <T> containAll(ts: Collection<T>): Matcher<Collection<T>> = object : Matcher<Collection<T>> {
+  override fun test(value: Collection<T>) = Result(
+          ts.all { value.contains(it) },
+          "Collection should contain all of ${ts.joinToString(", ", limit = 10) { stringRepr(it) }}",
+          "Collection should not contain all of ${ts.joinToString(", ", limit = 10) { stringRepr(it) }}"
+  )
+}
+
+fun <T> containsInOrder(vararg ts: T): Matcher<Collection<T>?> = containsInOrder(ts.asList())
+/** Assert that a collection contains a given subsequence, possibly with values in between. */
+fun <T> containsInOrder(subsequence: List<T>): Matcher<Collection<T>?> = neverNullMatcher { actual ->
+  require(subsequence.isNotEmpty()) { "expected values must not be empty" }
+  
+  var subsequenceIndex = 0
+  val actualIterator = actual.iterator()
+  
+  while (actualIterator.hasNext() && subsequenceIndex < subsequence.size) {
+    if (actualIterator.next() == subsequence[subsequenceIndex]) subsequenceIndex += 1
+  }
+  
+  Result(
+          subsequenceIndex == subsequence.size,
+          "${stringRepr(actual)} did not contain the elements ${stringRepr(subsequence)} in order",
+          "${stringRepr(actual)} should not contain the elements ${stringRepr(subsequence)} in order"
+  )
+}
+
+fun <T> haveSize(size: Int): Matcher<Collection<T>> = haveSizeMatcher(size)
+
+fun <T> singleElement(t: T): Matcher<Collection<T>> = object : Matcher<Collection<T>> {
+  override fun test(value: Collection<T>) = Result(
+          value.size == 1 && value.first() == t,
+          "Collection should be a single element of $t but has ${value.size} elements",
+          "Collection should not be a single element of $t"
+  )
+}
+
+fun <T : Comparable<T>> beSorted(): Matcher<List<T>> = sorted()
+fun <T : Comparable<T>> sorted(): Matcher<List<T>> = object : Matcher<List<T>> {
+  override fun test(value: List<T>): Result {
+    val failure = value.withIndex().firstOrNull { (i, it) -> i != value.lastIndex && it > value[i+1] }
+    val snippet = value.joinToString(",", limit = 10)
+    val elementMessage = when (failure) {
+      null -> ""
+      else -> ". Element ${failure.value} at index ${failure.index} was greater than element ${value[failure.index+1]}"
+    }
+    return Result(
+            failure == null,
+            "List [$snippet] should be sorted$elementMessage",
+            "List [$snippet] should not be sorted"
+    )
+  }
+}
