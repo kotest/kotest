f2812bc1d3d6bb314a29bdce31a4241699131151
Gustavo Fao <gustavo@valvassori.dev>
2019-07-02 19:10:43 -0300

    used suggested workaround to tests run

--- kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/result/matchers.kt
+++ kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/result/matchers.kt
@@ -1,59 +1,70 @@
 package io.kotlintest.matchers.result
 
-import io.kotlintest.Matcher
+import io.kotlintest.*
+import io.kotlintest.ErrorCollector
 import io.kotlintest.should
 import io.kotlintest.shouldNot
+import kotlin.Result
 
 fun <T> Result<T>.shouldBeSuccess(block: ((T) -> Unit)? = null) {
-  this should beSuccess()
+  BeSuccess<T>().test(this)
   block?.invoke(getOrNull()!!)
 }
-fun <T> Result<T>.shouldNotBeSuccess() = this shouldNot beSuccess()
-fun <T> beSuccess() = object : Matcher<Result<T>> {
-  override fun test(value: Result<T>) = io.kotlintest.Result(
-      value.isSuccess,
-      "Result should be a success.",
-      "Result should not be a success"
-  )
-}
 
-infix fun <T> Result<T>.shouldBeSuccess(expected: T) = this should beSuccess(expected)
-infix fun <T> Result<T>.shouldNotBeSuccess(expected: T) = this shouldNot beSuccess(expected)
-fun <T> beSuccess(expected: T) = object : Matcher<Result<T>> {
+fun <T> Result<T>.shouldNotBeSuccess() = test(inverse = true) { BeSuccess<T>().test(this) }
+
+infix fun <T> Result<T>.shouldBeSuccess(expected: T) = test { BeSuccess<T>(expected).test(this) }
+infix fun <T> Result<T>.shouldNotBeSuccess(expected: T) = test(inverse = true) { BeSuccess<T>(expected).test(this) }
+class BeSuccess<T>(val expected: T? = null) : Matcher<Result<T>> {
   override fun test(value: Result<T>): io.kotlintest.Result {
-    return value.fold(
-        { result ->
-          if (result == expected) {
-            io.kotlintest.Result(true, "Result should be a Success($expected)", "Result should not be a Success($expected)")
-          } else io.kotlintest.Result(false, "Result should be a success", "")
-        },
-        { error -> io.kotlintest.Result(false, "Result should be a success", "") }
-    )
+    return when {
+      !value.isSuccess -> defaultResult(false)
+      expected == null -> defaultResult(true)
+      else -> io.kotlintest.Result(
+          value.getOrNull() == expected,
+          "Result should be a Success($expected), but instead got Succes(${value.getOrNull()}).",
+          "Result should not be a Success($expected)"
+      )
+    }
   }
+
+  private fun defaultResult(passed: Boolean) =
+      io.kotlintest.Result(passed, "Result should be a success.", "Result should not be a success")
 }
 
-fun <T> Result<T>.shouldBeFailure(block: ((Throwable) -> Unit)? = null) {
-  this should beFailure()
+fun Result<Any>.shouldBeFailure(block: ((Throwable) -> Unit)? = null) {
+  test { BeFailure().test(this) }
   block?.invoke(exceptionOrNull()!!)
 }
-fun <T> Result<T>.shouldNotBeFailure() = this shouldNot beFailure()
-fun <T> beFailure() = object : Matcher<Result<T>> {
-  override fun test(value: Result<T>) = io.kotlintest.Result(
+
+fun Result<Any>.shouldNotBeFailure() = test(inverse = true) { BeFailure().test(this) }
+class BeFailure : Matcher<Result<Any>> {
+  override fun test(value: Result<Any>) = io.kotlintest.Result(
       value.isFailure,
       "Result should be a failure",
       "Result should not be a failure"
   )
 }
 
-inline fun <reified A : Throwable> Result<Any>.shouldBeFailureOfType() = this should beFailureOfType<A>()
-inline fun <reified A : Throwable> Result<Any>.shouldNotBeFailureOfType() = this shouldNot beFailureOfType<A>()
-inline fun <reified A : Throwable> beFailureOfType() = object : Matcher<Result<Any>> {
+inline fun <reified A : Throwable> Result<Any>.shouldBeFailureOfType() = test { BeFailureOfType(A::class.java).test(this) }
+inline fun <reified A : Throwable> Result<Any>.shouldNotBeFailureOfType() = test(inverse = true) { BeFailureOfType(A::class.java).test(this) }
+class BeFailureOfType<A : Throwable>(val clazz: Class<A>) : Matcher<Result<Any>> {
   override fun test(value: Result<Any>): io.kotlintest.Result {
     val error = value.exceptionOrNull()
     return when {
       value.isSuccess -> io.kotlintest.Result(false, "Result should be a failure", "")
-      error is A -> io.kotlintest.Result(true, "Result should be a Failure(${A::class})", "Result should not be a Failure(${A::class})")
-      else -> io.kotlintest.Result(false, "Result should be a Failure(${A::class}) but was Failure(${error!!::class})", "")
+      clazz.isInstance(error) -> io.kotlintest.Result(true, "Result should be a Failure(${clazz})", "Result should not be a Failure(${clazz})")
+      else -> io.kotlintest.Result(false, "Result should be a Failure(${clazz}) but was Failure(${error!!::class})", "")
     }
+  }
+}
+
+@PublishedApi
+internal fun test(inverse: Boolean = false, block: () -> io.kotlintest.Result) {
+  val result = block()
+  if ((inverse && result.passed) || (!inverse && !result.passed)) {
+    ErrorCollector.collectOrThrow(
+        Failures.failure(ErrorCollector.clueContextAsString() + if (inverse) result.negatedFailureMessage else result.failureMessage)
+    )
   }
 }
