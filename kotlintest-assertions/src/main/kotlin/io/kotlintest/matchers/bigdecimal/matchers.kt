fed425f700f3e8ecbb630839e8df2bd1030cc4be
Weverson Nascimento <weverson.sn@gmail.com>
2019-07-02 17:19:23 -0300

    Add matcher shouldHavePrecision to BigDecimalNumbers

--- kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/bigdecimal/matchers.kt
+++ kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/bigdecimal/matchers.kt
@@ -4,3 +4,4 @@ import io.kotlintest.shouldBe
 import java.math.BigDecimal
 
 fun BigDecimal.shouldBeZero() = this shouldBe BigDecimal.ZERO
+infix fun BigDecimal.shouldHavePrecision(precision: Int) = this.precision() shouldBe precision
--- kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/matchers/bigdecimal/BigDecimalMatchersTest.kt
+++ kotlintest-tests/kotlintest-tests-core/src/test/kotlin/com/sksamuel/kotlintest/matchers/bigdecimal/BigDecimalMatchersTest.kt
@@ -11,5 +11,11 @@ class BigDecimalMatchersTest : StringSpec() {
             BigDecimal(0).shouldBeZero()
             0.toBigDecimal().shouldBeZero()
         }
+        "shouldHavePrecision" {
+            BigDecimal(10).setScale(3) shouldHavePrecision 5
+            BigDecimal(10.1) shouldHavePrecision 51
+            10.1.toBigDecimal() shouldHavePrecision 3
+            BigDecimal.ZERO shouldHavePrecision 1
+        }
     }
 }
