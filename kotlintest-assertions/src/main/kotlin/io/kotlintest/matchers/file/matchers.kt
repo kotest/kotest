9bd8055b6fe780ddb3091e5ab088770c4d1ea141
Gustavo Fao <gustavo@valvassori.dev>
2019-07-02 21:53:54 -0300

    added shouldBeSymbolicLink and shouldHaveParent matchers for files

--- kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/file/matchers.kt
+++ kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/file/matchers.kt
@@ -7,6 +7,7 @@ import io.kotlintest.shouldBe
 import io.kotlintest.shouldNot
 import io.kotlintest.shouldNotBe
 import java.io.File
+import java.nio.file.Files
 import java.nio.file.Path
 
 fun File.shouldBeNonEmptyDirectory() = this should beNonEmptyDirectory()
@@ -187,6 +188,22 @@ fun startWithPath(prefix: String) = object : Matcher<File> {
   override fun test(value: File): Result = Result(value.toString().startsWith(prefix), "File $value should start with $prefix", "File $value should not start with $prefix")
 }
 
+fun File.shouldBeSymbolicLink() = this.toPath() should beSymbolicLink()
+fun File.shouldNotBeSymbolicLink() = this.toPath() shouldNot beSymbolicLink()
+
+infix fun File.shouldHaveParent(name: String) = this should haveParent(name)
+infix fun File.shouldNotHaveParent(name: String) = this shouldNot haveParent(name)
+fun haveParent(name: String) = object : Matcher<File> {
+  private fun isParentEqualExpected(parent: File?): Boolean =
+      parent != null && (parent.name == name || isParentEqualExpected(parent.parentFile))
+
+  override fun test(value: File) = Result(
+      isParentEqualExpected(value.parentFile),
+      "File $value should have parent $name",
+      "File $value should not have parent $name"
+  )
+}
+
 fun Path.shouldExist() = this.toFile() should exist()
 fun Path.shouldNotExist() = this.toFile() shouldNot exist()
 
@@ -257,3 +274,16 @@ fun containFiles(names: List<String>) = object : Matcher<Path> {
     return "$fileString $filesString should$negativeWord exist in $path"
   }
 }
+
+fun Path.shouldBeSymbolicLink() = this should beSymbolicLink()
+fun Path.shouldNotBeSymbolicLink() = this shouldNot beSymbolicLink()
+fun beSymbolicLink() = object : Matcher<Path> {
+  override fun test(value: Path) = Result(
+      Files.isSymbolicLink(value),
+      "File $value should be a symbolic link",
+      "File $value should not be a symbolic link"
+  )
+}
+
+infix fun Path.shouldHaveParent(name: String) = this.toFile() should haveParent(name)
+infix fun Path.shouldNotHaveParent(name: String) = this.toFile() shouldNot haveParent(name)
