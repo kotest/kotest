f765372d069dd3b958d1820b78a5c59daf3c77e4
sksamuel <sam@sksamuel.com>
2019-08-15 21:57:41 -0500

    Updated kotlin test plugin.js

--- kotlintest-runner/kotlintest-runner-js/src/jsMain/resources/kotlintest-plugin.js
+++ kotlintest-runner/kotlintest-runner-js/src/jsMain/resources/kotlintest-plugin.js
@@ -2,7 +2,11 @@ var kotlin_test = require('./build/node_modules/kotlin-test.js');
 
 kotlin_test.setAdapter({
     suite: function (name, ignored, fn) {
-        if (name != "FunSpec") describe(name, fn)
+        if (name == "FunSpec" ||
+            name == "BehaviorSpec" ||
+            name == "StringSpec")
+            return
+        describe(name, fn)
     },
     test: function (name, ignored, fn) {
         if (name == "kotlintest_generate_tests") {
