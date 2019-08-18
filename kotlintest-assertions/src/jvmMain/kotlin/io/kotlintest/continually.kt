abb915afebf103a0f6ec5b1abd7c8500ed05737e
sksamuel <sam@sksamuel.com>
2019-08-17 23:34:11 -0500

    Migrated continually to commons

--- kotlintest-assertions/src/jvmMain/kotlin/io/kotlintest/continually.kt
+++ kotlintest-assertions/src/jvmMain/kotlin/io/kotlintest/continually.kt
@@ -1,23 +1,5 @@
 package io.kotlintest
 
-import io.kotlintest.assertions.Failures
 import java.time.Duration
 
-fun <T> continually(duration: Duration, f: () -> T): T? {
-  val start = System.nanoTime()
-  val end = start + duration.toNanos()
-  fun period() = Duration.ofNanos(System.nanoTime() - start)
-  var times = 0
-  var result: T? = null
-  while (System.nanoTime() < end) {
-    try {
-      result = f()
-    } catch (e: AssertionError) {
-      if (times == 0)
-        throw e
-      throw Failures.failure("Test failed after ${period().toMillis()}ms; expected to pass for ${duration.toMillis()}ms; attempted $times times\nUnderlying failure was: ${e.message}", e)
-    }
-    times++
-  }
-  return result
-}
+fun <T> continually(duration: Duration, f: () -> T): T? = io.kotlintest.assertions.continually(duration.toMillis(), f)
