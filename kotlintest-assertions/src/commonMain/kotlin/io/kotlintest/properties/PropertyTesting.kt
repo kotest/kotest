16731783951279be6a466a39dee7ff88bc95515c
sksamuel <sam@sksamuel.com>
2019-09-03 23:20:39 -0500

    Disable shrink steps for kotlintest's own shrink tests

--- kotlintest-assertions/src/commonMain/kotlin/io/kotlintest/properties/PropertyTesting.kt
+++ kotlintest-assertions/src/commonMain/kotlin/io/kotlintest/properties/PropertyTesting.kt
@@ -4,5 +4,5 @@ import io.kotlintest.assertions.readSystemProperty
 
 object PropertyTesting {
   var shouldPrintGeneratedValues: Boolean = readSystemProperty("kotlintest.propertytest.output.generated-values", "false") == "true"
-  var shouldPrintShrinkSteps: Boolean = readSystemProperty("kotlintest.propertytest.output.shrink-steps", "false") == "true"
+  var shouldPrintShrinkSteps: Boolean = readSystemProperty("kotlintest.propertytest.output.shrink-steps", "true") == "true"
 }
