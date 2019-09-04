Inego/kotlintest

Implement should contain any matcher
Closes #946

kotlintest-assertions/src/commonMain/kotlin/io/kotlintest/matchers/collections/matchers.kt
@@ -423,6 +423,26 @@ infix fun <T> Collection<T>.shouldContainAll(ts: Collection<T>) = this should co
 infix fun <T> Array<T>.shouldNotContainAll(ts: Collection<T>) = asList().shouldNotContainAll(ts)
 infix fun <T> Collection<T>.shouldNotContainAll(ts: Collection<T>) = this shouldNot containAll(ts)
 
+fun <T> Array<T>.shouldContainAny(vararg ts: T) = asList().shouldContainAny(ts)
+fun <T> Collection<T>.shouldContainAny(vararg ts: T) = this should containAny(ts.asList())
+fun <T> Array<T>.shouldNotContainAny(vararg ts: T) = asList().shouldNotContainAny(ts)
+fun <T> Collection<T>.shouldNotContainAny(vararg ts: T) = this shouldNot containAny(ts.asList())
+infix fun <T> Array<T>.shouldContainAny(ts: Collection<T>) = asList().shouldContainAny(ts)
+infix fun <T> Collection<T>.shouldContainAny(ts: Collection<T>) = this should containAny(ts)
+infix fun <T> Array<T>.shouldNotContainAny(ts: Collection<T>) = asList().shouldNotContainAny(ts)
+infix fun <T> Collection<T>.shouldNotContainAny(ts: Collection<T>) = this shouldNot containAny(ts)
+
+fun <T> containAny(ts: Collection<T>) = object : Matcher<Collection<T>> {
+  override fun test(value: Collection<T>): MatcherResult {
+    if (ts.isEmpty()) throwEmptyCollectionError()
+    return MatcherResult(
+            ts.any { it in value },
+            { "Collection should contain any of ${ts.joinToString(separator = ", ", limit = 10) { stringRepr(it) }}" },
+            { "Collection should not contain any of ${ts.joinToString(separator = ", ", limit = 10) { stringRepr(it) }}" }
+    )
+  }
+}
+
 
 /**
  * Verifies that this instance is in [collection]

kotlintest-assertions/src/jvmTest/kotlin/com/sksamuel/kotlintest/matchers/collections/CollectionMatchersTest.kt
@@ -1,93 +1,10 @@
 package com.sksamuel.kotlintest.matchers.collections
 
-import io.kotlintest.matchers.collections.beEmpty
-import io.kotlintest.matchers.collections.monotonicallyDecreasing
-import io.kotlintest.matchers.collections.monotonicallyDecreasingWith
-import io.kotlintest.matchers.collections.monotonicallyIncreasing
-import io.kotlintest.matchers.collections.monotonicallyIncreasingWith
-import io.kotlintest.matchers.collections.strictlyDecreasing
-import io.kotlintest.matchers.collections.strictlyDecreasingWith
-import io.kotlintest.matchers.collections.strictlyIncreasing
-import io.kotlintest.matchers.collections.strictlyIncreasingWith
-import io.kotlintest.matchers.collections.atLeastSize
-import io.kotlintest.matchers.collections.atMostSize
-import io.kotlintest.matchers.collections.beLargerThan
-import io.kotlintest.matchers.collections.beSameSizeAs
-import io.kotlintest.matchers.collections.beSmallerThan
-import io.kotlintest.matchers.collections.contain
-import io.kotlintest.matchers.collections.containDuplicates
-import io.kotlintest.matchers.collections.containExactly
-import io.kotlintest.matchers.collections.containExactlyInAnyOrder
-import io.kotlintest.matchers.collections.containNoNulls
-import io.kotlintest.matchers.collections.containNull
-import io.kotlintest.matchers.collections.containOnlyNulls
-import io.kotlintest.matchers.collections.endWith
-import io.kotlintest.matchers.collections.haveElementAt
-import io.kotlintest.matchers.collections.shouldBeEmpty
-import io.kotlintest.matchers.collections.shouldBeLargerThan
-import io.kotlintest.matchers.collections.shouldBeMonotonicallyDecreasing
-import io.kotlintest.matchers.collections.shouldBeMonotonicallyDecreasingWith
-import io.kotlintest.matchers.collections.shouldBeMonotonicallyIncreasing
-import io.kotlintest.matchers.collections.shouldBeMonotonicallyIncreasingWith
-import io.kotlintest.matchers.collections.shouldBeOneOf
-import io.kotlintest.matchers.collections.shouldBeSameSizeAs
-import io.kotlintest.matchers.collections.shouldBeSingleton
-import io.kotlintest.matchers.collections.shouldBeSmallerThan
-import io.kotlintest.matchers.collections.shouldBeSorted
-import io.kotlintest.matchers.collections.shouldBeSortedWith
-import io.kotlintest.matchers.collections.shouldBeStrictlyDecreasing
-import io.kotlintest.matchers.collections.shouldBeStrictlyDecreasingWith
-import io.kotlintest.matchers.collections.shouldBeStrictlyIncreasing
-import io.kotlintest.matchers.collections.shouldBeStrictlyIncreasingWith
-import io.kotlintest.matchers.collections.shouldContain
-import io.kotlintest.matchers.collections.shouldContainAll
-import io.kotlintest.matchers.collections.shouldContainDuplicates
-import io.kotlintest.matchers.collections.shouldContainExactly
-import io.kotlintest.matchers.collections.shouldContainExactlyInAnyOrder
-import io.kotlintest.matchers.collections.shouldContainNoNulls
-import io.kotlintest.matchers.collections.shouldContainNull
-import io.kotlintest.matchers.collections.shouldContainOnlyNulls
-import io.kotlintest.matchers.collections.shouldEndWith
-import io.kotlintest.matchers.collections.shouldExist
-import io.kotlintest.matchers.collections.shouldHaveAtLeastSize
-import io.kotlintest.matchers.collections.shouldHaveAtMostSize
-import io.kotlintest.matchers.collections.shouldHaveElementAt
-import io.kotlintest.matchers.collections.shouldHaveSingleElement
-import io.kotlintest.matchers.collections.shouldHaveSize
-import io.kotlintest.matchers.collections.shouldNotBeEmpty
-import io.kotlintest.matchers.collections.shouldNotBeMonotonicallyDecreasing
-import io.kotlintest.matchers.collections.shouldNotBeMonotonicallyDecreasingWith
-import io.kotlintest.matchers.collections.shouldNotBeMonotonicallyIncreasing
-import io.kotlintest.matchers.collections.shouldNotBeMonotonicallyIncreasingWith
-import io.kotlintest.matchers.collections.shouldNotBeOneOf
-import io.kotlintest.matchers.collections.shouldNotBeSingleton
-import io.kotlintest.matchers.collections.shouldNotBeSorted
-import io.kotlintest.matchers.collections.shouldNotBeSortedWith
-import io.kotlintest.matchers.collections.shouldNotBeStrictlyDecreasing
-import io.kotlintest.matchers.collections.shouldNotBeStrictlyDecreasingWith
-import io.kotlintest.matchers.collections.shouldNotBeStrictlyIncreasing
-import io.kotlintest.matchers.collections.shouldNotBeStrictlyIncreasingWith
-import io.kotlintest.matchers.collections.shouldNotContainAll
-import io.kotlintest.matchers.collections.shouldNotContainDuplicates
-import io.kotlintest.matchers.collections.shouldNotContainExactly
-import io.kotlintest.matchers.collections.shouldNotContainExactlyInAnyOrder
-import io.kotlintest.matchers.collections.shouldNotContainNoNulls
-import io.kotlintest.matchers.collections.shouldNotContainNull
-import io.kotlintest.matchers.collections.shouldNotContainOnlyNulls
-import io.kotlintest.matchers.collections.shouldNotEndWith
-import io.kotlintest.matchers.collections.shouldNotHaveElementAt
-import io.kotlintest.matchers.collections.shouldNotHaveSize
-import io.kotlintest.matchers.collections.shouldNotStartWith
-import io.kotlintest.matchers.collections.shouldStartWith
-import io.kotlintest.matchers.collections.startWith
-import io.kotlintest.matchers.collections.containAll
-import io.kotlintest.matchers.collections.containsInOrder
-import io.kotlintest.matchers.collections.haveSize
-import io.kotlintest.matchers.collections.singleElement
-import io.kotlintest.matchers.collections.sorted
 import io.kotlintest.should
 import io.kotlintest.shouldBe
 import io.kotlintest.assertions.shouldFail
+import io.kotlintest.matchers.collections.*
+import io.kotlintest.matchers.throwable.shouldHaveMessage
 import io.kotlintest.shouldHave
 import io.kotlintest.shouldNot
 import io.kotlintest.shouldNotBe
@@ -766,6 +683,46 @@ class CollectionMatchersTest : WordSpec() {
         shouldThrow<AssertionError> { foo shouldNotBeOneOf list }
       }
 
+    }
+
+    "Contain any" should {
+      "Fail when the list is empty" {
+        shouldThrow<AssertionError> {
+          listOf(1, 2, 3).shouldContainAny(emptyList())
+        }.shouldHaveMessage("Asserting content on empty collection. Use Collection.shouldBeEmpty() instead.")
+      }
+
+      "Pass when one element is in the list" {
+        listOf(1, 2, 3).shouldContainAny(1)
+      }
+
+      "Pass when all elements are in the list" {
+        listOf(1, 2, 3).shouldContainAny(1, 2, 3)
+      }
+
+      "Fail when no element is in the list" {
+        shouldThrow<AssertionError> { listOf(1, 2, 3).shouldContainAny(4) }
+      }
+    }
+
+    "Contain any (negative)" should {
+      "Fail when the list is empty" {
+        shouldThrow<AssertionError> {
+          listOf(1, 2, 3).shouldNotContainAny(emptyList())
+        }.shouldHaveMessage("Asserting content on empty collection. Use Collection.shouldBeEmpty() instead.")
+      }
+
+      "Pass when no element is present in the list" {
+        listOf(1, 2, 3).shouldNotContainAny(4)
+      }
+
+      "Fail when one element is in the list" {
+        shouldThrow<AssertionError> { listOf(1, 2, 3).shouldNotContainAny(1) }
+      }
+
+      "Fail when all elements are in the list" {
+        shouldThrow<AssertionError> { listOf(1, 2, 3).shouldNotContainAny(1, 2, 3) }
+      }
     }
   }
 }

kotest/kotest

kotlintest-assertions/src/commonMain/kotlin/io/kotlintest/matchers/collections/matchers.kt
@@ -423,6 +423,26 @@ infix fun <T> Collection<T>.shouldContainAll(ts: Collection<T>) = this should co
 infix fun <T> Array<T>.shouldNotContainAll(ts: Collection<T>) = asList().shouldNotContainAll(ts)
 infix fun <T> Collection<T>.shouldNotContainAll(ts: Collection<T>) = this shouldNot containAll(ts)
 
+fun <T> Array<T>.shouldContainAnyOf(vararg ts: T) = asList().shouldContainAnyOf(ts)
+fun <T> Collection<T>.shouldContainAnyOf(vararg ts: T) = this should containAnyOf(ts.asList())
+fun <T> Array<T>.shouldNotContainAnyOf(vararg ts: T) = asList().shouldNotContainAnyOf(ts)
+fun <T> Collection<T>.shouldNotContainAnyOf(vararg ts: T) = this shouldNot containAnyOf(ts.asList())
+infix fun <T> Array<T>.shouldContainAnyOf(ts: Collection<T>) = asList().shouldContainAnyOf(ts)
+infix fun <T> Collection<T>.shouldContainAnyOf(ts: Collection<T>) = this should containAnyOf(ts)
+infix fun <T> Array<T>.shouldNotContainAnyOf(ts: Collection<T>) = asList().shouldNotContainAnyOf(ts)
+infix fun <T> Collection<T>.shouldNotContainAnyOf(ts: Collection<T>) = this shouldNot containAnyOf(ts)
+
+fun <T> containAnyOf(ts: Collection<T>) = object : Matcher<Collection<T>> {
+  override fun test(value: Collection<T>): MatcherResult {
+    if (ts.isEmpty()) throwEmptyCollectionError()
+    return MatcherResult(
+            ts.any { it in value },
+            { "Collection should contain any of ${ts.joinToString(separator = ", ", limit = 10) { stringRepr(it) }}" },
+            { "Collection should not contain any of ${ts.joinToString(separator = ", ", limit = 10) { stringRepr(it) }}" }
+    )
+  }
+}
+
 
 /**
  * Verifies that this instance is in [collection]

kotlintest-assertions/src/jvmTest/kotlin/com/sksamuel/kotlintest/matchers/collections/CollectionMatchersTest.kt
@@ -1,93 +1,10 @@
 package com.sksamuel.kotlintest.matchers.collections
 
-import io.kotlintest.matchers.collections.beEmpty
-import io.kotlintest.matchers.collections.monotonicallyDecreasing
-import io.kotlintest.matchers.collections.monotonicallyDecreasingWith
-import io.kotlintest.matchers.collections.monotonicallyIncreasing
-import io.kotlintest.matchers.collections.monotonicallyIncreasingWith
-import io.kotlintest.matchers.collections.strictlyDecreasing
-import io.kotlintest.matchers.collections.strictlyDecreasingWith
-import io.kotlintest.matchers.collections.strictlyIncreasing
-import io.kotlintest.matchers.collections.strictlyIncreasingWith
-import io.kotlintest.matchers.collections.atLeastSize
-import io.kotlintest.matchers.collections.atMostSize
-import io.kotlintest.matchers.collections.beLargerThan
-import io.kotlintest.matchers.collections.beSameSizeAs
-import io.kotlintest.matchers.collections.beSmallerThan
-import io.kotlintest.matchers.collections.contain
-import io.kotlintest.matchers.collections.containDuplicates
-import io.kotlintest.matchers.collections.containExactly
-import io.kotlintest.matchers.collections.containExactlyInAnyOrder
-import io.kotlintest.matchers.collections.containNoNulls
-import io.kotlintest.matchers.collections.containNull
-import io.kotlintest.matchers.collections.containOnlyNulls
-import io.kotlintest.matchers.collections.endWith
-import io.kotlintest.matchers.collections.haveElementAt
-import io.kotlintest.matchers.collections.shouldBeEmpty
-import io.kotlintest.matchers.collections.shouldBeLargerThan
-import io.kotlintest.matchers.collections.shouldBeMonotonicallyDecreasing
-import io.kotlintest.matchers.collections.shouldBeMonotonicallyDecreasingWith
-import io.kotlintest.matchers.collections.shouldBeMonotonicallyIncreasing
-import io.kotlintest.matchers.collections.shouldBeMonotonicallyIncreasingWith
-import io.kotlintest.matchers.collections.shouldBeOneOf
-import io.kotlintest.matchers.collections.shouldBeSameSizeAs
-import io.kotlintest.matchers.collections.shouldBeSingleton
-import io.kotlintest.matchers.collections.shouldBeSmallerThan
-import io.kotlintest.matchers.collections.shouldBeSorted
-import io.kotlintest.matchers.collections.shouldBeSortedWith
-import io.kotlintest.matchers.collections.shouldBeStrictlyDecreasing
-import io.kotlintest.matchers.collections.shouldBeStrictlyDecreasingWith
-import io.kotlintest.matchers.collections.shouldBeStrictlyIncreasing
-import io.kotlintest.matchers.collections.shouldBeStrictlyIncreasingWith
-import io.kotlintest.matchers.collections.shouldContain
-import io.kotlintest.matchers.collections.shouldContainAll
-import io.kotlintest.matchers.collections.shouldContainDuplicates
-import io.kotlintest.matchers.collections.shouldContainExactly
-import io.kotlintest.matchers.collections.shouldContainExactlyInAnyOrder
-import io.kotlintest.matchers.collections.shouldContainNoNulls
-import io.kotlintest.matchers.collections.shouldContainNull
-import io.kotlintest.matchers.collections.shouldContainOnlyNulls
-import io.kotlintest.matchers.collections.shouldEndWith
-import io.kotlintest.matchers.collections.shouldExist
-import io.kotlintest.matchers.collections.shouldHaveAtLeastSize
-import io.kotlintest.matchers.collections.shouldHaveAtMostSize
-import io.kotlintest.matchers.collections.shouldHaveElementAt
-import io.kotlintest.matchers.collections.shouldHaveSingleElement
-import io.kotlintest.matchers.collections.shouldHaveSize
-import io.kotlintest.matchers.collections.shouldNotBeEmpty
-import io.kotlintest.matchers.collections.shouldNotBeMonotonicallyDecreasing
-import io.kotlintest.matchers.collections.shouldNotBeMonotonicallyDecreasingWith
-import io.kotlintest.matchers.collections.shouldNotBeMonotonicallyIncreasing
-import io.kotlintest.matchers.collections.shouldNotBeMonotonicallyIncreasingWith
-import io.kotlintest.matchers.collections.shouldNotBeOneOf
-import io.kotlintest.matchers.collections.shouldNotBeSingleton
-import io.kotlintest.matchers.collections.shouldNotBeSorted
-import io.kotlintest.matchers.collections.shouldNotBeSortedWith
-import io.kotlintest.matchers.collections.shouldNotBeStrictlyDecreasing
-import io.kotlintest.matchers.collections.shouldNotBeStrictlyDecreasingWith
-import io.kotlintest.matchers.collections.shouldNotBeStrictlyIncreasing
-import io.kotlintest.matchers.collections.shouldNotBeStrictlyIncreasingWith
-import io.kotlintest.matchers.collections.shouldNotContainAll
-import io.kotlintest.matchers.collections.shouldNotContainDuplicates
-import io.kotlintest.matchers.collections.shouldNotContainExactly
-import io.kotlintest.matchers.collections.shouldNotContainExactlyInAnyOrder
-import io.kotlintest.matchers.collections.shouldNotContainNoNulls
-import io.kotlintest.matchers.collections.shouldNotContainNull
-import io.kotlintest.matchers.collections.shouldNotContainOnlyNulls
-import io.kotlintest.matchers.collections.shouldNotEndWith
-import io.kotlintest.matchers.collections.shouldNotHaveElementAt
-import io.kotlintest.matchers.collections.shouldNotHaveSize
-import io.kotlintest.matchers.collections.shouldNotStartWith
-import io.kotlintest.matchers.collections.shouldStartWith
-import io.kotlintest.matchers.collections.startWith
-import io.kotlintest.matchers.collections.containAll
-import io.kotlintest.matchers.collections.containsInOrder
-import io.kotlintest.matchers.collections.haveSize
-import io.kotlintest.matchers.collections.singleElement
-import io.kotlintest.matchers.collections.sorted
 import io.kotlintest.should
 import io.kotlintest.shouldBe
 import io.kotlintest.assertions.shouldFail
+import io.kotlintest.matchers.collections.*
+import io.kotlintest.matchers.throwable.shouldHaveMessage
 import io.kotlintest.shouldHave
 import io.kotlintest.shouldNot
 import io.kotlintest.shouldNotBe
@@ -766,6 +683,46 @@ class CollectionMatchersTest : WordSpec() {
         shouldThrow<AssertionError> { foo shouldNotBeOneOf list }
       }
 
+    }
+
+    "Contain any" should {
+      "Fail when the list is empty" {
+        shouldThrow<AssertionError> {
+          listOf(1, 2, 3).shouldContainAnyOf(emptyList())
+        }.shouldHaveMessage("Asserting content on empty collection. Use Collection.shouldBeEmpty() instead.")
+      }
+
+      "Pass when one element is in the list" {
+        listOf(1, 2, 3).shouldContainAnyOf(1)
+      }
+
+      "Pass when all elements are in the list" {
+        listOf(1, 2, 3).shouldContainAnyOf(1, 2, 3)
+      }
+
+      "Fail when no element is in the list" {
+        shouldThrow<AssertionError> { listOf(1, 2, 3).shouldContainAnyOf(4) }
+      }
+    }
+
+    "Contain any (negative)" should {
+      "Fail when the list is empty" {
+        shouldThrow<AssertionError> {
+          listOf(1, 2, 3).shouldNotContainAnyOf(emptyList())
+        }.shouldHaveMessage("Asserting content on empty collection. Use Collection.shouldBeEmpty() instead.")
+      }
+
+      "Pass when no element is present in the list" {
+        listOf(1, 2, 3).shouldNotContainAnyOf(4)
+      }
+
+      "Fail when one element is in the list" {
+        shouldThrow<AssertionError> { listOf(1, 2, 3).shouldNotContainAnyOf(1) }
+      }
+
+      "Fail when all elements are in the list" {
+        shouldThrow<AssertionError> { listOf(1, 2, 3).shouldNotContainAnyOf(1, 2, 3) }
+      }
     }
   }
 }