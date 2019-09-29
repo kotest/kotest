atmikev/kotlintest

Replace Kotlintest with Kotest

kotest-core/src/commonMain/kotlin/io/kotest/core/TestContext.kt
@@ -1,0 +1,44 @@
+package io.kotest.core
+
+import io.kotest.Description
+import io.kotest.Spec
+import io.kotest.TestCase
+import io.kotest.TestType
+import io.kotest.core.specs.KotlinTestDsl
+import kotlinx.coroutines.CoroutineScope
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * A [TestContext] is used as the receiver of a closure that is associated with a [TestCase].
+ * This allows the scope body to interact with the test engine, for instance, adding metadata
+ * during a test, reporting that an error was raised, or notifying the discovery
+ * of a nested scope.
+ *
+  * [TestContext] implements [CoroutineScope], which allows test closures to launch coroutines
+ * with the [CoroutineContext] provided by the test engine.
+ */
+@KotlinTestDsl
+abstract class TestContext(override val coroutineContext: CoroutineContext) : CoroutineScope {
+
+  infix operator fun String.invoke(test: suspend TestContext.() -> Unit) {
+    throw Exception("Nested tests are not allowed to be defined here. Please see the documentation for the spec styles")
+  }
+
+  /**
+   * Returns the [Description] of the [TestCase] that is attached to this [TestContext].
+   */
+  abstract fun description(): Description
+
+  /**
+   * Creates a new [TestCase] and then notifies the test runner of this nested test.
+   */
+  suspend fun registerTestCase(name: String, spec: Spec, test: suspend TestContext.() -> Unit, config: TestCaseConfig, type: TestType) {
+    val tc = TestCase(description().append(name), spec, test, sourceRef(), type, config)
+    registerTestCase(tc)
+  }
+
+  /**
+   * Notifies the test runner about a nested [TestCase].
+   */
+  abstract suspend fun registerTestCase(testCase: TestCase)
+}

kotest-tests/kotest-tests-junit5/src/jvmTest/kotlin/com/sksamuel/kotest/junit5/FunSpecEngineKitTest.kt
@@ -1,0 +1,46 @@
+package com.sksamuel.kotest.junit5
+
+import io.kotest.Project
+import io.kotest.specs.FunSpec
+import org.junit.platform.engine.discovery.DiscoverySelectors.selectClass
+import org.junit.platform.testkit.engine.EngineTestKit
+
+class FunSpecEngineKitTest : FunSpec({
+
+  test("verify container stats") {
+    EngineTestKit
+        .engine("kotlintest")
+        .selectors(selectClass(FunSpecTestCase::class.java))
+        .execute()
+        .containers()
+        .assertStatistics { it.started(2).succeeded(2).failed(0) }
+  }
+
+  test("verify test stats") {
+    EngineTestKit
+        .engine("kotlintest")
+        .selectors(selectClass(FunSpecTestCase::class.java))
+        .execute()
+        .tests()
+        .assertStatistics { it.skipped(1).started(3).succeeded(1).aborted(0).failed(2).finished(3) }
+  }
+
+  test("mark spec as failed if failOnIgnoredTests is set") {
+    Project.failOnIgnoredTests = true
+    EngineTestKit
+        .engine("kotlintest")
+        .selectors(selectClass(FailOnIgnoreTestCase::class.java))
+        .execute()
+        .containers()
+        .assertStatistics { it.started(2).failed(1) }
+    Project.failOnIgnoredTests = false
+  }
+})
+
+class FailOnIgnoreTestCase : FunSpec() {
+  init {
+    test("ignored").config(enabled = false) {
+
+    }
+  }
+}

kotest/kotest

kotest-core/src/commonMain/kotlin/io/kotest/core/TestContext.kt
@@ -1,0 +1,44 @@
+package io.kotest.core
+
+import io.kotest.Description
+import io.kotest.Spec
+import io.kotest.TestCase
+import io.kotest.TestType
+import io.kotest.core.specs.KotestDsl
+import kotlinx.coroutines.CoroutineScope
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * A [TestContext] is used as the receiver of a closure that is associated with a [TestCase].
+ * This allows the scope body to interact with the test engine, for instance, adding metadata
+ * during a test, reporting that an error was raised, or notifying the discovery
+ * of a nested scope.
+ *
+  * [TestContext] implements [CoroutineScope], which allows test closures to launch coroutines
+ * with the [CoroutineContext] provided by the test engine.
+ */
+@KotestDsl
+abstract class TestContext(override val coroutineContext: CoroutineContext) : CoroutineScope {
+
+  infix operator fun String.invoke(test: suspend TestContext.() -> Unit) {
+    throw Exception("Nested tests are not allowed to be defined here. Please see the documentation for the spec styles")
+  }
+
+  /**
+   * Returns the [Description] of the [TestCase] that is attached to this [TestContext].
+   */
+  abstract fun description(): Description
+
+  /**
+   * Creates a new [TestCase] and then notifies the test runner of this nested test.
+   */
+  suspend fun registerTestCase(name: String, spec: Spec, test: suspend TestContext.() -> Unit, config: TestCaseConfig, type: TestType) {
+    val tc = TestCase(description().append(name), spec, test, sourceRef(), type, config)
+    registerTestCase(tc)
+  }
+
+  /**
+   * Notifies the test runner about a nested [TestCase].
+   */
+  abstract suspend fun registerTestCase(testCase: TestCase)
+}

kotest-tests/kotest-tests-junit5/src/jvmTest/kotlin/com/sksamuel/kotest/junit5/FunSpecEngineKitTest.kt
@@ -1,0 +1,46 @@
+package com.sksamuel.kotest.junit5
+
+import io.kotest.Project
+import io.kotest.specs.FunSpec
+import org.junit.platform.engine.discovery.DiscoverySelectors.selectClass
+import org.junit.platform.testkit.engine.EngineTestKit
+
+class FunSpecEngineKitTest : FunSpec({
+
+  test("verify container stats") {
+    EngineTestKit
+        .engine("kotest")
+        .selectors(selectClass(FunSpecTestCase::class.java))
+        .execute()
+        .containers()
+        .assertStatistics { it.started(2).succeeded(2).failed(0) }
+  }
+
+  test("verify test stats") {
+    EngineTestKit
+        .engine("kotest")
+        .selectors(selectClass(FunSpecTestCase::class.java))
+        .execute()
+        .tests()
+        .assertStatistics { it.skipped(1).started(3).succeeded(1).aborted(0).failed(2).finished(3) }
+  }
+
+  test("mark spec as failed if failOnIgnoredTests is set") {
+    Project.failOnIgnoredTests = true
+    EngineTestKit
+        .engine("kotest")
+        .selectors(selectClass(FailOnIgnoreTestCase::class.java))
+        .execute()
+        .containers()
+        .assertStatistics { it.started(2).failed(1) }
+    Project.failOnIgnoredTests = false
+  }
+})
+
+class FailOnIgnoreTestCase : FunSpec() {
+  init {
+    test("ignored").config(enabled = false) {
+
+    }
+  }
+}