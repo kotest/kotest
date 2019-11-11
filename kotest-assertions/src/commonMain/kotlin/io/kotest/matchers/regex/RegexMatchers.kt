8a5923c0d226e2609d706b8fb3ce519d2decd97f
ashish kumar <ashishkujoy@gmail.com>
2019-11-11 21:32:51 +0530

    #1065 Rearrange function by moving infix version of function above non-infix

--- kotest-assertions/src/commonMain/kotlin/io/kotest/matchers/regex/RegexMatchers.kt
+++ kotest-assertions/src/commonMain/kotlin/io/kotest/matchers/regex/RegexMatchers.kt
@@ -5,68 +5,6 @@ import io.kotest.MatcherResult
 import io.kotest.should
 import io.kotest.shouldNot
 
-fun haveSamePatternMatcher(pattern: String) = object : Matcher<Regex> {
-   override fun test(value: Regex): MatcherResult {
-      return MatcherResult(
-         value.pattern == pattern,
-         { "Regex should have pattern ${pattern} but has pattern ${value.pattern}" },
-         { "Regex should not have pattern ${value.pattern}" }
-      )
-   }
-}
-
-fun haveSameRegexOptionsMatcher(options: Set<RegexOption>) = object : Matcher<Regex> {
-   override fun test(value: Regex): MatcherResult {
-      return MatcherResult(
-         value.options == options,
-         { "Regex should have options ${options} but has options ${value.options}" },
-         { "Regex should not have pattern ${value.options}" }
-      )
-   }
-}
-
-fun haveExactRegexOptionMatcher(option: RegexOption) = object : Matcher<Regex> {
-   override fun test(value: Regex): MatcherResult {
-      return MatcherResult(
-         value.options.contains(option),
-         { "Regex options should contains $option" },
-         { "Regex options should not contains $option" }
-      )
-   }
-}
-
-fun haveRegexOptionMatcher(options: Set<RegexOption>) = object : Matcher<Regex> {
-   override fun test(value: Regex): MatcherResult {
-      return MatcherResult(
-         value.options.containsAll(options),
-         { "Regex options should contains $options, but missing ${options.filterNot { value.options.contains(it) }}." },
-         { "Regex options should not contains $options, but containing ${options.filter { value.options.contains(it) }}." }
-      )
-   }
-}
-
-fun areEqualRegexMatcher(regex: Regex) = object : Matcher<Regex> {
-   override fun test(value: Regex): MatcherResult {
-      val patternMatchingResult = haveSamePatternMatcher(regex.pattern).test(value)
-      val optionMatchingResult = haveSameRegexOptionsMatcher(regex.options).test(value)
-      return MatcherResult(
-         patternMatchingResult.passed() && optionMatchingResult.passed(),
-         { "Regex should have pattern ${regex.pattern} and regex options ${regex.options}, but has pattern ${value.pattern} and regex options ${value.options}." },
-         { "Regex should not have pattern ${value.pattern} and regex options ${value.options}." }
-      )
-   }
-}
-
-fun beRegex(regex: Regex) = areEqualRegexMatcher(regex)
-
-fun havePatter(pattern: String) = haveSamePatternMatcher(pattern)
-
-fun haveExactOptions(options: Set<RegexOption>) = haveSameRegexOptionsMatcher(options)
-
-fun haveOption(option: RegexOption) = haveExactRegexOptionMatcher(option)
-
-fun haveOptions(options: Set<RegexOption>) = haveRegexOptionMatcher(options)
-
 /**
  * Assert that [Regex] is equal to [anotherRegex] by comparing their pattern and options([RegexOption]).
  * @see [shouldNotBeRegex]
@@ -81,19 +19,45 @@ infix fun Regex.shouldBeRegex(anotherRegex: Regex) = this should beRegex(another
  * */
 infix fun Regex.shouldNotBeRegex(anotherRegex: Regex) = this shouldNot beRegex(anotherRegex)
 
+fun beRegex(regex: Regex) = areEqualRegexMatcher(regex)
+
+fun areEqualRegexMatcher(regex: Regex) = object : Matcher<Regex> {
+   override fun test(value: Regex): MatcherResult {
+      val patternMatchingResult = haveSamePatternMatcher(regex.pattern).test(value)
+      val optionMatchingResult = haveSameRegexOptionsMatcher(regex.options).test(value)
+      return MatcherResult(
+         patternMatchingResult.passed() && optionMatchingResult.passed(),
+         { "Regex should have pattern ${regex.pattern} and regex options ${regex.options}, but has pattern ${value.pattern} and regex options ${value.options}." },
+         { "Regex should not have pattern ${value.pattern} and regex options ${value.options}." }
+      )
+   }
+}
+
 /**
  * Assert that [Regex] have pattern [regexPattern].
  * @see [shouldNotHavePattern]
- * @see [havePatter]
+ * @see [havePattern]
  * */
-infix fun Regex.shouldHavePattern(regexPattern: String) = this should havePatter(regexPattern)
+infix fun Regex.shouldHavePattern(regexPattern: String) = this should havePattern(regexPattern)
 
 /**
  * Assert that [Regex] does not have [regexPattern].
  * @see [shouldHavePattern]
- * @see [havePatter]
+ * @see [havePattern]
  * */
-infix fun Regex.shouldNotHavePattern(regexPattern: String) = this shouldNot havePatter(regexPattern)
+infix fun Regex.shouldNotHavePattern(regexPattern: String) = this shouldNot havePattern(regexPattern)
+
+fun havePattern(pattern: String) = haveSamePatternMatcher(pattern)
+
+fun haveSamePatternMatcher(pattern: String) = object : Matcher<Regex> {
+   override fun test(value: Regex): MatcherResult {
+      return MatcherResult(
+         value.pattern == pattern,
+         { "Regex should have pattern ${pattern} but has pattern ${value.pattern}" },
+         { "Regex should not have pattern ${value.pattern}" }
+      )
+   }
+}
 
 /**
  * Assert that [Regex] have exact regex options as [regexOptions]
@@ -110,6 +74,18 @@ infix fun Regex.shouldHaveExactRegexOptions(regexOptions: Set<RegexOption>) = th
 infix fun Regex.shouldNotHaveExactRegexOptions(regexOptions: Set<RegexOption>) =
    this shouldNot haveExactOptions(regexOptions)
 
+fun haveExactOptions(options: Set<RegexOption>) = haveSameRegexOptionsMatcher(options)
+
+fun haveSameRegexOptionsMatcher(options: Set<RegexOption>) = object : Matcher<Regex> {
+   override fun test(value: Regex): MatcherResult {
+      return MatcherResult(
+         value.options == options,
+         { "Regex should have options ${options} but has options ${value.options}" },
+         { "Regex should not have pattern ${value.options}" }
+      )
+   }
+}
+
 /**
  * Assert that [Regex] regex options contains [regexOption]
  * @see [shouldNotHaveRegexOption]
@@ -124,6 +100,17 @@ infix fun Regex.shouldHaveRegexOption(regexOption: RegexOption) = this should ha
  * */
 infix fun Regex.shouldNotHaveRegexOption(regexOption: RegexOption) = this shouldNot haveOption(regexOption)
 
+fun haveOption(option: RegexOption) = haveExactRegexOptionMatcher(option)
+
+fun haveExactRegexOptionMatcher(option: RegexOption) = object : Matcher<Regex> {
+   override fun test(value: Regex): MatcherResult {
+      return MatcherResult(
+         value.options.contains(option),
+         { "Regex options should contains $option" },
+         { "Regex options should not contains $option" }
+      )
+   }
+}
 
 /**
  * Assert that [Regex] regex options contains [regexOptions]
@@ -138,3 +125,15 @@ infix fun Regex.shouldHaveRegexOptions(regexOptions: Set<RegexOption>) = this sh
  * @see [haveOptions]
  * */
 infix fun Regex.shouldNotHaveRegexOptions(regexOptions: Set<RegexOption>) = this shouldNot haveOptions(regexOptions)
+
+fun haveOptions(options: Set<RegexOption>) = haveRegexOptionMatcher(options)
+
+fun haveRegexOptionMatcher(options: Set<RegexOption>) = object : Matcher<Regex> {
+   override fun test(value: Regex): MatcherResult {
+      return MatcherResult(
+         value.options.containsAll(options),
+         { "Regex options should contains $options, but missing ${options.filterNot { value.options.contains(it) }}." },
+         { "Regex options should not contains $options, but containing ${options.filter { value.options.contains(it) }}." }
+      )
+   }
+}
