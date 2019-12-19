5b262009771a8849f56cae39a631e4ee21c6b914
Ashish Kumar <ashishkujoy@gmail.com>
2019-12-19 23:31:34 +0530

    #1123 Moves timestamp test under date folder and brings shouldBe and shouldNotBe test back

--- kotest-assertions/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/date/timestampTest.kt
+++ kotest-assertions/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/date/timestampTest.kt
@@ -1,12 +1,25 @@
-package com.sksamuel.kotest.matchers
+package com.sksamuel.kotest.matchers.date
 
 import io.kotest.matchers.date.*
+import io.kotest.shouldBe
+import io.kotest.shouldNotBe
 import io.kotest.specs.FreeSpec
 import java.sql.Timestamp
 import java.time.Instant
 
 class TimeStampTest : FreeSpec() {
    init {
+      "two timestamp of same instance should be same" {
+         val nowInstance = Instant.now()
+         Timestamp.from(nowInstance) shouldBe Timestamp.from(nowInstance)
+      }
+
+      "two timestamp of different instance should be not be same" {
+         val nowInstance = Instant.now()
+         val anotherInstance = nowInstance.plusMillis(1000L)
+         Timestamp.from(nowInstance) shouldNotBe Timestamp.from(anotherInstance)
+      }
+
       "timestamp of current instance should be after the timestamp of past instance" {
          val nowInstance = Instant.now()
          val instanceBeforeFiveSecond = nowInstance.minusMillis(5000L)
