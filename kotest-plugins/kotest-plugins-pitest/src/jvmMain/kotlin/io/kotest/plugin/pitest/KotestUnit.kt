6923aea46b8f2f076fd4e85ea6a8ac69505893d1
sksamuel <sam@sksamuel.com>
2020-01-11 11:38:27 -0600

    Reusable composable specs

--- kotest-plugins/kotest-plugins-pitest/src/jvmMain/kotlin/io/kotest/plugin/pitest/KotestUnit.kt
+++ kotest-plugins/kotest-plugins-pitest/src/jvmMain/kotlin/io/kotest/plugin/pitest/KotestUnit.kt
@@ -13,32 +13,32 @@ import kotlin.reflect.KClass
 
 class KotestUnit(val klass: KClass<out SpecConfiguration>) : TestUnit {
 
-  override fun getDescription(): Description = Description(klass.description().fullName(), klass.java)
+   override fun getDescription(): Description = Description(klass.description().fullName(), klass.java)
 
-  override fun execute(rc: ResultCollector) {
+   override fun execute(rc: ResultCollector) {
 
-    val listener = object : TestEngineListener {
+      val listener = object : TestEngineListener {
 
-      private val started = mutableSetOf<io.kotest.core.Description>()
-      private val completed = mutableSetOf<io.kotest.core.Description>()
+         private val started = mutableSetOf<io.kotest.core.Description>()
+         private val completed = mutableSetOf<io.kotest.core.Description>()
 
-      override fun enterTestCase(testCase: TestCase) {
-        if (started.add(testCase.description))
-          rc.notifyStart(Description(testCase.description.fullName(), klass.java))
+         override fun testStarted(testCase: TestCase) {
+            if (started.add(testCase.description))
+               rc.notifyStart(Description(testCase.description.fullName(), klass.java))
+         }
+
+         override fun testFinished(testCase: TestCase, result: TestResult) {
+            val desc = Description(testCase.description.fullName(), klass.java)
+            if (completed.add(testCase.description)) {
+               when (result.error) {
+                  null -> rc.notifyEnd(desc)
+                  else -> rc.notifyEnd(desc, result.error)
+               }
+            }
+         }
       }
 
-      override fun exitTestCase(testCase: TestCase, result: TestResult) {
-        val desc = Description(testCase.description.fullName(), klass.java)
-        if (completed.add(testCase.description)) {
-          when (result.error) {
-            null -> rc.notifyEnd(desc)
-            else -> rc.notifyEnd(desc, result.error)
-          }
-        }
-      }
-    }
-
-    val engine = KotestEngine(listOf(klass), emptyList(), 1, emptySet(), emptySet(), listener)
-    engine.execute()
-  }
+      val engine = KotestEngine(listOf(klass), emptyList(), 1, emptySet(), emptySet(), listener)
+      engine.execute()
+   }
 }
