c9307938ffd8ea3da5d2d4b35926da95c8dbef0c
sksamuel <sam@sksamuel.com>
2020-01-12 11:52:33 -0600

    Eventually should be inline to support suspension #1098

--- kotest-assertions/src/jvmMain/kotlin/io/kotest/Eventually.kt
+++ kotest-assertions/src/jvmMain/kotlin/io/kotest/Eventually.kt
@@ -6,9 +6,9 @@ import io.kotest.until.fixedInterval
 import io.kotest.until.until
 import java.time.Duration
 
-fun <T> eventually(duration: Duration, f: () -> T): T = eventually(duration, Exception::class.java, f)
+inline fun <T> eventually(duration: Duration, f: () -> T): T = eventually(duration, Exception::class.java, f)
 
-fun <T, E : Throwable> eventually(duration: Duration, exceptionClass: Class<E>, f: () -> T): T {
+inline fun <T, E : Throwable> eventually(duration: Duration, exceptionClass: Class<E>, f: () -> T): T {
   val end = System.nanoTime() + duration.toNanos()
   var times = 0
   var lastError: Throwable? = null
