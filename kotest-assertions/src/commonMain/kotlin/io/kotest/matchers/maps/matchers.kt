00477664b29ee06b271b2110edb48329e75fcc72
sksamuel <sam@sksamuel.com>
2020-01-20 18:52:41 -0600

    Added tests for Type mismatch for haveKey: Map and (Mutable)Map #1067

--- kotest-assertions/src/commonMain/kotlin/io/kotest/matchers/maps/matchers.kt
+++ kotest-assertions/src/commonMain/kotlin/io/kotest/matchers/maps/matchers.kt
@@ -23,7 +23,9 @@ infix fun <K, V> Map<K, V>.shouldNotContainExactly(expected: Map<K, V>) = this s
 infix fun <K, V> Map<K, V>.shouldContainAll(expected: Map<K, V>) = this should containAll(expected)
 infix fun <K, V> Map<K, V>.shouldNotContainAll(expected: Map<K, V>) = this shouldNot containAll(expected)
 
+infix fun <K, V : Any> Map<K, V>.shouldHaveKey(key: K) = this should haveKey(key)
 infix fun <K, V : Any> Map<K, V>.shouldContainKey(key: K) = this should haveKey(key)
+infix fun <K, V : Any> Map<K, V>.shouldNotHaveKey(key: K) = this shouldNot haveKey(key)
 infix fun <K, V : Any> Map<K, V>.shouldNotContainKey(key: K) = this shouldNot haveKey(key)
 
 infix fun <K, V> Map<K, V>.shouldContainValue(value: V) = this should haveValue<V>(value)
@@ -32,10 +34,14 @@ infix fun <K, V> Map<K, V>.shouldNotContainValue(value: V) = this shouldNot have
 
 infix fun <K, V> Map<K, V>.shouldHaveSize(size: Int) = this should haveSize(size)
 
+fun <K, V> Map<K, V>.shouldHaveKeys(vararg keys: K) = this should haveKeys(*keys)
 fun <K, V> Map<K, V>.shouldContainKeys(vararg keys: K) = this should haveKeys(*keys)
+fun <K, V> Map<K, V>.shouldNotHaveKeys(vararg keys: K) = this shouldNot haveKeys(*keys)
 fun <K, V> Map<K, V>.shouldNotContainKeys(vararg keys: K) = this shouldNot haveKeys(*keys)
 
+fun <K, V> Map<K, V>.shouldHaveValues(vararg values: V) = this should haveValues(*values)
 fun <K, V> Map<K, V>.shouldContainValues(vararg values: V) = this should haveValues(*values)
+fun <K, V> Map<K, V>.shouldNotHaveValues(vararg values: V) = this shouldNot haveValues(*values)
 fun <K, V> Map<K, V>.shouldNotContainValues(vararg values: V) = this shouldNot haveValues(*values)
 
 fun <K, V> Map<K, V>.shouldBeEmpty() = this should beEmpty()
