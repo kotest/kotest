44fbfb3388041d5e197414c3b62eb33ad40b337b
ashish kumar joy <34642693+ashishkujoy@users.noreply.github.com>
2020-01-22 07:33:04 +0530

    Adds matcher for Char (#1173)
    *  #1113 Adds shouldBeInRange Matcher for char
    
    *  #1113 Adds shouldBeBetween Matcher for char
    
    *  #1113 Adds shouldBeEqualToIgnoreCase Matcher for char
    
    *  #1113 Adds comment describing char matchers

---
+++ kotest-assertions/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/char/CharMatchersTest.kt
@@ -0,0 +1,38 @@
+package com.sksamuel.kotest.matchers.char
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.char.*
+
+class CharMatchersTest : StringSpec() {
+   init {
+      "should be in char range" {
+         'd' shouldBeInRange ('a'..'z')
+         'E' shouldBeInRange ('A'..'z')
+         ']' shouldBeInRange ('A'..'z')
+      }
+
+      "should not be in char range" {
+         'd' shouldNotBeInRange ('e' .. 'z')
+         'd' shouldNotBeInRange ('a' .. 'c')
+      }
+
+      "should be between from and to char" {
+         'd'.shouldBeBetween('a', 'z')
+         'E'.shouldBeBetween('A', 'z')
+         ']'.shouldBeBetween('A', 'z')
+      }
+
+      "should be not between from and to char" {
+         'd'.shouldNotBeBetween('e','z')
+         'd'.shouldNotBeBetween('a','c')
+      }
+
+      "should be equal ignore case" {
+         'c' shouldBeEqualToIgnoreCase 'C'
+      }
+
+      "should not be equal ignore case" {
+         'c' shouldNotBeEqualToIgnoreCase 'D'
+      }
+   }
+}
