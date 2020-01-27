156d3334a9b61eaddde6774e7728c991f8878ffc
sksamuel <sam@sksamuel.com>
2020-01-27 05:10:34 -0600

    Inspectors are bugged when the message is very big #938

--- kotest-assertions/src/jvmTest/kotlin/com/sksamuel/kotest/inspectors/sizes.kt
+++ kotest-assertions/src/jvmTest/kotlin/com/sksamuel/kotest/inspectors/sizes.kt
@@ -1,13 +1,19 @@
 package com.sksamuel.kotest.inspectors
 
+import io.kotest.assertions.shouldFail
+import io.kotest.core.spec.CompositeSpec
 import io.kotest.core.spec.style.funSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
 val tests = funSpec {
    test("should error with large failure count #938") {
-      List(100_000) { it }.forAll {
-         it shouldBe -1
+      shouldFail {
+         List(100_000) { it }.forAll {
+            it shouldBe -1
+         }
       }
    }
 }
+
+class InspectorTests : CompositeSpec(tests)
