aa4e38d0d49497c9ce898d04d2fb7368be27239b
sksamuel <sam@sksamuel.com>
2020-01-31 22:58:50 -0600

    Added github workflow publish; removed travis

--- kotest-tests/kotest-tests-junit-report/src/jvmTest/kotlin/com/sksamuel/kotest/ci.kt
+++ kotest-tests/kotest-tests-junit-report/src/jvmTest/kotlin/com/sksamuel/kotest/ci.kt
@@ -4,7 +4,7 @@ import io.kotest.Tag
 
 fun isTravis() = System.getenv("TRAVIS") == "true"
 fun isAppveyor() = System.getenv("APPVEYOR") == "True"
-fun isGitHubActions() = System.getenc("GITHUB_ACTIONS") == "true"
+fun isGitHubActions() = System.getenv("GITHUB_ACTIONS") == "true"
 fun isCI() = isTravis() || isAppveyor() || isGitHubActions()
 
 object AppveyorTag : Tag()
