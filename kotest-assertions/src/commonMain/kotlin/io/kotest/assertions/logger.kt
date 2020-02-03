d08e93c8176a722aed6d66517e6d927fa4754143
sksamuel <sam@sksamuel.com>
2020-02-03 14:14:00 -0600

    Changed kotest log prop name

--- kotest-assertions/src/commonMain/kotlin/io/kotest/assertions/logger.kt
+++ kotest-assertions/src/commonMain/kotlin/io/kotest/assertions/logger.kt
@@ -3,7 +3,7 @@ package io.kotest.assertions
 import io.kotest.mpp.env
 import io.kotest.mpp.sysprop
 
-private fun enabled() = sysprop("kotest.log") != null || env("kotest.log") != null
+private fun enabled() = sysprop("KOTEST_DEBUG") != null || env("KOTEST_DEBUG") != null
 
 fun log(msg: String) = log(msg, null)
 
