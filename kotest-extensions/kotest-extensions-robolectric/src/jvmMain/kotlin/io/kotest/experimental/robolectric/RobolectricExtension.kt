2eb832460a097be767f3c23605ca15260f179c2c
sksamuel <sam@sksamuel.com>
2020-02-07 05:56:03 -0600

    Fixed roboelectric

--- kotest-extensions/kotest-extensions-robolectric/src/jvmMain/kotlin/io/kotest/experimental/robolectric/RobolectricExtension.kt
+++ kotest-extensions/kotest-extensions-robolectric/src/jvmMain/kotlin/io/kotest/experimental/robolectric/RobolectricExtension.kt
@@ -1,54 +1,48 @@
 package io.kotest.experimental.robolectric
 
-import io.kotest.Spec
-import io.kotest.TestCase
-import io.kotest.TestResult
-import io.kotest.extensions.ConstructorExtension
-import io.kotest.extensions.TestCaseExtension
+import io.kotest.core.extensions.ConstructorExtension
+import io.kotest.core.extensions.TestCaseExtension
+import io.kotest.core.spec.Spec
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
 import kotlin.reflect.KClass
 import kotlin.reflect.full.findAnnotation
 
 class RobolectricExtension : ConstructorExtension, TestCaseExtension {
 
-    private val containedRobolectricRunner = ContainedRobolectricRunner()
+   private val containedRobolectricRunner = ContainedRobolectricRunner()
 
-    override fun <T : Spec> instantiate(clazz: KClass<T>): Spec? {
-        if (clazz.isNotRobolectricClass()) return null
-        return containedRobolectricRunner.sdkEnvironment.bootstrappedClass<Spec>(clazz.java).newInstance()
-    }
+   override fun <T : Spec> instantiate(clazz: KClass<T>): Spec? {
+      if (clazz.isNotRobolectricClass()) return null
+      return containedRobolectricRunner.sdkEnvironment.bootstrappedClass<Spec>(clazz.java).newInstance()
+   }
 
-    private fun <T : Spec> KClass<T>.isNotRobolectricClass() =
-        findAnnotation<RobolectricTest>() == null
+   private fun <T : Spec> KClass<T>.isNotRobolectricClass() =
+      findAnnotation<RobolectricTest>() == null
 
-    override suspend fun intercept(
-        testCase: TestCase,
-        execute: suspend (TestCase, suspend (TestResult) -> Unit) -> Unit,
-        complete: suspend (TestResult) -> Unit
-    ) {
+   override suspend fun intercept(testCase: TestCase, execute: suspend (TestCase) -> TestResult): TestResult {
 
-        if (testCase.spec::class.isNotRobolectricClass()) {
-            complete(TestResult.Ignored)
-            return
-        }
+      if (testCase.spec::class.isNotRobolectricClass()) {
+         return TestResult.Ignored
+      }
 
-        val containedRobolectricRunner = ContainedRobolectricRunner()
+      val containedRobolectricRunner = ContainedRobolectricRunner()
 
-        beforeTest(containedRobolectricRunner)
-        execute(testCase) {
-            complete(it)
-        }
-        afterTest(containedRobolectricRunner)
-    }
+      beforeTest(containedRobolectricRunner)
+      val result = execute(testCase)
+      afterTest(containedRobolectricRunner)
+      return result
+   }
 
-    private fun beforeTest(containedRobolectricRunner: ContainedRobolectricRunner) {
-        Thread.currentThread().contextClassLoader = containedRobolectricRunner.sdkEnvironment.robolectricClassLoader
-        containedRobolectricRunner.containedBefore()
-    }
+   private fun beforeTest(containedRobolectricRunner: ContainedRobolectricRunner) {
+      Thread.currentThread().contextClassLoader = containedRobolectricRunner.sdkEnvironment.robolectricClassLoader
+      containedRobolectricRunner.containedBefore()
+   }
 
-    private fun afterTest(containedRobolectricRunner: ContainedRobolectricRunner) {
-        containedRobolectricRunner.containedAfter()
-        Thread.currentThread().contextClassLoader = RobolectricExtension::class.java.classLoader
-    }
+   private fun afterTest(containedRobolectricRunner: ContainedRobolectricRunner) {
+      containedRobolectricRunner.containedAfter()
+      Thread.currentThread().contextClassLoader = RobolectricExtension::class.java.classLoader
+   }
 }
 
 annotation class RobolectricTest
