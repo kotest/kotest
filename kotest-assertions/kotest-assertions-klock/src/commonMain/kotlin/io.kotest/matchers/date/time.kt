3d9cfc7e8217bd5bc935437afab1c91fadeeedf7
Sourabh Singh Rawat <s.rawat3.142@live.in>
2020-02-12 01:03:20 +0530

    Add time matchers for numbers.

--- kotest-assertions/kotest-assertions-klock/src/commonMain/kotlin/io.kotest/matchers/date/time.kt
+++ kotest-assertions/kotest-assertions-klock/src/commonMain/kotlin/io.kotest/matchers/date/time.kt
@@ -19,6 +19,19 @@ fun haveSameHours(time: Time): Matcher<Time> = object : Matcher<Time> {
       )
 }
 
+infix fun Time.shouldHaveHours(hours: Long) = this should haveHours(hours)
+
+infix fun Time.shouldNotHaveHours(hours: Long) = this shouldNot haveHours(hours)
+
+fun haveHours(hours: Long): Matcher<Time> = object : Matcher<Time> {
+   override fun test(value: Time): MatcherResult =
+      MatcherResult(
+         value.hour.toLong() == hours,
+         { "$value should have hours $hours" },
+         { "$value should not have hours $hours" }
+      )
+}
+
 infix fun Time.shouldHaveSameMinutesAs(time: Time) = this should haveSameMinutes(time)
 
 infix fun Time.shouldNotHaveSameMinutesAs(time: Time) = this shouldNot haveSameMinutes(time)
@@ -29,6 +42,19 @@ fun haveSameMinutes(time: Time): Matcher<Time> = object : Matcher<Time> {
          value.minute == time.minute,
          { "$value should have minutes ${time.minute}" },
          { "$value should not have minutes ${time.minute}" }
+      )
+}
+
+infix fun Time.shouldHaveMinutes(minutes: Long) = this should haveMinutes(minutes)
+
+infix fun Time.shouldNotHaveMinutes(minutes: Long) = this shouldNot haveMinutes(minutes)
+
+fun haveMinutes(minutes: Long): Matcher<Time> = object : Matcher<Time> {
+   override fun test(value: Time): MatcherResult =
+      MatcherResult(
+         value.minute.toLong() == minutes,
+         { "$value should have minutes $minutes" },
+         { "$value should not have minutes $minutes" }
       )
 }
 
@@ -45,6 +71,19 @@ fun haveSameSeconds(time: Time): Matcher<Time> = object : Matcher<Time> {
       )
 }
 
+infix fun Time.shouldHaveSeconds(seconds: Long) = this should haveSeconds(seconds)
+
+infix fun Time.shouldNotSameSeconds(seconds: Long) = this shouldNot haveSeconds(seconds)
+
+fun haveSeconds(seconds: Long): Matcher<Time> = object : Matcher<Time> {
+   override fun test(value: Time): MatcherResult =
+      MatcherResult(
+         value.second.toLong() == seconds,
+         { "$value should have seconds $seconds" },
+         { "$value should not have seconds $seconds" }
+      )
+}
+
 infix fun Time.shouldHaveSameMillisecondsAs(time: Time) = this should haveSameMilliseconds(time)
 
 infix fun Time.shouldNotHaveSameMillisecondsAs(time: Time) = this shouldNot haveSameMilliseconds(time)
@@ -55,6 +94,19 @@ fun haveSameMilliseconds(time: Time): Matcher<Time> = object : Matcher<Time> {
          value.millisecond == time.millisecond,
          { "$value should have milliseconds ${time.millisecond}" },
          { "$value should not have milliseconds ${time.millisecond}" }
+      )
+}
+
+infix fun Time.shouldHaveMilliseconds(millis: Long) = this should haveMilliseconds(millis)
+
+infix fun Time.shouldNotHaveMilliseconds(millis: Long) = this shouldNot haveMilliseconds(millis)
+
+fun haveMilliseconds(millis: Long): Matcher<Time> = object : Matcher<Time> {
+   override fun test(value: Time): MatcherResult =
+      MatcherResult(
+         value.millisecond.toLong() == millis,
+         { "$value should have milliseconds $millis" },
+         { "$value should not have milliseconds $millis" }
       )
 }
 
