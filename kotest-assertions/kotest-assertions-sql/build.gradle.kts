53b3805f8eaa2f4a7e820aa9140cd7f0dd0f2a62
Sourabh <s.rawat3.142@live.in>
2020-02-19 17:53:38 +0530

    Add matchers for ResultSet. (#1221)
    * Add basic matchers for ResultSet.
    
    * Deps fix.
    
    * Add matcher for Row in ResultSet.
    
    * Use generics for column value.

---
+++ kotest-assertions/kotest-assertions-sql/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+   id("java")
+   id("kotlin-multiplatform")
+   id("java-library")
+   id("com.adarshr.test-logger")
+}
+
+repositories {
+   jcenter()
+}
+
+kotlin {
+   sourceSets {
+
+      targets {
+         jvm {
+            compilations.all {
+               kotlinOptions {
+                  jvmTarget = "1.8"
+               }
+            }
+         }
+      }
+
+      targets.all {
+         compilations.all {
+            kotlinOptions {
+               freeCompilerArgs += "-Xuse-experimental=kotlin.Experimental"
+            }
+         }
+      }
+
+      val jvmMain by getting {
+         dependencies {
+            implementation(kotlin("stdlib-jdk8"))
+            implementation(project(":kotest-assertions"))
+            implementation(project(":kotest-assertions:kotest-assertions-core"))
+         }
+      }
+
+      val jvmTest by getting {
+         dependsOn(jvmMain)
+         dependencies {
+            implementation(project(":kotest-runner:kotest-runner-junit5"))
+            implementation(Libs.Mocking.mockk)
+         }
+      }
+   }
+}
+
+tasks.named<Test>("jvmTest") {
+   useJUnitPlatform()
+   filter {
+      isFailOnNoMatchingTests = false
+   }
+   testLogging {
+      showExceptions = true
+      showStandardStreams = true
+      events = setOf(
+         org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+         org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+      )
+      exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+   }
+}
+
+apply(from = "../../publish-mpp.gradle.kts")
