d35585e284e804c9d2c9ff80229781cbfe5d841b
Kerooker <leonardo.colman98@gmail.com>
2020-02-21 17:24:28 -0300

    Adjust default values for Float and Double Arb
    Closes #1216

--- kotest-assertions/kotest-assertions-klock/build.gradle.kts
+++ kotest-assertions/kotest-assertions-klock/build.gradle.kts
@@ -67,4 +67,4 @@ tasks.named<Test>("jvmTest") {
    }
 }
 
-apply(from = "../../publish.gradle")
+apply(from = "../../nopublish.gradle")
---
+++ kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/arbitrary/FloatTest.kt
@@ -0,0 +1,17 @@
+package com.sksamuel.kotest.property.arbitrary
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.sequences.shouldHaveAtLeastSize
+import io.kotest.property.arbitrary.Arb
+import io.kotest.property.arbitrary.numericFloats
+import io.kotest.property.arbitrary.take
+
+class FloatTest : FunSpec({
+    test("Numeric Float should generate negative values by default") {
+        Arb.numericFloats()
+            .take(10_000)
+            .filter { it < 0 }
+            .distinct()
+            .shouldHaveAtLeastSize(100)
+    }
+})
