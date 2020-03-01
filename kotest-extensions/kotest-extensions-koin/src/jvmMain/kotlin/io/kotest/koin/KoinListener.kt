4d648dd1a9430ab6cdcb598e17ab9c7442cb8944
Kerooker <leonardo.colman98@gmail.com>
2020-02-29 23:31:53 -0300

    Remove AutoScan from KoinListener
    This commit also improves the way we instantiate classes, providing a better error if anything is misdefined.
    
    Closes #1246

--- kotest-extensions/kotest-extensions-koin/src/jvmMain/kotlin/io/kotest/koin/KoinListener.kt
+++ kotest-extensions/kotest-extensions-koin/src/jvmMain/kotlin/io/kotest/koin/KoinListener.kt
@@ -1,14 +1,12 @@
 package io.kotest.koin
 
+import io.kotest.core.listeners.TestListener
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
-import io.kotest.core.listeners.TestListener
-import io.kotest.core.spec.AutoScan
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.core.module.Module
 
-@AutoScan
 class KoinListener(private val modules: List<Module>) : TestListener {
 
    constructor(module: Module) : this(listOf(module))
