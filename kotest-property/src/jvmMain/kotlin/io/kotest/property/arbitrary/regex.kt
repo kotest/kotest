e4afb69cd83228612437268afd7914ddd4ecabc5
Cory Thomas <dump247@users.noreply.github.com>
2020-02-29 20:28:28 -0600

    Fix the regex property generator so it produces new values (#1249)
    The arb was generating a sequence of the same value; the first value for the
    RandomSource seed.

--- kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/regex.kt
+++ kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/regex.kt
@@ -1,11 +1,18 @@
 package io.kotest.property.arbitrary
 
 import com.mifmif.common.regex.Generex
+import io.kotest.property.RandomSource
+import io.kotest.property.Sample
 
-fun Arb.Companion.regex(regex: String) = arb {
-   val generex = Generex(regex)
-   generex.setSeed(it.seed)
-   generex.random()
+fun Arb.Companion.regex(regex: String) = object : Arb<String> {
+   override fun edgecases(): List<String> = emptyList()
+
+   override fun samples(rs: RandomSource): Sequence<Sample<String>> {
+      val generex = Generex(regex)
+      generex.setSeed(rs.seed)
+
+      return generateSequence { Sample(generex.random()) }
+   }
 }
 
 fun Arb.Companion.regex(regex: Regex) = regex(regex.pattern)
