fd6ccdb35d07dc839754bad9d3ccb3bc1b75b8ff
Kerooker <leonardo.colman98@gmail.com>
2020-03-29 22:27:29 -0300

    Remove Android from main Kotest project
    Android code is being maintained in [Kotest-Android](https://github
    .com/kotest/kotest-android) project.
    
    This commit removes everything related to Android from the main codebase
     as it's already present in Android's codebase.
    
    Kotest's documentation will still point to Kotest-Android artifacts and
     Android related documentation, but to ease contributions and CI,
     Android should be kept in a separate project.
    
    This commit also removes our integration to JUnit4. We decided to
    integrate only with JUnit 5. If JUnit4 becomes necessary again, it will
    likely be for Android UI tests only, and thus it could be in
    Kotest-Android as well.

--- kotest-runner/kotest-runner-junit4/src/jvmMain/kotlin/io/kotest/runner/junit4/RunnerHelpers.kt
+++
@@ -1,7 +0,0 @@
-package io.kotest.runner.junit4
-
-import io.kotest.TestCase
-import org.junit.runner.Description
-
-fun describeTestCase(testCase: TestCase): Description =
-    Description.createTestDescription(testCase.spec.javaClass, testCase.description.tail().fullName())
