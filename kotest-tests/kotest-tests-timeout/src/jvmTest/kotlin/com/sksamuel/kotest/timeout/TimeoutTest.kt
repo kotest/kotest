cc63915901edef86340c4c7a124b530f661df315
sksamuel <sam@sksamuel.com>
2020-04-02 22:13:43 -0500

    Added extra timeout test #1283

--- kotest-tests/kotest-tests-timeout/src/jvmTest/kotlin/com/sksamuel/kotest/timeout/TimeoutTest.kt
+++ kotest-tests/kotest-tests-timeout/src/jvmTest/kotlin/com/sksamuel/kotest/timeout/TimeoutTest.kt
@@ -4,8 +4,10 @@ import io.kotest.core.spec.style.StringSpec
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 import kotlin.time.ExperimentalTime
 import kotlin.time.milliseconds
+import kotlin.time.toDuration
 
 @ExperimentalTime
 class TimeoutTest : StringSpec() {
@@ -19,12 +21,18 @@ class TimeoutTest : StringSpec() {
          Thread.sleep(1000000)
       }
 
-      "a testcase timeout should interrupt a suspended coroutine".config(timeout = 250.milliseconds) {
+      "a testcase timeout should interrupt a suspend function".config(timeout = 250.milliseconds) {
          // high value to ensure its interrupted, we'd notice a test that runs for 10 weeks
          delay(1000000)
       }
 
-      "a testcase timeout should interrupt suspended nested coroutines".config(timeout = 250.milliseconds) {
+      "a testcase timeout should interupt a nested coroutine".config(timeout = 250.milliseconds) {
+         launch {
+            delay(10.toDuration(TimeUnit.MINUTES))
+         }
+      }
+
+      "a testcase timeout should interrupt suspended coroutine scope".config(timeout = 250.milliseconds) {
          // high value to ensure its interrupted, we'd notice a test that runs for 10 weeks
          someCoroutine()
       }
