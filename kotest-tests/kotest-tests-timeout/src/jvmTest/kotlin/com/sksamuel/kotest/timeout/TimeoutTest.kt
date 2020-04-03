4f96ba9e4c5023e6a93b897403eafb39bc2a7dd7
Stephen Samuel <sam@sksamuel.com>
2020-04-03 00:13:29 -0500

    Fixed timeout for nested coroutine not wrapped in a scope (#1345)
    * Added extra timeout test #1283
    
    * Fixed timeout for nested coroutine not wrapped in a scope

--- kotest-tests/kotest-tests-timeout/src/jvmTest/kotlin/com/sksamuel/kotest/timeout/TimeoutTest.kt
+++ kotest-tests/kotest-tests-timeout/src/jvmTest/kotlin/com/sksamuel/kotest/timeout/TimeoutTest.kt
@@ -4,8 +4,10 @@ import io.kotest.core.spec.style.StringSpec
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 import kotlin.time.ExperimentalTime
 import kotlin.time.milliseconds
+import kotlin.time.toDuration
 
 @ExperimentalTime
 class TimeoutTest : StringSpec() {
@@ -14,18 +16,24 @@ class TimeoutTest : StringSpec() {
 
       extension(expectFailureExtension)
 
-      "a testcase timeout should interrupt a blocked thread".config(timeout = 250.milliseconds) {
+      "a testcase timeout should interrupt a blocked thread".config(timeout = 50.milliseconds) {
          // high value to ensure its interrupted, we'd notice a test that runs for 10 weeks
          Thread.sleep(1000000)
       }
 
-      "a testcase timeout should interrupt a suspended coroutine".config(timeout = 250.milliseconds) {
+      "a testcase timeout should interrupt a suspend function".config(timeout = 50.milliseconds) {
          // high value to ensure its interrupted, we'd notice a test that runs for 10 weeks
          delay(1000000)
       }
 
-      "a testcase timeout should interrupt suspended nested coroutines".config(timeout = 250.milliseconds) {
-         // high value to ensure its interrupted, we'd notice a test that runs for 10 weeks
+      "a testcase timeout should interupt a nested coroutine".config(timeout = 50.milliseconds) {
+         launch {
+            // a  high value to ensure its interrupted, we'd notice a test that runs for ever
+            delay(10.toDuration(TimeUnit.HOURS))
+         }
+      }
+
+      "a testcase timeout should interrupt suspended coroutine scope".config(timeout = 50.milliseconds) {
          someCoroutine()
       }
    }
