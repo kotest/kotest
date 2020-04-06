c3ab82bbdcb3bd89a36379270fd35675701b2d87
Ashish Kumar Joy <ashishkujoy@gmail.com>
2020-04-06 18:39:47 +0530

    #1353 Adds usage of listeners

--- kotest-extensions/kotest-extensions-testcontainer/src/jvmMain/kotlin/ContainerPerSpecListener.kt
+++ kotest-extensions/kotest-extensions-testcontainer/src/jvmMain/kotlin/ContainerPerSpecListener.kt
@@ -5,6 +5,35 @@ import io.kotest.core.test.TestResult
 import org.testcontainers.containers.GenericContainer
 import kotlin.reflect.KClass
 
+
+/**
+ * [ContainerPerSpecListener] starts the given [genericContainer] before execution of any test in the spec
+ * and stops after execution of all tests.
+ *
+ * [genericContainer] can any of [GenericContainer] [ContainerisedDockerCompose] [LocalStackContainer] etc.
+ *
+ * This should be use when you want to a single container for all test in a single test class.
+ *
+ * @see
+ * [ContainerPerSpecListener]
+ *
+ * Usage:
+ *
+ * class RedisRepositoryTest: StringSpec() {
+ *    private val redisContainer = GenericContainer<Nothing>("redis")
+ *
+ *    override fun listeners(): List<TestListener> {
+ *       return super.listeners() + ContainerPerSpecListener(redisContainer)
+ *    }
+ *
+ *    init {
+ *       "should be able to connect redis server" {
+ *         // your test goes here
+ *       }
+ *    }
+ * }
+ * */
+
 class ContainerPerSpecListener(private val genericContainer: GenericContainer<Nothing>) : TestListener {
 
    override suspend fun finalizeSpec(kclass: KClass<out Spec>, results: Map<TestCase, TestResult>) {
--- kotest-extensions/kotest-extensions-testcontainer/src/jvmMain/kotlin/ContainerPerTestListener.kt
+++ kotest-extensions/kotest-extensions-testcontainer/src/jvmMain/kotlin/ContainerPerTestListener.kt
@@ -3,6 +3,32 @@ import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
 import org.testcontainers.containers.GenericContainer
 
+
+/**
+ * [ContainerPerTestListener] starts the given [genericContainer] before execution of each test in the spec
+ * and stops after execution of each test.
+ *
+ * [genericContainer] can any of [GenericContainer] [ContainerisedDockerCompose] [LocalStackContainer] etc.
+ *
+ * This should be use when you want fresh container for each test.
+ *
+ * @see[ContainerPerSpecListener]
+ * Usage:
+ *
+ * class RedisRepositoryTest: StringSpec() {
+ *    private val redisContainer = GenericContainer<Nothing>("redis")
+ *
+ *    override fun listeners(): List<TestListener> {
+ *       return super.listeners() + ContainerPerTestListener(redisContainer)
+ *    }
+ *
+ *    init {
+ *       "should be able to connect redis server" {
+ *         // your test goes here
+ *       }
+ *    }
+ * }
+ * */
 class ContainerPerTestListener(private val genericContainer: GenericContainer<Nothing>) : TestListener {
 
    override suspend fun beforeTest(testCase: TestCase) {
