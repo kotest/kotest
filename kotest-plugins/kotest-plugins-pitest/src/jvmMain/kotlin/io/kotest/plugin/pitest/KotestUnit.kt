ae42d8979845b55b1362d80ff97a3c5d70e06d0f
sksamuel <sam@sksamuel.com>
2020-04-11 14:02:59 -0500

    Added extra tests for #1376

--- kotest-plugins/kotest-plugins-pitest/src/jvmMain/kotlin/io/kotest/plugin/pitest/KotestUnit.kt
+++ kotest-plugins/kotest-plugins-pitest/src/jvmMain/kotlin/io/kotest/plugin/pitest/KotestUnit.kt
@@ -1,6 +1,5 @@
 package io.kotest.plugin.pitest
 
-import io.kotest.core.config.Project
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
 import io.kotest.core.spec.description
