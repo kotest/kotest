70feaad52f961986e211b768a228ccea1036a173
sksamuel <sam@sksamuel.com>
2020-04-12 21:01:13 -0500

    Check order of callbacks in ShouldSpec with when and WordsSpec with when #1328

---
+++ kotest-tests/kotest-tests-core/src/jvmTest/kotlin/com/sksamuel/kotest/specs/wordspec/WhenCallbackOrderTest.kt
@@ -0,0 +1,31 @@
+package com.sksamuel.kotest.specs.wordspec
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+class WhenCallbackOrderTest : WordSpec({
+
+   var before = ""
+   var after = ""
+
+   beforeTest {
+      before += it.name
+   }
+
+   afterTest { (test, _) ->
+      after += test.name
+   }
+
+   afterSpec {
+      before shouldBe "w whens shouldt"
+      after shouldBe "ts shouldw when"
+   }
+
+   "w" When {
+      "s" should {
+         "t" {
+         }
+      }
+   }
+
+})
