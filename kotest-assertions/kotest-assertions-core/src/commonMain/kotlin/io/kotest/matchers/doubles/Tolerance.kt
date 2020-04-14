6e575939c77f747454223b6009a070fa4e72eff9
sksamuel <sam@sksamuel.com>
2020-04-13 21:52:33 -0500

    Improve error message in tolerance matchers #1230

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/doubles/Tolerance.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/doubles/Tolerance.kt
@@ -13,25 +13,36 @@ import kotlin.math.abs
  * 0.1 shouldBe (0.4 plusOrMinus 0.2)   // Assertion fails
  * ```
  */
-infix fun Double.plusOrMinus(tolerance: Double): ToleranceMatcher = ToleranceMatcher(
-  this,
-  tolerance)
+infix fun Double.plusOrMinus(tolerance: Double): ToleranceMatcher {
+   require(tolerance >= 0)
+   return ToleranceMatcher(this, tolerance)
+}
 
 class ToleranceMatcher(private val expected: Double?, private val tolerance: Double) : Matcher<Double?> {
+
   override fun test(value: Double?): MatcherResult {
     return if(value == null || expected == null) {
       MatcherResult(value == expected, "$value should be equal to $expected", "$value should not be equal to $expected")
     } else if (expected.isNaN() && value.isNaN()) {
-      println("[WARN] By design, Double.Nan != Double.Nan; see https://stackoverflow.com/questions/8819738/why-does-double-nan-double-nan-return-false/8819776#8819776")
-      MatcherResult(false,
-             "By design, Double.Nan != Double.Nan; see https://stackoverflow.com/questions/8819738/why-does-double-nan-double-nan-return-false/8819776#8819776",
-             "By design, Double.Nan != Double.Nan; see https://stackoverflow.com/questions/8819738/why-does-double-nan-double-nan-return-false/8819776#8819776"
-      )
+       println("[WARN] By design, Double.Nan != Double.Nan; see https://stackoverflow.com/questions/8819738/why-does-double-nan-double-nan-return-false/8819776#8819776")
+       MatcherResult(
+          false,
+          "By design, Double.Nan != Double.Nan; see https://stackoverflow.com/questions/8819738/why-does-double-nan-double-nan-return-false/8819776#8819776",
+          "By design, Double.Nan != Double.Nan; see https://stackoverflow.com/questions/8819738/why-does-double-nan-double-nan-return-false/8819776#8819776"
+       )
     } else {
-      if (tolerance == 0.0)
-        println("[WARN] When comparing doubles consider using tolerance, eg: a shouldBe (b plusOrMinus c)")
-      val diff = abs(value - expected)
-      MatcherResult(diff <= tolerance, "$value should be equal to $expected", "$value should not be equal to $expected")
+       if (tolerance == 0.0)
+          println("[WARN] When comparing doubles consider using tolerance, eg: a shouldBe (b plusOrMinus c)")
+       val diff = abs(value - expected)
+
+       val passed = diff <= tolerance
+       val low = expected - tolerance
+       val high = expected + tolerance
+       val msg = when (tolerance) {
+          0.0 -> "$value should be equal to $expected"
+          else -> "$value should be equal to $expected within tolerance of $tolerance (lowest acceptable value is $low; highest acceptable value is $high)"
+       }
+       MatcherResult(passed, msg,"$value should not be equal to $expected")
     }
   }
 }
