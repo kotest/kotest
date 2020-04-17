1e3638faf0f4cfa817392ed2b8b2243fc0f91ed3
sksamuel <sam@sksamuel.com>
2020-04-17 00:19:22 -0500

    Added context support to should spec

--- src/test/kotlin/io/kotest/plugin/intellij/styles/ShouldSpecStyleTest.kt
+++ src/test/kotlin/io/kotest/plugin/intellij/styles/ShouldSpecStyleTest.kt
@@ -20,7 +20,7 @@ class ShouldSpecStyleTest : LightCodeInsightFixtureTestCase() {
 
       val gutters = myFixture.findAllGutters()
       println(gutters.map { it.tooltipText }.joinToString("\n"))
-      gutters.size shouldBe 3
+      gutters.size shouldBe 6
 
       assertSoftly {
 
@@ -30,11 +30,25 @@ class ShouldSpecStyleTest : LightCodeInsightFixtureTestCase() {
 
          gutters[1].icon shouldBe AllIcons.RunConfigurations.TestState.Run
          gutters[1].tooltipText shouldBe "Run should top level test"
-         (gutters[1] as LineMarkerInfo.LineMarkerGutterIconRenderer<*>).lineMarkerInfo.startOffset shouldBe 170
+         (gutters[1] as LineMarkerInfo.LineMarkerGutterIconRenderer<*>).lineMarkerInfo.startOffset shouldBe 173
 
          gutters[2].icon shouldBe AllIcons.RunConfigurations.TestState.Run
          gutters[2].tooltipText shouldBe "Run should top level test with config"
-         (gutters[2] as LineMarkerInfo.LineMarkerGutterIconRenderer<*>).lineMarkerInfo.startOffset shouldBe 237
+         (gutters[2] as LineMarkerInfo.LineMarkerGutterIconRenderer<*>).lineMarkerInfo.startOffset shouldBe 247
+
+         gutters[3].icon shouldBe AllIcons.RunConfigurations.TestState.Run
+         gutters[3].tooltipText shouldBe "Run some context"
+         (gutters[3] as LineMarkerInfo.LineMarkerGutterIconRenderer<*>).lineMarkerInfo.startOffset shouldBe 333
+
+         gutters[4].icon shouldBe AllIcons.RunConfigurations.TestState.Run
+         gutters[4].tooltipText shouldBe "Run some context -- should top level test"
+         (gutters[4] as LineMarkerInfo.LineMarkerGutterIconRenderer<*>).lineMarkerInfo.startOffset shouldBe 369
+
+         gutters[5].icon shouldBe AllIcons.RunConfigurations.TestState.Run
+         gutters[5].tooltipText shouldBe "Run some context -- should top level test with config"
+         (gutters[5] as LineMarkerInfo.LineMarkerGutterIconRenderer<*>).lineMarkerInfo.startOffset shouldBe 452
+
+
       }
    }
 
