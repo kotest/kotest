e176cc3e14364d74ee593533b50eb9b08df1f5d1
sksamuel <sam@sksamuel.com>
2020-04-22 05:15:12 -0500

    Add x variants for expect spec #1383

--- kotest-core/src/commonMain/kotlin/io/kotest/core/spec/style/ExpectSpecDsl.kt
+++ kotest-core/src/commonMain/kotlin/io/kotest/core/spec/style/ExpectSpecDsl.kt
@@ -3,7 +3,11 @@ package io.kotest.core.spec.style
 import io.kotest.core.Tag
 import io.kotest.core.extensions.TestCaseExtension
 import io.kotest.core.spec.SpecDsl
-import io.kotest.core.test.*
+import io.kotest.core.test.EnabledIf
+import io.kotest.core.test.TestContext
+import io.kotest.core.test.TestType
+import io.kotest.core.test.createTestName
+import io.kotest.core.test.deriveTestConfig
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
@@ -17,7 +21,20 @@ interface ExpectSpecDsl : SpecDsl {
          TestType.Container
       )
 
-   class TestBuilder(val context: TestContext, val name: String, private val dsl: SpecDsl) {
+   fun xcontext(name: String, test: suspend ExpectScope.() -> Unit) =
+      addTest(
+         createTestName("Context: ", name),
+         { ExpectScope(this, this@ExpectSpecDsl).test() },
+         defaultConfig().copy(enabled = false),
+         TestType.Container
+      )
+
+   class TestBuilder(
+      val context: TestContext,
+      val name: String,
+      private val dsl: SpecDsl,
+      private val xdisabled: Boolean
+   ) {
 
       @OptIn(ExperimentalTime::class)
       suspend fun config(
@@ -30,8 +47,10 @@ interface ExpectSpecDsl : SpecDsl {
          enabledIf: EnabledIf? = null,
          test: suspend TestContext.() -> Unit
       ) {
-         val config = dsl.defaultConfig().deriveTestConfig(enabled, tags, extensions, timeout, enabledIf, invocations, threads)
-         context.registerTestCase(name, test, config, TestType.Test)
+         val config =
+            dsl.defaultConfig().deriveTestConfig(enabled, tags, extensions, timeout, enabledIf, invocations, threads)
+         val config2 = if (xdisabled) config.copy(enabled = false) else config
+         context.registerTestCase(name, test, config2, TestType.Test)
       }
    }
 }
@@ -47,8 +66,25 @@ class ExpectScope(val context: TestContext, private val dsl: ExpectSpecDsl) {
          TestType.Container
       )
 
+   suspend fun xcontext(name: String, test: suspend ExpectScope.() -> Unit) =
+      context.registerTestCase(
+         createTestName("Context: ", name),
+         { ExpectScope(this, this@ExpectScope.dsl).test() },
+         dsl.defaultConfig().copy(enabled = false),
+         TestType.Container
+      )
+
    suspend fun expect(name: String, test: suspend TestContext.() -> Unit) =
       context.registerTestCase(createTestName("Expect: ", name), test, dsl.defaultConfig(), TestType.Test)
 
-   fun expect(name: String) = ExpectSpecDsl.TestBuilder(context, createTestName("Expect: ", name), dsl)
+   suspend fun xexpect(name: String, test: suspend TestContext.() -> Unit) =
+      context.registerTestCase(
+         createTestName("Expect: ", name),
+         test,
+         dsl.defaultConfig().copy(enabled = false),
+         TestType.Test
+      )
+
+   fun expect(name: String) = ExpectSpecDsl.TestBuilder(context, createTestName("Expect: ", name), dsl, false)
+   fun xexpect(name: String) = ExpectSpecDsl.TestBuilder(context, createTestName("Expect: ", name), dsl, true)
 }
