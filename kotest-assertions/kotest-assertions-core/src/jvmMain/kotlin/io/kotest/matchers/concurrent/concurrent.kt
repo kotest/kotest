8d1ddf96814dc7a9af7d6bafde8ba339f4bfd44d
Brian Baldino <bbaldino@gmail.com>
2020-05-21 09:22:28 -0700

    add missing imports

--- kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/concurrent/concurrent.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/concurrent/concurrent.kt
@@ -5,6 +5,8 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicReference
 import kotlin.concurrent.thread
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.TimeoutCancellationException
 
 fun <A> shouldCompleteWithin(timeout: Long, unit: TimeUnit, thunk: () -> A) {
 
