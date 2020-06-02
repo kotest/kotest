4354170a27d06583477b7ea974bcc537f39cd48c
AJ Alt <ajalt@users.noreply.github.com>
2020-06-01 17:28:33 -0700

    Replace nativeMain source set with individual targets (#1476)

---
+++ kotest-assertions/kotest-assertions-shared/src/mingwX64Main/kotlin/io/kotest/assertions/diffLargeString.kt
@@ -0,0 +1,3 @@
+package io.kotest.assertions
+
+actual fun diffLargeString(expected: String, actual: String): Pair<String, String>? = null
---
+++ kotest-assertions/kotest-assertions-shared/src/mingwX64Main/kotlin/io/kotest/assertions/show/platformShow.kt
@@ -0,0 +1,3 @@
+package io.kotest.assertions.show
+
+actual fun <A : Any> platformShow(a: A): Show<A>? = null
