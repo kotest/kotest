b1f02811a0e4f5171252fc5497c559cc26b50f64
sksamuel <sam@sksamuel.com>
2020-06-06 17:10:42 -0500

    Updated property test output to show exceptions for each step as well as initial and final #1279

--- kotest-mpp/src/linuxX64Main/kotlin/io/kotest/mpp/stacktraces.kt
+++ kotest-mpp/src/linuxX64Main/kotlin/io/kotest/mpp/stacktraces.kt
@@ -4,4 +4,5 @@ actual object StackTraces {
    actual fun Throwable.throwableLocation(): String? = null
    actual fun Throwable.location(n: Int): List<String>? = null
    actual fun <T : Throwable> cleanStackTrace(throwable: T): T = throwable
+   actual fun root(throwable: Throwable): Throwable = throwable
 }
--- kotest-mpp/src/macosX64Main/kotlin/io/kotest/mpp/stacktraces.kt
+++ kotest-mpp/src/macosX64Main/kotlin/io/kotest/mpp/stacktraces.kt
@@ -4,4 +4,6 @@ actual object StackTraces {
    actual fun Throwable.throwableLocation(): String? = null
    actual fun Throwable.location(n: Int): List<String>? = null
    actual fun <T : Throwable> cleanStackTrace(throwable: T): T = throwable
+   actual fun root(throwable: Throwable): Throwable = throwable
+
 }
