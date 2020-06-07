3d0a4e08b73718e064d40a3fc2fb905d8449410b
sksamuel <sam@sksamuel.com>
2020-06-06 19:19:16 -0500

    Updated arrow matchers to use new inspectors

--- kotest-assertions/kotest-assertions-arrow/src/jvmMain/kotlin/io/kotest/assertions/arrow/nel/Inspectors.kt
+++ kotest-assertions/kotest-assertions-arrow/src/jvmMain/kotlin/io/kotest/assertions/arrow/nel/Inspectors.kt
@@ -1,23 +1,24 @@
 package io.kotest.assertions.arrow.nel
 
 import arrow.core.NonEmptyList
+import io.kotest.inspectors.*
 
-fun <T> NonEmptyList<T>.forAll(fn: (T) -> Unit) = io.kotest.inspectors.forAll(this.all, fn)
+fun <T> NonEmptyList<T>.forAll(fn: (T) -> Unit) = all.forAll(fn)
 
-fun <T> NonEmptyList<T>.forOne(fn: (T) -> Unit) = io.kotest.inspectors.forExactly(1, this.all, fn)
+fun <T> NonEmptyList<T>.forOne(fn: (T) -> Unit) = all.forOne(fn)
 
-fun <T> NonEmptyList<T>.forExactly(k: Int, fn: (T) -> Unit) = io.kotest.inspectors.forExactly(k, this.all, fn)
+fun <T> NonEmptyList<T>.forExactly(k: Int, fn: (T) -> Unit) = all.forExactly(k, fn)
 
-fun <T> NonEmptyList<T>.forSome(fn: (T) -> Unit) = io.kotest.inspectors.forSome(this.all, fn)
+fun <T> NonEmptyList<T>.forSome(fn: (T) -> Unit) = all.forSome(fn)
 
-fun <T> NonEmptyList<T>.forAny(f: (T) -> Unit) = io.kotest.inspectors.forAny(this.all, f)
+fun <T> NonEmptyList<T>.forAny(f: (T) -> Unit) = all.forAny(f)
 
-fun <T> NonEmptyList<T>.forAtLeastOne(f: (T) -> Unit) = io.kotest.inspectors.forAtLeastOne(this.all, f)
+fun <T> NonEmptyList<T>.forAtLeastOne(f: (T) -> Unit) = all.forAtLeastOne(f)
 
-fun <T> NonEmptyList<T>.forAtLeast(k: Int, fn: (T) -> Unit) = io.kotest.inspectors.forAtLeast(k, this.all, fn)
+fun <T> NonEmptyList<T>.forAtLeast(k: Int, fn: (T) -> Unit) = all.forAtLeast(k, fn)
 
-fun <T> NonEmptyList<T>.forAtMostOne(f: (T) -> Unit) = io.kotest.inspectors.forAtMost(1, this.all, f)
+fun <T> NonEmptyList<T>.forAtMostOne(f: (T) -> Unit) = all.forAtMostOne(f)
 
-fun <T> NonEmptyList<T>.forAtMost(k: Int, fn: (T) -> Unit) = io.kotest.inspectors.forAtMost(k, this.all, fn)
+fun <T> NonEmptyList<T>.forAtMost(k: Int, fn: (T) -> Unit) = all.forAtMost(k, fn)
 
-fun <T> NonEmptyList<T>.forNone(f: (T) -> Unit) = io.kotest.inspectors.forNone(this.all, f)
+fun <T> NonEmptyList<T>.forNone(f: (T) -> Unit) = all.forNone(f)
