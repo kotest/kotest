572f9b11bc53a13e66738d8294da1a32df2b5e66
sksamuel <sam@sksamuel.com>
2020-06-19 12:16:14 -0500

    Native thread local fix

--- kotest-assertions/kotest-assertions-shared/src/macosX64Main/kotlin/io/kotest/assertions/counter.kt
+++ kotest-assertions/kotest-assertions-shared/src/macosX64Main/kotlin/io/kotest/assertions/counter.kt
@@ -1,4 +1,16 @@
 package io.kotest.assertions
 
+actual val assertionCounter: AssertionCounter = NativeAssertionCounter
+
 @ThreadLocal
-actual val assertionCounter: AssertionCounter = BasicAssertionCounter
+object NativeAssertionCounter : AssertionCounter {
+   private var counter = 0
+   override fun get(): Int = counter
+   override fun reset() {
+      counter = 0
+   }
+
+   override fun inc() {
+      counter++
+   }
+}
