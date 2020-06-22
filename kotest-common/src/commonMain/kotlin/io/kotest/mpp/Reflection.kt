6ebcd03f7179c9fb8ab587ddeb5726e829fd6bbc
sksamuel <sam@sksamuel.com>
2020-06-22 17:48:46 -0500

    Fixed kdoc

--- kotest-common/src/commonMain/kotlin/io/kotest/mpp/Reflection.kt
+++ kotest-common/src/commonMain/kotlin/io/kotest/mpp/Reflection.kt
@@ -39,7 +39,7 @@ interface Reflection {
 
    /**
     * Returns the names of the parameters if supported. Eg, for `fun foo(a: String, b: Boolean)` on the JVM
-    * it would return [a, b] and on unsupported platforms an empty list.
+    * it would return a, b and on unsupported platforms an empty list.
     */
    fun paramNames(fn: Function<*>): List<String>?
 
