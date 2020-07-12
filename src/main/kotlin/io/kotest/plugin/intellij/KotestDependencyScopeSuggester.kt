fae0561ced77812d2af463b97bcfe0b31243301e
sksamuel <sam@sksamuel.com>
2020-07-12 17:57:36 -0500

    Added dependency scope and library resolvers

---
+++ src/main/kotlin/io/kotest/plugin/intellij/KotestDependencyScopeSuggester.kt
@@ -0,0 +1,29 @@
+package io.kotest.plugin.intellij
+
+import com.intellij.openapi.roots.DependencyScope
+import com.intellij.openapi.roots.LibraryDependencyScopeSuggester
+import com.intellij.openapi.roots.OrderRootType
+import com.intellij.openapi.roots.libraries.Library
+import com.intellij.openapi.roots.libraries.LibraryUtil
+import com.intellij.openapi.vfs.VirtualFile
+
+class KotestDependencyScopeSuggester : LibraryDependencyScopeSuggester() {
+
+   private val jarMarkers = listOf("io.kotest.core.spec.style.FunSpec")
+
+   override fun getDefaultDependencyScope(library: Library): DependencyScope? {
+      val files = library.getFiles(OrderRootType.CLASSES)
+      val testJars = files.filter { isTestJarRoot(it) }.count()
+      val regularJars = files.size - testJars
+      return if (testJars > regularJars) DependencyScope.TEST else null
+   }
+
+   private fun isTestJarRoot(file: VirtualFile): Boolean {
+      for (marker in jarMarkers) {
+         if (LibraryUtil.isClassAvailableInLibrary(listOf(file), marker)) {
+            return true
+         }
+      }
+      return false
+   }
+}
---
+++ src/main/kotlin/io/kotest/plugin/intellij/KotestExternalLibraryResolver.kt
@@ -0,0 +1,21 @@
+package io.kotest.plugin.intellij
+
+import com.intellij.codeInsight.daemon.quickFix.ExternalLibraryResolver
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.roots.ExternalLibraryDescriptor
+import com.intellij.util.ThreeState
+
+class KotestExternalLibraryResolver : ExternalLibraryResolver() {
+
+   companion object {
+      val KotestDescriptor = ExternalLibraryDescriptor("io.kotest", "kotest-runner-junit5-jvm", "4.1.0", null, "4.1.1")
+      val Specs = listOf("FunSpec", "BehaviorSpec", "StringSpec", "ShouldSpec", "AnnotationSpec", "ExpectSpec", "FeatureSpec", "DescribeSpec", "WordSpec", "FreeSpec")
+   }
+
+   override fun resolveClass(shortClassName: String, isAnnotation: ThreeState, contextModule: Module): ExternalClassResolveResult? {
+      if (Specs.contains(shortClassName)) {
+         return ExternalClassResolveResult("io.kotest.core.spec.style.$shortClassName", KotestDescriptor)
+      }
+      return null
+   }
+}
