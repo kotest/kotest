f6d5e57cc5ef208d434fa27e2c6980cb8f57cd82
Shane Lathrop <slathrop@shoprunner.com>
2020-07-15 08:11:46 -0500

    remove debugging and unnecessary default value

--- kotest-extensions/kotest-extensions-http/src/jvmMain/kotlin/io/kotest/extensions/http/test.kt
+++ kotest-extensions/kotest-extensions-http/src/jvmMain/kotlin/io/kotest/extensions/http/test.kt
@@ -6,12 +6,11 @@ suspend fun http(resource: String, f: suspend (HttpResponse) -> Unit) {
    http(resource, mapOf(), f)
 }
 
-suspend fun http(resource: String, parameters: Map<String, String> = mapOf(), f: suspend (HttpResponse) -> Unit) {
+suspend fun http(resource: String, parameters: Map<String, String>, f: suspend (HttpResponse) -> Unit) {
    val lines = object : Any() {}.javaClass.getResourceAsStream(resource).use { it.bufferedReader().readLines() }
       .map { line ->
          parameters.entries.fold(line) { acc, op -> acc.replace("{{${op.key}}}", op.value) }
       }
-   println("lines : $lines")
    val req = parseHttpRequest(lines)
    val resp = runRequest(req)
    f(resp)
