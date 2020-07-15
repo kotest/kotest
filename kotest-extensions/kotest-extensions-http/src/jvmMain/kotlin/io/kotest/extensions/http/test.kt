60d74625efdda9572300acc3873339130ec3d101
Shane Lathrop <slathrop@shoprunner.com>
2020-07-15 07:56:43 -0500

    add the ability to make parameter substitutions in http files when executed using kotest

--- kotest-extensions/kotest-extensions-http/src/jvmMain/kotlin/io/kotest/extensions/http/test.kt
+++ kotest-extensions/kotest-extensions-http/src/jvmMain/kotlin/io/kotest/extensions/http/test.kt
@@ -3,7 +3,15 @@ package io.kotest.extensions.http
 import io.ktor.client.statement.HttpResponse
 
 suspend fun http(resource: String, f: suspend (HttpResponse) -> Unit) {
+   http(resource, mapOf(), f)
+}
+
+suspend fun http(resource: String, parameters: Map<String, String> = mapOf(), f: suspend (HttpResponse) -> Unit) {
    val lines = object : Any() {}.javaClass.getResourceAsStream(resource).use { it.bufferedReader().readLines() }
+      .map { line ->
+         parameters.entries.fold(line) { acc, op -> acc.replace("{{${op.key}}}", op.value) }
+      }
+   println("lines : $lines")
    val req = parseHttpRequest(lines)
    val resp = runRequest(req)
    f(resp)
