dd50fbf18646e59e34c11f6fbbb777ae091bedbd
Christian Stoenescu <christian.stoenescu@gmail.com>
2020-07-22 23:03:40 +0200

    Add support of new scoped listener callbacks to the Koin extension Also fixed a problem where startKoin was called on every beforeTest instead of just once per test case

--- kotest-extensions/kotest-extensions-koin/src/jvmMain/kotlin/io/kotest/koin/KoinListener.kt
+++ kotest-extensions/kotest-extensions-koin/src/jvmMain/kotlin/io/kotest/koin/KoinListener.kt
@@ -3,6 +3,8 @@ package io.kotest.koin
 import io.kotest.core.listeners.TestListener
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
+import io.kotest.core.test.TestType
+import org.koin.core.context.KoinContextHandler
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.core.module.Module
@@ -17,13 +19,41 @@ class KoinListener(
    constructor(module: Module, mockProvider: Provider<*>? = null) : this(listOf(module), mockProvider)
 
    override suspend fun beforeTest(testCase: TestCase) {
-      startKoin {
-         if(mockProvider != null) MockProvider.register(mockProvider)
-         modules(modules)
-      }
+      startKoinIfNotStarted()
    }
 
    override suspend fun afterTest(testCase: TestCase, result: TestResult) {
       stopKoin()
+   }
+
+   override suspend fun beforeContainer(testCase: TestCase) {
+      if (testCase.type == TestType.Container) startKoinIfNotStarted()
+   }
+
+   override suspend fun afterContainer(testCase: TestCase, result: TestResult) {
+      if (testCase.type == TestType.Container) stopKoin()
+   }
+
+   override suspend fun beforeEach(testCase: TestCase) {
+      if (testCase.type == TestType.Test) startKoinIfNotStarted()
+   }
+
+   override suspend fun afterEach(testCase: TestCase, result: TestResult) {
+      if (testCase.type == TestType.Test) stopKoin()
+   }
+
+   override suspend fun beforeAny(testCase: TestCase) {
+      startKoinIfNotStarted()
+   }
+
+   override suspend fun afterAny(testCase: TestCase, result: TestResult) {
+      stopKoin()
+   }
+
+   private fun startKoinIfNotStarted() {
+      KoinContextHandler.getOrNull() ?: startKoin {
+         if(mockProvider != null) MockProvider.register(mockProvider)
+         modules(modules)
+      }
    }
 }
