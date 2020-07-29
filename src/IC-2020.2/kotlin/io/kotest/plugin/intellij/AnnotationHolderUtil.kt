709875757ffaebdb4c7a4e3bd93b424b1f6ca785
sksamuel <sam@sksamuel.com>
2020-07-29 09:17:57 -0500

    Removed unused build roots

--- src/IC-2020.2/kotlin/io/kotest/plugin/intellij/AnnotationHolderUtil.kt
+++
@@ -1,9 +0,0 @@
-package io.kotest.plugin.intellij
-
-import com.intellij.lang.annotation.AnnotationHolder
-import com.intellij.lang.annotation.HighlightSeverity
-import com.intellij.psi.PsiElement
-
-fun AnnotationHolder.createWarnAnnotation(element: PsiElement, msg: String) {
-   newAnnotation(HighlightSeverity.WARNING, msg).range(element).create()
-}
