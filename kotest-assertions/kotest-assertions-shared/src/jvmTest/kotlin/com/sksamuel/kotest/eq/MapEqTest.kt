fad1667a06e7bcd3c7189a23fe062f2a2651df4f
Ashish Kumar <ashishku@thoughtworks.com>
2020-08-08 15:37:29 +0530

    Adds a test for complex map equality check

--- kotest-assertions/kotest-assertions-shared/src/jvmTest/kotlin/com/sksamuel/kotest/eq/MapEqTest.kt
+++ kotest-assertions/kotest-assertions-shared/src/jvmTest/kotlin/com/sksamuel/kotest/eq/MapEqTest.kt
@@ -145,4 +145,37 @@ class MapEqTest : FunSpec({
       }
    }
 
+
+   test("should compare complex structures") {
+      val complexStructure = mapOf(
+         "array" to arrayOf(1, 2),
+         "list" to listOf(1, 2, 3),
+         "bytearray" to byteArrayOf(1, 2, 3, 4),
+         "int" to 2,
+         "string" to "sss",
+         "map" to mapOf(
+            "2" to 2,
+            "list" to listOf("1", 2, "5", null, mapOf("1" to byteArrayOf(1, 2, 3, 5)), setOf(listOf(1, 2, 3))),
+            "bytearray" to byteArrayOf(1, 2, 3, 5)
+         ),
+         "null" to null
+      )
+
+      val complexStructureCopy = mapOf(
+         "array" to arrayOf(1, 2),
+         "list" to listOf(1, 2, 3),
+         "bytearray" to byteArrayOf(1, 2, 3, 4),
+         "int" to 2,
+         "string" to "sss",
+         "map" to mapOf(
+            "2" to 2,
+            "list" to listOf("1", 2, "5", null, mapOf("1" to byteArrayOf(1, 2, 3, 5)), setOf(listOf(1, 2, 3))),
+            "bytearray" to byteArrayOf(1, 2, 3, 5)
+         ),
+         "null" to null
+      )
+
+      complexStructure shouldBe complexStructureCopy
+   }
+
 })
