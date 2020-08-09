fac434df445b898e901dc9d2395914118ead3d0d
Ashish Kumar Joy <34642693+ashishkujoy@users.noreply.github.com>
2020-08-09 08:56:05 +0530

    Adds Iterable eq for checking equality of iterables (#1614)

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/eq/Eq.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/eq/Eq.kt
@@ -1,6 +1,8 @@
 package io.kotest.assertions.eq
 
 import io.kotest.assertions.AssertionsConfig
+import io.kotest.assertions.show.show
+import kotlin.math.exp
 
 /**
  * A [Eq] typeclass compares two values for equality, returning an [AssertionError] if they are
@@ -15,15 +17,35 @@ interface Eq<T> {
 /**
  * Locates the applicable [Eq] for the inputs, and invokes it, returning the error if any.
  */
-fun <T> eq(actual: T, expected: T): Throwable? = when {
-   actual is Map<*, *> && expected is Map<*, *> -> MapEq.equals(actual, expected)
-   actual is Throwable && expected is Throwable -> ThrowableEq.equals(actual, expected)
-   actual is Regex && expected is Regex -> RegexEq.equals(actual, expected)
-   actual is String && expected is String -> StringEq.equals(actual, expected)
-   actual is Number && expected is Number -> NumberEq.equals(actual, expected)
-   shouldShowDataClassDiff(actual, expected) -> DataClassEq.equals(actual as Any, expected as Any)
-   else -> DefaultEq.equals(actual as Any, expected as Any)
+fun <T: Any?> eq(actual: T, expected: T): Throwable? {
+   // if we have null and non null, usually that's a failure, but people can override equals to allow it
+   return when {
+      actual === expected -> null
+      actual == null && expected == null -> null
+      actual == null && expected != null && actual != expected -> actualIsNull(expected)
+      actual != null && expected == null && actual != expected -> expectedIsNull(actual)
+      actual !=null && expected != null -> when {
+         actual is Map<*, *> && expected is Map<*, *> -> MapEq.equals(actual, expected)
+         actual is Throwable && expected is Throwable -> ThrowableEq.equals(actual, expected)
+         actual is Regex && expected is Regex -> RegexEq.equals(actual, expected)
+         actual is String && expected is String -> StringEq.equals(actual, expected)
+         actual is Number && expected is Number -> NumberEq.equals(actual, expected)
+         actual is Iterable<*> && expected is Iterable<*> -> IterableEq.equals(actual, expected)
+         actual is Array<*> && expected is Array<*> -> IterableEq.equals(actual.toList(), expected.toList())
+         shouldShowDataClassDiff(actual, expected) -> DataClassEq.equals(actual as Any, expected as Any)
+         else -> DefaultEq.equals(actual as Any, expected as Any)
+      }
+      else -> null
+   }
 }
 
 private fun <T> shouldShowDataClassDiff(actual: T, expected: T) =
    AssertionsConfig.showDataClassDiff && isDataClassInstance(actual) && isDataClassInstance(expected)
+
+fun actualIsNull(expected: Any): AssertionError {
+   return AssertionError("Expected ${expected.show().value} but actual was null")
+}
+
+fun expectedIsNull(actual: Any): AssertionError {
+   return AssertionError("Expected null but actual was ${actual.show().value}")
+}
---
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/eq/IterableEq.kt
@@ -0,0 +1,48 @@
+package io.kotest.assertions.eq
+
+import io.kotest.assertions.Actual
+import io.kotest.assertions.Expected
+import io.kotest.assertions.failure
+import io.kotest.assertions.show.show
+
+object IterableEq : Eq<Iterable<*>> {
+   override fun equals(actual: Iterable<*>, expected: Iterable<*>): Throwable? {
+      return when {
+         actual is Set<*> && expected is Set<*> -> checkSetEquality(actual, expected)
+         else -> checkIterableEquality(actual, expected)
+      }
+   }
+
+   private fun checkSetEquality(actual: Set<*>, expected: Set<*>): Throwable? {
+      return if (actual.size != expected.size || !equalsIgnoringOrder(actual, expected)) {
+         generateError(actual, expected)
+      } else null
+   }
+
+   private fun equalsIgnoringOrder(actual: Set<*>, expected: Set<*>): Boolean {
+      return actual.all { elementInActualSet ->
+         expected.any { elementInExpectedSet ->
+            eq(elementInActualSet, elementInExpectedSet) == null
+         }
+      }
+   }
+
+   private fun checkIterableEquality(actual: Iterable<*>, expected: Iterable<*>): Throwable? {
+      val actualAsList = actual.toList()
+      val expectedAsList = expected.toList()
+
+      if(actualAsList.size != expectedAsList.size) {
+         return generateError(actual, expected)
+      }
+
+      val anyUnequalElement = actualAsList.zip(expectedAsList).any {
+         eq(it.first, it.second) != null
+      }
+
+      return if(anyUnequalElement) {
+         generateError(actual, expected)
+      } else null
+   }
+
+   private fun generateError(actual: Any, expected: Any) = failure(Expected(expected.show()), Actual(actual.show()))
+}
