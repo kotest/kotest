10ddc94b909e04fe0b47dad9364fe0737f323d04
sksamuel <sam@sksamuel.com>
2020-08-22 13:14:07 -0500

    Updated taycan reporter

--- kotest-framework/kotest-framework-launcher/src/jvmMain/kotlin/io/kotest/framework/console/TaycanReporter.kt
+++ kotest-framework/kotest-framework-launcher/src/jvmMain/kotlin/io/kotest/framework/console/TaycanReporter.kt
@@ -7,6 +7,7 @@ import io.kotest.core.test.Description
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
 import io.kotest.core.test.TestStatus
+import io.kotest.core.test.TestType
 import kotlin.reflect.KClass
 
 /**
@@ -14,22 +15,27 @@ import kotlin.reflect.KClass
  */
 class TaycanReporter(private val term: TermColors) : ConsoleWriter {
 
+   private val isWindows = System.getProperty("os.name").contains("win")
+
    private var start = System.currentTimeMillis()
-   private var testsFailed = 0
+   private var testsFailed = emptyList<Pair<TestCase, TestResult>>()
    private var testsIgnored = 0
    private var testsPassed = 0
    private var specsFailed = emptyList<Description>()
    private var specsPassed = 0
+   private var specCount = 0
 
    private fun green(str: String) = term.green(str)
    private fun greenBold(str: String) = term.green.plus(term.bold).invoke(str)
    private fun red(str: String) = term.red(str)
+   private fun brightRed(str: String) = term.brightRed(str)
+   private fun brightRedBold(str: String) = term.brightRed.plus(term.bold).invoke(str)
    private fun redBold(str: String) = term.red.plus(term.bold).invoke(str)
    private fun yellow(str: String) = term.yellow(str)
    private fun yellowBold(str: String) = term.yellow.plus(term.bold).invoke(str)
    private fun black(str: String) = term.black(str)
    private fun white(str: String) = term.white(str)
-   private fun whiteBold(str: String) = term.white.plus(term.bold).invoke(str)
+   private fun bold(str: String) = term.bold(str)
    private fun blackOnGreen(str: String) = term.black.on(term.green).invoke(str)
    private fun blackOnRed(str: String) = term.black.on(term.red).invoke(str)
    private fun blackOnBrightRed(str: String) = term.black.on(term.brightRed).invoke(str)
@@ -42,31 +48,41 @@ class TaycanReporter(private val term: TermColors) : ConsoleWriter {
       "Preparing to sacrifice your code to the gods of test",
       "Initializing all Kotest subsystems",
       "Battle commanders are ready to declare war on bugs",
-      "Be afraid. Be very afraid - of failing tests",
-      "Lock test-foils in attack position"
+      "Be afraid - be very afraid - of failing tests",
+      "Lock test-foils in attack position",
+      "I test code and chew bubblegum, and I'm all out of bubblegum"
    )
 
    override fun engineStarted(classes: List<KClass<out Spec>>) {
-      print(whiteBold(">> "))
-      println(whiteBold(intros.shuffled().first()))
-      println(whiteBold("${classes.size} specs will be executed"))
+      print(bold(">> "))
+      println(bold(intros.shuffled().first()))
+      print(greenBold(classes.size.toString()))
+      println(white(" specs will be executed"))
       println()
    }
 
-   override fun hasErrors(): Boolean {
-      return testsFailed > 0
-   }
+   override fun hasErrors(): Boolean = testsFailed.isNotEmpty()
 
    override fun engineFinished(t: List<Throwable>) {
       val duration = System.currentTimeMillis() - start
       val seconds = duration / 1000
 
-      if (testsFailed == 0) {
-         println(whiteBold(">> All tests passed"))
+      if (testsFailed.isEmpty()) {
+         println(bold(">> All tests passed"))
       } else {
          println(redBold(">> There were test failures"))
-         specsFailed.distinct().forEach {
-            println(red("- ${it.displayName()}"))
+         println()
+         specsFailed.distinct().forEach { spec ->
+            println(brightRedBold("   ${spec.displayName()}"))
+            testsFailed.filter { it.first.description.spec() == spec }.forEach { (testCase, result) ->
+               println(brightRed("   - ${testCase.description.testDisplayPath().value}"))
+               if (result.error != null) {
+                  println()
+                  println(brightRed(result.error.toString()).lines()
+                     .joinToString(System.lineSeparator()) { "     $it" })
+               }
+               println()
+            }
          }
       }
 
@@ -74,46 +90,48 @@ class TaycanReporter(private val term: TermColors) : ConsoleWriter {
       printSpecCounts()
       printTestsCounts()
       print(white("Time:    "))
-      println(whiteBold("${seconds}s"))
+      println(bold("${seconds}s"))
    }
 
    private fun printSpecCounts() {
-      print(white("Specs:   "))
+      print("Specs:   ")
       print(greenBold("$specsPassed passed"))
-      print(white(", "))
+      print(", ")
       if (specsFailed.isEmpty()) {
-         print(whiteBold("${specsFailed.size} failed"))
-         print(whiteBold(", "))
+         print(bold("${specsFailed.size} failed"))
+         print(bold(", "))
       } else {
          print(redBold("${specsFailed.size} failed"))
-         print(whiteBold(", "))
+         print(bold(", "))
       }
-      println(white("${testsPassed + testsFailed} total"))
+      println("${specsFailed.size + specsPassed} total")
    }
 
    private fun printTestsCounts() {
-      print(white("Tests:   "))
+      print("Tests:   ")
       print(greenBold("$testsPassed passed"))
-      print(white(", "))
-      if (testsFailed > 0) {
-         print(redBold("$testsFailed failed"))
-         print(white(", "))
+      print(", ")
+      if (testsFailed.isEmpty()) {
+         print(bold("${testsFailed.size} failed"))
+         print(", ")
       } else {
-         print(whiteBold("$testsFailed failed"))
-         print(white(", "))
+         print(redBold("${testsFailed.size} failed"))
+         print(", ")
       }
       if (testsIgnored > 0) {
          print(yellowBold("$testsIgnored ignored"))
-         print(white(", "))
+         print(", ")
       } else {
-         print(whiteBold("$testsIgnored ignored"))
-         print(white(", "))
+         print(bold("$testsIgnored ignored"))
+         print(", ")
       }
-      println(white("${testsPassed + testsFailed} total"))
+      println("${testsPassed + testsFailed.size + testsIgnored} total")
    }
 
    override fun specStarted(kclass: KClass<out Spec>) {
-      println(white(kclass.toDescription().displayName()))
+      specCount++
+      print(bold("$specCount. ".padEnd(4, ' ')))
+      println(bold(kclass.toDescription().displayName()))
    }
 
    override fun specFinished(kclass: KClass<out Spec>, t: Throwable?, results: Map<TestCase, TestResult>) {
@@ -124,24 +142,31 @@ class TaycanReporter(private val term: TermColors) : ConsoleWriter {
    }
 
    override fun testFinished(testCase: TestCase, result: TestResult) {
-      when (result.status) {
-         TestStatus.Success -> testsPassed++
-         TestStatus.Failure, TestStatus.Error -> {
-            testsFailed++
-            specsFailed += testCase.description.spec()
+      if (testCase.type == TestType.Test) {
+         when (result.status) {
+            TestStatus.Success -> testsPassed++
+            TestStatus.Failure, TestStatus.Error -> {
+               testsFailed += Pair(testCase, result)
+               specsFailed += testCase.description.spec()
+            }
+            else -> testsIgnored++
          }
-         else -> testsIgnored++
+         print("".padEnd(testCase.description.depth() * 4, ' '))
+         when (result.status) {
+            TestStatus.Success -> print(green(if (isWindows) "√" else "✔"))
+            TestStatus.Error, TestStatus.Failure -> print(red(if (isWindows) "X" else "✘"))
+            else -> Unit
+         }
+         print(" ")
+         println(testCase.displayName)
       }
-      when (result.status) {
-         TestStatus.Success -> print(blackOnGreen("  OK   "))
-         TestStatus.Error -> print(blackOnBrightRed(" ERROR "))
-         TestStatus.Failure -> print(blackOnBrightRed(" ERROR "))
-         else -> Unit
-      }
-      print("  ")
-      println(testCase.displayName)
    }
 
    override fun testStarted(testCase: TestCase) {
+      // containers get output immediately
+      if (testCase.type == TestType.Container) {
+         print("".padEnd(testCase.description.depth() * 4, ' '))
+         println(testCase.displayName)
+      }
    }
 }
