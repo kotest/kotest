63ddebe0f9afc4c89cdbe6c3aba5f6dae2ff5014
sksamuel <sam@sksamuel.com>
2020-08-26 16:37:34 -0500

    Ensure assertions is 1.3 compatible

--- kotest-assertions/kotest-assertions-shared/build.gradle.kts
+++ kotest-assertions/kotest-assertions-shared/build.gradle.kts
@@ -112,6 +112,11 @@ kotlin {
    }
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+   kotlinOptions.jvmTarget = "1.8"
+   kotlinOptions.apiVersion = "1.3"
+}
+
 tasks.named<Test>("jvmTest") {
    useJUnitPlatform()
    filter {
