4bbbbda54a28a6db67e5a5dfa85c9fd4d5b54ef4
sksamuel <sam@sksamuel.com>
2020-09-01 06:37:54 -0500

    Set snapshot base to 4.3.0

--- buildSrc/src/main/kotlin/Ci.kt
+++ buildSrc/src/main/kotlin/Ci.kt
@@ -2,7 +2,7 @@ object Ci {
 
    // this is the version used for building snapshots
    // .buildnumber-snapshot will be appended
-   private const val snapshotBase = "4.2.0"
+   private const val snapshotBase = "4.3.0"
 
    private val githubBuildNumber = System.getenv("GITHUB_RUN_NUMBER")
 
