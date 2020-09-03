b30d648b5934850592829167d3680fae9f5e7ea5
Sam Sam <sam@sksamuel.com>
2020-09-03 09:55:57 -0500

    Added spring listener lifecycle mode #1643 (#1684)

---
+++ kotest-extensions/kotest-extensions-spring/src/jvmTest/kotlin/io/kotest/spring/SpringRootLifecycleModeTest.kt
@@ -0,0 +1,55 @@
+package io.kotest.spring
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.test.context.TestContext
+import org.springframework.test.context.TestExecutionListener
+import org.springframework.test.context.TestExecutionListeners
+
+@TestExecutionListeners(value = [SpringRootExecutionListener::class])
+class SpringRootLifecycleModeTest : DescribeSpec() {
+   init {
+
+      listener(SpringTestListener(SpringTestLifecycleMode.Root))
+
+      beforeSpec {
+         before shouldBe 0
+      }
+
+      describe("the outer scope should be initialized by the spring listener") {
+         before shouldBe 1
+         it("inner scope should have no effect") {
+            before shouldBe 1
+            after shouldBe 0
+         }
+         after shouldBe 0
+      }
+
+      describe("the outer scope should be initialized by the spring listener part 2") {
+         before shouldBe 2
+         it("inner scope should have no effect") {
+            before shouldBe 2
+            after shouldBe 1
+         }
+         after shouldBe 1
+      }
+
+      afterSpec {
+         after shouldBe 2
+      }
+   }
+}
+
+private var before = 0
+private var after = 0
+
+private class SpringRootExecutionListener : TestExecutionListener {
+
+   override fun beforeTestMethod(testContext: TestContext) {
+      before++
+   }
+
+   override fun afterTestMethod(testContext: TestContext) {
+      after++
+   }
+}
