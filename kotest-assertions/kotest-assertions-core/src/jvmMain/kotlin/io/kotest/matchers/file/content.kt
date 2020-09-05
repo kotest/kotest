16e901ac2b56898592bf6debb67d746264ccb6bc
Sam Sam <sam@sksamuel.com>
2020-09-05 12:05:52 -0500

    Multi-line-string comparison for file contents #823 (#1692)
    * Multi-line-string comparison for file contents #823

---
+++ kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/file/content.kt
@@ -0,0 +1,43 @@
+package io.kotest.matchers.file
+
+import io.kotest.matchers.Matcher
+import io.kotest.matchers.MatcherResult
+import io.kotest.matchers.should
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+
+fun File.shouldHaveSameContentAs(other: File, charset: Charset = Charset.forName("utf8")) =
+   this should object : Matcher<File> {
+      override fun test(value: File): MatcherResult {
+
+         val lines1 = BufferedReader(InputStreamReader(FileInputStream(value), charset))
+         val lines2 = BufferedReader(InputStreamReader(FileInputStream(other), charset))
+
+         var passed = true
+         var index = 0
+         var a: String? = ""
+         var b: String? = ""
+         while (passed && a != null && b != null) {
+            a = lines1.readLine()
+            b = lines2.readLine()
+            passed = a == b
+            index++
+         }
+
+         val diff = when {
+            a == null -> "File $other has additional lines, starting at line $index: $b"
+            b == null -> "File $value has additional lines, starting at line $index: $a"
+            else -> "Instead they differ at line $index:\n+ $a\n- $b"
+         }
+
+         return MatcherResult(
+            passed,
+            "Files $value and $other should have the same content.\n$diff",
+            "Files $value and $other should not have the same content"
+         )
+      }
+   }
+
