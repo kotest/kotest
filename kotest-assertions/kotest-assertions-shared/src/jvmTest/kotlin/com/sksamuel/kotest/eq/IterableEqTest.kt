6bbbfec0cf69089d3d5d9ec0aea848eb055dca90
sksamuel <sam@sksamuel.com>
2020-09-09 17:05:18 -0500

    IterableEq should maintain performance while supporting nested arrays #1707

--- kotest-assertions/kotest-assertions-shared/src/jvmTest/kotlin/com/sksamuel/kotest/eq/IterableEqTest.kt
+++ kotest-assertions/kotest-assertions-shared/src/jvmTest/kotlin/com/sksamuel/kotest/eq/IterableEqTest.kt
@@ -6,8 +6,12 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import kotlin.time.ExperimentalTime
+import kotlin.time.seconds
 
+@OptIn(ExperimentalTime::class)
 class IterableEqTest : FunSpec({
+
    test("should give null for two equal sets") {
       IterableEq.equals(setOf(1, 2, 3), setOf(2, 3, 1)).shouldBeNull()
    }
@@ -30,26 +34,25 @@ class IterableEqTest : FunSpec({
 
       assertSoftly {
          error.shouldNotBeNull()
-         error.message shouldBe "expected:<[1, 2, 3]> but was:<[3]>"
+         error.message shouldBe "Elements differ at index 0: expected:<[1, 2, 3]> but was:<[3]>"
       }
    }
 
    test("should give null for equal deeply nested arrays") {
-      val array1 = arrayListOf(1, arrayOf(arrayListOf("a", "c"), "b"), mapOf("a" to arrayListOf(1 ,2, 3)))
-      val array2 = arrayListOf(1, arrayOf(arrayListOf("a", "c"), "b"), mapOf("a" to arrayListOf(1 ,2, 3)))
-
+      val array1 = arrayOf(1, arrayOf(arrayOf("a", "c"), "b"), mapOf("a" to arrayOf(1, 2, 3)))
+      val array2 = arrayOf(1, arrayOf(arrayOf("a", "c"), "b"), mapOf("a" to arrayOf(1, 2, 3)))
       IterableEq.equals(array1.toList(), array2.toList()).shouldBeNull()
    }
 
    test("should give error for unequal deeply nested arrays") {
-      val array1 = arrayListOf(1, arrayOf(arrayOf("a", "c"), "b"), mapOf("a" to arrayListOf(1 ,2, 3)))
-      val array2 = arrayListOf(1, arrayOf(arrayOf("a", "e"), "b"), mapOf("a" to arrayListOf(1 ,2, 3)))
+      val array1 = arrayOf(1, arrayOf(arrayOf("a", "c"), "b"), mapOf("a" to arrayOf(1, 2, 3)))
+      val array2 = arrayOf(1, arrayOf(arrayOf("a", "e"), "b"), mapOf("a" to arrayOf(1, 2, 3)))
 
       val error = IterableEq.equals(array1.toList(), array2.toList())
 
       assertSoftly {
          error.shouldNotBeNull()
-         error.message shouldBe """expected:<[1, [["a", "e"], "b"], [("a", [1, 2, 3])]]> but was:<[1, [["a", "c"], "b"], [("a", [1, 2, 3])]]>"""
+         error.message shouldBe """Elements differ at index 1: expected:<[1, [["a", "e"], "b"], [("a", [1, 2, 3])]]> but was:<[1, [["a", "c"], "b"], [("a", [1, 2, 3])]]>"""
       }
    }
 
@@ -68,7 +71,32 @@ class IterableEqTest : FunSpec({
    }
 
    test("should return true for deeply nested arrays in sets") {
-      setOf(arrayListOf(1, 2, 3), 1, listOf(arrayListOf(1, 2, 3))) shouldBe setOf(arrayListOf(1, 2, 3), 1, listOf(arrayListOf(1, 2, 3)))
+      setOf(
+         arrayOf(1, 2, 3),
+         1,
+         listOf(arrayOf(1, 2, 3))
+      ) shouldBe setOf(
+         arrayOf(1, 2, 3),
+         1,
+         listOf(arrayOf(1, 2, 3))
+      )
+   }
+
+   test("should have linear performance for lists").config(timeout = 5.seconds) {
+      val a = List(10000000) { "foo" }
+      val b = List(10000000) { "foo" }
+      IterableEq.equals(a, b) shouldBe null
+   }
+
+   test("should have linear performance for primitive sets").config(timeout = 5.seconds) {
+      IterableEq.equals(List(1000) { it }.toSet(), List(1000) { it }.reversed().toSet()) shouldBe null
+   }
+
+   test("should have linear performance for string sets").config(timeout = 5.seconds) {
+      IterableEq.equals(
+         List(1000) { it.toString() }.toSet(),
+         List(1000) { it.toString() }.reversed().toSet()
+      ) shouldBe null
    }
 })
 
--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/test/Description.kt
+++ kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/test/Description.kt
@@ -134,7 +134,11 @@ sealed class Description {
    fun id(): TestId = id
 
    @JsName("id_val")
-   val id = TestId(chain().joinToString("/") { it.displayName().replace(" ", "_").replace(idRegex, "") })
+   val id: TestId by lazy {
+      TestId(chain().joinToString("/") {
+         it.displayName().replace(" ", "_").replace(idRegex, "")
+      })
+   }
 
    /**
     * Returns true if this description is the immediate parent of the given argument.
