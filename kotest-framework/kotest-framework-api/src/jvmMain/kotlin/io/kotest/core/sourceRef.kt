58ecb091c3165e502dc74522bdedbb5a50c2eea8
sksamuel <sam@sksamuel.com>
2020-09-12 13:39:29 -0500

    Added test for source ref

--- kotest-framework/kotest-framework-api/src/jvmMain/kotlin/io/kotest/core/sourceRef.kt
+++ kotest-framework/kotest-framework-api/src/jvmMain/kotlin/io/kotest/core/sourceRef.kt
@@ -4,8 +4,8 @@ package io.kotest.core
  * On the JVM we can create a stack trace to get the line number.
  */
 actual fun sourceRef(): SourceRef {
-   val stack = Throwable().stackTrace
+   val stack = Thread.currentThread().stackTrace
    return stack.dropWhile {
-      it.className.startsWith("io.kotest")
-   }[0].run { SourceRef(lineNumber, fileName) }
+      it.className.startsWith("io.kotest") || it.className.startsWith("java.lang")
+   }.first().let { SourceRef(it.lineNumber, it.fileName ?: "unknown") }
 }
