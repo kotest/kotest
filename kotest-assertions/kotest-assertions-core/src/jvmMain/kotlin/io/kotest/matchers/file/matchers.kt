94773cc09c843ca84a944f1fa82488b7608510a8
sksamuel <sam@sksamuel.com>
2020-09-12 08:27:37 -0500

    Added File/Path.shouldHaveNameWithoutExtension assertions #1696

--- kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/file/matchers.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/file/matchers.kt
@@ -44,24 +44,6 @@ fun exist() = object : Matcher<File> {
   override fun test(value: File) = MatcherResult(value.exists(), "File $value should exist", "File $value should not exist")
 }
 
-fun File.shouldHaveExtension(vararg exts: String) = this should haveExtension(*exts)
-fun File.shouldNotHaveExtension(vararg exts: String) = this shouldNot haveExtension(*exts)
-fun haveExtension(vararg exts: String) = object : Matcher<File> {
-  override fun test(value: File) = MatcherResult(exts.any { value.name.endsWith(it) }, "File $value should end with one of ${exts.joinToString(",")}", "File $value should not end with one of ${exts.joinToString(",")}")
-}
-
-infix fun File.shouldHavePath(name: String) = this should havePath(name)
-infix fun File.shouldNotHavePath(name: String) = this shouldNot havePath(name)
-fun havePath(name: String) = object : Matcher<File> {
-  override fun test(value: File) = MatcherResult(value.path == name, "File $value should have path $name", "File $value should not have path $name")
-}
-
-infix fun File.shouldHaveName(name: String) = this should haveName(name)
-infix fun File.shouldNotHaveName(name: String) = this shouldNot haveName(name)
-fun haveName(name: String) = object : Matcher<File> {
-  override fun test(value: File) = MatcherResult(value.name == name, "File $value should have name $name", "File $value should not have name $name")
-}
-
 infix fun File.shouldContainFile(name: String) = this should containFile(name)
 infix fun File.shouldNotContainFile(name: String) = this shouldNot containFile(name)
 fun containFile(name: String) = object : Matcher<File> {
---
+++ kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/paths/names.kt
@@ -0,0 +1,32 @@
+package io.kotest.matchers.paths
+
+import io.kotest.matchers.Matcher
+import io.kotest.matchers.MatcherResult
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldNot
+import java.nio.file.Path
+
+fun Path.shouldHaveExtension(vararg exts: String) = this should haveExtension(*exts)
+fun Path.shouldNotHaveExtension(vararg exts: String) = this shouldNot haveExtension(*exts)
+fun haveExtension(vararg exts: String) = object : Matcher<Path> {
+   override fun test(value: Path) = MatcherResult(
+      exts.any { value.fileName.toString().endsWith(it) },
+      "Path $value should end with one of ${exts.joinToString(",")}",
+      "Path $value should not end with one of ${exts.joinToString(",")}"
+   )
+}
+
+
+infix fun Path.shouldHaveNameWithoutExtension(name: String) = this should haveNameWithoutExtension(name)
+infix fun Path.shouldNotHaveNameWithoutExtension(name: String) = this shouldNot haveNameWithoutExtension(name)
+fun haveNameWithoutExtension(name: String) = object : Matcher<Path> {
+   override fun test(value: Path): MatcherResult {
+      val filename = value.fileName.toString()
+      val actual = if (filename.contains(".")) filename.split('.').dropLast(1).joinToString(".") else filename
+      return MatcherResult(
+         actual == name,
+         "Path $value should have name without extension of $name but was $actual",
+         "Path $value should not have name without extension of $name"
+      )
+   }
+}
