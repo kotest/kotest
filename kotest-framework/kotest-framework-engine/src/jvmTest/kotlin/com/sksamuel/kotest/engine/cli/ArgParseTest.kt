ed8f6b8ccf02b80ad9aaba6869baec5960e29b0b
sksamuel <sam@sksamuel.com>
2020-10-04 12:48:01 -0500

    Removed clickt as a dependency for engine #1748

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/cli/ArgParseTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/cli/ArgParseTest.kt
@@ -1,7 +1,6 @@
 package com.sksamuel.kotest.engine.cli
 
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.engine.cli.Arg
 import io.kotest.engine.cli.parseArgs
 import io.kotest.matchers.shouldBe
 
@@ -9,47 +8,47 @@ class ArgParseTest : FunSpec() {
    init {
       test("parsing args happy path") {
          val args = listOf("--reporter", "taycan", "--package", "com.foo.bar.baz", "--spec", "FooBarTest")
-         parseArgs(args) shouldBe listOf(
-            Arg("reporter", "taycan"),
-            Arg("package", "com.foo.bar.baz"),
-            Arg("spec", "FooBarTest"),
+         parseArgs(args) shouldBe mapOf(
+            "reporter" to "taycan",
+            "package" to "com.foo.bar.baz",
+            "spec" to "FooBarTest",
          )
       }
 
       test("parsing args with spaces in a value mid stream") {
          val args = listOf("--reporter", "taycan", "--testname", "my test should be great", "--spec", "FooBarTest")
          parseArgs(args) shouldBe listOf(
-            Arg("reporter", "taycan"),
-            Arg("testname", "my test should be great"),
-            Arg("spec", "FooBarTest"),
+            "reporter" to "taycan",
+            "testname" to "my test should be great",
+            "spec" to "FooBarTest",
          )
       }
 
       test("parsing args with spaces in the final element") {
          val args = listOf("--reporter", "taycan", "--spec", "FooBarTest", "--testname", "my test should be great")
          parseArgs(args) shouldBe listOf(
-            Arg("reporter", "taycan"),
-            Arg("spec", "FooBarTest"),
-            Arg("testname", "my test should be great"),
+            "reporter" to "taycan",
+            "spec" to "FooBarTest",
+            "testname" to "my test should be great",
          )
       }
 
       test("parsing args with non alpha") {
          val args = listOf("--reporter", "taycan", "--spec", "FooBarTest", "--tags", "(Linux || Windows) && Hive")
          parseArgs(args) shouldBe listOf(
-            Arg("reporter", "taycan"),
-            Arg("spec", "FooBarTest"),
-            Arg("tags", "(Linux || Windows) && Hive"),
+            "reporter" to "taycan",
+            "spec" to "FooBarTest",
+            "tags" to "(Linux || Windows) && Hive",
          )
       }
 
       test("parsing args with empty value") {
          val args = listOf("--reporter", "taycan", "--spec", "FooBarTest", "--tags", "--testname", "wibble test")
          parseArgs(args) shouldBe listOf(
-            Arg("reporter", "taycan"),
-            Arg("spec", "FooBarTest"),
-            Arg("tags", ""),
-            Arg("testname", "wibble test"),
+            "reporter" to "taycan",
+            "spec" to "FooBarTest",
+            "tags" to "",
+            "testname" to "wibble test",
          )
       }
    }
