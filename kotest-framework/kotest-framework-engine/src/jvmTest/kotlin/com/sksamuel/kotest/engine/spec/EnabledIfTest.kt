b3500d67c9632996880c410162486d8b6f89a03a
sksamuel <sam@sksamuel.com>
2020-10-11 14:46:49 -0500

    Added test for enabled if

---
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/spec/EnabledIfTest.kt
@@ -0,0 +1,19 @@
+package com.sksamuel.kotest.engine.spec
+
+import io.kotest.core.annotation.EnabledCondition
+import io.kotest.core.annotation.EnabledIf
+import io.kotest.core.spec.Spec
+import io.kotest.core.spec.style.FunSpec
+import kotlin.reflect.KClass
+
+class NeverEnabled : EnabledCondition {
+   override fun enabled(specKlass: KClass<out Spec>): Boolean = false
+}
+
+@EnabledIf(NeverEnabled::class)
+class EnabledIfTest : FunSpec() {
+   init {
+      // this spec should not be created
+      error("boom")
+   }
+}
