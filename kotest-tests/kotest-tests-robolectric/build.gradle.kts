9075923c886276c2e1d3c06483ca8c01f70f9f1c
Alex Facciorusso <alexfacciorusso93@gmail.com>
2020-10-13 13:44:14 +0100

    Fixed Robolectric test app (#1765)
    * Fixed Robolectric test app
    
    * Fixed Robolectric test

--- kotest-tests/kotest-tests-robolectric/build.gradle.kts
+++ kotest-tests/kotest-tests-robolectric/build.gradle.kts
@@ -3,81 +3,110 @@ import org.jetbrains.kotlin.config.KotlinCompilerVersion
 plugins {
    id("com.android.application")
    kotlin("android")
-   kotlin("android.extensions")
+   kotlin("kapt")
 }
 
 android {
    compileSdkVersion(30)
 
-   compileOptions {
-      sourceCompatibility = JavaVersion.VERSION_1_8
-      targetCompatibility = JavaVersion.VERSION_1_8
+   testOptions {
+      unitTests {
+         isReturnDefaultValues = true
+         isIncludeAndroidResources = true
+      }
    }
 
-   kotlinOptions {
-      jvmTarget = JavaVersion.VERSION_1_8.toString()
-   }
-
-   lintOptions {
-      tasks.lint.get().enabled = false
+   buildFeatures {
+      viewBinding = true
    }
 
    defaultConfig {
-      applicationId = "io.kotest.androidtests"
+      applicationId = "com.sksamuel.robolectric"
       minSdkVersion(21)
       targetSdkVersion(30)
+      versionCode = 1
+      versionName = "1.0"
 
       testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
    }
 
-   testOptions {
-      unitTests.isIncludeAndroidResources = true
-      unitTests.isReturnDefaultValues = true
-   }
+   compileOptions {
+      // Flag to enable support for the new language APIs
+      isCoreLibraryDesugaringEnabled = true
+      sourceCompatibility = JavaVersion.VERSION_1_8
+      targetCompatibility = JavaVersion.VERSION_1_8
 
-   packagingOptions {
-      exclude("META-INF/LICENSE.md")
-      exclude("META-INF/LICENSE-notice.md")
+   }
+   kotlinOptions {
+      jvmTarget = "1.8"
+      freeCompilerArgs = freeCompilerArgs + listOf(
+         "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi"
+      )
    }
 
    useLibrary("android.test.runner")
    useLibrary("android.test.base")
    useLibrary("android.test.mock")
 
+   packagingOptions {
+      exclude("META-INF/LICENSE.md")
+      exclude("META-INF/LICENSE-notice.md")
+   }
 }
 
 dependencies {
-   // Kotlin
-   implementation(kotlin("stdlib-jdk8", KotlinCompilerVersion.VERSION))
-   implementation(kotlin("reflect", KotlinCompilerVersion.VERSION))
-   implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
+   implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
+   implementation(kotlin("script-runtime", KotlinCompilerVersion.VERSION))
+   implementation(AndroidLibs.KotlinX.coroutinesAndroid)
 
-   // Android
-   implementation("androidx.core:core-ktx:1.3.2")
+   implementation(AndroidLibs.AndroidX.coreKtx)
+   implementation(AndroidLibs.AndroidX.appCompat)
+   implementation(AndroidLibs.AndroidX.constraintLayout)
+   implementation(AndroidLibs.AndroidX.ArchCore.common)
+   implementation(AndroidLibs.AndroidX.Fragment.fragmentKtx)
+   implementation(AndroidLibs.AndroidX.Navigation.runtimeKtx)
+   implementation(AndroidLibs.AndroidX.Navigation.fragmentKtx)
+   implementation(AndroidLibs.AndroidX.Navigation.uiKtx)
+   implementation(AndroidLibs.AndroidX.Lifecycle.runtimeKtx)
+   implementation(AndroidLibs.AndroidX.Lifecycle.liveDataKtx)
+   implementation(AndroidLibs.AndroidX.Lifecycle.viewModelKtx)
 
-   // Test
-   testImplementation(project(Projects.Engine))
-   testImplementation(project(Projects.AssertionsShared))
+   coreLibraryDesugaring(AndroidLibs.desugarJdk)
+
+   testDependencies()
+}
+
+tasks {
+   withType<Test> {
+      useJUnitPlatform()
+   }
+}
+
+fun DependencyHandlerScope.testDependencies() {
+   testImplementation(kotlin("test", KotlinCompilerVersion.VERSION))
+   testImplementation(kotlin("reflect", KotlinCompilerVersion.VERSION))
+   testImplementation(Libs.Coroutines.test)
+   testImplementation(AndroidLibs.liveDataTestingKtx)
+   testImplementation(AndroidLibs.AndroidX.ArchCore.testing)
+   testImplementation(AndroidLibs.AndroidX.Testing.extJUnit)
+   testImplementation(AndroidLibs.AndroidX.Espresso.core)
+
    testImplementation(project(Projects.JunitRunner))
-   testImplementation(project(Projects.extension("robolectric")))
-   testImplementation(Libs.Robolectric.robolectric)
+   testImplementation(project(Projects.AssertionsCore))
+   testImplementation(project(Projects.Property))
 
-   testImplementation("androidx.test:core:1.3.0")
-   testImplementation("androidx.test:runner:1.3.0")
-   testImplementation("androidx.test:rules:1.3.0")
-   testImplementation("androidx.test.ext:junit:1.1.2")
-   testImplementation("androidx.test.espresso:espresso-core:3.3.0")
+   testImplementation(AndroidLibs.AndroidX.Testing.runner)
+   testImplementation(AndroidLibs.AndroidX.Testing.rules)
+   testImplementation(project(Projects.extension("robolectric")))
+   testImplementation(AndroidLibs.robolectric)
+
+   androidTestImplementation(AndroidLibs.AndroidX.Espresso.core)
+   debugImplementation(AndroidLibs.AndroidX.Fragment.testing)
 }
 
 // Workaround for Robolectric https://github.com/robolectric/robolectric/issues/5092
 configurations.all {
    resolutionStrategy {
       force("androidx.test:monitor:1.3.0")
-   }
-}
-
-tasks {
-   withType<Test> {
-      useJUnitPlatform()
    }
 }
--- kotest-tests/kotest-tests-robolectric/src/main/java/com/sksamuel/robolectric/MainActivity.kt
+++ kotest-tests/kotest-tests-robolectric/src/main/java/com/sksamuel/robolectric/MainActivity.kt
@@ -1,10 +1,9 @@
-package io.kotest.androidtest
+package com.sksamuel.robolectric
 
 import android.app.Activity
 import android.os.Bundle
 
 class MainActivity : Activity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
