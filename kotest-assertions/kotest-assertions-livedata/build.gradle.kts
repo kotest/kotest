2431b40dd3b9156506b69804a816deec536ac468
Alex Facciorusso <alex.facciorusso@tsb.co.uk>
2020-10-17 00:57:07 +0100

    WIP: Created files for LiveData assertions

---
+++ kotest-assertions/kotest-assertions-livedata/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+   id("java")
+   kotlin("multiplatform")
+   id("java-library")
+   id("com.adarshr.test-logger")
+}
+
+repositories {
+   jcenter()
+}
+
+kotlin {
+   sourceSets {
+
+      targets {
+         jvm {
+            compilations.all {
+               kotlinOptions {
+                  jvmTarget = "1.8"
+               }
+            }
+         }
+      }
+
+      targets.all {
+         compilations.all {
+            kotlinOptions {
+               freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+            }
+         }
+      }
+
+      val commonMain by getting {
+         dependencies {
+            implementation(project(Projects.AssertionsShared))
+            implementation(AndroidLibs.AndroidX.Lifecycle.liveDataKtx)
+         }
+      }
+
+      val jvmMain by getting {
+         dependsOn(commonMain)
+      }
+
+      val jvmTest by getting {
+         dependsOn(jvmMain)
+         dependencies {
+            implementation(project(Projects.JunitRunner))
+         }
+      }
+   }
+}
+
+tasks.named<Test>("jvmTest") {
+   useJUnitPlatform()
+   filter {
+      isFailOnNoMatchingTests = false
+   }
+   testLogging {
+      showExceptions = true
+      showStandardStreams = true
+      events = setOf(org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+         org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED)
+      exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+   }
+}
+
+apply(from = "../../publish-mpp.gradle.kts")
