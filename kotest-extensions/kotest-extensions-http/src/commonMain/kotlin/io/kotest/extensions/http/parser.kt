6c1ec5e6580e07a4cfc7c4b9c33aaa190a6c17b9
Shane Lathrop <slathrop@shoprunner.com>
2020-10-26 17:28:05 -0500

    Add ability to configure ktor timeout on http executions

--- kotest-extensions/kotest-extensions-http/src/commonMain/kotlin/io/kotest/extensions/http/parser.kt
+++ kotest-extensions/kotest-extensions-http/src/commonMain/kotlin/io/kotest/extensions/http/parser.kt
@@ -1,12 +1,14 @@
 package io.kotest.extensions.http
 
 import io.ktor.client.HttpClient
+import io.ktor.client.features.HttpTimeout
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.header
 import io.ktor.client.request.request
 import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpMethod
+import io.ktor.utils.io.core.use
 
 fun parseHttpRequest(lines: List<String>): HttpRequestBuilder {
    val builder = HttpRequestBuilder()
@@ -31,4 +33,15 @@ fun parseHttpRequest(lines: List<String>): HttpRequestBuilder {
 suspend fun runRequest(req: HttpRequestBuilder): HttpResponse {
    val client = HttpClient()
    return client.request<HttpResponse>(req)
+}
+
+suspend fun runRequest(req: HttpRequestBuilder, timeout: Long): HttpResponse {
+   return HttpClient() {
+      install(HttpTimeout) {
+         requestTimeoutMillis = timeout
+         socketTimeoutMillis = timeout
+      }
+   }.use {
+      it.request<HttpResponse>(req)
+   }
 }
