3aeb1f7f4d6b5dbfc5ea7a280887c5d8809a392f
Joseph Cooper <cooper.joseph@gmail.com>
2020-10-28 14:31:12 +0000

    Comments from review

--- kotest-bom/build.gradle.kts
+++ kotest-bom/build.gradle.kts
@@ -6,25 +6,31 @@ plugins {
 version = Ci.publishVersion
 
 val bomProject = project
+
+// Explicitly exclude subprojects that will never be published so that when configuring this project
+//   we don't force their configuration and do unecessary work
 val excludeFromBom = listOf("kotest-examples", "kotest-tests")
 fun projectsFilter(candidateProject: Project) =
    excludeFromBom.all { !candidateProject.name.contains(it) }
       && candidateProject.name != bomProject.name
 
+// Declare that this subproject depends on all subprojects matching the filter
+// When this subproject is configured, it will force configuration of all subprojects
+// so that we can declare dependencies on them
 rootProject.subprojects.filter(::projectsFilter).forEach { bomProject.evaluationDependsOn(it.path) }
 
 dependencies {
    constraints {
       rootProject.subprojects.filter { project ->
-         project.tasks.findByName("publish")?.enabled == true &&
-            projectsFilter(project)
+         // Only declare dependencies on projects that will have publications
+         projectsFilter(project) && project.tasks.findByName("publish")?.enabled == true
       }.forEach { api(project(it.path)) }
    }
 }
 
 publishing {
    publications {
-      create<MavenPublication>("Bom") {
+      create<MavenPublication>("KotestBom") {
          from(components["javaPlatform"])
       }
    }
