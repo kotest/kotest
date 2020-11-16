90c8e76ba3a6919da5d5678af5eb53d2199b9559
Leo Tietz <leo.tietz@gmail.com>
2020-11-16 01:19:01 +0100

    Revert Kotlin API version to 1.3 (#1844)

--- kotest-assertions/kotest-assertions-shared/build.gradle.kts
+++ kotest-assertions/kotest-assertions-shared/build.gradle.kts
@@ -111,7 +111,7 @@ kotlin {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
    kotlinOptions.jvmTarget = "1.8"
-   kotlinOptions.apiVersion = "1.4"
+   kotlinOptions.apiVersion = "1.3"
 }
 
 tasks.named<Test>("jvmTest") {
