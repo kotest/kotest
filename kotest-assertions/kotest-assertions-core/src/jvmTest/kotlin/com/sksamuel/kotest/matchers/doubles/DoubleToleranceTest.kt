57e9dc0169eaf2432ed169d4a2d2710a03cff28e
Marvin Sielenkemper <43669788+sielenk-yara@users.noreply.github.com>
2020-11-27 21:16:39 +0100

    Fix for "Unexpected ToleranceMatcher behavior [...]" (#1868)
    Added test for ToleranceMatcher, fixed behavior for infinite value and finite tolerance, required tolerance to be finite
    
    Closes #1832

--- kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/doubles/DoubleToleranceTest.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/doubles/DoubleToleranceTest.kt
@@ -4,13 +4,23 @@ import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.doubles.plusOrMinus
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.numericDoubles
+import io.kotest.property.checkAll
 
-class DoubleTolerenceTest : FunSpec({
+class DoubleToleranceTest : FunSpec({
 
    test("double with tolerance should include tolerance in error message") {
       shouldThrowAny {
          1.0 shouldBe (1.5 plusOrMinus 0.4)
       }.message shouldBe "1.0 should be equal to 1.5 within tolerance of 0.4 (lowest acceptable value is 1.1; highest acceptable value is 1.9)"
+   }
+
+   test("infinite double with finite tolerance should equal the same infinite double") {
+      checkAll(Arb.numericDoubles(from = 0.0)) { eps ->
+         Double.NEGATIVE_INFINITY shouldBe (Double.NEGATIVE_INFINITY plusOrMinus eps)
+         Double.POSITIVE_INFINITY shouldBe (Double.POSITIVE_INFINITY plusOrMinus eps)
+      }
    }
 })
 
