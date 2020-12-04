26371ce9f5544ffe8ec7d6d43c7b927cc7fa6b46
Mitchell Yuwono <myuwono@atlassian.com>
2020-12-05 10:24:03 +1100

    Fix generex not cached

--- kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/stringsjvm.kt
+++ kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/stringsjvm.kt
@@ -2,7 +2,9 @@ package io.kotest.property.arbitrary
 
 import com.mifmif.common.regex.Generex
 import io.kotest.property.Arb
-import kotlin.random.asJavaRandom
+import io.kotest.property.RandomSource
+import io.kotest.property.Sample
+import io.kotest.property.sampleOf
 
 /**
  * Generate strings that match the given pattern.
@@ -10,7 +12,14 @@ import kotlin.random.asJavaRandom
  * The returned arb uses the [Generex](https://github.com/mifmif/Generex) library to generate strings. Generex
  * supports a very restricted subset of regular expression constructs.
  */
-fun Arb.Companion.stringPattern(pattern: String) = arbitrary {
-   val generex = Generex(pattern, it.random.asJavaRandom())
-   generex.random()
+fun Arb.Companion.stringPattern(pattern: String): Arb<String> = object : Arb<String>() {
+   override fun edgecases(): List<String> = emptyList()
+   override fun values(rs: RandomSource): Sequence<Sample<String>> = sequence { sampleStringPattern(rs) }
+   override fun sample(rs: RandomSource): Sample<String> = sampleStringPattern(rs)
+
+   private val generex = Generex(pattern)
+   private fun sampleStringPattern(rs: RandomSource): Sample<String> = synchronized(this) {
+      generex.setSeed(rs.random.nextLong())
+      sampleOf(generex.random(), StringShrinker)
+   }
 }
