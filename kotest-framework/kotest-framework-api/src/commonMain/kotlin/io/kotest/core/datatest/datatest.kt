edc430f7e7b2d7079301b0b4bb94265c2c1582d8
sksamuel <sam@sksamuel.com>
2020-12-13 08:07:40 -0600

    Fixed regression on data test api

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/datatest/datatest.kt
+++ kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/datatest/datatest.kt
@@ -4,13 +4,6 @@ import io.kotest.assertions.failure
 import io.kotest.core.spec.style.scopes.ContainerScope
 import io.kotest.core.test.createTestName
 
-@Deprecated("Use ContainerScope.forAll(vararg Pair<String, T>) or ContainerScope.forAll(List<Pair<String, T>>). Will be removed in 4.6")
-suspend fun <T> ContainerScope.forAll(first: Pair<String, T>, vararg rest: Pair<String, T>, test: suspend (T) -> Unit) {
-   (listOf(first) + rest).forEach { (name, t) ->
-      addTest(createTestName(name), false) { test(t) }
-   }
-}
-
 suspend fun <T> ContainerScope.forNone(vararg data: Pair<String, T>, test: suspend (T) -> Unit) {
    data.forEach { (name, t) ->
       addTest(createTestName(name), false) {
@@ -48,15 +41,6 @@ suspend fun <T : Any> ContainerScope.forAll(ts: List<T>, test: suspend (T) -> Un
 suspend fun <T : Any> ContainerScope.forAll(vararg ts: T, test: suspend (T) -> Unit) {
    val idents = Identifiers()
    ts.forEach { t ->
-      val name = idents.stableIdentifier(t)
-      addTest(createTestName(name), false) { test(t) }
-   }
-}
-
-@Deprecated("Use ContainerScope.forAll(vararg) or ContainerScope.forAll(list). Will be removed in 4.6")
-suspend fun <T : Any> ContainerScope.forAll(first: T, vararg rest: T, test: suspend (T) -> Unit) {
-   val idents = Identifiers()
-   (listOf(first) + rest).forEach { t ->
       val name = idents.stableIdentifier(t)
       addTest(createTestName(name), false) { test(t) }
    }
