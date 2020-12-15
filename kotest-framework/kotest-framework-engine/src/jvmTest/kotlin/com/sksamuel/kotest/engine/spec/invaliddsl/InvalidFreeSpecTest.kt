dda44042d1305e840dbb5d9ee819ff1ed5ea6930
sksamuel <sam@sksamuel.com>
2020-12-15 09:27:39 -0600

    Invalid freespec usage should throw at runtime #1882

---
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/spec/invaliddsl/InvalidFreeSpecTest.kt
@@ -0,0 +1,26 @@
+package com.sksamuel.kotest.engine.spec.invaliddsl
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.test.TestResult
+import io.kotest.core.test.TestStatus
+
+class InvalidFreeSpecTest : FreeSpec() {
+   init {
+
+      aroundTest { (testCase, fn) ->
+         val result = fn(testCase)
+         if (result.status == TestStatus.Success && testCase.displayName == "should fail")
+            TestResult.failure(AssertionError("leaf"), 0)
+         else
+            TestResult.success(0)
+      }
+
+      "context" - {
+         "leaf" {
+            "invalid" - {
+
+            }
+         }
+      }
+   }
+}
