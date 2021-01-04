c929e7e9a6c01e4116b138e7353683b277be4c97
sksamuel <sam@sksamuel.com>
2021-01-03 19:43:03 -0600

    Extract notification logic #1960

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/listeners/spec.kt
+++ kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/listeners/spec.kt
@@ -77,9 +77,30 @@ interface FinalizeSpecListener : Listener {
     * @param kclass the [Spec] class
     * @param results a map of each test case mapped to its result.
     */
+   @Deprecated("Use finalizeSpec(KClass, Map<TestCase, TestResult>, Throwable?). This method will be removed in 4.7 and 5.0")
    suspend fun finalizeSpec(
       kclass: KClass<out Spec>,
       results: Map<TestCase, TestResult>
+   ): Unit = finalizeSpec(kclass, results, null)
+
+   /**
+    * Called once per [Spec], after all tests have completed for that spec.
+    *
+    * Regardless of how many times the spec is instantiated,
+    * for example, if [InstancePerTest] or [InstancePerLeaf] isolation
+    * modes are used, this callback will only be invoked once.
+    *
+    * The results parameter contains every [TestCase], along with
+    * the result of that test, including tests that were ignored (which
+    * will have a TestResult that has TestStatus.Ignored).
+    *
+    * @param kclass the [Spec] class
+    * @param results a map of each test case mapped to its result.
+    */
+   suspend fun finalizeSpec(
+      kclass: KClass<out Spec>,
+      results: Map<TestCase, TestResult>,
+      error: Throwable?
    ): Unit = Unit
 }
 
--- kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/spec/SpecExecutor.kt
+++ kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/spec/SpecExecutor.kt
@@ -1,34 +1,26 @@
 package io.kotest.engine.spec
 
-import io.kotest.core.config.Configuration
-import io.kotest.mpp.log
 import io.kotest.core.config.configuration
-import io.kotest.core.config.specInstantiationListeners
-import io.kotest.core.config.testListeners
+import io.kotest.core.extensions.SpecExtension
+import io.kotest.core.extensions.resolvedSpecExtensions
+import io.kotest.core.internal.isActive
+import io.kotest.core.internal.resolvedThreads
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.Spec
+import io.kotest.core.spec.materializeAndOrderRootTests
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
+import io.kotest.engine.NotificationManager
+import io.kotest.engine.launchers.testLauncher
+import io.kotest.engine.listener.TestEngineListener
 import io.kotest.engine.spec.runners.ConcurrentInstancePerLeafSpecRunner
 import io.kotest.engine.spec.runners.InstancePerLeafSpecRunner
 import io.kotest.engine.spec.runners.InstancePerTestSpecRunner
 import io.kotest.engine.spec.runners.SingleInstanceSpecRunner
-import io.kotest.engine.listener.TestEngineListener
-import io.kotest.core.extensions.SpecExtension
-import io.kotest.core.test.TestCase
-import io.kotest.core.test.TestResult
-import io.kotest.core.extensions.resolvedSpecExtensions
-import io.kotest.core.internal.resolvedThreads
-import io.kotest.core.internal.isActive
-import io.kotest.core.internal.resolvedConcurrentTests
-import io.kotest.core.spec.IsolationMode
-import io.kotest.core.spec.Spec
-import io.kotest.core.spec.materializeAndOrderRootTests
-import io.kotest.engine.dispatchers.coroutineDispatcherFactory
-import io.kotest.engine.launchers.ConcurrentTestLauncher
-import io.kotest.engine.launchers.SequentialTestLauncher
-import io.kotest.engine.launchers.TestLauncher
-import io.kotest.engine.launchers.testLauncher
 import io.kotest.fp.Try
 import io.kotest.fp.flatten
 import io.kotest.fp.success
-import kotlin.math.max
+import io.kotest.mpp.log
 import kotlin.reflect.KClass
 
 /**
@@ -40,80 +32,27 @@ import kotlin.reflect.KClass
  */
 class SpecExecutor(private val listener: TestEngineListener) {
 
-   /**
-    * Executes the given [Spec].
-    */
+   private val notifications = NotificationManager(listener)
+
    suspend fun execute(kclass: KClass<out Spec>) {
       log("SpecExecutor execute [$kclass]")
-      notifySpecStarted(kclass)
-         .flatMap { invokePrepareSpecListeners(kclass) }
+      notifications.specStarted(kclass)
          .flatMap { createInstance(kclass) }
          .flatMap { runTestsIfAtLeastOneActive(it) }
-         .flatMap { invokeFinalizeSpecListeners(kclass, it) }
          .fold(
-            { notifySpecFinished(kclass, it, emptyMap()) },
-            { notifySpecFinished(kclass, null, it) }
+            { notifications.specFinished(kclass, it, emptyMap()) },
+            { notifications.specFinished(kclass, null, it) }
          )
    }
 
    /**
-    * Notifies the [TestEngineListener] that we are about to start execution of a [Spec].
-    * This is called only once per spec regardless of the number of instantiation events.
-    */
-   private fun notifySpecStarted(kclass: KClass<out Spec>) = Try {
-      log("Executing engine listener callback:specStarted $kclass")
-      listener.specStarted(kclass)
-   }
-
-   private fun notifySpecInstantiated(spec: Spec) = Try {
-      log("Executing engine listener callback:specInstantiated spec:$spec")
-      val listeners = configuration.specInstantiationListeners()
-      listener.specInstantiated(spec)
-      listeners.forEach {
-         it.specInstantiated(spec)
-      }
-   }
-
-   private fun notifySpecInstantiationError(kclass: KClass<out Spec>, t: Throwable) =
-      Try {
-         val listeners = configuration.specInstantiationListeners()
-         t.printStackTrace()
-         log("Executing engine listener callback:specInstantiationError $kclass error:$t")
-         listener.specInstantiationError(kclass, t)
-         listeners.forEach {
-            it.specInstantiationError(kclass, t)
-         }
-      }
-
-   /**
-    * Notifies the [TestEngineListener] that we have finished the execution of a [Spec].
-    * This is called once per spec regardless of the number of instantiation events.
-    */
-   private fun notifySpecFinished(
-      kclass: KClass<out Spec>,
-      t: Throwable?,
-      results: Map<TestCase, TestResult>
-   ) = Try {
-      t?.printStackTrace()
-      log("Executing engine listener callback:specFinished $kclass $t")
-      listener.specFinished(kclass, t, results)
-   }
-
-   /**
-    * Creates an instance of the supplied [Spec] by delegating to the project constructors,
-    * and notifies the [TestEngineListener] of the instantiation event.
+    * Creates an instance of the supplied [Spec] by delegating to the constructor extensions,
+    * and notifies of the instantiation event.
     */
    private fun createInstance(kclass: KClass<out Spec>): Try<Spec> =
       createAndInitializeSpec(kclass)
-         .fold(
-            {
-               notifySpecInstantiationError(kclass, it)
-               Try.Failure(it)
-            },
-            { spec ->
-               notifySpecInstantiated(spec).map { spec }
-            }
-         )
+         .onFailure { notifications.specInstantiationError(kclass, it) }
+         .flatMap { spec -> notifications.specInstantiated(spec).map { spec } }
 
    /**
     * The root tests on this spec are retrieved, and if none are active, then no
@@ -175,38 +114,5 @@ class SpecExecutor(private val listener: TestEngineListener) {
             else -> ConcurrentInstancePerLeafSpecRunner(listener, threads)
          }
       }
-   }
-
-   /**
-    * Notifies the user listeners that a new [Spec] is starting.
-    * This is only invoked once per spec class, regardless of the number of invocations.
-    * If this errors then no further callbacks or tests will be executed.
-    */
-   private suspend fun invokePrepareSpecListeners(kclass: KClass<out Spec>): Try<Unit> =
-      Try {
-         // prepareSpec can only be registered at the project level
-         // It makes no sense to call prepareSpec after a spec has already been instantiated.
-         val listeners = configuration.testListeners()
-         log("Notifying ${listeners.size} test listeners of callback 'prepareSpec'")
-         listeners.forEach {
-            it.prepareSpec(kclass)
-         }
-         log("'prepareSpec' callbacks complete")
-      }
-
-   /**
-    * Notifies the user listeners that a [Spec] has finished all tests.
-    */
-   private suspend fun invokeFinalizeSpecListeners(
-      kclass: KClass<out Spec>,
-      results: Map<TestCase, TestResult>
-   ): Try<Map<TestCase, TestResult>> = Try {
-      log("Notifying finalizeSpec")
-      // finalize spec's can be registered at the project level or using the dsl
-      // dsl callbacks are just registered at the project level with a spec class check
-      configuration.testListeners().forEach {
-         it.finalizeSpec(kclass, results)
-      }
-      results
    }
 }
