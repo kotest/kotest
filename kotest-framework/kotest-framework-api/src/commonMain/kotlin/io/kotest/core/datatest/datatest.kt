eb12528db4938f5187493c43386af4b9abb68541
Jim Schneidereit <jimschneidereit@gmail.com>
2021-01-04 15:58:30 -0800

    refactor original data driven functions based on root scope functions

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/datatest/datatest.kt
+++ kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/datatest/datatest.kt
@@ -5,17 +5,11 @@ import io.kotest.core.spec.style.scopes.ContainerScope
 import io.kotest.core.test.createTestName
 import kotlin.jvm.*
 
-suspend fun <T> ContainerScope.forNone(vararg data: Pair<String, T>, test: suspend (T) -> Unit) {
-   data.forEach { (name, t) ->
-      addTest(createTestName(name), false) {
-         try {
-            test(t)
-            null
-         } catch (e: AssertionError) {
-            e
-         } ?: throw failure("Test passed for $t but expected failure")
-      }
-   }
+suspend fun <T> ContainerScope.forNone(vararg data: Pair<String, T>, test: suspend (T) -> Unit) = forNone(data.toList(), test)
+
+suspend fun <T : Any> ContainerScope.forNone(vararg data: T, test: suspend (T) -> Unit) {
+   val identifiers = Identifiers()
+   forNone(data.map { Pair(identifiers.stableIdentifier(it), it) }, test)
 }
 
 suspend fun <T> ContainerScope.forNone(data: List<Pair<String, T>>, test: suspend (T) -> Unit) {
@@ -31,20 +25,13 @@ suspend fun <T> ContainerScope.forNone(data: List<Pair<String, T>>, test: suspen
    }
 }
 
-suspend fun <T : Any> ContainerScope.forAll(ts: List<T>, test: suspend (T) -> Unit) {
-   val idents = Identifiers()
-   ts.forEach { t ->
-      val name = idents.stableIdentifier(t)
-      addTest(createTestName(name), false) { test(t) }
-   }
-}
+suspend fun <T : Any> ContainerScope.forAll(vararg ts: T, test: suspend (T) -> Unit) = forAll(ts.toList(), test)
 
-suspend fun <T : Any> ContainerScope.forAll(vararg ts: T, test: suspend (T) -> Unit) {
-   val idents = Identifiers()
-   ts.forEach { t ->
-      val name = idents.stableIdentifier(t)
-      addTest(createTestName(name), false) { test(t) }
-   }
+suspend fun <T : Any> ContainerScope.forAll(vararg data: Pair<String, T>, test: suspend (T) -> Unit) = forAll(data.toList(), test)
+
+suspend fun <T : Any> ContainerScope.forAll(ts: List<T>, test: suspend (T) -> Unit) {
+   val identifiers = Identifiers()
+   forAll(ts.map { Pair(identifiers.stableIdentifier(it), it) }, test)
 }
 
 @JvmName("forAllWithNames")
@@ -54,23 +41,3 @@ suspend fun <T : Any> ContainerScope.forAll(data: List<Pair<String, T>>, test: s
    }
 }
 
-suspend fun <T : Any> ContainerScope.forAll(vararg data: Pair<String, T>, test: suspend (T) -> Unit) {
-   data.forEach { (name, t) ->
-      addTest(createTestName(name), false) { test(t) }
-   }
-}
-
-suspend fun <T : Any> ContainerScope.forNone(vararg data: T, test: suspend (T) -> Unit) {
-   val idents = Identifiers()
-   data.forEach { t ->
-      val name = idents.stableIdentifier(t)
-      addTest(createTestName(name), false) {
-         try {
-            test(t)
-            null
-         } catch (e: AssertionError) {
-            e
-         } ?: throw failure("Test passed for $t but expected failure")
-      }
-   }
-}
