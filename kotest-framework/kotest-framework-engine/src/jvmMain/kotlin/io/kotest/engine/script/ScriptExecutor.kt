d257fe29cd7adff4c8b08dc3faf1580b52839849
sksamuel <sam@sksamuel.com>
2021-01-08 07:56:51 -0600

    removed defunct methods

--- kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/script/ScriptExecutor.kt
+++ kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/script/ScriptExecutor.kt
@@ -1,12 +1,8 @@
 package io.kotest.engine.script
 
 import io.kotest.core.DuplicatedTestNameException
-import io.kotest.core.config.configuration
-import io.kotest.core.config.specInstantiationListeners
 import io.kotest.core.internal.TestCaseExecutor
 import io.kotest.core.script.ScriptRuntime
-import io.kotest.core.spec.Spec
-import io.kotest.core.spec.toDescription
 import io.kotest.core.test.DescriptionName
 import io.kotest.core.test.NestedTest
 import io.kotest.core.test.TestCase
@@ -77,31 +73,12 @@ class ScriptExecutor(private val listener: TestEngineListener) {
    }
 
    /**
-    * Notifies the [TestEngineListener] that we are about to start execution of a [ScriptTemplateWithArgs].
-    * This is called only once per class regardless of the number of instantiation events.
-    */
-   private fun notifySpecStarted(kclass: KClass<out Spec>) = Try {
-      log("ScriptExecutor: Executing engine listener callback:specStarted $kclass")
-      listener.testStarted(kclass.toDescription())
-
-   }
-
-   private fun notifySpecInstantiated(spec: Spec) = Try {
-      log("ScriptExecutor: Executing engine listener callback:specInstantiated spec:$spec")
-      val listeners = configuration.specInstantiationListeners()
-      listener.specInstantiated(spec)
-      listeners.forEach {
-         it.specInstantiated(spec)
-      }
-   }
-
-   /**
     * Creates an instance of the given [ScriptTemplateWithArgs] by delegating to the project constructors,
     * and notifies the [TestEngineListener] of the instantiation event.
     */
    private fun createInstance(kclass: KClass<out ScriptTemplateWithArgs>): Try<ScriptTemplateWithArgs> {
       log("ScriptExecutor: Creating instance of $kclass")
-      return createAndInitializeScript(kclass, this.javaClass.classLoader).apply { log("Qeqewe $this") }
+      return createAndInitializeScript(kclass, this.javaClass.classLoader)
    }
 
    private suspend fun runTest(
