8b2c2222426b0003f585edc86be8f04fb399a394
sksamuel <sam@sksamuel.com>
2021-01-24 13:33:10 -0600

    Arb.list duplicated value as edgecase #1892

--- kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/arbitrary/CollectionsTest.kt
+++ kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/arbitrary/CollectionsTest.kt
@@ -2,6 +2,7 @@ package com.sksamuel.kotest.property.arbitrary
 
 import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.collections.shouldHaveAtLeastSize
 import io.kotest.matchers.collections.shouldHaveAtMostSize
 import io.kotest.matchers.shouldBe
@@ -11,6 +12,7 @@ import io.kotest.property.arbitrary.double
 import io.kotest.property.arbitrary.int
 import io.kotest.property.arbitrary.list
 import io.kotest.property.arbitrary.of
+import io.kotest.property.arbitrary.positiveInts
 import io.kotest.property.arbitrary.set
 import io.kotest.property.arbitrary.single
 import io.kotest.property.checkAll
@@ -43,6 +45,17 @@ class CollectionsTest : FunSpec({
          it.shouldHaveAtMostSize(10)
          it.toSet() shouldBe setOf(1)
       }
+   }
+
+   test("Arb.list should include repeated elements in edge cases") {
+      val edgecase = Arb.positiveInts().edgecases().firstOrNull()
+      Arb.list(Arb.positiveInts()).edgecases() shouldContain listOf(edgecase, edgecase)
+      Arb.list(Arb.positiveInts(), 4..6).edgecases() shouldContain listOf(edgecase, edgecase, edgecase, edgecase)
+
+   }
+
+   test("Arb.list should include empty list in edge cases") {
+      Arb.list(Arb.positiveInts()).edgecases() shouldContain emptyList()
    }
 
    test("Arb.list should generate lists of length up to 100 by default") {
