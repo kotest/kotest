3ae77ed3019f5e32f507a297d633bba2b4c85f76
Sam Sam <sam@sksamuel.com>
2021-01-29 22:36:45 -0600

    Allow registration of custom Show typeclasses #2021 (#2023)
    * Allow registration of custom Show typeclasses #2021
    
    * Allow registration of custom Show typeclasses #2021
    
    * Allow registration of custom Show typeclasses #2021

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/show/Show.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/show/Show.kt
@@ -18,6 +18,46 @@ interface Show<in A> {
 }
 
 /**
+ * Global object that allows for registration of custom [Show] typeclasses.
+ */
+object Shows {
+
+   private val shows = mutableMapOf<KClass<*>, Show<*>>().apply {
+      put(String::class, StringShow)
+      put(Map::class, MapShow)
+      put(BooleanArray::class, ArrayShow)
+      put(IntArray::class, ArrayShow)
+      put(ShortArray::class, ArrayShow)
+      put(FloatArray::class, ArrayShow)
+      put(DoubleArray::class, ArrayShow)
+      put(LongArray::class, ArrayShow)
+      put(ByteArray::class, ArrayShow)
+      put(CharArray::class, ArrayShow)
+      put(Array::class, ArrayShow)
+      put(List::class, ListShow<Any>())
+      put(Iterable::class, IterableShow<Any>())
+      put(Long::class, DefaultShow)
+      put(Int::class, DefaultShow)
+      put(Short::class, DefaultShow)
+      put(Byte::class, DefaultShow)
+      put(Double::class, DefaultShow)
+      put(Float::class, DefaultShow)
+      put(Boolean::class, DefaultShow)
+      put(KClass::class, KClassShow)
+   }
+
+   fun <T : Any> add(kclass: KClass<out T>, show: Show<T>) {
+      shows[kclass] = show
+   }
+
+   fun remove(kclass: KClass<*>) {
+      shows.remove(kclass)
+   }
+
+   fun all() = shows.toMap()
+}
+
+/**
  * Represents a value that has been appropriately formatted for display in output logs or error messages.
  * For example, a null might be formatted as <null>.
  */
@@ -32,27 +72,16 @@ fun <T : Any> showFor(t: T): Show<T> = platformShow(t) ?: commonShowFor(t)
 expect fun <A : Any> platformShow(a: A): Show<A>?
 
 @Suppress("UNCHECKED_CAST")
-fun <T : Any> commonShowFor(t: T): Show<T> = when (t) {
-   is String -> StringShow as Show<T>
-   is Map<*, *> -> MapShow as Show<T>
-   is BooleanArray -> ArrayShow
-   is IntArray -> ArrayShow
-   is ShortArray -> ArrayShow
-   is FloatArray -> ArrayShow
-   is DoubleArray -> ArrayShow
-   is LongArray -> ArrayShow
-   is ByteArray -> ArrayShow
-   is CharArray -> ArrayShow
-   is Array<*> -> ArrayShow
-   is List<*> -> ListShow<T>() as Show<T>
-   is Iterable<*> -> IterableShow<T>() as Show<T>
-   is Long, t is Boolean, t is Int, t is Double, t is Float, t is Short, t is Byte -> DefaultShow
-   is KClass<*> -> KClassShow as Show<T>
-   else -> when {
-      // this won't work in JS or native, so they'll get the boring old toString version
-      io.kotest.mpp.reflection.isDataClass(t::class) -> dataClassShow<T>()
-      else -> DefaultShow
+fun <T : Any> commonShowFor(t: T): Show<T> {
+   // lookup a show from the registered typeclasses
+   val kclass: KClass<*>? = Shows.all().keys.firstOrNull { it.isInstance(t) }
+   if (kclass != null) {
+      val show: Show<*>? = Shows.all()[kclass]
+      return show as Show<T>
    }
+   // this won't work in JS or native, so they'll get the boring old toString version
+   if (io.kotest.mpp.reflection.isDataClass(t::class)) return dataClassShow<T>()
+   return DefaultShow
 }
 
 internal fun recursiveRepr(root: Any, node: Any?): Printed {
