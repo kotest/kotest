35960b1c7fd11bde43bdda0b7bcf7a3cbd1eef03
AJ Alt <ajalt@users.noreply.github.com>
2021-02-03 12:43:34 -0800

    Avoid creating extra lambdas in blocking forAll (#2036)

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/data/blocking/forAll3.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/data/blocking/forAll3.kt
@@ -12,7 +12,7 @@ fun <A, B, C> forAll(vararg rows: Row3<A, B, C>, testfn: (A, B, C) -> Unit) {
    val paramA = params.getOrElse(0) { "a" }
    val paramB = params.getOrElse(1) { "b" }
    val paramC = params.getOrElse(2) { "c" }
-   table(headers(paramA, paramB, paramC), *rows).forAll { A, B, C -> testfn(A, B, C) }
+   table(headers(paramA, paramB, paramC), *rows).forAll(testfn)
 }
 
 fun <A, B, C> forNone(vararg rows: Row3<A, B, C>, testfn: (A, B, C) -> Unit) {
@@ -20,5 +20,5 @@ fun <A, B, C> forNone(vararg rows: Row3<A, B, C>, testfn: (A, B, C) -> Unit) {
    val paramA = params.getOrElse(0) { "a" }
    val paramB = params.getOrElse(1) { "b" }
    val paramC = params.getOrElse(2) { "c" }
-   table(headers(paramA, paramB, paramC), *rows).forNone { A, B, C -> testfn(A, B, C) }
+   table(headers(paramA, paramB, paramC), *rows).forNone(testfn)
 }
