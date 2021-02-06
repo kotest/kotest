bed96851b2c4477cc2f813126c9395f6821635ae
sksamuel <sam@sksamuel.com>
2021-02-06 15:31:57 -0600

    Upgrade to kotlin 1.4.30

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,6 +1,6 @@
 object Libs {
 
-   const val kotlinVersion = "1.4.21"
+   const val kotlinVersion = "1.4.30"
    const val dokkaVersion = "0.10.1"
    const val adarshrTestLoggerVersion = "2.0.0"
    const val gradleVersionsPluginVersion = "0.28.0"
@@ -8,7 +8,7 @@ object Libs {
    const val kotestGradlePlugin = "0.1.4"
 
    object Kotlin {
-      private const val kotlinScriptVersion = "1.4.21-2"
+      private const val kotlinScriptVersion = "1.4.30"
       const val kotlinScriptRuntime = "org.jetbrains.kotlin:kotlin-script-runtime:$kotlinVersion"
       const val kotlinScriptUtil = "org.jetbrains.kotlin:kotlin-script-util:$kotlinScriptVersion"
       const val kotlinScriptJvm = "org.jetbrains.kotlin:kotlin-scripting-jvm:$kotlinScriptVersion"
