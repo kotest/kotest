a76a672f381ce1a740cac786706ab24162906a24
Sam Sam <sam@sksamuel.com>
2021-02-06 22:53:22 -0600

    Updated shouldBeEmpty and shouldNotBeEmpty to work for nullable references (#2055)

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/empty.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/empty.kt
@@ -3,35 +3,42 @@ package io.kotest.matchers.collections
 import io.kotest.assertions.show.show
 import io.kotest.matchers.Matcher
 import io.kotest.matchers.MatcherResult
+import io.kotest.matchers.invokeMatcher
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldNot
 
-fun <T> Iterable<T>.shouldBeEmpty(): Iterable<T> {
+fun <T> Iterable<T>?.shouldBeEmpty(): Iterable<T> {
+   if (this == null) fail()
    toList().shouldBeEmpty()
    return this
 }
 
-fun <T> Array<T>.shouldBeEmpty(): Array<T> {
+fun <T> Array<T>?.shouldBeEmpty(): Array<T> {
+   if (this == null) fail()
    asList().shouldBeEmpty()
    return this
 }
 
-fun <T> Collection<T>.shouldBeEmpty(): Collection<T> {
+fun <T> Collection<T>?.shouldBeEmpty(): Collection<T> {
+   if (this == null) fail()
    this should beEmpty()
    return this
 }
 
-fun <T> Iterable<T>.shouldNotBeEmpty(): Iterable<T> {
+fun <T> Iterable<T>?.shouldNotBeEmpty(): Iterable<T> {
+   if (this == null) fail()
    toList().shouldNotBeEmpty()
    return this
 }
 
-fun <T> Array<T>.shouldNotBeEmpty(): Array<T> {
+fun <T> Array<T>?.shouldNotBeEmpty(): Array<T> {
+   if (this == null) fail()
    asList().shouldNotBeEmpty()
    return this
 }
 
-fun <T> Collection<T>.shouldNotBeEmpty(): Collection<T> {
+fun <T> Collection<T>?.shouldNotBeEmpty(): Collection<T> {
+   if (this == null) fail()
    this shouldNot beEmpty()
    return this
 }
@@ -42,4 +49,9 @@ fun <T> beEmpty(): Matcher<Collection<T>> = object : Matcher<Collection<T>> {
       { "Collection should be empty but contained ${value.show().value}" },
       { "Collection should not be empty" }
    )
+}
+
+private fun fail(): Nothing {
+   invokeMatcher(null, Matcher.failure("Should be empty but was null"))
+   throw NotImplementedError()
 }
