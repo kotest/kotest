6437b02c256dc2f395073943c473c202a6b2265d
sksamuel <sam@sksamuel.com>
2021-02-07 15:54:59 -0600

    Fixed allure test grouping #1871

---
+++ kotest-tests/kotest-tests-allure/src/jvmTest/kotlin/com/sksamuel/kotest/DummyShouldSpec.kt
@@ -0,0 +1,20 @@
+package com.sksamuel.kotest
+
+import io.kotest.assertions.fail
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.booleans.shouldBeTrue
+
+class DummyShouldSpec : ShouldSpec() {
+   init {
+      context("context a") {
+         should("work") {
+            true.shouldBeTrue()
+         }
+      }
+      context("context b") {
+         should("work") {
+            true.shouldBeTrue()
+         }
+      }
+   }
+}
