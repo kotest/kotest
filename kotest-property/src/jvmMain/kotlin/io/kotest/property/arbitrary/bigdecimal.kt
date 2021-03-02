de29c03f451e6d9d20a91f3a9fae87b9f5046423
Ashish Kumar <ashishku@thoughtworks.com>
2021-03-02 22:22:29 +0530

    Fixes Arb.bigdecimal getting hanged for min and max value which other than Doble min and max

--- kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/bigdecimal.kt
+++ kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/bigdecimal.kt
@@ -4,9 +4,22 @@ import io.kotest.property.Arb
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-fun Arb.Companion.bigDecimal() = Arb.double().map { it.toBigDecimal() }
+private val bigDecimalEdgecases = listOf(
+   BigDecimal(0.0),
+   BigDecimal(1.0),
+   BigDecimal(-1.0),
+   BigDecimal("1e-300"),
+   BigDecimal("-1e-300"),
+)
 
-fun Arb.Companion.bigDecimal(scale: Int, roundingMode: RoundingMode) = bigDecimal().map { it.setScale(scale, roundingMode) }
+fun Arb.Companion.bigDecimal(): Arb<BigDecimal> {
+   return arbitrary(bigDecimalEdgecases) {
+      BigDecimal(it.random.nextInt() * it.random.nextDouble())
+   }
+}
+
+fun Arb.Companion.bigDecimal(scale: Int, roundingMode: RoundingMode) =
+   bigDecimal().map { it.setScale(scale, roundingMode) }
 
 fun Arb.Companion.bigDecimal(min: BigDecimal, max: BigDecimal): Arb<BigDecimal> {
    return bigDecimal().filter { it in min..max }
