5d624cb7a9d2e75c6f84e63ad5fdfe138e2679b7
sksamuel <sam@sksamuel.com>
2021-04-11 23:45:57 -0500

    Added another test

---
+++ kotest-framework/kotest-framework-engine/src/commonTest/kotlin/io/kotest/core/spec/style/ShouldSpecTestTypeTest.kt
@@ -0,0 +1,21 @@
+package io.kotest.core.spec.style
+
+import io.kotest.core.test.TestType
+import io.kotest.matchers.shouldBe
+
+class ShouldSpecTestTypeTest : ShouldSpec() {
+   init {
+      context("context") {
+         this.testCase.type shouldBe TestType.Container
+         context("context 2") {
+            this.testCase.type shouldBe TestType.Container
+            should("should") {
+               this.testCase.type shouldBe TestType.Test
+            }
+         }
+         should("should") {
+            this.testCase.type shouldBe TestType.Test
+         }
+      }
+   }
+}
