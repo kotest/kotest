37468d406c14b3662012f36c431165f6516295bb
malteesch <accounts@malteesch.de>
2021-04-21 11:17:21 +0200

    refactor(datatest): replace deprecated typealias with original

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/datatest/datatest.kt
+++ kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/datatest/datatest.kt
@@ -1,21 +1,20 @@
 package io.kotest.core.datatest
 
 import io.kotest.assertions.failure
-import io.kotest.core.spec.style.scopes.ContainerScope
-import io.kotest.core.spec.style.scopes.RootScope
+import io.kotest.core.spec.style.scopes.*
 import io.kotest.core.test.Identifiers
 import io.kotest.core.test.TestType
 import io.kotest.core.test.createTestName
 import kotlin.jvm.*
 
-suspend fun <T> ContainerScope.forNone(vararg data: Pair<String, T>, test: suspend (T) -> Unit) =
+suspend fun <T> ContainerContext.forNone(vararg data: Pair<String, T>, test: suspend (T) -> Unit) =
    forNone(data.toList(), test)
 
-suspend fun <T : Any> ContainerScope.forNone(vararg data: T, test: suspend (T) -> Unit) {
+suspend fun <T : Any> ContainerContext.forNone(vararg data: T, test: suspend (T) -> Unit) {
    forNone(data.map { Pair(Identifiers.stableIdentifier(it), it) }, test)
 }
 
-suspend fun <T> ContainerScope.forNone(data: List<Pair<String, T>>, test: suspend (T) -> Unit) {
+suspend fun <T> ContainerContext.forNone(data: List<Pair<String, T>>, test: suspend (T) -> Unit) {
    data.forEach { (name, t) ->
       addTest(name, TestType.Test) {
          try {
@@ -28,17 +27,17 @@ suspend fun <T> ContainerScope.forNone(data: List<Pair<String, T>>, test: suspen
    }
 }
 
-suspend fun <T : Any> ContainerScope.forAll(vararg ts: T, test: suspend (T) -> Unit) = forAll(ts.toList(), test)
+suspend fun <T : Any> ContainerContext.forAll(vararg ts: T, test: suspend (T) -> Unit) = forAll(ts.toList(), test)
 
-suspend fun <T : Any> ContainerScope.forAll(vararg data: Pair<String, T>, test: suspend (T) -> Unit) =
+suspend fun <T : Any> ContainerContext.forAll(vararg data: Pair<String, T>, test: suspend (T) -> Unit) =
    forAll(data.toList(), test)
 
-suspend fun <T : Any> ContainerScope.forAll(ts: List<T>, test: suspend (T) -> Unit) {
+suspend fun <T : Any> ContainerContext.forAll(ts: List<T>, test: suspend (T) -> Unit) {
    forAll(ts.map { Pair(Identifiers.stableIdentifier(it), it) }, test)
 }
 
 @JvmName("forAllWithNames")
-suspend fun <T : Any> ContainerScope.forAll(data: List<Pair<String, T>>, test: suspend (T) -> Unit) {
+suspend fun <T : Any> ContainerContext.forAll(data: List<Pair<String, T>>, test: suspend (T) -> Unit) {
    data.forEach { (name, t) ->
       addTest(name, TestType.Test) { test(t) }
    }
