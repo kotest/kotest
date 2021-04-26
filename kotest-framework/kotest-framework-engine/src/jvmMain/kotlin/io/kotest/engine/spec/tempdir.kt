105ca1098a236c9af39c3e7609ba3cb050e0c80f
Sebastian Schuberth <sschuberth@gmail.com>
2021-04-26 13:03:51 +0200

    tempdir: Delete temporary directories recursively
    While delete() works on directories, they must be empty for delete() to
    succeed. However, temporary directories should be deleted recursively,
    including any contained files and / or subdirectories.

--- kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/spec/tempdir.kt
+++ kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/spec/tempdir.kt
@@ -6,7 +6,7 @@ import java.io.File
 fun TestConfiguration.tempdir(prefix: String? = null, suffix: String? = null): File {
    val dir = kotlin.io.path.createTempDirectory(prefix ?: javaClass.name).toFile()
    afterSpec {
-      dir.delete()
+      dir.deleteRecursively()
    }
    return dir
 }
