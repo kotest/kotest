de709efbbc03fd476eaaf2104ae49673e0b96fe5
sksamuel <sam@sksamuel.com>
2021-05-02 11:23:23 -0500

    Updated line markers for other builds

--- src/IC-202/kotlin/io/kotest/plugin/intellij/markers.kt
+++ src/IC-202/kotlin/io/kotest/plugin/intellij/markers.kt
@@ -2,14 +2,16 @@ package io.kotest.plugin.intellij
 
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.openapi.editor.markup.MarkupEditorFilter
+import com.intellij.openapi.editor.markup.MarkupEditorFilterFactory
 import com.intellij.psi.PsiElement
 import javax.swing.Icon
 
-fun createLineMarker(element: PsiElement, text: String, icon: Icon) = LineMarkerInfo(
-   element,
-   element.textRange,
-   icon,
-   { text },
-   { _, _ -> },
-   GutterIconRenderer.Alignment.LEFT
-)
+/**
+ * A Line marker that does not appear in diffs
+ */
+class MainEditorLineMarkerInfo(element: PsiElement, text: String, icon: Icon) : LineMarkerInfo<PsiElement>(
+   element, element.textRange, icon, { text }, { _, _ -> }, GutterIconRenderer.Alignment.LEFT
+) {
+   override fun getEditorFilter(): MarkupEditorFilter = MarkupEditorFilterFactory.createIsNotDiffFilter()
+}
