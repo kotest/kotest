14d6a0c0050c12d4b6dd6a3e00908afd0c2d9822
sksamuel <sam@sksamuel.com>
2021-05-04 19:10:43 -0500

    Fixed test

--- kotest-framework/kotest-framework-api/src/jvmTest/kotlin/io/kotest/core/test/IdentifiersTest.kt
+++ kotest-framework/kotest-framework-api/src/jvmTest/kotlin/io/kotest/core/test/IdentifiersTest.kt
@@ -7,7 +7,7 @@ class IdentifiersTest : FunSpec() {
    init {
       test("should make repeated names unique") {
          Identifiers.uniqueTestName("foo", emptySet()) shouldBe "foo"
-         Identifiers.uniqueTestName("foo", setOf("foo")) shouldBe "(1) foo (1)"
+         Identifiers.uniqueTestName("foo", setOf("foo")) shouldBe "(1) foo"
          Identifiers.uniqueTestName("foo", setOf("foo", "foo (1)")) shouldBe "(2) foo"
          Identifiers.uniqueTestName("foo", setOf("foo", "foo (1)", "(2) foo")) shouldBe "(3) foo"
          Identifiers.uniqueTestName("foo", setOf("foo", "foo (1")) shouldBe "(1) foo"
