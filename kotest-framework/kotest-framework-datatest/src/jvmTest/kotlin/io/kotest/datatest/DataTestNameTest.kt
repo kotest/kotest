219876fcb6317f96a8aabc31733d75d4037686b6
Sam Sam <sam@sksamuel.com>
2021-05-18 15:04:12 -0500

    Support function for data test names in data-driven tests #2247 (#2260)

--- kotest-framework/kotest-framework-datatest/src/jvmTest/kotlin/io/kotest/datatest/DataTestNameTest.kt
+++ kotest-framework/kotest-framework-datatest/src/jvmTest/kotlin/io/kotest/datatest/DataTestNameTest.kt
@@ -1,7 +1,6 @@
 package io.kotest.datatest
 
 import io.kotest.common.ExperimentalKotest
-import io.kotest.core.datatest.forAll
 import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.test.TestCase
@@ -10,30 +9,49 @@ import io.kotest.matchers.shouldBe
 
 @OptIn(ExperimentalKotest::class)
 class DataTestNameTest: FunSpec({
+
    context("Data test with pair") {
       withData(
          Pair(2,1),
          Pair(1,2),
       ) {}
    }
+
    context("Data test with triple") {
       withData(
          Triple(1,2,3),
          Triple(3,2,1),
       ) {}
    }
+
    context("Data test for type extending WithDataTestName") {
       withData(
          ADummyClass("a1", "b1"),
          ADummyClass("a2", "b2")
       ) {}
    }
+
    context("Data test for type annotated with IsStableType") {
       withData(
          AnotherDummyClass("a1", "b1"),
          AnotherDummyClass("a2", "b2")
       ) {}
    }
+
+   context("data test names with function") {
+      withData<SimpleClass>(
+         { "simple${it.a}${it.b}" },
+         SimpleClass("a1", "b1"),
+         SimpleClass("a2", "b2"),
+      ) {}
+   }
+
+   withData<SimpleClass>(
+      { "simple${it.a}${it.b}" },
+      SimpleClass("a1", "b1"),
+      SimpleClass("a2", "b2"),
+   ) {}
+
 }) {
    override fun afterAny(testCase: TestCase, result: TestResult) {
       DataTestNamesStore.names.add(testCase.description.displayName())
@@ -53,6 +71,11 @@ class DataTestNameTest: FunSpec({
          "AnotherDummyClass(a : a1, b : b1)",
          "AnotherDummyClass(a : a2, b : b2)",
          "Data test for type annotated with IsStableType",
+         "simplea1b1",
+         "simplea2b2",
+         "data test names with function",
+         "simplea1b1",
+         "simplea2b2",
       )
    }
 
@@ -69,7 +92,10 @@ private class ADummyClass(private val a: String, private val b: String) : WithDa
    override fun dataTestName() = "ADummyClass(a : $a, b : $b)"
 }
 
+
+private class SimpleClass(val a: String, val b: String)
+
 @IsStableType
-private data class AnotherDummyClass(private val a: String, private val b: String){
+private data class AnotherDummyClass(private val a: String, private val b: String) {
    override fun toString() = "AnotherDummyClass(a : $a, b : $b)"
 }
