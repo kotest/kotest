fab1c79659e7622e9d3b976bf5a80a31e6c35596
sksamuel <sam@sksamuel.com>
2021-05-19 10:57:33 -0500

    Removed some deprecated arb methods

--- kotest-property/src/commonMain/kotlin/io/kotest/property/arbitrary/builders.kt
+++ kotest-property/src/commonMain/kotlin/io/kotest/property/arbitrary/builders.kt
@@ -64,60 +64,6 @@ fun <A> arbitrary(shrinker: Shrinker<A>, fn: (RandomSource) -> A): Arb<A> =
    arbitrary(emptyList(), shrinker, fn)
 
 /**
- * Creates a new [Arb] that performs shrinking using the supplied shrinker and generates each value
- * from successive invocations of the given function f.
- */
-@Deprecated(
-   "use arbitrary(). This function Will be removed in 4.7",
-   ReplaceWith("arbitrary(fn)")
-)
-fun <A> arb(fn: (RandomSource) -> A): Arb<A> =
-   arbitrary(fn)
-
-/**
- * Creates a new [Arb] that performs no shrinking, uses the supplied edge case values,
- * and generates values from the given function that is invoked once to return a sequence of values.
- */
-@Deprecated(
-   "This function will no longer accept Sequence<A>. Use arbitrary with (RandomSource -> A) for compatibility. This function Will be removed in 4.7"
-)
-@JvmName("arbSequence")
-fun <A> arb(edgecases: List<A> = emptyList(), fn: (RandomSource) -> Sequence<A>) = object : Arb<A>() {
-   override fun edgecase(rs: RandomSource): A? = if (edgecases.isEmpty()) null else edgecases.random(rs.random)
-   override fun values(rs: RandomSource): Sequence<Sample<A>> = fn(rs).map { Sample(it) }
-   override fun sample(rs: RandomSource): Sample<A> = Sample(fn(rs).first())
-}
-
-/**
- * Creates a new [Arb] that performs shrinking using the supplier shrinker, uses the
- * supplied edge case values, and provides values from sequence returning function.
- */
-@Deprecated(
-   "This function will no longer accept Sequence<A>. Use arbitrary with (RandomSource -> A) for compatibility. This function Will be removed in 4.7"
-)
-@JvmName("arbSequence")
-fun <A> arb(
-   edgecases: List<A> = emptyList(),
-   shrinker: Shrinker<A>,
-   fn: (RandomSource) -> Sequence<A>
-) = object : Arb<A>() {
-   override fun edgecase(rs: RandomSource): A? = if (edgecases.isEmpty()) null else edgecases.random(rs.random)
-   override fun values(rs: RandomSource): Sequence<Sample<A>> = fn(rs).map { sampleOf(it, shrinker) }
-   override fun sample(rs: RandomSource): Sample<A> = sampleOf(fn(rs).first(), shrinker)
-}
-
-/**
- * Creates a new [Arb] that performs shrinking using the supplied shrinker and generates each value
- * from successive invocations of the given function f.
- */
-@Deprecated(
-   "Use arbitrary with (RandomSource -> A). This function Will be removed in 4.7",
-   ReplaceWith("arbitrary(shrinker, fn)")
-)
-fun <A> arb(shrinker: Shrinker<A>, fn: (RandomSource) -> A): Arb<A> =
-   arbitrary(shrinker, fn)
-
-/**
  * Creates a new [Arb] with the given edgecases, that performs shrinking using the supplied shrinker and
  * generates each value from successive invocations of the given function f.
  */
@@ -127,27 +73,6 @@ fun <A> arb(shrinker: Shrinker<A>, fn: (RandomSource) -> A): Arb<A> =
 )
 fun <A> arb(shrinker: Shrinker<A>, edgecases: List<A> = emptyList(), fn: (RandomSource) -> A): Arb<A> =
    arbitrary(edgecases, shrinker, fn)
-
-/**
- * Creates a new [Arb] with the given edgecases, that performs shrinking using the supplied shrinker and
- * generates each value from successive invocations of the given function f.
- */
-@Deprecated(
-   "Use arbitrary with (RandomSource -> A). This function Will be removed in 4.7",
-   ReplaceWith("arbitrary(edgecases, fn)")
-)
-fun <A> arb(edgecases: List<A> = emptyList(), fn: (RandomSource) -> A): Arb<A> =
-   arbitrary(edgecases, fn)
-
-/**
- * Returns an [Arb] where each value is generated from the given function.
- */
-@Deprecated(
-   "Use arbitrary with (RandomSource -> A). This function Will be removed in 4.7",
-   ReplaceWith("arbitrary(edgeCases, fn)")
-)
-fun <A> Arb.Companion.create(edgeCases: List<A> = emptyList(), fn: (RandomSource) -> A): Arb<A> =
-   arbitrary(edgeCases, fn)
 
 /**
  * Returns an [Arb] which repeatedly generates a single value.
