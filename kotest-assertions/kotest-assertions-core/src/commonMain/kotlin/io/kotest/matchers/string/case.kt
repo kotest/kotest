319c6e0e206d838bc4b7b07b89a1dc9b2b9bc91c
sksamuel <sam@sksamuel.com>
2021-05-22 10:04:25 -0500

    Migrating string matchers to work with CharSequence

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/string/case.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/string/case.kt
@@ -1,12 +1,13 @@
 package io.kotest.matchers.string
 
 import io.kotest.assertions.show.show
+import io.kotest.matchers.Matcher
 import io.kotest.matchers.MatcherResult
 import io.kotest.matchers.neverNullMatcher
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldNot
 
-fun String?.shouldBeUpperCase(): String? {
+fun <A : CharSequence?> A.shouldBeUpperCase(): A {
    this should beUpperCase()
    return this
 }
@@ -16,27 +17,27 @@ fun String?.shouldNotBeUpperCase(): String? {
    return this
 }
 
-fun beUpperCase() = neverNullMatcher<String> { value ->
+fun <A : CharSequence?> beUpperCase(): Matcher<A> = neverNullMatcher { value ->
    MatcherResult(
-      value.toUpperCase() == value,
+      value.toString().toUpperCase() == value,
       "${value.show().value} should be upper case",
       "${value.show().value} should not should be upper case"
    )
 }
 
-fun String?.shouldBeLowerCase(): String? {
+fun <A : CharSequence?> A.shouldBeLowerCase(): A {
    this should beLowerCase()
    return this
 }
 
-fun String?.shouldNotBeLowerCase(): String? {
+fun <A : CharSequence?> A.shouldNotBeLowerCase(): A {
    this shouldNot beLowerCase()
    return this
 }
 
-fun beLowerCase() = neverNullMatcher<String> { value ->
+fun <A : CharSequence?> beLowerCase() = neverNullMatcher<A> { value ->
    MatcherResult(
-      value.toLowerCase() == value,
+      value.toString().toLowerCase() == value,
       "${value.show().value} should be lower case",
       "${value.show().value} should not should be lower case"
    )
