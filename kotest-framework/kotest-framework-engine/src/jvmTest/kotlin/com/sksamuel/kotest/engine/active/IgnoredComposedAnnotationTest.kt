f471deba3913bda97b780e6bc492d690d227dd5f
Sam Sam <sam@sksamuel.com>
2021-05-22 14:47:09 -0500

    Support composed annotations plus caching of annotation lookups (#2279)
    * Support composed annotations plus caching of annotation lookups
    
    * Fixes and tests
    
    * removed println
    
    * Made it work without needing io.kotest pacakge

---
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/active/IgnoredComposedAnnotationTest.kt
@@ -0,0 +1,19 @@
+package com.sksamuel.kotest.engine.active
+
+import io.kotest.core.annotation.Ignored
+import io.kotest.core.spec.Isolate
+import io.kotest.core.spec.style.FunSpec
+
+@Isolate
+@Ignored
+annotation class ComposedAnno
+
+@ComposedAnno
+class IgnoredComposedAnnotationTest : FunSpec() {
+   init {
+      error("Boom")
+      test("should not be invoked") {
+         error("Boom")
+      }
+   }
+}
