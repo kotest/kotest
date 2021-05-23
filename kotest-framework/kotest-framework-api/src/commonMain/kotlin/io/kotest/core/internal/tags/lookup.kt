ceac0704b9fbf9ce2a1a2bc85dc94b2223f2a40b
sksamuel <sam@sksamuel.com>
2021-05-23 16:52:57 -0500

    fix

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/internal/tags/lookup.kt
+++ kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/internal/tags/lookup.kt
@@ -21,6 +21,14 @@ class ConfigurationTagProvider(private val configuration: Configuration) : TagPr
 }
 
 /**
+ * Returns the tags specified on the given class from the @Tags annotation if present.
+ */
+fun KClass<*>.tags(): Set<Tag> {
+   val annotation = annotation<io.kotest.core.annotation.Tags>() ?: return emptySet()
+   return annotation.values.map { NamedTag(it) }.toSet()
+}
+
+/**
  * Returns runtime active [Tag]'s by invocating all registered [TagExtension]s and combining
  * any returned tags into a [Tags] container.
  */
