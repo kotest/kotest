fa09631241ce755109c713c6460c856c64dbcbca
Sebastian Schuberth <sschuberth@gmail.com>
2021-05-29 10:28:13 +0200

    Improve the temp{dir,file} functions
    Previously, the logic to use javaClass.name as the default prefix could
    have been bypassed by setting the prefix to null. Avoid that by making
    javaClass.name the fallback if the prefix is null. This is a fixup for
    5e78d68.
    
    Also align the temp{dir,file} functions to both use the new API functions
    kotlin.io.path.createTemp{Directory,File}, and avoid a duplicate
    toFile() conversion.

--- kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/spec/tempfile.kt
+++ kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/spec/tempfile.kt
@@ -2,10 +2,9 @@ package io.kotest.engine.spec
 
 import io.kotest.core.TestConfiguration
 import java.io.File
-import java.nio.file.Files
 
-fun TestConfiguration.tempfile(prefix: String? = javaClass.name, suffix: String? = ".tmp"): File {
-   val file = Files.createTempFile(prefix, suffix).toFile()
+fun TestConfiguration.tempfile(prefix: String? = null, suffix: String? = null): File {
+   val file = kotlin.io.path.createTempFile(prefix ?: javaClass.name, suffix).toFile()
    afterSpec {
       file.delete()
    }
