e6380e1741d3656e70c08b4831c1a79feaa57518
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2021-06-01 21:20:55 +0200

    Fix Arb.boolArray naming

--- kotest-property/src/commonMain/kotlin/io/kotest/property/arbitrary/bools.kt
+++ kotest-property/src/commonMain/kotlin/io/kotest/property/arbitrary/bools.kt
@@ -9,8 +9,8 @@ import io.kotest.property.Gen
 fun Arb.Companion.bool(): Arb<Boolean> = arbitrary(listOf(true, false)) { it.random.nextBoolean() }
 
 /**
- * Returns an [Arb] that produces [BooleanArray]s where [generateArrayLength] produces the length of the arrays and
- * [generateContents] produces the content of the arrays.
+ * Returns an [Arb] that produces [BooleanArray]s where [length] produces the length of the arrays and
+ * [content] produces the content of the arrays.
  */
-fun Arb.Companion.boolArray(generateArrayLength: Gen<Int>, generateContents: Arb<Boolean>): Arb<BooleanArray> =
-   toPrimitiveArray(generateArrayLength, generateContents, Collection<Boolean>::toBooleanArray)
+fun Arb.Companion.boolArray(length: Gen<Int>, content: Arb<Boolean>): Arb<BooleanArray> =
+   toPrimitiveArray(length, content, Collection<Boolean>::toBooleanArray)
