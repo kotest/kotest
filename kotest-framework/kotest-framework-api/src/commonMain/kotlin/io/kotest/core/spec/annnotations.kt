e64290cabc46d528cc1583e4a924816cd3ff1369
Sam Sam <sam@sksamuel.com>
2021-07-10 21:40:45 -0500

    Fixed dep for kotlin reflect (#2346)

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/spec/annnotations.kt
+++ kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/spec/annnotations.kt
@@ -10,6 +10,8 @@ annotation class AutoScan
  * Note: This name must be globally unique. Two specs, even in different packages,
  * cannot share the same name, so if @DisplayName is used, developers must ensure it does not
  * clash with another spec.
+ *
+ * This annotation only works on JVM targets. On other targets this annotation will be ignored.
  */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
