529624a1bdda3554e296b12ea998a44d1b7ee48c
sksamuel <sam@sksamuel.com>
2021-07-11 13:15:41 -0500

    Deprecated version 2 of the data test library

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/datatest/datatest.kt
+++ kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/datatest/datatest.kt
@@ -7,13 +7,16 @@ import io.kotest.core.test.TestType
 import io.kotest.core.test.createTestName
 import kotlin.jvm.*
 
+@Deprecated("Deprecated without replacement. Will be removed in 6.0")
 suspend fun <T> ContainerContext.forNone(vararg data: Pair<String, T>, test: suspend (T) -> Unit) =
    forNone(data.toList(), test)
 
+@Deprecated("Deprecated without replacement. Will be removed in 6.0")
 suspend fun <T : Any> ContainerContext.forNone(vararg data: T, test: suspend (T) -> Unit) {
    forNone(data.map { Pair(Identifiers.stableIdentifier(it), it) }, test)
 }
 
+@Deprecated("Deprecated without replacement. Will be removed in 6.0")
 suspend fun <T> ContainerContext.forNone(data: List<Pair<String, T>>, test: suspend (T) -> Unit) {
    data.forEach { (name, t) ->
       addTest(name, TestType.Test) {
@@ -27,28 +30,35 @@ suspend fun <T> ContainerContext.forNone(data: List<Pair<String, T>>, test: susp
    }
 }
 
+@Deprecated("Replaced with withData. Will be removed in 6.0")
 suspend fun <T : Any> ContainerContext.forAll(vararg ts: T, test: suspend (T) -> Unit) = forAll(ts.toList(), test)
 
+@Deprecated("Replaced with withData. Will be removed in 6.0")
 suspend fun <T : Any> ContainerContext.forAll(vararg data: Pair<String, T>, test: suspend (T) -> Unit) =
    forAll(data.toList(), test)
 
+@Deprecated("Replaced with withData. Will be removed in 6.0")
 suspend fun <T : Any> ContainerContext.forAll(ts: List<T>, test: suspend (T) -> Unit) {
    forAll(ts.map { Pair(Identifiers.stableIdentifier(it), it) }, test)
 }
 
 @JvmName("forAllWithNames")
+@Deprecated("Replaced with withData. Will be removed in 6.0")
 suspend fun <T : Any> ContainerContext.forAll(data: List<Pair<String, T>>, test: suspend (T) -> Unit) {
    data.forEach { (name, t) ->
       addTest(name, TestType.Test) { test(t) }
    }
 }
 
+@Deprecated("Deprecated without replacement. Will be removed in 6.0")
 fun <T> RootContext.forNone(vararg data: Pair<String, T>, test: suspend (T) -> Unit) = this.forNone(data.toList(), test)
 
+@Deprecated("Deprecated without replacement. Will be removed in 6.0")
 fun <T : Any> RootContext.forNone(vararg data: T, test: suspend (T) -> Unit) {
    this.forNone(data.map { Pair(Identifiers.stableIdentifier(it), it) }, test)
 }
 
+@Deprecated("Deprecated without replacement. Will be removed in 6.0")
 fun <T> RootContext.forNone(data: List<Pair<String, T>>, test: suspend (T) -> Unit) {
    data.forEach { (name, t) ->
       registration().addTest(createTestName(name), false) {
@@ -62,16 +72,20 @@ fun <T> RootContext.forNone(data: List<Pair<String, T>>, test: suspend (T) -> Un
    }
 }
 
+@Deprecated("Replaced with withData. Will be removed in 6.0")
 fun <T : Any> RootContext.forAll(vararg ts: T, test: suspend (T) -> Unit) = this.forAll(ts.toList(), test)
 
+@Deprecated("Replaced with withData. Will be removed in 6.0")
 fun <T : Any> RootContext.forAll(vararg data: Pair<String, T>, test: suspend (T) -> Unit) =
    this.forAll(data.toList(), test)
 
+@Deprecated("Replaced with withData. Will be removed in 6.0")
 fun <T : Any> RootContext.forAll(ts: List<T>, test: suspend (T) -> Unit) {
    this.forAll(ts.map { Pair(Identifiers.stableIdentifier(it), it) }, test)
 }
 
 @JvmName("forAllWithNames")
+@Deprecated("Replaced with withData. Will be removed in 6.0")
 fun <T : Any> RootContext.forAll(data: List<Pair<String, T>>, test: suspend (T) -> Unit) {
    data.forEach { (name, t) ->
       registration().addTest(createTestName(name), false) { test(t) }
