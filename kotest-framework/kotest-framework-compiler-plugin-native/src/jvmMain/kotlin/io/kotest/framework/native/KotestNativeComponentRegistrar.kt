360d4b70f53ac5ed13701b83f33edf2c58ec324c
sksamuel <sam@sksamuel.com>
2021-07-10 22:17:49 -0500

    Removed message collector from native plugin

--- kotest-framework/kotest-framework-compiler-plugin-native/src/jvmMain/kotlin/io/kotest/framework/native/KotestNativeComponentRegistrar.kt
+++ kotest-framework/kotest-framework-compiler-plugin-native/src/jvmMain/kotlin/io/kotest/framework/native/KotestNativeComponentRegistrar.kt
@@ -2,8 +2,6 @@ package io.kotest.framework.native
 
 import com.intellij.mock.MockProject
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
-import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
-import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
 
@@ -13,7 +11,6 @@ class KotestNativeComponentRegistrar : ComponentRegistrar {
       project: MockProject,
       configuration: CompilerConfiguration
    ) {
-      val messageCollector = configuration.get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
-      IrGenerationExtension.registerExtension(project, SpecIrGenerationExtension(messageCollector))
+      IrGenerationExtension.registerExtension(project, SpecIrGenerationExtension())
    }
 }
--- kotest-framework/kotest-framework-compiler-plugin-native/src/jvmMain/kotlin/io/kotest/framework/native/SpecIrGenerationExtension.kt
+++ kotest-framework/kotest-framework-compiler-plugin-native/src/jvmMain/kotlin/io/kotest/framework/native/SpecIrGenerationExtension.kt
@@ -5,7 +5,6 @@ import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.ir.addChild
 import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
-import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.ir.builders.declarations.addGetter
 import org.jetbrains.kotlin.ir.builders.declarations.buildField
 import org.jetbrains.kotlin.ir.builders.declarations.buildProperty
@@ -16,7 +15,7 @@ import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.util.kotlinFqName
 import org.jetbrains.kotlin.name.Name
 
-class SpecIrGenerationExtension(private val messageCollector: MessageCollector) : IrGenerationExtension {
+class SpecIrGenerationExtension : IrGenerationExtension {
    override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
 
       moduleFragment.transform(object : IrElementTransformerVoidWithContext() {
@@ -27,14 +26,11 @@ class SpecIrGenerationExtension(private val messageCollector: MessageCollector) 
                // we use a public val to register each spec, so this name must be unique across all files
                // therefore using FQN spec name seems a safe bet
                val entryPointPropertyName = "kotestSpecEntryPoint_${spec.kotlinFqName.asString().replace(".", "_")}"
-//               messageCollector.report(
-//                  CompilerMessageSeverity.WARNING,
-//                  "entryPointPropertyName=$entryPointPropertyName"
-//               )
 
                val registerKotestProperty = pluginContext.irFactory.buildProperty {
                   name = Name.identifier(entryPointPropertyName)
                }.apply {
+
                   parent = declaration
                   backingField = pluginContext.irFactory.buildField {
                      type = pluginContext.irBuiltIns.unitType
