a8785b15385d383f2855346263b104fbfb3091db
sksamuel <sam@sksamuel.com>
2021-07-12 08:39:13 -0500

    Support word spec should in test filter #2319

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/launcher/TestPathTestCaseFilterTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/launcher/TestPathTestCaseFilterTest.kt
@@ -3,6 +3,7 @@ package com.sksamuel.kotest.engine.launcher
 import io.kotest.core.filter.TestFilterResult
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.WordSpec
 import io.kotest.core.spec.toDescription
 import io.kotest.engine.launcher.TestPathTestCaseFilter
 import io.kotest.matchers.shouldBe
@@ -22,13 +23,29 @@ class TestPathTestCaseFilterTest : FunSpec() {
          TestPathTestCaseFilter("foo", Spec1::class).filter(Spec1::class.toDescription().appendTest("foo").appendTest("bar")) shouldBe TestFilterResult.Include
       }
       test("filter should include parent tests of the target") {
-         TestPathTestCaseFilter("foo -- bar", Spec1::class).filter(Spec1::class.toDescription().appendTest("foo")) shouldBe TestFilterResult.Include
+         TestPathTestCaseFilter("foo -- bar", Spec1::class).filter(
+            Spec1::class.toDescription().appendTest("foo")
+         ) shouldBe TestFilterResult.Include
       }
       test("filter should include the target spec") {
-         TestPathTestCaseFilter("foo -- bar", Spec1::class).filter(Spec1::class.toDescription()) shouldBe TestFilterResult.Include
+         TestPathTestCaseFilter(
+            "foo -- bar",
+            Spec1::class
+         ).filter(Spec1::class.toDescription()) shouldBe TestFilterResult.Include
       }
       test("filter should exclude another spec") {
-         TestPathTestCaseFilter("foo -- bar", Spec1::class).filter(Spec2::class.toDescription()) shouldBe TestFilterResult.Exclude
+         TestPathTestCaseFilter(
+            "foo -- bar",
+            Spec1::class
+         ).filter(Spec2::class.toDescription()) shouldBe TestFilterResult.Exclude
+      }
+      test("filter should work for word spec") {
+         TestPathTestCaseFilter("a container -- pass a test", WordSpec1::class).filter(
+            WordSpec1::class.toDescription().appendTest("a container should").appendTest("pass a test")
+         ) shouldBe TestFilterResult.Include
+         TestPathTestCaseFilter("a container -- pass a test", WordSpec1::class).filter(
+            WordSpec1::class.toDescription().appendTest("a container should").appendTest("skip a test")
+         ) shouldBe TestFilterResult.Exclude
       }
    }
 }
@@ -44,5 +61,14 @@ private class Spec2 : StringSpec() {
    init {
       "foo"{}
       "boo"{}
+   }
+}
+
+private class WordSpec1 : WordSpec() {
+   init {
+      "a container" should {
+         "skip a test".config(enabled = false) {}
+         "pass a test" { 1 shouldBe 1 }
+      }
    }
 }
