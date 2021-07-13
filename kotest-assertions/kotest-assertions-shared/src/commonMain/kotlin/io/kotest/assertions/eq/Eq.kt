ab71857503b7c2610007f15777f85700778e51d2
Ashish Kumar Joy <34642693+ashishkujoy@users.noreply.github.com>
2021-07-14 04:11:34 +0530

    #2293 Fixes ShouldContainExactly for collection containing byte arrays (#2360)

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/eq/Eq.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/eq/Eq.kt
@@ -10,13 +10,13 @@ import io.kotest.assertions.show.show
  * This equality typeclass is at the heart of the shouldBe matcher.
  */
 interface Eq<T> {
-   fun equals(actual: T, expected: T): Throwable?
+   fun equals(actual: T, expected: T, strictNumberEq: Boolean = false): Throwable?
 }
 
 /**
  * Locates the applicable [Eq] for the inputs, and invokes it, returning the error if any.
  */
-fun <T : Any?> eq(actual: T, expected: T): Throwable? {
+fun <T : Any?> eq(actual: T, expected: T, strictNumberEq: Boolean = false): Throwable? {
    // if we have null and non null, usually that's a failure, but people can override equals to allow it
    return when {
       actual === expected -> null
@@ -24,16 +24,16 @@ fun <T : Any?> eq(actual: T, expected: T): Throwable? {
       actual == null && expected != null && actual != expected -> actualIsNull(expected)
       actual != null && expected == null && actual != expected -> expectedIsNull(actual)
       actual != null && expected != null -> when {
-         actual is Map<*, *> && expected is Map<*, *> -> MapEq.equals(actual, expected)
+         actual is Map<*, *> && expected is Map<*, *> -> MapEq.equals(actual, expected, strictNumberEq)
          actual is Regex && expected is Regex -> RegexEq.equals(actual, expected)
          actual is String && expected is String -> StringEq.equals(actual, expected)
-         actual is Number && expected is Number -> NumberEq.equals(actual, expected)
+         actual is Number && expected is Number -> NumberEq.equals(actual, expected, strictNumberEq)
          IterableEq.isValidIterable(actual) && IterableEq.isValidIterable(expected) -> {
-            IterableEq.equals(IterableEq.asIterable(actual), IterableEq.asIterable(expected))
+            IterableEq.equals(IterableEq.asIterable(actual), IterableEq.asIterable(expected), strictNumberEq)
          }
-         shouldShowDataClassDiff(actual, expected) -> DataClassEq.equals(actual as Any, expected as Any)
+         shouldShowDataClassDiff(actual, expected) -> DataClassEq.equals(actual as Any, expected as Any, strictNumberEq)
          actual is Throwable && expected is Throwable -> ThrowableEq.equals(actual, expected)
-         else -> DefaultEq.equals(actual as Any, expected as Any)
+         else -> DefaultEq.equals(actual as Any, expected as Any,strictNumberEq)
       }
       else -> null
    }
