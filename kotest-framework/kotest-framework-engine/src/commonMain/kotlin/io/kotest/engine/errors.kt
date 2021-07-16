3396e28118a336dbc0a7167e5ecabdd959db367e
sksamuel <sam@sksamuel.com>
2021-07-16 09:02:10 -0500

    Fixed tests

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/errors.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/errors.kt
@@ -7,4 +7,4 @@ data class ProjectTimeoutException(val timeout: Long) :
    Exception("Test suite did not complete with ${timeout / 1000} seconds")
 
 data class TestTimeoutException(val duration: Long, val testName: String) :
-   Exception("Test $testName did not complete within ${duration}ms")
+   Exception("Test '$testName' did not complete within ${duration}ms")
