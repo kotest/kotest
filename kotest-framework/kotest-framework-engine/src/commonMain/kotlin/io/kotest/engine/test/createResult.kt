ba8981da8d518e08a061a0fa425436a680a7d720
sksamuel <sam@sksamuel.com>
2021-07-31 12:49:44 -0500

    Simplified create test result

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/test/createResult.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/test/createResult.kt
@@ -2,6 +2,7 @@ package io.kotest.engine.test
 
 import io.kotest.core.test.TestResult
 import io.kotest.core.test.TestStatus
+import io.kotest.mpp.bestName
 import kotlin.jvm.JvmName
 
 /**
@@ -12,14 +13,20 @@ import kotlin.jvm.JvmName
  * If the throwable is either an [AssertionError] or one of the platform specific assertion types,
  * then a [TestStatus.Failure] will be returned, otherwise a [TestStatus.Error] will be returned.
  */
-expect fun createTestResult(duration: Long, error: Throwable?): TestResult
+fun createTestResult(duration: Long, error: Throwable?): TestResult = when {
+   error == null -> TestResult.success(duration)
+   error.isFrameworkAssertionType() -> TestResult.failure(error as AssertionError, duration)
+   error is AssertionError -> TestResult.failure(error, duration)
+   else -> TestResult.error(error, duration)
+}
 
 @JvmName("throwableToTestResult")
 fun Throwable.toTestResult(duration: Long): TestResult = createTestResult(duration, this)
 
-internal fun defaultCreateResult(duration: Long, error: Throwable?) = when (error) {
-   null -> TestResult.success(duration)
-   is AssertionError -> TestResult.failure(error, duration)
-   else -> TestResult.error(error, duration)
-}
-
+fun Throwable.isFrameworkAssertionType() =
+   listOf(
+      "org.opentest4j.AssertionFailedError",
+      "AssertionFailedError",
+      "org.junit.ComparisonFailure",
+      "ComparisonFailure"
+   ).contains(this::class.bestName())
--- kotest-framework/kotest-framework-engine/src/jsMain/kotlin/io/kotest/engine/test/createResult.kt
+++
@@ -1,6 +0,0 @@
-package io.kotest.engine.test
-
-import io.kotest.core.test.TestResult
-
-actual fun createTestResult(duration: Long, error: Throwable?): TestResult =
-   defaultCreateResult(duration, error)
