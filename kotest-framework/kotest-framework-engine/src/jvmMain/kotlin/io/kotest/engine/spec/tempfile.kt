96a3e881ff50f9b3fc5c400a23082792f6053647
sksamuel <sam@sksamuel.com>
2021-08-01 11:40:49 -0500

    tempfile and tempdir should fail test when deletion fails #2351

--- kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/spec/tempfile.kt
+++ kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/spec/tempfile.kt
@@ -7,7 +7,10 @@ import java.nio.file.Files
 fun TestConfiguration.tempfile(prefix: String? = javaClass.name, suffix: String? = ".tmp"): File {
    val file = Files.createTempFile(prefix, suffix).toFile()
    afterSpec {
-      file.delete()
+      if (!file.delete())
+         throw TempFileDeletionException(file)
    }
    return file
 }
+
+class TempFileDeletionException(val file: File) : Exception("Temp file '$file' could not be deleted")
