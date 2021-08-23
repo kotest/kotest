ab24fd009d6ff83a275090fef27dc2cf6a7a2156
Nico Richard <nicolas.richard@skipthedishes.ca>
2021-08-23 15:51:21 -0500

    improve tests

--- kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/PropTestConfigTest.kt
+++ kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/PropTestConfigTest.kt
@@ -5,254 +5,291 @@ import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.PropTestConfig
 import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.orNull
+import io.kotest.property.checkAll
 import io.kotest.property.internal.proptest
 
 class PropTestConfigTest : FunSpec() {
    init {
+      val defaultIterations = 1000
+
       test("PropTestConfig iterations should be used by proptest1 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations)
-         ) {
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               1000,
+               Arb.int(),
+               PropTestConfig(iterations = iterations)
+            ) {
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
 
       test("PropTestConfig iterations should be used by proptest2 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations)
-         ) { _, _ ->
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               1000,
+               Arb.int(),
+               Arb.int(),
+               PropTestConfig(iterations = iterations)
+            ) { _, _ ->
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
 
       test("PropTestConfig iterations should be used by proptest3 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations))
-         { _, _, _ ->
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               1000,
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               PropTestConfig(iterations = iterations)
+            )
+            { _, _, _ ->
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
 
       test("PropTestConfig iterations should be used by proptest4 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations))
-         { _, _, _, _ ->
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               1000,
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               PropTestConfig(iterations = iterations)
+            )
+            { _, _, _, _ ->
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
 
       test("PropTestConfig iterations should be used by proptest5 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations))
-         { _, _, _, _, _ ->
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               1000,
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               PropTestConfig(iterations = iterations)
+            )
+            { _, _, _, _, _ ->
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
 
       test("PropTestConfig iterations should be used by proptest6 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations))
-         { _, _, _, _, _, _ ->
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               1000,
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               PropTestConfig(iterations = iterations)
+            )
+            { _, _, _, _, _, _ ->
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
 
       test("PropTestConfig iterations should be used by proptest7 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations))
-         { _, _, _, _, _, _, _ ->
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               1000,
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               PropTestConfig(iterations = iterations)
+            )
+            { _, _, _, _, _, _, _ ->
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
 
       test("PropTestConfig iterations should be used by proptest8 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations))
-         { _, _, _, _, _, _, _, _ ->
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               1000,
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               PropTestConfig(iterations = iterations)
+            )
+            { _, _, _, _, _, _, _, _ ->
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
 
       test("PropTestConfig iterations should be used by proptest9 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations))
-         { _, _, _, _, _, _, _, _, _ ->
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               1000,
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               PropTestConfig(iterations = iterations)
+            )
+            { _, _, _, _, _, _, _, _, _ ->
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
 
       test("PropTestConfig iterations should be used by proptest10 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations))
-         { _, _, _, _, _, _, _, _, _, _ ->
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               1000,
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               PropTestConfig(iterations = iterations)
+            )
+            { _, _, _, _, _, _, _, _, _, _ ->
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
 
       test("PropTestConfig iterations should be used by proptest11 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations))
-         { _, _, _, _, _, _, _, _, _, _, _ ->
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               1000,
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               PropTestConfig(iterations = iterations)
+            )
+            { _, _, _, _, _, _, _, _, _, _, _ ->
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
 
       test("PropTestConfig iterations should be used by proptest12 if present") {
-         val expectedIterations = 10
+         checkAll(Arb.int(1..1000).orNull()) { iterations ->
+            val expectedIterations = iterations ?: defaultIterations
 
-         var iterationCount = 0
-         proptest(
-            1000,
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            Arb.int(),
-            PropTestConfig(iterations = expectedIterations))
-         { _, _, _, _, _, _, _, _, _, _, _, _ ->
-            iterationCount++
+            var iterationCount = 0
+            proptest(
+               defaultIterations,
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               Arb.int(),
+               PropTestConfig(iterations = iterations))
+            { _, _, _, _, _, _, _, _, _, _, _, _ ->
+               iterationCount++
+            }
+
+            iterationCount shouldBe expectedIterations
          }
-
-         iterationCount shouldBe expectedIterations
       }
    }
 }
