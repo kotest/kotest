30c03c16c8f24358843102a53e55baade7567485
Jim Schneidereit <schneidereit@pm.me>
2021-08-31 07:55:47 -0700

    add test for #2447

---
+++ kotest-assertions/kotest-assertions-shared/src/jvmTest/kotlin/io/kotest/assertions/CluesTests.kt
@@ -0,0 +1,19 @@
+package io.kotest.assertions
+
+import io.kotest.core.spec.style.FunSpec
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+class CluesTests : FunSpec({
+   test("withClue should not fail on coroutine thread switch") {
+      withContext(Dispatchers.Unconfined) {
+         withClue("should not fail") {
+            Thread.currentThread().run { println("withClue block begins on $name, id $id") }
+            delay(10)
+            Thread.currentThread().run { println("withClue block ends on $name, id $id") }
+         }
+      }
+   }
+})
