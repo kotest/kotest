63b9a486e987d0c1f24fc06b5f35a80806945198
sksamuel <sam@sksamuel.com>
2021-09-07 02:39:16 -0500

    Working JS engine

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/TestSuiteScheduler.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/TestSuiteScheduler.kt
@@ -1,7 +1,10 @@
 package io.kotest.engine
 
 import io.kotest.common.ExperimentalKotest
+import io.kotest.engine.listener.NoopTestEngineListener
 import io.kotest.engine.listener.TestEngineListener
+import io.kotest.engine.spec.SpecExecutor
+import io.kotest.mpp.log
 
 /**
  * A [TestSuiteScheduler] is responsible for launching each spec from a [TestSuite] into a coroutine.
@@ -9,6 +12,21 @@ import io.kotest.engine.listener.TestEngineListener
 @ExperimentalKotest
 interface TestSuiteScheduler {
    suspend fun schedule(suite: TestSuite, listener: TestEngineListener): EngineResult
+}
+
+/**
+ * A [TestSuiteScheduler] that launches specs sequentially in a loop.
+ */
+object LoopingTestSuiteScheduler : TestSuiteScheduler {
+
+   override suspend fun schedule(suite: TestSuite, listener: TestEngineListener): EngineResult {
+      log { "LoopingTestSuiteScheduler: Executing ${suite.specs} specs" }
+      suite.specs.forEach {
+         val executor = SpecExecutor(NoopTestEngineListener)
+         executor.execute(it)
+      }
+      return EngineResult(emptyList())
+   }
 }
 
 /**
--- kotest-framework/kotest-framework-engine/src/jsMain/kotlin/io/kotest/engine/createTestSuiteScheduler.kt
+++ kotest-framework/kotest-framework-engine/src/jsMain/kotlin/io/kotest/engine/createTestSuiteScheduler.kt
@@ -1,36 +1,6 @@
 package io.kotest.engine
 
-import io.kotest.core.spec.SpecRef
-import io.kotest.core.spec.toDescription
-import io.kotest.engine.listener.TestEngineListener
-import io.kotest.mpp.log
-
 /**
  * Returns the appropriate TestSuiteScheduler for the platform.
  */
-actual fun createTestSuiteScheduler(): TestSuiteScheduler = JsTestSuiteScheduler
-
-/**
- * A [TestSuiteScheduler] that launches specs sequentially.
- */
-object JsTestSuiteScheduler : TestSuiteScheduler {
-
-   override suspend fun schedule(suite: TestSuite, listener: TestEngineListener): EngineResult {
-      log { "JsTestSuiteScheduler: Executing ${suite.specs} specs" }
-      execute(suite.specs)
-      return EngineResult(emptyList())
-   }
-
-   private suspend fun execute(specs: List<SpecRef>) {
-      specs.forEach {
-         describe(it.instance().getOrThrow()::class.toDescription().displayName() + "_qqqq") {
-            it("gggggg") { done -> done(null) }
-         }
-      }
-
-//      specs.forEach {
-//         val executor = SpecExecutor(NoopTestEngineListener)
-//         executor.execute(it)
-//      }
-   }
-}
+actual fun createTestSuiteScheduler(): TestSuiteScheduler = LoopingTestSuiteScheduler
