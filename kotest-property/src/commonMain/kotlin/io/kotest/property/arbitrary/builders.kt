73e7fc79faad0ee488b496cd23d542222018c83b
Mitchell Yuwono <myuwono@atlassian.com>
2021-09-11 22:56:10 +1000

    introduce continuation passing style arbitrary builder

--- kotest-property/src/commonMain/kotlin/io/kotest/property/arbitrary/builders.kt
+++ kotest-property/src/commonMain/kotlin/io/kotest/property/arbitrary/builders.kt
@@ -6,13 +6,47 @@ import io.kotest.property.RandomSource
 import io.kotest.property.Sample
 import io.kotest.property.Shrinker
 import io.kotest.property.sampleOf
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.RestrictsSuspension
+import kotlin.coroutines.intrinsics.COROUTINE_SUSPENDED
+import kotlin.coroutines.intrinsics.suspendCoroutineUninterceptedOrReturn
+import kotlin.coroutines.resume
+import kotlin.coroutines.startCoroutine
 
 /**
  * Creates a new [Arb] that performs no shrinking, has no edge cases and
  * generates values from the given function.
  */
-fun <A> arbitrary(fn: (RandomSource) -> A): Arb<A> =
-   arbitrary(emptyList(), fn)
+fun <A> arbitrary(fn: suspend ArbitraryBuilderSyntax.(RandomSource) -> A): Arb<A> =
+   arbitraryBuilder { rs -> fn(rs) }
+
+/**
+ * Creates a new [Arb] that performs shrinking using the supplied [Shrinker], has no edge cases and
+ * generates values from the given function.
+ */
+fun <A> arbitrary(shrinker: Shrinker<A>, fn: suspend ArbitraryBuilderSyntax.(RandomSource) -> A): Arb<A> =
+   arbitraryBuilder(shrinker) { rs -> fn(rs) }
+
+/**
+ * Creates a new [Arb] that classifies the generated values using the supplied [Classifier], has no edge cases and
+ * generates values from the given function.
+ */
+fun <A> arbitrary(classifier: Classifier<A>, fn: suspend ArbitraryBuilderSyntax.(RandomSource) -> A): Arb<A> =
+   arbitraryBuilder(null, classifier) { rs -> fn(rs) }
+
+/**
+ * Creates a new [Arb] that performs shrinking using the supplied [Shrinker],
+ * classifies the generated values using the supplied [Classifier], has no edge cases and
+ * generates values from the given function.
+ */
+fun <A> arbitrary(
+   shrinker: Shrinker<A>,
+   classifier: Classifier<A>,
+   fn: suspend ArbitraryBuilderSyntax.(RandomSource) -> A
+): Arb<A> =
+   arbitraryBuilder(shrinker, classifier) { rs -> fn(rs) }
 
 /**
  * Creates a new [Arb] that performs no shrinking, uses the given edge cases and
@@ -56,12 +90,36 @@ fun <A> arbitrary(
       override fun sample(rs: RandomSource): Sample<A> = sampleOf(sampleFn(rs), shrinker)
    }
 
-/**
- * Creates a new [Arb] that performs shrinking using the supplied [Shrinker], has no edge cases and
- * generates values from the given function.
- */
-fun <A> arbitrary(shrinker: Shrinker<A>, fn: (RandomSource) -> A): Arb<A> =
-   arbitrary(emptyList(), shrinker, fn)
+fun <A> arbitraryBuilder(
+   shrinker: Shrinker<A>? = null,
+   classifier: Classifier<A>? = null,
+   edgecaseFn: EdgecaseFn<A>? = null,
+   builderFn: suspend ArbitraryBuilderSyntax.(RandomSource) -> A
+): Arb<A> = object : Arb<A>() {
+   override fun edgecase(rs: RandomSource): A? = computeArb().edgecase(rs)
+   override fun sample(rs: RandomSource): Sample<A> = computeArb().sample(rs)
+
+   private fun computeArb(): Arb<A> {
+      val continuation = ArbContinuation<A>()
+      val wrapReturn: suspend ArbitraryBuilderSyntax.() -> Arb<A> = {
+         val value: A = builderFn(randomSource.bind())
+         ArbitraryBuilder(
+            { value },
+            classifier,
+            shrinker,
+            { rs ->
+               // use edgecase function if provided, otherwise retain those from flatmap compositions
+               if (edgecaseFn != null) edgecaseFn.invoke(rs) else value
+            }
+         ).build()
+      }
+      wrapReturn.startCoroutine(continuation, continuation)
+      return continuation.returnedArb()
+   }
+
+   // passthrough arb to extract the propagated RandomSource
+   private val randomSource: Arb<RandomSource> = ArbitraryBuilder.create { it }.withEdgecaseFn { it }.build()
+}
 
 typealias SampleFn<A> = (RandomSource) -> A
 typealias EdgecaseFn<A> = (RandomSource) -> A?
@@ -90,5 +148,28 @@ class ArbitraryBuilder<A>(
          val sample = sampleFn(rs)
          return if (shrinker == null) Sample(sample) else sampleOf(sample, shrinker)
       }
+   }
+}
+
+@RestrictsSuspension
+interface ArbitraryBuilderSyntax {
+   suspend fun <T> Arb<T>.bind(): T
+}
+
+@Suppress("UNCHECKED_CAST")
+private class ArbContinuation<A> : Continuation<Arb<A>>, ArbitraryBuilderSyntax {
+   override val context: CoroutineContext = EmptyCoroutineContext
+   private lateinit var returnedArb: Arb<A>
+
+   fun returnedArb(): Arb<A> = returnedArb
+
+   override fun resumeWith(result: Result<Arb<A>>) = result.map { returnedArb = it }.getOrThrow()
+
+   override suspend fun <B> Arb<B>.bind(): B = suspendCoroutineUninterceptedOrReturn { c ->
+      returnedArb = this.flatMap { b: B ->
+         c.resume(b)
+         returnedArb
+      }
+      COROUTINE_SUSPENDED
    }
 }
--- kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/arbitrary/BuilderTest.kt
+++ kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/arbitrary/BuilderTest.kt
@@ -1,15 +1,25 @@
 package com.sksamuel.kotest.property.arbitrary
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.ints.shouldBeBetween
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldHaveLengthBetween
 import io.kotest.property.Arb
+import io.kotest.property.RandomSource
+import io.kotest.property.arbitrary.Codepoint
+import io.kotest.property.arbitrary.alphanumeric
 import io.kotest.property.arbitrary.arbitrary
+import io.kotest.property.arbitrary.arbitraryBuilder
+import io.kotest.property.arbitrary.edgecases
+import io.kotest.property.arbitrary.flatMap
 import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.map
 import io.kotest.property.arbitrary.next
 import io.kotest.property.arbitrary.string
 import io.kotest.property.arbitrary.take
+import io.kotest.property.arbitrary.withEdgecases
 import kotlin.random.nextInt
 
 class BuilderTest : FunSpec() {
@@ -32,6 +42,55 @@ class BuilderTest : FunSpec() {
 
          personArb.next().name.shouldHaveLengthBetween(10, 12)
          personArb.next().age.shouldBeBetween(21, 150)
+      }
+
+      context("arbitrary builder using continuation") {
+         test("should be equivalent to chaining flatMaps") {
+            val arbFlatMaps: Arb<String> =
+               Arb.string(5, Codepoint.alphanumeric()).withEdgecases("edge1", "edge2").flatMap { first ->
+                  Arb.int(1..9).withEdgecases(5).flatMap { second ->
+                     Arb.int(101..109).withEdgecases(100 + second).map { third ->
+                        "$first $second $third"
+                     }
+                  }
+               }
+
+            val arb: Arb<String> = arbitrary {
+               val first = Arb.string(5, Codepoint.alphanumeric()).withEdgecases("edge1", "edge2").bind()
+               val second = Arb.int(1..9).withEdgecases(5).bind()
+               val third = Arb.int(101..109).withEdgecases(100 + second).bind()
+               "$first $second $third"
+            }
+
+            val flatMapsResult = arbFlatMaps.generate(RandomSource.seeded(12345L)).take(100).map { it.value }.toList()
+            val builderResult = arb.generate(RandomSource.seeded(12345L)).take(100).map { it.value }.toList()
+
+            // should be equivalent
+            builderResult shouldContainExactly flatMapsResult
+         }
+
+         test("should bind edgecases") {
+            val arb: Arb<String> = arbitrary {
+               val first = Arb.string(5, Codepoint.alphanumeric()).withEdgecases("edge1", "edge2").bind()
+               val second = Arb.int(1..9).withEdgecases(5).bind()
+               val third = Arb.int(101..109).withEdgecases(100 + second, 109).bind()
+               "$first $second $third"
+            }
+
+            arb.edgecases() shouldContainExactlyInAnyOrder setOf(
+               "edge1 5 105",
+               "edge2 5 105",
+               "edge1 5 109",
+               "edge2 5 109",
+            )
+         }
+
+         test("should modify edgecases") {
+            val edges = setOf("edge1", "edge2")
+            val arb = arbitraryBuilder(edgecaseFn = { edges.random(it.random) }) { "abcd" }
+
+            arb.edgecases() shouldContainExactlyInAnyOrder edges
+         }
       }
    }
 }
