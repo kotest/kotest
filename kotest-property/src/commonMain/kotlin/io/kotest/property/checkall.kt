d940a454d593b446c7bf2130e462c0a6f054608e
sksamuel <sam@sksamuel.com>
2021-09-19 20:45:12 -0500

    Added hook for shrinking

--- kotest-property/src/commonMain/kotlin/io/kotest/property/checkall.kt
+++ kotest-property/src/commonMain/kotlin/io/kotest/property/checkall.kt
@@ -1,18 +1,31 @@
 package io.kotest.property
 
+// todo have these methods delegate to one that uses PropTestConfig
 fun checkAll(iterations: Int, seed: Long, test: PropertyContext.() -> Unit): PropertyContext {
    val context = PropertyContext(RandomSource.seeded(seed))
    repeat(iterations) {
-      context.test()
-      context.reset()
+      try {
+         context.test()
+      } catch (t: Throwable) {
+         // shrink here then throw
+      } finally {
+         context.reset()
+      }
    }
    return context
 }
 
+// todo have these methods delegate to one that uses PropTestConfig
 fun checkAll(iterations: Int, test: PropertyContext.() -> Unit): PropertyContext {
    val context = PropertyContext(RandomSource.default())
    repeat(iterations) {
-      context.test()
+      try {
+         context.test()
+      } catch (t: Throwable) {
+         // shrink here then throw
+      } finally {
+         context.reset()
+      }
       context.reset()
    }
    return context
