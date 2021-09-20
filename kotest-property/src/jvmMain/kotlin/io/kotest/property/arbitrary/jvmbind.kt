0c73a164b717d2cdbe31af6ea4b620ebb51fda3b
Sam Sam <sam@sksamuel.com>
2021-09-19 20:46:31 -0500

    Property testing should use bind as default for data class #2355 (#2526)

--- kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/jvmbind.kt
+++ kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/jvmbind.kt
@@ -14,13 +14,24 @@ import kotlin.reflect.full.primaryConstructor
  * If your class has more complex requirements, you can use Arb.bind(gen1, gen2...) where
  * the parameter generators are supplied programatically.
  */
-inline fun <reified T : Any> Arb.Companion.bind(): Arb<T> {
-   val kclass = T::class
+inline fun <reified T : Any> Arb.Companion.bind(): Arb<T> = bind(T::class)
+
+/**
+ * Returns an [Arb] where each value is a randomly created instance of [T].
+ * These instances are created by selecting the primaryConstructor of T and then
+ * auto-detecting a generator for each parameter of that constructor.
+ * T must be a class type.
+ *
+ * Note: This method only supports "basic" parameter types - string, boolean and so on.
+ * If your class has more complex requirements, you can use Arb.bind(gen1, gen2...) where
+ * the parameter generators are supplied programatically.
+ */
+fun <T : Any> Arb.Companion.bind(kclass: KClass<T>): Arb<T> {
    val constructor = kclass.primaryConstructor ?: error("could not locate a primary constructor")
    check(constructor.parameters.isNotEmpty()) { "${kclass.qualifiedName} constructor must contain at least 1 parameter" }
 
    val arbs: List<Arb<Any>> = constructor.parameters.map { param ->
-      defaultForClass<Any>(param.type.classifier as KClass<*>)
+      defaultForClass(param.type.classifier as KClass<*>)
          ?: error("Could not locate generator for parameter ${kclass.qualifiedName}.${param.name}")
    }
 
