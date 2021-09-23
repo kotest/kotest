fc5312cdc76dc3b88ce7d464b6bc3aa10e99e10a
sksamuel <sam@sksamuel.com>
2021-09-23 05:54:52 -0500

    Fixed instantiate

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/extensions/PostInstantiationExtension.kt
+++ kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/extensions/PostInstantiationExtension.kt
@@ -9,6 +9,12 @@ import io.kotest.core.spec.Spec
  * This extension allows the spec to be replaced with another instance of the same type, which can
  * be useful for proxying or dependency injection.
  *
+ * This extension is invoked regardless of whether the spec has any tests, or if the spec
+ * was inactive. It is executed every single time a spec is created, so for isolation modes that result
+ * in the same spec being instantiated multiple times, this extension will be invoked multiple times.
+ *
+ * Any changes to the coroutine context will not be propagated downstream.
+ *
  * Note: This extension invoked only when classes are created by Kotest and is therefore
  * restricted to JVM platforms. This restriction may be dropped in future releases.
  */
