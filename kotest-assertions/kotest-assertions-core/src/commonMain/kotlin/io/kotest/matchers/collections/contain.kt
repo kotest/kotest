37dc649fd85a1961c955e4aaf6bf1aae64a5bb60
sksamuel <sam@sksamuel.com>
2021-10-19 16:20:34 -0500

    Updated iterables.shouldContain to chain

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/contain.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/contain.kt
@@ -6,13 +6,35 @@ import io.kotest.matchers.MatcherResult
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldNot
 
-infix fun <T> Iterable<T>.shouldNotContain(t: T) = toList().shouldNotContain(t)
-infix fun <T> Array<T>.shouldNotContain(t: T) = asList().shouldNotContain(t)
-infix fun <T, C : Collection<T>> C.shouldNotContain(t: T) = this shouldNot contain(t)
+infix fun <T> Iterable<T>.shouldNotContain(t: T): Iterable<T> {
+   toList().shouldNotContain(t)
+   return this
+}
 
-infix fun <T> Iterable<T>.shouldContain(t: T) = toList().shouldContain(t)
-infix fun <T> Array<T>.shouldContain(t: T) = asList().shouldContain(t)
-infix fun <T, C : Collection<T>> C.shouldContain(t: T) = this should contain(t)
+infix fun <T> Array<T>.shouldNotContain(t: T): Array<T> {
+   asList().shouldNotContain(t)
+   return this
+}
+
+infix fun <T, C : Collection<T>> C.shouldNotContain(t: T): C {
+   this shouldNot contain(t)
+   return this
+}
+
+infix fun <T> Iterable<T>.shouldContain(t: T): Iterable<T> {
+   toList().shouldContain(t)
+   return this
+}
+
+infix fun <T> Array<T>.shouldContain(t: T): Array<T> {
+   asList().shouldContain(t)
+   return this
+}
+
+infix fun <T, C : Collection<T>> C.shouldContain(t: T): C {
+   this should contain(t)
+   return this
+}
 
 fun <T, C : Collection<T>> contain(t: T) = object : Matcher<C> {
    override fun test(value: C) = MatcherResult(
