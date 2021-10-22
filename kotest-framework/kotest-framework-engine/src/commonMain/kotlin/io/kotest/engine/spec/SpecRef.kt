54fe92611af739c541e55d36ffb14a4a974c27c9
sksamuel <sam@sksamuel.com>
2021-10-21 22:38:26 -0500

    Fixed tests

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/spec/SpecRef.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/spec/SpecRef.kt
@@ -1,18 +1,22 @@
 package io.kotest.engine.spec
 
+import io.kotest.common.ExperimentalKotest
 import io.kotest.core.spec.Spec
 import io.kotest.core.spec.SpecRef
 import kotlin.reflect.KClass
 
-internal data class ReflectiveSpecRef(override val kclass: KClass<out Spec>) : SpecRef {
+@ExperimentalKotest
+data class ReflectiveSpecRef(override val kclass: KClass<out Spec>) : SpecRef {
    override suspend fun instance(): Result<Spec> = instantiate(kclass)
 }
 
-internal data class FunctionSpecRef(val fn: () -> Spec, override val kclass: KClass<out Spec>) : SpecRef {
+@ExperimentalKotest
+data class FunctionSpecRef(val fn: () -> Spec, override val kclass: KClass<out Spec>) : SpecRef {
    override suspend fun instance(): Result<Spec> = Result.success(fn())
 }
 
-internal data class InstanceSpecRef(val spec: Spec) : SpecRef {
+@ExperimentalKotest
+data class InstanceSpecRef(val spec: Spec) : SpecRef {
    override val kclass: KClass<out Spec> = spec::class
    override suspend fun instance(): Result<Spec> = Result.success(spec)
 }
