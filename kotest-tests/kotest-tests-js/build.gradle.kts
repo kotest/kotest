ff3a2034ad03046715c326997f94ef1d19a56151
sksamuel <sam@sksamuel.com>
2021-10-26 06:54:10 -0500

    Updated js timeout

--- kotest-tests/kotest-tests-js/build.gradle.kts
+++ kotest-tests/kotest-tests-js/build.gradle.kts
@@ -40,8 +40,8 @@ kotlin {
 }
 
 // must be a published version and not one in the current build
-//configure<io.kotest.framework.multiplatform.gradle.KotestPluginExtension> {
-//   compilerPluginVersion.set("5.0.0.399-SNAPSHOT")
-//}
+configure<io.kotest.framework.multiplatform.gradle.KotestPluginExtension> {
+   compilerPluginVersion.set("5.0.0.624-SNAPSHOT")
+}
 
 apply(from = "../../nopublish.gradle")
