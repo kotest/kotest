2809c832e1333641cf480525eea45b037fc173ff
sksamuel <sam@sksamuel.com>
2021-10-26 08:24:59 -0500

    Updated all coroutine logging tests

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/interceptors/CoroutineLoggingInterceptorTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/interceptors/CoroutineLoggingInterceptorTest.kt
@@ -10,11 +10,14 @@ import io.kotest.engine.test.logging.LogExtension
 import io.kotest.engine.test.logging.debug
 import io.kotest.engine.test.logging.error
 import io.kotest.engine.test.logging.info
+import io.kotest.engine.test.logging.trace
 import io.kotest.engine.test.logging.warn
 import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 import kotlin.coroutines.coroutineContext
 
+@ExperimentalKotest
 class DatabaseLogExtension : LogExtension {
    val database = mutableListOf<String>()
    override suspend fun handleLogs(testCase: TestCase, logs: List<LogEntry>) {
@@ -47,6 +50,26 @@ class TestWithFailingLog : FunSpec() {
 }
 
 @ExperimentalKotest
+class ShouldLogWithTestAndProjectLevelExtensions : FunSpec() {
+   init {
+      val db1 = DatabaseLogExtension()
+      val db2 = DatabaseLogExtension()
+      register(db1)
+      beforeTest {
+         coroutineContext.configuration.logLevel = LogLevel.Debug
+         coroutineContext.configuration.registry().add(db2)
+      }
+      test("should log to project and test extensions") {
+         debug { "wobble" }
+      }
+      afterTest {
+         db1.database.shouldContain("wobble")
+         db2.database.shouldContain("wobble")
+      }
+   }
+}
+
+@ExperimentalKotest
 class TestIgnoreWhenLoggingOff : FunSpec() {
    init {
       val database = DatabaseLogExtension()
@@ -66,92 +89,109 @@ class TestIgnoreWhenLoggingOff : FunSpec() {
    }
 }
 
-class CoroutineLoggingInterceptorTest : FunSpec({
-//
-//   context("ignores logs lower priority than ERROR by config") {
-//      reset(LogLevel.Error)
-//
-//      test("execute") {
-//         info { "info" }
-//         warn { "warn" }
-//         error { "error" }
-//         debug { "debug" }
-//      }
-//
-//      all {
-//         console.logs.shouldContainInOrder("error")
-//         database.logs.shouldContainInOrder("error")
-//      }
-//   }
-//
-//   context("ignores logs lower priority than WARN by config") {
-//      reset(LogLevel.Warn)
-//
-//      test("execute") {
-//         info { "info" }
-//         warn { "warn" }
-//         error { "error" }
-//         debug { "debug" }
-//      }
-//
-//      val expected = arrayOf("warn", "error")
-//
-//      all {
-//         console.logs.shouldContainInOrder(*expected)
-//         database.logs.shouldContainInOrder(*expected)
-//      }
-//   }
-//
-//   context("ignores logs lower priority than INFO by config") {
-//      reset(LogLevel.Debug)
-//
-//      test("execute") {
-//         info { "info" }
-//         warn { "warn" }
-//         error { "error" }
-//         debug { "debug" }
-//      }
-//
-//      val expected = arrayOf("info", "warn", "error")
-//
-//      all {
-//         console.logs.shouldContainInOrder(*expected)
-//         database.logs.shouldContainInOrder(*expected)
-//      }
-//   }
-//
-//   context("accepts all logs when DEBUG by config") {
-//      reset(LogLevel.Debug)
-//
-//      test("execute") {
-//         info { "info" }
-//         warn { "warn" }
-//         error { "error" }
-//         debug { "debug" }
-//      }
-//
-//      val expected = arrayOf("info", "warn", "error", "debug")
-//
-//      all {
-//         console.logs.shouldContainInOrder(*expected)
-//         database.logs.shouldContainInOrder(*expected)
-//      }
-//   }
-//
-//   context("accepts all logs when TRACE by config using TestLogger") {
-//      reset(LogLevel.Trace)
-//
-//      test("execute logs") {
-//         logAll(logger!!)
-//      }
-//
-//      val expected = arrayOf("info", "warn", "error", "debug", "trace")
-//
-//      all {
-//         console.logs.shouldContainInOrder(*expected)
-//         database.logs.shouldContainInOrder(*expected)
-//      }
-//   }
-})
+@ExperimentalKotest
+class TestErrorLogLevel : FunSpec() {
+   init {
+      val database = DatabaseLogExtension()
+      register(database)
+      beforeTest {
+         coroutineContext.configuration.logLevel = LogLevel.Error
+      }
+      test("ignores logs lower priority than ERROR by config") {
+         info { "info" }
+         warn { "warn" }
+         error { "error" }
+         debug { "debug" }
+         trace { "trace" }
+      }
+      afterTest {
+         database.database.shouldBe(listOf("error"))
+      }
+   }
+}
+
+@ExperimentalKotest
+class TestDebugLogLevel : FunSpec() {
+   init {
+      val database = DatabaseLogExtension()
+      register(database)
+      beforeTest {
+         coroutineContext.configuration.logLevel = LogLevel.Debug
+      }
+      test("ignores logs lower priority than DEBUG by config") {
+         info { "info" }
+         warn { "warn" }
+         error { "error" }
+         debug { "debug" }
+         trace { "trace" }
+      }
+      afterTest {
+         database.database.shouldBe(listOf("info", "warn", "error", "debug"))
+      }
+   }
+}
+
+@ExperimentalKotest
+class TestTraceLogLevel : FunSpec() {
+   init {
+      val database = DatabaseLogExtension()
+      register(database)
+      beforeTest {
+         coroutineContext.configuration.logLevel = LogLevel.Trace
+      }
+      test("ignores logs lower priority than TRACE by config") {
+         info { "info" }
+         warn { "warn" }
+         error { "error" }
+         debug { "debug" }
+         trace { "trace" }
+      }
+      afterTest {
+         database.database.shouldBe(listOf("info", "warn", "error", "debug", "trace"))
+      }
+   }
+}
+
+@ExperimentalKotest
+class TestInfoLogLevel : FunSpec() {
+   init {
+      val database = DatabaseLogExtension()
+      register(database)
+      beforeTest {
+         coroutineContext.configuration.logLevel = LogLevel.Info
+      }
+      test("ignores logs lower priority than INFO by config") {
+         info { "info" }
+         warn { "warn" }
+         error { "error" }
+         debug { "debug" }
+         trace { "trace" }
+      }
+      afterTest {
+         database.database.shouldBe(listOf("info", "warn", "error"))
+      }
+   }
+}
+
+@ExperimentalKotest
+class TestWarnLogLevel : FunSpec() {
+   init {
+      val database = DatabaseLogExtension()
+      register(database)
+      beforeTest {
+         coroutineContext.configuration.logLevel = LogLevel.Warn
+      }
+      test("ignores logs lower priority than INFO by config") {
+         info { "info" }
+         warn { "warn" }
+         error { "error" }
+         debug { "debug" }
+         trace { "trace" }
+      }
+      afterTest {
+         database.database.shouldBe(listOf("warn", "error"))
+      }
+   }
+}
 
 private class CannotLogException(override val message: String) : Exception()
