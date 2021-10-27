27206be2a1ba3a0e26436e092fa3d231c886f061
sksamuel <sam@sksamuel.com>
2021-10-27 04:01:30 -0500

    Updated location hint format

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/teamcity/locations.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/teamcity/locations.kt
@@ -7,13 +7,17 @@ import kotlin.reflect.KClass
 object Locations {
 
    fun locationHint(kclass: KClass<*>): String =
-      "kotest:class/" + kclass.bestName() + ":1"
+      "kotest:class://" + kclass.bestName() + ":1"
+
+   // note that everything before the :// is considered the "protocol" by the intellij plugin
+   private fun fileHint(fileName: String, lineNumber: Int) = "kotest:file://${fileName}:${lineNumber}"
+   private fun classHint(fqn: String, lineNumber: Int) = "kotest:class://${fqn}:${lineNumber}"
 
    fun locationHint(sourceRef: SourceRef): String? = when (sourceRef) {
-      is SourceRef.FileSource -> "kotest:file/${sourceRef.fileName}:1"
-      is SourceRef.FileLineSource -> "kotest:file/${sourceRef.fileName}:${sourceRef.lineNumber}"
-      is SourceRef.ClassLineSource -> "kotest:class/${sourceRef.fqn}:${sourceRef.lineNumber}"
-      is SourceRef.ClassSource -> "kotest:class/${sourceRef.fqn}:1"
+      is SourceRef.FileLineSource -> fileHint(sourceRef.fileName, sourceRef.lineNumber)
+      is SourceRef.FileSource -> fileHint(sourceRef.fileName, 1)
+      is SourceRef.ClassLineSource -> classHint(sourceRef.fqn, sourceRef.lineNumber)
+      is SourceRef.ClassSource -> classHint(sourceRef.fqn, 1)
       SourceRef.None -> null
    }
 }
