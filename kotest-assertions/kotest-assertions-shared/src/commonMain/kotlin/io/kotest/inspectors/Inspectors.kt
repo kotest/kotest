11eba378eb27cfdffd021d4ff80e57b61bba7a9c
sksamuel <sam@sksamuel.com>
2021-10-30 13:12:32 -0500

    Inspectors should return the collection to allow chaining #2588

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/inspectors/Inspectors.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/inspectors/Inspectors.kt
@@ -1,34 +1,68 @@
 package io.kotest.inspectors
 
-fun <T> Sequence<T>.forAll(fn: (T) -> Unit) = toList().forAll(fn)
-fun <T> Array<T>.forAll(fn: (T) -> Unit) = asList().forAll(fn)
-fun <T> Collection<T>.forAll(fn: (T) -> Unit) {
+fun <T> Sequence<T>.forAll(fn: (T) -> Unit): Sequence<T> {
+   toList().forAll(fn)
+   return this
+}
+
+fun <T> Array<T>.forAll(fn: (T) -> Unit): Array<T> {
+   asList().forAll(fn)
+   return this
+}
+
+fun <T, C : Collection<T>> C.forAll(fn: (T) -> Unit): C {
    val results = runTests(this, fn)
    val passed = results.filterIsInstance<ElementPass<T>>()
    if (passed.size < this.size) {
       val msg = "${passed.size} elements passed but expected ${this.size}"
       buildAssertionError(msg, results)
    }
+   return this
 }
 
-fun <T> Sequence<T>.forOne(fn: (T) -> Unit) = toList().forOne(fn)
-fun <T> Array<T>.forOne(fn: (T) -> Unit) = asList().forOne(fn)
-fun <T> Collection<T>.forOne(fn: (T) -> Unit) = forExactly(1, fn)
+fun <T> Sequence<T>.forOne(fn: (T) -> Unit): Sequence<T> {
+   toList().forOne(fn)
+   return this
+}
 
-fun <T> Sequence<T>.forExactly(k: Int, fn: (T) -> Unit) = toList().forExactly(k, fn)
-fun <T> Array<T>.forExactly(k: Int, fn: (T) -> Unit) = toList().forExactly(k, fn)
-fun <T> Collection<T>.forExactly(k: Int, fn: (T) -> Unit) {
+fun <T> Array<T>.forOne(fn: (T) -> Unit): Array<T> {
+   asList().forOne(fn)
+   return this
+}
+
+fun <T, C : Collection<T>> C.forOne(fn: (T) -> Unit): C = forExactly(1, fn)
+
+fun <T> Sequence<T>.forExactly(k: Int, fn: (T) -> Unit): Sequence<T> {
+   toList().forExactly(k, fn)
+   return this
+}
+
+fun <T> Array<T>.forExactly(k: Int, fn: (T) -> Unit): Array<T> {
+   toList().forExactly(k, fn)
+   return this
+}
+
+fun <T, C : Collection<T>> C.forExactly(k: Int, fn: (T) -> Unit): C {
    val results = runTests(this, fn)
    val passed = results.filterIsInstance<ElementPass<T>>()
    if (passed.size != k) {
-    val msg = "${passed.size} elements passed but expected $k"
-    buildAssertionError(msg, results)
-  }
+      val msg = "${passed.size} elements passed but expected $k"
+      buildAssertionError(msg, results)
+   }
+   return this
 }
 
-fun <T> Sequence<T>.forSome(fn: (T) -> Unit) = toList().forSome(fn)
-fun <T> Array<T>.forSome(fn: (T) -> Unit) = toList().forSome(fn)
-fun <T> Collection<T>.forSome(fn: (T) -> Unit) {
+fun <T> Sequence<T>.forSome(fn: (T) -> Unit): Sequence<T> {
+   toList().forSome(fn)
+   return this
+}
+
+fun <T> Array<T>.forSome(fn: (T) -> Unit): Array<T> {
+   toList().forSome(fn)
+   return this
+}
+
+fun <T, C : Collection<T>> C.forSome(fn: (T) -> Unit): C {
    val results = runTests(this, fn)
    val passed = results.filterIsInstance<ElementPass<T>>()
    if (passed.isEmpty()) {
@@ -36,49 +70,104 @@ fun <T> Collection<T>.forSome(fn: (T) -> Unit) {
    } else if (passed.size == size) {
       buildAssertionError("All elements passed but expected < $size", results)
    }
+   return this
 }
 
-fun <T> Sequence<T>.forAny(fn: (T) -> Unit) = toList().forAny(fn)
-fun <T> Array<T>.forAny(fn: (T) -> Unit) = toList().forAny(fn)
-fun <T> Collection<T>.forAny(fn: (T) -> Unit) = forAtLeastOne(fn)
+fun <T> Sequence<T>.forAny(fn: (T) -> Unit): Sequence<T> {
+   toList().forAny(fn)
+   return this
+}
 
-fun <T> Sequence<T>.forAtLeastOne(fn: (T) -> Unit) = toList().forAtLeastOne(fn)
-fun <T> Array<T>.forAtLeastOne(fn: (T) -> Unit) = toList().forAtLeastOne(fn)
-fun <T> Collection<T>.forAtLeastOne(f: (T) -> Unit) = forAtLeast(1, f)
+fun <T> Array<T>.forAny(fn: (T) -> Unit): Array<T> {
+   toList().forAny(fn)
+   return this
+}
 
-fun <T> Sequence<T>.forAtLeast(k: Int, fn: (T) -> Unit) = toList().forAtLeast(k, fn)
-fun <T> Array<T>.forAtLeast(k: Int, fn: (T) -> Unit) = toList().forAtLeast(k, fn)
-fun <T> Collection<T>.forAtLeast(k: Int, fn: (T) -> Unit) {
+fun <T, C : Collection<T>> C.forAny(fn: (T) -> Unit): C {
+   forAtLeastOne(fn)
+   return this
+}
+
+fun <T> Sequence<T>.forAtLeastOne(fn: (T) -> Unit): Sequence<T> {
+   toList().forAtLeastOne(fn)
+   return this
+}
+
+fun <T> Array<T>.forAtLeastOne(fn: (T) -> Unit): Array<T> {
+   toList().forAtLeastOne(fn)
+   return this
+}
+
+fun <T, C : Collection<T>> C.forAtLeastOne(f: (T) -> Unit) = forAtLeast(1, f)
+
+fun <T> Sequence<T>.forAtLeast(k: Int, fn: (T) -> Unit): Sequence<T> {
+   toList().forAtLeast(k, fn)
+   return this
+}
+
+fun <T> Array<T>.forAtLeast(k: Int, fn: (T) -> Unit): Array<T> {
+   toList().forAtLeast(k, fn)
+   return this
+}
+
+fun <T, C : Collection<T>> C.forAtLeast(k: Int, fn: (T) -> Unit): C {
    val results = runTests(this, fn)
    val passed = results.filterIsInstance<ElementPass<T>>()
    if (passed.size < k) {
-    val msg = "${passed.size} elements passed but expected at least $k"
-    buildAssertionError(msg, results)
-  }
+      val msg = "${passed.size} elements passed but expected at least $k"
+      buildAssertionError(msg, results)
+   }
+   return this
 }
 
-fun <T> Sequence<T>.forAtMostOne(fn: (T) -> Unit) = toList().forAtMostOne(fn)
-fun <T> Array<T>.forAtMostOne(fn: (T) -> Unit) = toList().forAtMostOne(fn)
-fun <T> Collection<T>.forAtMostOne(fn: (T) -> Unit) = forAtMost(1, fn)
+fun <T> Sequence<T>.forAtMostOne(fn: (T) -> Unit): Sequence<T> {
+   toList().forAtMostOne(fn)
+   return this
+}
 
-fun <T> Sequence<T>.forAtMost(k: Int, fn: (T) -> Unit) = toList().forAtMost(k, fn)
-fun <T> Array<T>.forAtMost(k: Int, fn: (T) -> Unit) = toList().forAtMost(k, fn)
-fun <T> Collection<T>.forAtMost(k: Int, fn: (T) -> Unit) {
+fun <T> Array<T>.forAtMostOne(fn: (T) -> Unit): Array<T> {
+   toList().forAtMostOne(fn)
+   return this
+}
+
+fun <T, C : Collection<T>> C.forAtMostOne(fn: (T) -> Unit) = forAtMost(1, fn)
+
+fun <T> Sequence<T>.forAtMost(k: Int, fn: (T) -> Unit): Sequence<T> {
+   toList().forAtMost(k, fn)
+   return this
+}
+
+fun <T> Array<T>.forAtMost(k: Int, fn: (T) -> Unit): Array<T> {
+   toList().forAtMost(k, fn)
+   return this
+}
+
+fun <T, C : Collection<T>> C.forAtMost(k: Int, fn: (T) -> Unit): C {
    val results = runTests(this, fn)
    val passed = results.filterIsInstance<ElementPass<T>>()
    if (passed.size > k) {
       val msg = "${passed.size} elements passed but expected at most $k"
       buildAssertionError(msg, results)
    }
+   return this
 }
 
-fun <T> Sequence<T>.forNone(fn: (T) -> Unit) = toList().forNone(fn)
-fun <T> Array<T>.forNone(fn: (T) -> Unit) = toList().forNone(fn)
-fun <T> Collection<T>.forNone(f: (T) -> Unit) {
+fun <T> Sequence<T>.forNone(fn: (T) -> Unit): Sequence<T> {
+   toList().forNone(fn)
+   return this
+}
+
+fun <T> Array<T>.forNone(fn: (T) -> Unit): Array<T> {
+   toList().forNone(fn)
+   return this
+}
+
+fun <T, C : Collection<T>> C.forNone(f: (T) -> Unit): C {
    val results = runTests(this, f)
    val passed = results.filterIsInstance<ElementPass<T>>()
    if (passed.isNotEmpty()) {
-    val msg = "${passed.size} elements passed but expected ${0}"
-    buildAssertionError(msg, results)
-  }
+      val msg = "${passed.size} elements passed but expected ${0}"
+      buildAssertionError(msg, results)
+   }
+   return this
 }
