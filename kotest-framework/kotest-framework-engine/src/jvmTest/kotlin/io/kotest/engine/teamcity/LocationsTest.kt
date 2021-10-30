d9715f5c985b82fefda638fd612c4ddd068f9f30
sksamuel <sam@sksamuel.com>
2021-10-30 10:37:59 -0500

    Added tests for locations

---
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/io/kotest/engine/teamcity/LocationsTest.kt
@@ -0,0 +1,24 @@
+package io.kotest.engine.teamcity
+
+import io.kotest.core.SourceRef
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class LocationsTest : FunSpec({
+
+   test("ClassSource hint") {
+      Locations.location(SourceRef.ClassSource("foo.bar")) shouldBe "kotest:class://foo.bar:1"
+   }
+
+   test("ClassLineSource hint") {
+      Locations.location(SourceRef.ClassLineSource("foo.bar", 34)) shouldBe "kotest:class://foo.bar:34"
+   }
+
+   test("FileSource hint") {
+      Locations.location(SourceRef.FileSource("foo.kt")) shouldBe "kotest:file://foo.kt:1"
+   }
+
+   test("FileLineSource hint") {
+      Locations.location(SourceRef.FileLineSource("foo.kt", 34)) shouldBe "kotest:file://foo.kt:34"
+   }
+})
