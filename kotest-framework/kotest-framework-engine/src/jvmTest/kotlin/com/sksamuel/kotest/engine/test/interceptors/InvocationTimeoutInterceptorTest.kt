0330dddd81a4d24f983b6a65936a4d209fc01be9
sksamuel <sam@sksamuel.com>
2021-11-03 05:58:54 -0500

    Fixed test

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/interceptors/InvocationTimeoutInterceptorTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/interceptors/InvocationTimeoutInterceptorTest.kt
@@ -1,5 +1,6 @@
 package com.sksamuel.kotest.engine.test.interceptors
 
+import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.descriptors.append
 import io.kotest.core.descriptors.toDescriptor
 import io.kotest.core.names.TestName
@@ -10,12 +11,13 @@ import io.kotest.core.test.TestResult
 import io.kotest.core.test.TestType
 import io.kotest.engine.test.interceptors.InvocationTimeoutInterceptor
 import io.kotest.engine.test.scopes.NoopTestScope
-import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.delay
 import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
 import kotlin.time.milliseconds
 
+@OptIn(ExperimentalTime::class)
 class InvocationTimeoutInterceptorTest : FunSpec() {
    init {
       test("InvocationTimeoutInterceptor should error after timeout") {
@@ -29,13 +31,15 @@ class InvocationTimeoutInterceptorTest : FunSpec() {
             TestType.Test,
          )
 
-         InvocationTimeoutInterceptor(TimeSource.Monotonic.markNow()).intercept(
-            tc.copy(config = tc.config.copy(invocationTimeout = Duration.milliseconds(1))),
-            NoopTestScope(tc, coroutineContext)
-         ) { _, _ ->
-            delay(10000)
-            TestResult.Success(0.milliseconds)
-         }.isError shouldBe true
+         shouldThrowAny {
+            InvocationTimeoutInterceptor(TimeSource.Monotonic.markNow()).intercept(
+               tc.copy(config = tc.config.copy(invocationTimeout = Duration.milliseconds(1))),
+               NoopTestScope(tc, coroutineContext)
+            ) { _, _ ->
+               delay(10000)
+               TestResult.Success(0.milliseconds)
+            }
+         }
       }
    }
 }
