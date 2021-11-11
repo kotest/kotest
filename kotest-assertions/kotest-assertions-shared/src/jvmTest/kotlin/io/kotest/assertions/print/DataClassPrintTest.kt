a712be63ae7521db31e7c1ddc44e8a86f1c9e6b9
Jim Schneidereit <jimschneidereit@gmail.com>
2021-11-11 14:32:40 -0800

    trying to reproduce #2611

--- kotest-assertions/kotest-assertions-shared/src/jvmTest/kotlin/io/kotest/assertions/print/DataClassPrintTest.kt
+++ kotest-assertions/kotest-assertions-shared/src/jvmTest/kotlin/io/kotest/assertions/print/DataClassPrintTest.kt
@@ -1,7 +1,11 @@
 package io.kotest.assertions.print
 
+import io.kotest.assertions.AssertionFailedError
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.mpp.uniqueId
+
 
 class DataClassPrintTest : FunSpec() {
    init {
@@ -22,5 +26,45 @@ class DataClassPrintTest : FunSpec() {
 - pennnant: R08
 )"""
       }
+
+      test("should be resilient to types with cyclic references") {
+         val foo1Parent = mutableListOf<Foo>()
+
+         val foo1 = Foo(null, foo1Parent)
+         val foo2 = Foo(Bar(foo1), listOf())
+         val foo3 = Foo(null, listOf())
+
+         foo1Parent.add(foo2)
+
+         foo1.print()
+         foo2.print()
+
+         shouldThrow<AssertionFailedError> {
+            foo1 shouldBe foo3
+         }
+      }
    }
 }
+
+private data class Bar(val foo: Foo) {
+   private val id = uniqueId()
+   override fun toString() = "Bar(id=$id)"
+   override fun hashCode() = id.hashCode()
+   override fun equals(other: Any?) = when (other) {
+      null -> false
+      is Bar -> other.id == id
+      else -> false
+   }
+}
+
+private data class Foo(val bar: Bar?, val parents: List<Foo>) {
+   private val id = uniqueId()
+   override fun toString() = "Foo(id=$id)"
+   override fun hashCode() = id.hashCode()
+   override fun equals(other: Any?) = when (other) {
+      null -> false
+      is Foo -> other.id == id
+      else -> false
+   }
+}
+
