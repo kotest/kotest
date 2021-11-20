77b15db73910e972f504d56b79cdae1da3d12e2f
sksamuel <sam@sksamuel.com>
2021-11-20 17:24:23 -0600

    Changed inspectors to be inline to support suspendable functions #2657

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/inspectors/runTests.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/inspectors/runTests.kt
@@ -3,7 +3,7 @@ package io.kotest.inspectors
 import io.kotest.assertions.ErrorCollectionMode
 import io.kotest.assertions.errorCollector
 
-fun <T> runTests(col: Collection<T>, f: (T) -> Unit): List<ElementResult<T>> {
+inline fun <T> runTests(col: Collection<T>, f: (T) -> Unit): List<ElementResult<T>> {
    return col.map {
       val originalAssertionMode = errorCollector.getCollectionMode()
       try {
