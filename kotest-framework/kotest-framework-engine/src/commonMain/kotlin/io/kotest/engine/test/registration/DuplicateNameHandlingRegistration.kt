a9b228b63f31a15c3b611c30596347b81280c307
sksamuel <sam@sksamuel.com>
2021-11-21 13:13:45 -0600

    Moved registration to be a coroutine context element

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/test/registration/DuplicateNameHandlingRegistration.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/test/registration/DuplicateNameHandlingRegistration.kt
@@ -1,5 +1,6 @@
 package io.kotest.engine.test.registration
 
+import io.kotest.core.descriptors.Descriptor
 import io.kotest.core.names.DuplicateTestNameMode
 import io.kotest.core.spec.Registration
 import io.kotest.core.test.NestedTest
@@ -8,13 +9,14 @@ import io.kotest.core.test.TestResult
 import io.kotest.engine.test.names.DuplicateTestNameHandler
 
 class DuplicateNameHandlingRegistration(
-   mode: DuplicateTestNameMode,
+   private val mode: DuplicateTestNameMode,
    private val delegate: Registration,
 ) : Registration {
 
-   private val handler = DuplicateTestNameHandler(mode)
+   private val handlers = mutableMapOf<Descriptor.TestDescriptor, DuplicateTestNameHandler>()
 
    override suspend fun runNestedTestCase(parent: TestCase, nested: NestedTest): TestResult? {
+      val handler = handlers.getOrPut(parent.descriptor) { DuplicateTestNameHandler(mode) }
       val withOverrideName = when (val uniqueName = handler.handle(nested.name)) {
          null -> nested
          else -> nested.copy(name = nested.name.copy(testName = uniqueName))
