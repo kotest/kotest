93f9577b3a6a175a18881b08a768a8a5324cc692
Ashish Kumar Joy <34642693+ashishkujoy@users.noreply.github.com>
2021-12-15 17:12:44 +0530

    Fixes global assert softly causing execution error. (#2734)
    * Fixes global assert softly causing execution error.
    
    * Cleans stacktrace of collected error

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/test/interceptors/SoftAssertInterceptor.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/test/interceptors/SoftAssertInterceptor.kt
@@ -1,6 +1,8 @@
 package io.kotest.engine.test.interceptors
 
 import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.collectiveError
+import io.kotest.assertions.errorCollector
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
 import io.kotest.core.test.TestScope
@@ -25,6 +27,13 @@ internal class SoftAssertInterceptor() : TestExecutionInterceptor {
       if (!testCase.config.assertSoftly) return test(testCase, scope)
 
       logger.log { Pair(testCase.name.testName, "Invoking test with soft assert") }
-      return assertSoftly { test(testCase, scope) }
+      return assertSoftly {
+         val result = test(testCase, scope)
+         // assertSoftly throws the collected error in finally block,
+         // as result test are getting report as execution error instead of assertion error.
+         // Here we are collecting error manually and creating failure if error is present so that assertSoftly won't
+         // throw any error and test report correctly assertion error instead of execution error.
+         errorCollector.collectiveError()?.let { TestResult.Failure(result.duration, it) } ?: result
+      }
    }
 }
