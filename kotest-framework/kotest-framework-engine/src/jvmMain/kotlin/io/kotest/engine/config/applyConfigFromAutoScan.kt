c6a1b4e2c06f167e5766fd58f515229a0f5d3b7e
sksamuel <sam@sksamuel.com>
2022-01-11 19:05:56 -0600

    Added system property to disable classpath scanning for config #2766

--- kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/config/applyConfigFromAutoScan.kt
+++ kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/config/applyConfigFromAutoScan.kt
@@ -1,9 +1,9 @@
 package io.kotest.engine.config
 
+import io.kotest.core.annotation.AutoScan
 import io.kotest.core.config.ProjectConfiguration
 import io.kotest.core.extensions.Extension
 import io.kotest.core.internal.KotestEngineProperties
-import io.kotest.core.annotation.AutoScan
 import io.kotest.mpp.instantiateOrObject
 
 /**
@@ -19,13 +19,12 @@ internal actual fun applyConfigFromAutoScan(configuration: ProjectConfiguration)
    if (System.getProperty(KotestEngineProperties.disableAutoScanClassPathScanning) == "true")
       return
 
-   val cg = classgraph()
-   val scan = cg.scan()
-   scan.getClassesWithAnnotation(AutoScan::class.java.name)
-      .map { Class.forName(it.name) }
-      .mapNotNull { instantiateOrObject(it).getOrNull() }
-      .filterIsInstance<Extension>().forEach {
-         configuration.registry.add(it)
-      }
-   scan.close()
+   classgraph().scan().use { result ->
+      result.getClassesWithAnnotation(AutoScan::class.java.name)
+         .map { Class.forName(it.name) }
+         .mapNotNull { instantiateOrObject(it).getOrNull() }
+         .filterIsInstance<Extension>().forEach {
+            configuration.registry.add(it)
+         }
+   }
 }
