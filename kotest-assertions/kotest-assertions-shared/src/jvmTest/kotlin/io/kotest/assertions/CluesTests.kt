5f7a1935eb5ecd463c85d759fd640b35fd4e20c5
Oliver O <oliver.o456i@gmail.com>
2022-01-22 00:03:01 +0100

    CluesTests: use a better dispatcher, relax thread concurrency validation

--- kotest-assertions/kotest-assertions-shared/src/jvmTest/kotlin/io/kotest/assertions/CluesTests.kt
+++ kotest-assertions/kotest-assertions-shared/src/jvmTest/kotlin/io/kotest/assertions/CluesTests.kt
@@ -1,10 +1,11 @@
 package io.kotest.assertions
 
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.collections.shouldHaveAtLeastSize
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -24,11 +25,20 @@ class CluesTests : FunSpec({
    }
 
    test("concurrent withClue invocations should be isolated from each other") {
-      withContext(Dispatchers.Default) {
+      @OptIn(ExperimentalCoroutinesApi::class)
+      withContext(Dispatchers.IO.limitedParallelism(8)) {
          val repetitionCount = 100
          val parentCoroutineCount = 2
          val childCoroutineCount = 10
          val stepCount = 2
+
+         // These parameters specify a certain quota of repetitions to succeed in dispatching coroutines to
+         // a specified minimum number of threads. This safeguards against subtle differences in dispatcher
+         // implementations (how many threads to allocate, how many threads to actually use for coroutines).
+         val minimumThreadCount = 2
+         val minimumThreadRepetitionQuota = repetitionCount / 2
+
+         var minimumThreadRepetitionCount = 0
 
          for (repetitionNumber in 1..repetitionCount) {
             val threadIds = mutableSetOf<Long>()
@@ -57,8 +67,11 @@ class CluesTests : FunSpec({
                }
             }
 
-            threadIds shouldHaveAtLeastSize 2
+            if (threadIds.size >= minimumThreadCount)
+               minimumThreadRepetitionCount++
          }
+
+         minimumThreadRepetitionCount shouldBeGreaterThanOrEqual minimumThreadRepetitionQuota
       }
    }
 })
