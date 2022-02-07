7b8fbcd5f8b1e98c34d0d84e255cf662e8e20007
sksamuel <sam@sksamuel.com>
2022-02-06 21:42:33 -0600

    Fixed tests

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/blocking/DescribeSpecBlockingTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/blocking/DescribeSpecBlockingTest.kt
@@ -7,28 +7,28 @@ import io.kotest.matchers.shouldBe
 class DescribeSpecBlockingTest : DescribeSpec() {
    init {
 
-      val threads = mutableSetOf<String>()
+      val threads = mutableSetOf<Long>()
 
       context("not blocking context") {
-         threads.add(Thread.currentThread().name)
+         threads.add(Thread.currentThread().id)
          describe("not blocking nested test") {
-            threads.add(Thread.currentThread().name)
+            threads.add(Thread.currentThread().id)
          }
       }
 
       describe("not blocking root test") {
-         threads.add(Thread.currentThread().name)
+         threads.add(Thread.currentThread().id)
       }
 
       context("blocking context").config(blockingTest = true) {
-         threads.add(Thread.currentThread().name)
+         threads.add(Thread.currentThread().id)
          describe("blocking nested test").config(blockingTest = true) {
-            threads.add(Thread.currentThread().name)
+            threads.add(Thread.currentThread().id)
          }
       }
 
       describe("blocking root test").config(blockingTest = true) {
-         threads.add(Thread.currentThread().name)
+         threads.add(Thread.currentThread().id)
       }
 
       afterSpec {
