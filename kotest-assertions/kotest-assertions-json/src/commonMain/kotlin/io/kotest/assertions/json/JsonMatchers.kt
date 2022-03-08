0152a05fdc4f3ca337941f0b403e9919c3b88b92
Scott Fedorov <49917876+scottdfedorov@users.noreply.github.com>
2022-03-08 11:43:28 -0600

    Add methods for confirm json is valid and is array or is object. Solves issues: #2815 #2816

--- kotest-assertions/kotest-assertions-json/src/commonMain/kotlin/io/kotest/assertions/json/JsonMatchers.kt
+++ kotest-assertions/kotest-assertions-json/src/commonMain/kotlin/io/kotest/assertions/json/JsonMatchers.kt
@@ -1,17 +1,17 @@
 package io.kotest.assertions.json
 
-import io.kotest.assertions.Actual
-import io.kotest.assertions.Expected
-import io.kotest.assertions.print.printed
 import io.kotest.matchers.ComparableMatcherResult
 import io.kotest.matchers.Matcher
 import io.kotest.matchers.MatcherResult
 import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNot
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlin.reflect.KClass
 
 @OptIn(ExperimentalSerializationApi::class)
 internal val pretty by lazy { Json { prettyPrint = true; prettyPrintIndent = "  " } }
@@ -46,21 +46,57 @@ fun matchJson(expected: String?) = object : Matcher<String?> {
          return MatcherResult(
             false,
             { "expected: expected json to be valid json: $expected" },
-            {
-               "expected: expected json to be invalid json: $expected"
-            })
+            { "expected: expected json to be invalid json: $expected" }
+         )
       }
 
       return ComparableMatcherResult(
          actualJson == expectedJson,
-         {
-            "expected json to match, but they differed\n\n"
-         },
-         {
-            "expected not to match with: $expectedJson but match: $actualJson"
-         },
+         { "expected json to match, but they differed\n\n" },
+         { "expected not to match with: $expectedJson but match: $actualJson" },
          actualJson.toString(),
          expectedJson.toString()
+      )
+   }
+}
+
+@OptIn(ExperimentalSerializationApi::class)
+fun beValidJson() = object : Matcher<String?> {
+   override fun test(value: String?): MatcherResult {
+      return try {
+         value?.let(pretty::parseToJsonElement)
+         MatcherResult(
+              true,
+              { "expected: actual json to be valid json: $value" },
+              { "expected: actual json to be invalid json: $value" }
+         )
+      } catch (ex: Exception) {
+         MatcherResult(
+              false,
+              { "expected: actual json to be valid json: $value" },
+              { "expected: actual json to be invalid json: $value" }
+         )
+      }
+   }
+}
+
+@OptIn(ExperimentalSerializationApi::class)
+fun beElementOf(kClass: KClass<*>) = object : Matcher<String?> {
+
+   override fun test(value: String?): MatcherResult {
+      val element = try {
+         value?.let(pretty::parseToJsonElement)
+      } catch (ex: Exception) {
+         return MatcherResult(
+            false,
+            { "expected: actual json to be valid json: $value" },
+            { "expected: actual json to be invalid json: $value" }
+         )
+      }
+      return MatcherResult(
+         kClass.isInstance(element),
+         { "expected: $value to be valid json of type: ${kClass.simpleName}" },
+         { "expected: $value to not be of type: ${kClass.simpleName}" }
       )
    }
 }
@@ -95,6 +131,36 @@ fun String.shouldBeEmptyJsonArray(): String {
 
 fun String.shouldBeEmptyJsonObject(): String {
    this should matchJson("{}")
+   return this
+}
+
+fun String.shouldBeJsonArray(): String {
+   this should beElementOf(JsonArray::class)
+   return this
+}
+
+fun String.shouldNotBeJsonArray(): String {
+   this shouldNot beElementOf(JsonArray::class)
+   return this
+}
+
+fun String.shouldBeJsonObject(): String {
+   this should beElementOf(JsonObject::class)
+   return this
+}
+
+fun String.shouldNotBeJsonObject(): String {
+   this shouldNot beElementOf(JsonObject::class)
+   return this
+}
+
+fun String.shouldBeValidJson(): String {
+   this should beValidJson()
+   return this
+}
+
+fun String.shouldNotBeValidJson(): String {
+   this shouldNot beValidJson()
    return this
 }
 
