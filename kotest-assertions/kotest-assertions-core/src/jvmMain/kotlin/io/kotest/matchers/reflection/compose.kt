fcebe5899a51b8dcc20241fcf6ab8de1fbf35f37
Łukasz Pięta <pieta.lukasz98@gmail.com>
2022-03-12 22:00:44 +0100

    #2845 Matcher compose (#2864)
    * Implement Matcher compose as companion object extension, using reflection
    
    * Return accumulated MatcherResult
    
    * Write test of compose matcher

---
+++ kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/reflection/compose.kt
@@ -0,0 +1,28 @@
+package io.kotest.matchers.reflection
+
+import io.kotest.matchers.Matcher
+import io.kotest.matchers.MatcherResult
+import kotlin.reflect.KProperty1
+
+@Suppress("UNCHECKED_CAST")
+fun <T : Any?> Matcher.Companion.compose(
+   vararg pairs: Pair<Matcher<*>, KProperty1<T, *>>
+): Matcher<T> = object : Matcher<T> {
+   override fun test(value: T): MatcherResult {
+      val results = pairs.map { (matcher, prop) ->
+         (matcher as Matcher<Any?>).test(prop.get(value))
+      }
+
+      return MatcherResult(
+         results.all { it.passed() },
+         {
+            results.map { it.failureMessage() }.fold("") { acc: String, s: String -> acc + s + "\n" }
+               .trimIndent()
+         },
+         {
+            results.map { it.negatedFailureMessage() }.fold("") { acc: String, s: String -> acc + s + "\n" }
+               .trimIndent()
+         },
+      )
+   }
+}
