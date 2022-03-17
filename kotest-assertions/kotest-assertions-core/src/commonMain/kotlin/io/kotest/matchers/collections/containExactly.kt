733a17a9c4718b6801e5f8d1905a2e1515244bee
Emil Kantis <emil.kantis@protonmail.com>
2022-03-17 20:49:57 +0100

    Include Disallowed-reason (if any) for shouldContainExactly failures

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/containExactly.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/containExactly.kt
@@ -1,6 +1,7 @@
 package io.kotest.matchers.collections
 
 import io.kotest.assertions.AssertionsConfig
+import io.kotest.assertions.eq.IterableEq
 import io.kotest.assertions.eq.eq
 import io.kotest.assertions.print.Printed
 import io.kotest.assertions.print.print
@@ -41,8 +42,11 @@ fun <T> containExactly(vararg expected: T): Matcher<Collection<T>?> = containExa
 
 /** Assert that a collection contains exactly the given values and nothing else, in order. */
 fun <T, C : Collection<T>> containExactly(expected: C): Matcher<C?> = neverNullMatcher { actual ->
+   fun Throwable?.isDisallowedIterableComparisonFailure() =
+      this?.message?.startsWith(IterableEq.trigger) == true
 
-   val passed = eq(actual, expected, strictNumberEq = true) == null
+   val failureReason = eq(actual, expected, strictNumberEq = true)
+   val passed = failureReason == null
 
    val failureMessage = {
 
@@ -50,7 +54,13 @@ fun <T, C : Collection<T>> containExactly(expected: C): Matcher<C?> = neverNullM
       val extra = actual.filterNot { expected.contains(it) }
 
       val sb = StringBuilder()
-      sb.append("Expecting: ${expected.print().value} but was: ${actual.print().value}")
+
+      if (failureReason.isDisallowedIterableComparisonFailure()) {
+         sb.append(failureReason?.message)
+      } else {
+         sb.append("Expecting: ${expected.print().value} but was: ${actual.print().value}")
+      }
+
       sb.append("\n")
       if (missing.isNotEmpty()) {
          sb.append("Some elements were missing: ")
@@ -70,7 +80,11 @@ fun <T, C : Collection<T>> containExactly(expected: C): Matcher<C?> = neverNullM
 
    val negatedFailureMessage = { "Collection should not contain exactly ${expected.print().value}" }
 
-   if (actual.size <= AssertionsConfig.maxCollectionEnumerateSize && expected.size <= AssertionsConfig.maxCollectionEnumerateSize) {
+   if (
+      actual.size <= AssertionsConfig.maxCollectionEnumerateSize &&
+      expected.size <= AssertionsConfig.maxCollectionEnumerateSize &&
+      !failureReason.isDisallowedIterableComparisonFailure()
+   ) {
       ComparableMatcherResult(
          passed,
          failureMessage,
