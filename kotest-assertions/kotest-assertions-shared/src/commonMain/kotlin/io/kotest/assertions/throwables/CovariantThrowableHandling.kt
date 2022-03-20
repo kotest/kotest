f287989125f18e3383b23667e22b54ee4653f502
Łukasz Pięta <pieta.lukasz98@gmail.com>
2022-03-20 20:53:00 +0100

    #2847 Implement ShouldThrowWithMessage (#2885)
    * Implement ShouldThrowWithMessage
    
    * Fix typo
    
    * Fix test

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/throwables/CovariantThrowableHandling.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/throwables/CovariantThrowableHandling.kt
@@ -32,6 +32,39 @@ import io.kotest.mpp.bestName
 inline fun <reified T : Throwable> shouldThrowUnit(block: () -> Unit): T = shouldThrow { block() }
 
 /**
+ * Verifies if a block of code throws a Throwable of type [T] or subtypes with specified message
+ *
+ * Use this function to wrap a block of code that you'd like to verify whether it throws [T] (or subclasses) or not.
+ *
+ * This should be used when [shouldThrowWithMessage] can't be used, such as when doing assignments (assignments are statements,
+ * therefore has no return value).
+ *
+ * This function will include all subclasses of [T]. For example, if you test for [java.io.IOException] and
+ * the code block throws [java.io.FileNotFoundException], the test will pass.
+ *
+ * If you wish to test for a specific class strictly (excluding subclasses), use [shouldThrowExactlyUnit] instead.
+ *
+ * If you don't care about the thrown exception type, use [shouldThrowAnyUnit].
+ *
+ *
+ * ```
+ *     val thrownException: FooException = shouldThrowUnit<FooException> {
+ *         // Code that we expect to throw FooException
+ *         throw FooException()
+ *     }
+ * ```
+ *
+ * @see [shouldThrowWithMessage]
+ */
+inline fun <reified T : Throwable> shouldThrowUnitWithMessage(message: String, block: () -> Unit): T =
+   shouldThrowUnit<T>(block).let {
+      when (it.message) {
+         message -> it
+         else -> throw failure("Expected exception message $message but was ${it.message} instead.", it)
+      }
+   }
+
+/**
  * Verifies that a block of code doesn't throw a Throwable of type [T] or subtypes
  *
  * Use this function to wrap a block of code that you'd like to verify whether it throws [T] (or subclasses) or not.
@@ -100,10 +133,46 @@ inline fun <reified T : Throwable> shouldThrow(block: () -> Any?): T {
       null -> throw failure("Expected exception ${expectedExceptionClass.bestName()} but no exception was thrown.")
       is T -> thrownThrowable               // This should be before `is AssertionError`. If the user is purposefully trying to verify `shouldThrow<AssertionError>{}` this will take priority
       is AssertionError -> throw thrownThrowable
-      else -> throw failure("Expected exception ${expectedExceptionClass.bestName()} but a ${thrownThrowable::class.simpleName} was thrown instead.",
-         thrownThrowable)
+      else -> throw failure(
+         "Expected exception ${expectedExceptionClass.bestName()} but a ${thrownThrowable::class.simpleName} was thrown instead.",
+         thrownThrowable
+      )
    }
 }
+
+/**
+ * Verifies if a block of code will throw a Throwable of type [T] or subtypes with specified message.
+ *
+ * Use this function to wrap a block of code that you'd like to verify whether it throws [T] (or subclasses) or not.
+ *
+ * This function will include subclasses of [T]. For example, if you test for [java.io.IOException] and
+ * the code block throws [java.io.FileNotFoundException], the test will pass.
+ *
+ * If you wish to test for a specific class strictly (excluding subclasses), use [shouldThrowExactly] instead.
+ *
+ * If you don't care about which exception is thrown, then use [shouldThrowAny].
+ *
+ * **Attention to assignment operations**:
+ *
+ * When doing an assignment to a variable, the code won't compile, because an assignment is not of type [Any], as required
+ * by [block]. If you need to test that an assignment throws a [Throwable], use [shouldThrowUnitWithMessage] or it's variations.
+ *
+ * ```
+ *     val thrownException: FooException = shouldThrow<FooException> {
+ *         // Code that we expect to throw FooException
+ *         throw FooException()
+ *     }
+ * ```
+ *
+ * @see [shouldThrowUnitWithMessage]
+ */
+inline fun <reified T : Throwable> shouldThrowWithMessage(message: String, block: () -> Any?): T =
+   shouldThrow<T>(block).let {
+      when (it.message) {
+         message -> it
+         else -> throw failure("Expected exception message $message but was ${it.message} instead.", it)
+      }
+   }
 
 /**
  * Verifies that a block of code will not throw a Throwable of type [T] or subtypes
