1536df8cf85c790b488534313834ec19592d3c30
Sam Sam <sam@sksamuel.com>
2022-04-30 12:42:07 -0500

    Fix JS code generation for 1.6.21 by using main (#2947)

--- kotest-framework/kotest-framework-multiplatform-plugin-js/src/jvmMain/kotlin/io/kotest/framework/multiplatform/js/SpecIrGenerationExtension.kt
+++ kotest-framework/kotest-framework-multiplatform-plugin-js/src/jvmMain/kotlin/io/kotest/framework/multiplatform/js/SpecIrGenerationExtension.kt
@@ -8,16 +8,14 @@ import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.cli.common.toLogger
 import org.jetbrains.kotlin.ir.IrStatement
-import org.jetbrains.kotlin.ir.builders.declarations.addGetter
-import org.jetbrains.kotlin.ir.builders.declarations.buildField
-import org.jetbrains.kotlin.ir.builders.declarations.buildProperty
-import org.jetbrains.kotlin.ir.builders.irBlock
+import org.jetbrains.kotlin.ir.builders.declarations.buildFun
 import org.jetbrains.kotlin.ir.builders.irBlockBody
 import org.jetbrains.kotlin.ir.builders.irCall
 import org.jetbrains.kotlin.ir.builders.irVararg
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.declarations.name
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.util.constructors
@@ -67,55 +65,83 @@ class SpecIrGenerationExtension(private val messageCollector: MessageCollector) 
             val withConfigFn = launcherClass.getSimpleFunction(EntryPoint.WithConfigMethodName)
                ?: error("Cannot find function ${EntryPoint.WithConfigMethodName}")
 
-            val launcher = pluginContext.irFactory.buildProperty {
-               name = Name.identifier(EntryPoint.LauncherValName)
-            }.apply {
-               parent = file
-
-               backingField = pluginContext.irFactory.buildField {
-                  type = pluginContext.irBuiltIns.unitType
-                  isFinal = true
-                  isExternal = false
-                  isStatic = true // top level vals must be static
-                  name = Name.identifier(EntryPoint.LauncherValName)
-               }.also { field ->
-                  field.correspondingPropertySymbol = this@apply.symbol
-                  field.initializer = pluginContext.irFactory.createExpressionBody(startOffset, endOffset) {
-                     this.expression = DeclarationIrBuilder(pluginContext, field.symbol).irBlock {
-                        +irCall(promiseFn).also { promise: IrCall ->
-                           promise.dispatchReceiver = irCall(withSpecsFn).also { withSpecs ->
-                              withSpecs.putValueArgument(
-                                 0,
-                                 irVararg(
-                                    pluginContext.irBuiltIns.stringType,
-                                    specs.map { irCall(it.constructors.first()) }
-                                 )
+            val main = pluginContext.irFactory.buildFun {
+               name = Name.identifier("main")
+               returnType = pluginContext.irBuiltIns.unitType
+            }.also { func: IrSimpleFunction ->
+               func.body = DeclarationIrBuilder(pluginContext, func.symbol).irBlockBody {
+                  +irCall(promiseFn).also { promise: IrCall ->
+                     promise.dispatchReceiver = irCall(withSpecsFn).also { withSpecs ->
+                        withSpecs.putValueArgument(
+                           0,
+                           irVararg(
+                              pluginContext.irBuiltIns.stringType,
+                              specs.map { irCall(it.constructors.first()) }
+                           )
+                        )
+                        withSpecs.dispatchReceiver = irCall(withConfigFn).also { withConfig ->
+                           withConfig.putValueArgument(
+                              0,
+                              irVararg(
+                                 pluginContext.irBuiltIns.stringType,
+                                 configs.map { irCall(it.constructors.first()) }
                               )
-                              withSpecs.dispatchReceiver = irCall(withConfigFn).also { withConfig ->
-                                 withConfig.putValueArgument(
-                                    0,
-                                    irVararg(
-                                       pluginContext.irBuiltIns.stringType,
-                                       configs.map { irCall(it.constructors.first()) }
-                                    )
-                                 )
-                                 withConfig.dispatchReceiver = irCall(launcherConstructor)
-                              }
-                           }
+                           )
+                           withConfig.dispatchReceiver = irCall(launcherConstructor)
                         }
                      }
                   }
                }
-
-               addGetter {
-                  returnType = pluginContext.irBuiltIns.unitType
-               }.also { func ->
-                  func.body = DeclarationIrBuilder(pluginContext, func.symbol).irBlockBody {
-                  }
-               }
             }
 
-            file.addChild(launcher)
+//            val launcher = pluginContext.irFactory.buildProperty {
+//               name = Name.identifier(EntryPoint.LauncherValName)
+//            }.apply {
+//               parent = file
+//               backingField = pluginContext.irFactory.buildField {
+//                  type = pluginContext.irBuiltIns.unitType
+//                  isFinal = true
+//                  isExternal = false
+//                  isStatic = true // top level vals must be static
+//                  name = Name.identifier(EntryPoint.LauncherValName)
+//               }.also { field ->
+//                  field.correspondingPropertySymbol = this@apply.symbol
+//                  field.initializer = pluginContext.irFactory.createExpressionBody(startOffset, endOffset) {
+//                     this.expression = DeclarationIrBuilder(pluginContext, field.symbol).irBlock {
+//                        +irCall(promiseFn).also { promise: IrCall ->
+//                           promise.dispatchReceiver = irCall(withSpecsFn).also { withSpecs ->
+//                              withSpecs.putValueArgument(
+//                                 0,
+//                                 irVararg(
+//                                    pluginContext.irBuiltIns.stringType,
+//                                    specs.map { irCall(it.constructors.first()) }
+//                                 )
+//                              )
+//                              withSpecs.dispatchReceiver = irCall(withConfigFn).also { withConfig ->
+//                                 withConfig.putValueArgument(
+//                                    0,
+//                                    irVararg(
+//                                       pluginContext.irBuiltIns.stringType,
+//                                       configs.map { irCall(it.constructors.first()) }
+//                                    )
+//                                 )
+//                                 withConfig.dispatchReceiver = irCall(launcherConstructor)
+//                              }
+//                           }
+//                        }
+//                     }
+//                  }
+//               }
+//
+//               addGetter {
+//                  returnType = pluginContext.irBuiltIns.unitType
+//               }.also { func: IrSimpleFunction ->
+//                  func.body = DeclarationIrBuilder(pluginContext, func.symbol).irBlockBody {
+//                  }
+//               }
+//            }
+
+            file.addChild(main)
             return fragment
          }
 
@@ -128,7 +154,8 @@ class SpecIrGenerationExtension(private val messageCollector: MessageCollector) 
          override fun visitFileNew(declaration: IrFile): IrFile {
             super.visitFileNew(declaration)
             val specs = declaration.specs()
-            messageCollector.toLogger().log("${declaration.name} contains ${specs.size} spec(s): ${specs.joinToString(", ") { it.kotlinFqName.asString() }}")
+            messageCollector.toLogger()
+               .log("${declaration.name} contains ${specs.size} spec(s): ${specs.joinToString(", ") { it.kotlinFqName.asString() }}")
             this.specs.addAll(specs)
             return declaration
          }
