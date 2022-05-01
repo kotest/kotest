e014975c46dcd61ed1bfa20b86f2328f9211f1bd
sksamuel <sam@sksamuel.com>
2022-05-01 14:17:18 -0500

    Handle process not defined

--- kotest-common/src/jsMain/kotlin/io/kotest/mpp/sysprop.kt
+++ kotest-common/src/jsMain/kotlin/io/kotest/mpp/sysprop.kt
@@ -3,6 +3,6 @@ package io.kotest.mpp
 actual fun sysprop(name: String): String? = null
 actual fun env(name: String): String? = try {
    js("process.env[name]") as String?
-} catch (e: Exception) {
+} catch (e: Throwable) {
    null
 }
