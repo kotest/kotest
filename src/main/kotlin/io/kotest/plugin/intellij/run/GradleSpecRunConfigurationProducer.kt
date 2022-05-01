0f2e4d21a28a00cf4f357a8f1fa176dc90a299f1
sksamuel <sam@sksamuel.com>
2022-05-01 14:30:31 -0500

    Updated docs for GradleSpecRunConfigurationProducer

--- src/main/kotlin/io/kotest/plugin/intellij/run/GradleSpecRunConfigurationProducer.kt
+++ src/main/kotlin/io/kotest/plugin/intellij/run/GradleSpecRunConfigurationProducer.kt
@@ -8,7 +8,11 @@ import org.jetbrains.kotlin.asJava.toLightClass
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.plugins.gradle.execution.test.runner.TestClassGradleConfigurationProducer
 
+/**
+ * Shows a gradle run icon if the test runner is set to gradle or choose per test.
+ */
 class GradleSpecRunConfigurationProducer : TestClassGradleConfigurationProducer() {
+
    override fun getPsiClassForLocation(contextLocation: Location<*>): PsiClass? {
       val leaf = contextLocation.psiElement
       if (leaf is LeafPsiElement) {
