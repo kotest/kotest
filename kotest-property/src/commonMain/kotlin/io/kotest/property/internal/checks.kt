773c6f616be67b87c9da5654d59ecac899fb217e
Sam Sam <sam@sksamuel.com>
2022-05-22 15:30:16 -0500

    Update kotest-property/src/commonMain/kotlin/io/kotest/property/internal/checks.kt
    Co-authored-by: Emil Kantis <emil.kantis@protonmail.com>

--- kotest-property/src/commonMain/kotlin/io/kotest/property/internal/checks.kt
+++ kotest-property/src/commonMain/kotlin/io/kotest/property/internal/checks.kt
@@ -5,7 +5,7 @@ import io.kotest.property.PropertyContext
 import io.kotest.property.PropertyTesting
 import kotlin.math.min
 
-fun PropTestConfig.checkFailOnSeed() {
+internal fun PropTestConfig.checkFailOnSeed() {
    if (seed != null && PropertyTesting.failOnSeed)
       error("A seed is specified on this property-test and failOnSeed is true")
 }
