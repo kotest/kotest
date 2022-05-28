d5cb313244a8334632d650558d7fa67b6a1a218f
Charles Korn <me@charleskorn.com>
2022-05-28 10:00:06 +1000

    Enable building native targets for kotest-assertions-json.

--- kotest-assertions/kotest-assertions-json/build.gradle.kts
+++ kotest-assertions/kotest-assertions-json/build.gradle.kts
@@ -12,6 +12,27 @@ kotlin {
          browser()
          nodejs()
       }
+
+      linuxX64()
+
+      mingwX64()
+
+      macosX64()
+      macosArm64()
+
+      tvos()
+      tvosSimulatorArm64()
+
+      watchosArm32()
+      watchosArm64()
+      watchosX86()
+      watchosX64()
+      watchosSimulatorArm64()
+
+      iosX64()
+      iosArm64()
+      iosArm32()
+      iosSimulatorArm64()
    }
 
    sourceSets {
