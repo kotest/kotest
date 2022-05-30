6022664e5b6cc5417131329892e305068999d00f
Norbert Nowak <78920164+nor-ek@users.noreply.github.com>
2022-05-30 16:25:09 +0200

    Array size validation (#2974)

--- kotest-assertions/kotest-assertions-json/src/commonTest/kotlin/io.kotest.assertions.json/schema/ArraySchemaTest.kt
+++ kotest-assertions/kotest-assertions-json/src/commonTest/kotlin/io.kotest.assertions.json/schema/ArraySchemaTest.kt
@@ -34,7 +34,7 @@ class ArraySchemaTest : FunSpec(
 
       test("array with partial inner match is not ok") {
          val missingAge =
-         """
+            """
             [
                { "name": "bob" },
                { "name": "bob", "age": 3 },
@@ -47,6 +47,37 @@ class ArraySchemaTest : FunSpec(
          shouldFail { missingAge shouldMatchSchema personArray }.message shouldBe """
             $[0].age => Expected number, but was undefined
             $[2].age => Expected number, but was undefined
+         """.trimIndent()
+      }
+
+      test("Should parse schema with min,max values") {
+         val schema = parseSchema(
+            """
+               { "type": "array", "minItems": 2, "maxItems": 3, "elementType": {"type": "number"} }
+            """.trimIndent()
+         )
+         "[1]" shouldNotMatchSchema schema
+      }
+
+      test("Array size smaller than minItems") {
+         val array = "[1]"
+         val sizeBoundedArray = jsonSchema {
+            array(minItems = 2, maxItems = 3) { number() }
+         }
+         array shouldNotMatchSchema sizeBoundedArray
+         shouldFail { array shouldMatchSchema sizeBoundedArray }.message shouldBe """
+            $ => Expected items between 2 and 3, but was 1
+         """.trimIndent()
+      }
+
+      test("Array size larger than maxItems") {
+         val array = "[1,2]"
+         val sizeBoundedArray = jsonSchema {
+            array(minItems = 0, maxItems = 1) { number() }
+         }
+         array shouldNotMatchSchema sizeBoundedArray
+         shouldFail { array shouldMatchSchema sizeBoundedArray }.message shouldBe """
+            $ => Expected items between 0 and 1, but was 2
          """.trimIndent()
       }
    }
