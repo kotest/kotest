b8499d7eaa7249916e0924b049d61448b90d018d
Emil Kantis <emil.kantis@protonmail.com>
2022-06-01 22:53:03 +0200

    add readme and move generic config to kotlin-conventions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,3 @@
-allprojects {
-   apply(plugin = "com.adarshr.test-logger")
-
-   repositories {
-      mavenCentral()
-      mavenLocal()
-      maven("https://oss.sonatype.org/content/repositories/snapshots/")
-      google()
-   }
-
-   group = "io.kotest"
-   version = Ci.publishVersion
-
-   tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-      kotlinOptions {
-         freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
-         jvmTarget = "1.8"
-         apiVersion = "1.6"
-         languageVersion = "1.6"
-      }
-   }
-
-   tasks.withType<Test> {
-   }
-}
-
 // TODO: Remove me after https://youtrack.jetbrains.com/issue/KT-49109 is fixed
 rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.0.0"
--- buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -1,10 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
    kotlin("multiplatform")
    id("com.adarshr.test-logger")
 }
 
+group = "io.kotest"
+version = Ci.publishVersion
+
 repositories {
    mavenCentral()
+   mavenLocal()
+   maven("https://oss.sonatype.org/content/repositories/snapshots/")
+   google()
 }
 
 testlogger {
@@ -19,3 +27,11 @@ tasks.withType<Test>() {
    }
 }
 
+tasks.withType<KotlinCompile>().configureEach {
+   kotlinOptions {
+      freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+      jvmTarget = "1.8"
+      apiVersion = "1.6"
+      languageVersion = "1.6"
+   }
+}
