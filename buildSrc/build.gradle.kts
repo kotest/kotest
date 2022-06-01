7a4fb1b2555c27f358b4a5f2989be43a991fca43
Emil Kantis <emil.kantis@protonmail.com>
2022-06-01 19:52:32 +0200

    Use gradle plugin dependency based on version catalog

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ repositories {
 }
 
 dependencies {
-   implementation("com.adarshr:gradle-test-logger-plugin:3.2.0")
-   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+   implementation(libs.testlogger.gradle.plugin)
+   implementation(libs.kotlin.gradle.plugin)
 }
