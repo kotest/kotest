71b93bbd2a31ded755a2edd8314d11ea14bd1947
Emil Kantis <emil.kantis@protonmail.com>
2022-06-03 09:58:47 +0200

    tvOS fix

--- buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -13,6 +13,7 @@ repositories {
    mavenLocal()
    maven("https://oss.sonatype.org/content/repositories/snapshots/")
    google()
+   gradlePluginPortal() // tvOS builds need to be able to fetch a kotlin gradle plugin
 }
 
 testlogger {
