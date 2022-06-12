64e3c020427cfacdbf28e439d28f200bb5c81e22
sksamuel <sam@sksamuel.com>
2022-06-12 14:38:39 -0500

    Added new statistics for property testing

---
+++ kotest-property/src/commonMain/kotlin/io/kotest/property/statistics/coverage.kt
@@ -0,0 +1,62 @@
+package io.kotest.property.statistics
+
+import io.kotest.assertions.fail
+import io.kotest.common.ExperimentalKotest
+import io.kotest.property.PropertyContext
+
+/**
+ * Asserts that the given [classification] was applied to at least [percentage] number of tests.
+ *
+ * For example, to check that at least 25% of the iterations where classified as 'even':
+ *
+ *       checkCoverage("even", 25.0) {
+ *          forAll(Arb.int()) { a ->
+ *             classify(a % 2 == 0, "even")
+ *             a + a == 2 * a
+ *          }
+ *       }
+ *
+ */
+@ExperimentalKotest
+suspend fun withCoveragePercentage(
+   classification: Any?,
+   percentage: Double,
+   f: suspend () -> PropertyContext
+): PropertyContext {
+   val context = f()
+   val stats = context.statistics()[null] ?: emptyMap()
+   val count = stats[classification] ?: 0
+   val attempts = context.attempts()
+   val actual = (count.toDouble() / attempts.toDouble()) * 100.0
+   if (actual < percentage)
+      fail("Required coverage of $percentage% for [${classification}] but was [${actual.toInt()}%]")
+   return context
+}
+
+
+/**
+ * Asserts that the given [classification] was applied to at least [percentage] number of tests.
+ *
+ * For example, to check that at least 25% of the iterations where classified as 'even':
+ *
+ *       checkCoverage("even", 25.0) {
+ *          forAll(Arb.int()) { a ->
+ *             classify(a % 2 == 0, "even")
+ *             a + a == 2 * a
+ *          }
+ *       }
+ *
+ */
+@ExperimentalKotest
+suspend fun withCoverageCount(
+   classification: Any?,
+   count: Int,
+   f: suspend () -> PropertyContext
+): PropertyContext {
+   val context = f()
+   val stats = context.statistics()[null] ?: emptyMap()
+   val actual = stats[classification] ?: 0
+   if (actual < count)
+      fail("Required coverage of $count for [${classification}] but was [${actual}]")
+   return context
+}
---
+++ kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/stats/CollectTest.kt
@@ -0,0 +1,135 @@
+package com.sksamuel.kotest.property.stats
+
+import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.extensions.system.captureStandardOut
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldNotContain
+import io.kotest.property.Arb
+import io.kotest.property.PropTestConfig
+import io.kotest.property.PropertyTesting
+import io.kotest.property.arbitrary.enum
+import io.kotest.property.checkAll
+import io.kotest.property.statistics.StatisticsReportMode
+import io.kotest.property.statistics.withCoverageCount
+import io.kotest.property.statistics.withCoveragePercentage
+import java.math.RoundingMode
+
+class CollectTest : FunSpec() {
+   init {
+      test("collecting stats") {
+         val stdout = captureStandardOut {
+            checkAll(PropTestConfig(seed = 1231245), Arb.enum<RoundingMode>()) {
+               collect(it)
+            }
+         }
+         stdout.shouldContain("Statistics: [collecting stats] (1000 iterations, 1 args)")
+         stdout.shouldContain(
+            """
+HALF_DOWN                                                     142 (14%)
+HALF_UP                                                       141 (14%)
+CEILING                                                       132 (13%)
+FLOOR                                                         122 (12%)
+UP                                                            119 (12%)
+UNNECESSARY                                                   119 (12%)
+HALF_EVEN                                                     118 (12%)
+DOWN                                                          107 (11%)
+""".trim()
+         )
+      }
+
+      test("collecting labelled stats") {
+         val stdout = captureStandardOut {
+            checkAll(PropTestConfig(seed = 1231245), Arb.enum<RoundingMode>()) {
+               collect(it)
+               collect("mylabel", it)
+               collect("mylabel2", it)
+            }
+         }
+         stdout.shouldContain("Statistics: [collecting labelled stats] (1000 iterations, 1 args)")
+         stdout.shouldContain("Statistics: [collecting labelled stats] (1000 iterations, 1 args) [mylabel]")
+         stdout.shouldContain("Statistics: [collecting labelled stats] (1000 iterations, 1 args) [mylabel2]")
+         stdout.shouldContain(
+            """
+HALF_DOWN                                                     142 (14%)
+HALF_UP                                                       141 (14%)
+CEILING                                                       132 (13%)
+FLOOR                                                         122 (12%)
+UP                                                            119 (12%)
+UNNECESSARY                                                   119 (12%)
+HALF_EVEN                                                     118 (12%)
+DOWN                                                          107 (11%)
+""".trim()
+         )
+      }
+
+      test("no output when StatisticsReportMode.OFF") {
+         PropertyTesting.statisticsReportMode = StatisticsReportMode.OFF
+         val stdout = captureStandardOut {
+            checkAll(Arb.enum<RoundingMode>()) {
+               collect(it)
+               collect("mylabel", it)
+               collect("mylabel2", it)
+            }
+         }
+         stdout.shouldNotContain("mylabel")
+         stdout.shouldNotContain("mylabel2")
+         stdout.shouldNotContain("Statistics:")
+         stdout.shouldNotContain("1000 iterations")
+      }
+
+      test("no output for successful test and StatisticsReportMode.FAILED") {
+         PropertyTesting.statisticsReportMode = StatisticsReportMode.FAILED
+         val stdout = captureStandardOut {
+            checkAll(Arb.enum<RoundingMode>()) {
+               collect(it)
+               collect("mylabel", it)
+               collect("mylabel2", it)
+            }
+         }
+         stdout.shouldNotContain("mylabel")
+         stdout.shouldNotContain("mylabel2")
+         stdout.shouldNotContain("Statistics:")
+         stdout.shouldNotContain("1000 iterations")
+      }
+
+      test("no output for failed test and StatisticsReportMode.SUCCESS") {
+         PropertyTesting.statisticsReportMode = StatisticsReportMode.SUCCESS
+         val stdout = captureStandardOut {
+            shouldThrowAny {
+               checkAll(Arb.enum<RoundingMode>()) {
+                  it shouldBe RoundingMode.HALF_UP
+                  collect(it)
+                  collect("mylabel", it)
+                  collect("mylabel2", it)
+               }
+            }
+         }
+         stdout.shouldNotContain("mylabel")
+         stdout.shouldNotContain("mylabel2")
+         stdout.shouldNotContain("Statistics:")
+         stdout.shouldNotContain("1000 iterations")
+      }
+
+      test("withCoveragePercentage should fail if coverage % is not sufficient") {
+         shouldThrowAny {
+            withCoveragePercentage(RoundingMode.HALF_UP, 30.0) {
+               checkAll(PropTestConfig(seed = 1), Arb.enum<RoundingMode>()) {
+                  collect(it)
+               }
+            }
+         }.message.shouldContain("Required coverage of 30.0% for [HALF_UP] but was [12%]")
+      }
+
+      test("withCoverageCount should fail if coverage number is not sufficient") {
+         shouldThrowAny {
+            withCoverageCount(RoundingMode.HALF_UP, 200) {
+               checkAll(PropTestConfig(seed = 1), Arb.enum<RoundingMode>()) {
+                  collect(it)
+               }
+            }
+         }.message.shouldContain("Required coverage of 200 for [HALF_UP] but was [125]")
+      }
+   }
+}
