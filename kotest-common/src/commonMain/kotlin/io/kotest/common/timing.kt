ed0a400ee9d46bb38a9ac5e1c2fb75233623c178
sksamuel <sam@sksamuel.com>
2022-06-12 19:20:23 -0500

    Removed used of measure time

--- kotest-common/src/commonMain/kotlin/io/kotest/common/timing.kt
+++ kotest-common/src/commonMain/kotlin/io/kotest/common/timing.kt
@@ -6,8 +6,6 @@ import kotlin.contracts.contract
 import kotlin.jvm.JvmInline
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
-import kotlin.time.TimeMark
-import kotlin.time.TimeSource
 
 
 /**
@@ -28,27 +26,27 @@ inline fun measureTimeMillisCompat(block: () -> Unit): Long {
 @KotestInternal
 @SoftDeprecated("temp fix for breaking change in Kotlin 1.6 -> 1.7")
 @JvmInline
-value class ValueTimeMarkCompat internal constructor(internal val startMillis: Long)  {
+value class ValueTimeMarkCompat internal constructor(internal val startMillis: Long) {
 
-     fun elapsedNow(): Duration = MonotonicTimeSourceCompat.elapsedFrom(this)
+   fun elapsedNow(): Duration = MonotonicTimeSourceCompat.elapsedFrom(this)
 
-     operator fun plus(duration: Duration): ValueTimeMarkCompat =
+   operator fun plus(duration: Duration): ValueTimeMarkCompat =
       MonotonicTimeSourceCompat.adjustReading(this, duration)
 
-     operator fun minus(duration: Duration): ValueTimeMarkCompat =
+   operator fun minus(duration: Duration): ValueTimeMarkCompat =
       MonotonicTimeSourceCompat.adjustReading(this, -duration)
 
-     fun hasPassedNow(): Boolean = !elapsedNow().isNegative()
+   fun hasPassedNow(): Boolean = !elapsedNow().isNegative()
 
-     fun hasNotPassedNow(): Boolean = elapsedNow().isNegative()
+   fun hasNotPassedNow(): Boolean = elapsedNow().isNegative()
 }
 
 
 @KotestInternal
 @SoftDeprecated("temp fix for breaking change in Kotlin 1.6 -> 1.7")
-object MonotonicTimeSourceCompat  {
+object MonotonicTimeSourceCompat {
 
-     fun markNow(): ValueTimeMarkCompat = ValueTimeMarkCompat(timeInMillis())
+   fun markNow(): ValueTimeMarkCompat = ValueTimeMarkCompat(timeInMillis())
 
    fun elapsedFrom(timeMark: ValueTimeMarkCompat): Duration =
       (timeInMillis() - timeMark.startMillis).milliseconds
