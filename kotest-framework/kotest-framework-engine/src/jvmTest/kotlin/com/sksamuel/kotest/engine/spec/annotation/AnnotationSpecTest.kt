a2868404279695df6d2aaec35753201519252e0a
Adam <897017+aSemy@users.noreply.github.com>
2022-07-10 10:00:10 +0200

    additional tests for annotation @Nested

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/spec/annotation/AnnotationSpecTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/spec/annotation/AnnotationSpecTest.kt
@@ -1,10 +1,14 @@
 package com.sksamuel.kotest.engine.spec.annotation
 
+import io.kotest.assertions.fail
+import io.kotest.assertions.withClue
 import io.kotest.core.descriptors.DescriptorId
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.test.TestResult
 import io.kotest.engine.TestEngineLauncher
 import io.kotest.engine.listener.CollectingTestEngineListener
+import io.kotest.matchers.maps.shouldContainKeys
 import io.kotest.matchers.maps.shouldHaveSize
 import io.kotest.matchers.shouldBe
 
@@ -17,12 +21,6 @@ class AnnotationSpecTest : DescribeSpec({
          TestEngineLauncher(listener).withClasses(AnnotationSpecClass::class).launch()
          listener.tests.shouldHaveSize(2)
       }
-
-//      it("should detect nested classes") {
-//         val listener = CollectingTestEngineListener()
-//         TestEngineLauncher(listener).withClasses(AnnotationSpecWithNested::class).launch()
-//         listener.tests.shouldHaveSize(2)
-//      }
 
       it("should support throwing exceptions with @Test(expected=foo)") {
          val listener = CollectingTestEngineListener()
@@ -44,9 +42,52 @@ class AnnotationSpecTest : DescribeSpec({
          val ds = listener.tests.mapKeys { it.key.descriptor.id }
          ds[DescriptorId("test3")]?.isFailure shouldBe true
       }
-   }
-})
 
+      describe("with @Nested tests") {
+
+         it("should detect nested classes") {
+            val listener = CollectingTestEngineListener()
+            TestEngineLauncher(listener).withClasses(AnnotationSpecWithNested::class).launch()
+            listener.tests.shouldHaveSize(2)
+            val ds = listener.tests.mapKeys { it.key.descriptor.id }
+            ds.shouldContainKeys(
+               DescriptorId("foo"),
+               DescriptorId("bar"),
+            )
+         }
+
+         it("should succeed with valid tests") {
+            val listener = CollectingTestEngineListener()
+            TestEngineLauncher(listener).withClasses(AnnotationSpecWithNested::class).launch()
+            val ds = listener.tests.mapKeys { it.key.descriptor.id }
+
+            val fooTest = ds[DescriptorId("foo")] ?: fail("invalid test set up - no test 'foo'")
+            fooTest.shouldBeSuccess()
+
+            val barTest = ds[DescriptorId("bar")] ?: fail("invalid test set up - no test 'bar'")
+            barTest.shouldBeSuccess()
+         }
+      }
+   }
+}) {
+   companion object {
+      private fun TestResult.shouldBeSuccess() {
+
+         val desc = "test '${name}' " + when (this) {
+            is TestResult.Error   -> "errored: $cause"
+            is TestResult.Failure -> "failed: $cause"
+            is TestResult.Ignored -> "ignored: $reason"
+            is TestResult.Success -> "passed"
+         }
+
+         withClue(desc) {
+            isSuccess shouldBe true
+         }
+      }
+   }
+}
+
+@Suppress("unused")
 private class AnnotationSpecClass : AnnotationSpec() {
 
    @Test
@@ -56,9 +97,9 @@ private class AnnotationSpecClass : AnnotationSpec() {
    @Test
    private fun test2() {
    }
-
 }
 
+@Suppress("unused")
 private class AnnotationSpecWithExceptions : AnnotationSpec() {
 
    private class FooException : RuntimeException()
@@ -80,16 +121,17 @@ private class AnnotationSpecWithExceptions : AnnotationSpec() {
    }
 }
 
-//class AnnotationSpecWithNested : AnnotationSpec() {
-//
-//   @Test
-//   fun foo() {
-//   }
-//
-//   @Nested
-//   class MyNested : AnnotationSpec() {
-//      @Test
-//      fun bar() {
-//      }
-//   }
-//}
+@Suppress("unused")
+class AnnotationSpecWithNested : AnnotationSpec() {
+
+   @Test
+   fun foo() {
+   }
+
+   @Nested
+   class MyNested : AnnotationSpec() {
+      @Test
+      fun bar() {
+      }
+   }
+}
