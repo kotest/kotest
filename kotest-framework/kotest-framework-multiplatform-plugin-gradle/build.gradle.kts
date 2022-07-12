a85017a556174767edce0a50956869211eaedd32
Charles Korn <me@charleskorn.com>
2022-07-12 20:14:53 +1000

    Revert "Remove Gradle plugin test for now."
    This reverts commit ec14c550beba282e60a6e10bfad8c07121478a15.

--- kotest-framework/kotest-framework-multiplatform-plugin-gradle/build.gradle.kts
+++ kotest-framework/kotest-framework-multiplatform-plugin-gradle/build.gradle.kts
@@ -25,6 +25,51 @@ repositories {
 dependencies {
    compileOnly(gradleApi())
    compileOnly(libs.kotlin.gradle.plugin)
+
+   testImplementation(project(Projects.Assertions.Core))
+   testImplementation(project(Projects.Framework.api))
+   testImplementation(project(Projects.Framework.engine))
+   testImplementation(project(Projects.JunitRunner))
+}
+
+tasks.withType<Test> {
+   // Build these libraries ahead of time so that the test project doesn't try to build them itself (if it tries to build them while we are as well, this can lead to conflicts)
+   setOf(
+      Projects.Assertions.Core,
+      Projects.Framework.api,
+      Projects.Framework.engine
+   ).forEach { project ->
+      setOf(
+         "jvmJar",
+         "compileKotlinLinuxX64",
+         "compileKotlinMacosX64",
+         "compileKotlinMacosArm64",
+         "compileKotlinMingwX64",
+      ).forEach { task ->
+         dependsOn("$project:$task")
+      }
+   }
+
+   setOf(
+      Projects.JunitRunner,
+      ":kotest-framework:kotest-framework-multiplatform-plugin-js",
+      ":kotest-framework:kotest-framework-multiplatform-plugin-native"
+   ).forEach { project ->
+      dependsOn("$project:jvmJar")
+   }
+
+   dependsOn("jar")
+
+   useJUnitPlatform()
+
+   systemProperty("kotestVersion", Ci.publishVersion)
+
+   testLogging {
+      showExceptions = true
+      showStandardStreams = true
+      events = setOf(TestLogEvent.FAILED, TestLogEvent.SKIPPED, TestLogEvent.STANDARD_ERROR, TestLogEvent.STANDARD_OUT)
+      exceptionFormat = TestExceptionFormat.FULL
+   }
 }
 
 tasks {
