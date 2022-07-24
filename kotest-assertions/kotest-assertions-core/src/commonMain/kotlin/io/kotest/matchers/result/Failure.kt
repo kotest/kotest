6a552b038c8d706bfd4ad25f5c5d3e8917ffa60e
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-07-24 18:39:37 -0300

    ♻️ Refactors Failure Matchers to its own file

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/result/Failure.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/result/Failure.kt
@@ -4,47 +4,120 @@ import io.kotest.matchers.Matcher
 import io.kotest.matchers.MatcherResult
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldNot
+import kotlin.js.JsName
+import kotlin.jvm.JvmName
 import kotlin.reflect.KClass
 
-fun Result<Any?>.shouldBeFailure(block: ((Throwable) -> Unit)? = null): Throwable {
-   this should BeFailure()
-   return exceptionOrNull()!!.also { block?.invoke(it) }
+/**
+ * Asserts that this result is not a Failure
+ *
+ * success("abc").shouldNotBeFailure()       // Assertion passes
+ * failure(MyException).shouldNotBeFailure() // Assertion fails
+ *
+ * @see shouldBeSuccess
+ */
+fun Result<*>.shouldNotBeFailure() = this shouldNot FailureMatcher()
+
+/**
+ * Asserts that this result is any Failure
+ *
+ * failure(MyException).shouldBeFailure()  // Assertion passes
+ * success("abc").shouldBeFailure()        // Assertion fails
+ *
+ * @see shouldNotBeSuccess
+ */
+fun Result<*>.shouldBeFailure(): Throwable {
+  this should FailureMatcher()
+  return exceptionOrNull()!!
 }
 
-fun <T> Result<T>.shouldNotBeFailure() = this shouldNot BeFailure()
-
-inline fun <reified A : Throwable> Result<Any?>.shouldBeFailureOfType(): A {
-   this should BeFailureOfType(A::class)
-   return exceptionOrNull() as A
+/**
+ * Asserts that this result fails with exactly [expected]
+ *
+ * failure(MyException) shouldBeFailure MyException     // Assertion passes
+ * failure(OtherException) shouldBeFailure MyException  // Assertion fails
+ * success("abc") shouldBeFailure MyException           // Assertion fails
+ *
+ */
+infix fun Result<*>.shouldBeFailure(expected: Throwable): Throwable {
+  this should FailureMatcher(expected)
+  return exceptionOrNull()!!
 }
 
+/**
+ * Asserts that this result is a failure, and lets you use its error in [block]
+ *
+ * failure(MyException) shouldBeFailure {
+ *   it shouldHaveMessage "MyException"
+ * }
+ */
+infix fun Result<*>.shouldBeFailure(block: ((Throwable) -> Unit)): Throwable {
+  this should FailureMatcher()
+  return exceptionOrNull()!!.also { block(it) }
+}
+
+/**
+ * Asserts that this result is a failure of type [T]
+ *
+ * failure(MyException).shouldBeFailure<MyException>()      // Assertion passes
+ * failure(MyException).shouldBeFailure<MyOtherException>() // Assertion fails
+ * success("abc").shouldBeFailure<MyException>()            // Assertion fails
+ *
+ */
+@JvmName("shouldBeFailureT")
+inline fun <reified T : Throwable> Result<*>.shouldBeFailure(): T {
+  this should FailureTypeMatcher(T::class)
+  return exceptionOrNull() as T
+}
+
+internal val AnyError = object : Throwable() {}
+
+@Suppress("FunctionName")
+@JsName("EmptyConstructorFailureMatcher")
+fun FailureMatcher(): Matcher<Result<*>> = FailureMatcher(AnyError)
+class FailureMatcher<T : Throwable>(val expected: T) : Matcher<Result<*>> {
+  override fun test(value: Result<*>): MatcherResult {
+    if (value.isSuccess) return MatcherResult(false, { "Expected a Failure, but got $value" }, { "" })
+
+    val actual = value.exceptionOrNull()
+    if (expected === AnyError) {
+      return MatcherResult(true, { "" }, { "" })
+    }
+
+    return MatcherResult(
+      actual == expected,
+      { "Result should be Failure($expected) but was Failure($actual)" },
+      { "Result should not be a failure, but was $actual" }
+    )
+  }
+}
+
+@Deprecated("Use FailureMatcher directly", ReplaceWith("FailureMatcher"))
+typealias BeFailure = FailureMatcher<Throwable>
+
+@Deprecated("Use FailureMatcher directly", ReplaceWith("FailureMatcher"))
+fun beFailure() = FailureMatcher()
+
+class FailureTypeMatcher<T : Throwable>(val clazz: KClass<T>) : Matcher<Result<*>> {
+  override fun test(value: Result<*>): MatcherResult {
+    if (value.isSuccess) return MatcherResult(false, { "Expected a Failure, but got $value" }, { "" })
+
+    val error = value.exceptionOrNull()!!
+
+    return MatcherResult(
+      clazz.isInstance(error),
+      { "Result should be a failure of type $clazz but was ${error::class}" },
+      { "" }
+    )
+  }
+}
+
+@Deprecated("Use FailureTypeMatcher directly")
+typealias BeFailureOfType<T> = FailureTypeMatcher<T>
+
+@Deprecated("Use shouldBeFailure<T> directly", ReplaceWith("shouldBeFailure"))
+inline fun <reified A : Throwable> Result<Any?>.shouldBeFailureOfType(): A = shouldBeFailure<A>()
+
+@Deprecated("This method isn't easy to understand. Prefer writing a different assertion")
 inline fun <reified A : Throwable> Result<Any?>.shouldNotBeFailureOfType() = this shouldNot BeFailureOfType(A::class)
-fun beFailure(): BeFailure = BeFailure()
-class BeFailure : Matcher<Result<Any?>> {
-   override fun test(value: Result<Any?>) = MatcherResult(
-      value.isFailure,
-      { "Result should be a failure but was ${value.getOrNull()}" },
-      { "Result should not be a failure" })
-}
 
-class BeFailureOfType<A : Throwable>(private val clazz: KClass<A>) : Matcher<Result<Any?>> {
-   override fun test(value: Result<Any?>): MatcherResult {
-      val error = value.exceptionOrNull()
-      return when {
-         value.isSuccess -> MatcherResult(
-            false,
-            { "Result should be a failure but was success" },
-            { "" })
-         clazz.isInstance(error) -> MatcherResult(
-            true,
-            { "Result should be a Failure($clazz)" },
-            { "Result should not be a Failure($clazz)" })
-         else -> {
-            MatcherResult(
-               false,
-               { "Result should be a Failure($clazz) but was Failure(${error!!::class})" },
-               { "" })
-         }
-      }
-   }
-}
