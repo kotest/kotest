18ae52a80769aad16c9287a9dbc1e3b1be47db60
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-07-24 14:44:24 -0300

    ♻️ Refactors Success Matchers to its own file
    Fixes #3111

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/result/Failure.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/result/Failure.kt
@@ -6,20 +6,6 @@ import io.kotest.matchers.should
 import io.kotest.matchers.shouldNot
 import kotlin.reflect.KClass
 
-fun <T> Result<T>.shouldBeSuccess(block: ((T) -> Unit)? = null): T {
-   this should beSuccess()
-   return getOrThrow().also { block?.invoke(it) }
-}
-
-fun <T> Result<T>.shouldNotBeSuccess() = this shouldNot beSuccess()
-
-infix fun <T> Result<T>.shouldBeSuccess(expected: T): T {
-   this should beSuccess(expected)
-   return getOrThrow()
-}
-
-infix fun <T> Result<T>.shouldNotBeSuccess(expected: T) = this shouldNot beSuccess(expected)
-
 fun Result<Any?>.shouldBeFailure(block: ((Throwable) -> Unit)? = null): Throwable {
    this should BeFailure()
    return exceptionOrNull()!!.also { block?.invoke(it) }
@@ -33,39 +19,6 @@ inline fun <reified A : Throwable> Result<Any?>.shouldBeFailureOfType(): A {
 }
 
 inline fun <reified A : Throwable> Result<Any?>.shouldNotBeFailureOfType() = this shouldNot BeFailureOfType(A::class)
-
-fun <T> beSuccess(): Matcher<Result<T>> = object : Matcher<Result<T>> {
-   override fun test(value: Result<T>): MatcherResult {
-      return MatcherResult(
-         value.isSuccess,
-         { "Result should be a Success but was $value" },
-         { "Result should not be a Success" })
-   }
-}
-
-fun <T> beSuccess(expected: T?): BeSuccess<T> = BeSuccess(expected)
-class BeSuccess<T>(val expected: T?) : Matcher<Result<T>> {
-   override fun test(value: Result<T>): MatcherResult {
-      return value.fold(
-         {
-            MatcherResult(
-               it == expected,
-               { "Result should be a Success($expected), but instead got Success($it)." },
-               { "Result should not be a Success($expected)" })
-         },
-         {
-            defaultResult(false)
-         }
-      )
-   }
-
-   private fun defaultResult(passed: Boolean) =
-      MatcherResult(
-         passed,
-         { "Result should be a success." },
-         { "Result should not be a success" })
-}
-
 fun beFailure(): BeFailure = BeFailure()
 class BeFailure : Matcher<Result<Any?>> {
    override fun test(value: Result<Any?>) = MatcherResult(
