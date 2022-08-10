32d046e11b8aba2e36f8c499f3f6ae08cea0491d
Emil Kantis <emil.kantis@protonmail.com>
2022-08-10 22:24:46 +0200

    Removed Kotlin/JS workaround for M1 Mac
    https://youtrack.jetbrains.com/issue/KT-49109 has been fixed. Thanks @tomuvak for the notice :)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,3 @@ repositories {
    google()
    gradlePluginPortal() // tvOS builds need to be able to fetch a kotlin gradle plugin
 }
-
-// TODO: Remove me after https://youtrack.jetbrains.com/issue/KT-49109 is fixed
-rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
-   rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.0.0"
-}
