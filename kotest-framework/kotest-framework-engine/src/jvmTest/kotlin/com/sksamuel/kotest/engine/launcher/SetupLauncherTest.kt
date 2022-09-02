b849754e4d42c1bfe0d9b6788b2ab82fb25f0961
Nikunj Yadav <nikunj@brex.com>
2022-09-02 00:24:19 -0700

    Wildcard support in launcher

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/launcher/SetupLauncherTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/launcher/SetupLauncherTest.kt
@@ -2,10 +2,18 @@ package com.sksamuel.kotest.engine.launcher
 
 import io.kotest.core.annotation.Isolate
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
+import io.kotest.core.test.TestType
 import io.kotest.engine.launcher.LauncherArgs
 import io.kotest.engine.launcher.setupLauncher
+import io.kotest.engine.listener.AbstractTestEngineListener
 import io.kotest.engine.listener.NoopTestEngineListener
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+import io.mockk.InternalPlatformDsl.toArray
+import kotlin.reflect.KClass
 
 @Isolate
 class SetupLauncherTest : FunSpec() {
