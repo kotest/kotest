2d13bdb5b0b5fd03e553e4b96a762ae4122fbeba
sksamuel <sam@sksamuel.com>
2022-10-02 18:56:35 -0500

    Fix withData for all BehaviorSpec scopes.
    Fixes #3148

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/spec/style/behaviorSpec.kt
+++ kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/spec/style/behaviorSpec.kt
@@ -1,10 +1,15 @@
 package io.kotest.core.spec.style
 
+import io.kotest.common.ExperimentalKotest
 import io.kotest.core.factory.TestFactory
 import io.kotest.core.factory.TestFactoryConfiguration
 import io.kotest.core.factory.build
+import io.kotest.core.names.TestName
 import io.kotest.core.spec.DslDrivenSpec
+import io.kotest.core.spec.style.scopes.BehaviorSpecGivenContainerScope
 import io.kotest.core.spec.style.scopes.BehaviorSpecRootScope
+import io.kotest.core.spec.style.scopes.BehaviorSpecWhenContainerScope
+import io.kotest.core.spec.style.scopes.ContainerScope
 
 /**
  * Creates a [TestFactory] from the given block.
@@ -24,4 +29,48 @@ abstract class BehaviorSpec(body: BehaviorSpec.() -> Unit = {}) : DslDrivenSpec(
    init {
       body()
    }
+
+   /**
+    * Adds a [BehaviorSpecGivenContainerScope] to this container.
+    */
+   @Suppress("FunctionName")
+   @ExperimentalKotest
+   suspend fun ContainerScope.Given(name: String, test: suspend BehaviorSpecGivenContainerScope.() -> Unit) =
+      registerContainer(
+         TestName("Given: ", name, true),
+         disabled = false,
+         null,
+      ) { BehaviorSpecGivenContainerScope(this).test() }
+
+   /**
+    * Adds a [BehaviorSpecGivenContainerScope] to this container.
+    */
+   @ExperimentalKotest
+   suspend fun ContainerScope.given(name: String, test: suspend BehaviorSpecGivenContainerScope.() -> Unit) =
+      registerContainer(
+         TestName("Given: ", name, true),
+         disabled = false,
+         null,
+      ) { BehaviorSpecGivenContainerScope(this).test() }
+
+   /**
+    * Adds a [BehaviorSpecWhenContainerScope] to this container.
+    */
+   @Suppress("FunctionName")
+   suspend fun ContainerScope.When(name: String, test: suspend BehaviorSpecWhenContainerScope.() -> Unit) =
+      registerContainer(
+         TestName("When: ", name, true),
+         disabled = false,
+         null,
+      ) { BehaviorSpecWhenContainerScope(this).test() }
+
+   /**
+    * Adds a [BehaviorSpecWhenContainerScope] to this container.
+    */
+   suspend fun ContainerScope.`when`(name: String, test: suspend BehaviorSpecWhenContainerScope.() -> Unit) =
+      registerContainer(
+         TestName("When: ", name, true),
+         disabled = false,
+         null,
+      ) { BehaviorSpecWhenContainerScope(this).test() }
 }
