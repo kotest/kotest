69295919f41087586011bce0fdaea66e068a7f77
sksamuel <sam@sksamuel.com>
2022-10-02 22:53:07 -0500

    Removed catalog publishing

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,9 +1,1 @@
 rootProject.name = "buildSrc"
-
-dependencyResolutionManagement {
-   versionCatalogs {
-      create("libs") {
-         from(files("../gradle/libs.versions.toml"))
-      }
-   }
-}
