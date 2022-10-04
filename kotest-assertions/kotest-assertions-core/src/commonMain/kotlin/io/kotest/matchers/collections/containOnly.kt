bc919e44a9b0c0070acc08d0b512f8c0b27dbac0
p1729 <pankajkumar1729@gmail.com>
2022-10-04 08:05:27 +0530

    #3213 add some more examples

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/containOnly.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/containOnly.kt
@@ -8,9 +8,11 @@ import io.kotest.assertions.AssertionsConfig.maxCollectionPrintSize as printSize
 /**
  * Assert that a collection contains only the given elements.
  * For example,
- *  list(1, 1, 2) shouldContainOnly listOf(1, 2)    // Assertion passes
- *  list(1, 2, 1) shouldContainOnly listOf(1)       // Assertion fails
- *  list(1) shouldContainOnly listOf(2, 2)          // Assertion fails
+ *  listOf(1, 1, 2) shouldContainOnly listOf(1, 2)    // Assertion passes
+ *  listOf(1, 2, 1) shouldContainOnly listOf(1)       // Assertion fails
+ *  listOf(1) shouldContainOnly listOf(2, 2)          // Assertion fails
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
  */
 @JvmName("shouldContainOnly_iterable")
 infix fun <T> Iterable<T>?.shouldContainOnly(expected: Iterable<T>) =
@@ -22,6 +24,8 @@ infix fun <T> Iterable<T>?.shouldContainOnly(expected: Iterable<T>) =
  *  arrayOf(1, 1, 2) shouldContainOnly arrayOf(1, 2)    // Assertion passes
  *  arrayOf(1, 2, 1) shouldContainOnly arrayOf(1)       // Assertion fails
  *  arrayOf(1) shouldContainOnly arrayOf(2, 2)          // Assertion fails
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
  */
 @JvmName("shouldContainOnly_array")
 infix fun <T> Array<T>?.shouldContainOnly(expected: Array<T>) =
@@ -30,9 +34,11 @@ infix fun <T> Array<T>?.shouldContainOnly(expected: Array<T>) =
 /**
  * Assert that a collection contains only the given elements.
  * For example,
- *  list(1, 1, 2) shouldContainOnly(1, 2)    // Assertion passes
- *  list(1, 2, 1) shouldContainOnly(1)       // Assertion fails
- *  list(1) shouldContainOnly(2, 2)          // Assertion fails
+ *  listOf(1, 1, 2) shouldContainOnly(1, 2)    // Assertion passes
+ *  listOf(1, 2, 1) shouldContainOnly(1)       // Assertion fails
+ *  listOf(1) shouldContainOnly(2, 2)          // Assertion fails
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
  */
 fun <T> Iterable<T>?.shouldContainOnly(vararg expected: T) =
     this?.toList() should containOnly(*expected)
@@ -43,17 +49,31 @@ fun <T> Iterable<T>?.shouldContainOnly(vararg expected: T) =
  *  arrayOf(1, 1, 2) shouldContainOnly(1, 2)    // Assertion passes
  *  arrayOf(1, 2, 1) shouldContainOnly(1)       // Assertion fails
  *  arrayOf(1) shouldContainOnly(2, 2)          // Assertion fails
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
  */
 fun <T> Array<T>?.shouldContainOnly(vararg expected: T) =
     this?.asList() should containOnly(*expected)
 
 /**
  * Assert that a collection contains only the given elements.
+ * For example,
+ *  listOf(1, 1, 2) shouldContainOnly listOf(1, 2)    // Assertion passes
+ *  listOf(1, 2, 1) shouldContainOnly listOf(1)       // Assertion fails
+ *  listOf(1) shouldContainOnly listOf(2, 2)          // Assertion fails
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
  */
 infix fun <T, C : Collection<T>> C?.shouldContainOnly(expected: C) = this should containOnly(expected)
 
 /**
  * Assert that a collection contains only the given elements.
+ * For example,
+ *  listOf(1, 1, 2) shouldContainOnly(1, 2)    // Assertion passes
+ *  listOf(1, 2, 1) shouldContainOnly(1)       // Assertion fails
+ *  listOf(1) shouldContainOnly(2, 2)          // Assertion fails
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
  */
 fun <T> Collection<T>?.shouldContainOnly(vararg expected: T) = this should containOnly(*expected)
 
@@ -91,15 +111,71 @@ fun <T, C : Collection<T>> containOnly(expectedCollection: C): Matcher<C?> = nev
     MatcherResult(passed, failureMessageSupplier, negatedFailureMessageSupplier)
 }
 
+/**
+ * Assert that a collection should not have only the given elements
+ * For example,
+ *  listOf(1, 1, 2) shouldNotContainOnly listOf(1, 2)    // Assertion fails
+ *  listOf(1, 2, 1) shouldNotContainOnly listOf(1)       // Assertion passes
+ *  listOf(1) shouldNotContainOnly listOf(2, 2)          // Assertion passes
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
+ */
 @JvmName("shouldNotContainOnly_iterable")
 infix fun <T> Iterable<T>?.shouldNotContainOnly(expected: Iterable<T>) =
     this?.toList() shouldNot containOnly(expected.toList())
 
+/**
+ * Assert that an array should not have only the given elements
+ * For example,
+ *  arrayOf(1, 1, 2) shouldNotContainOnly arrayOf(1, 2)    // Assertion fails
+ *  arrayOf(1, 2, 1) shouldNotContainOnly arrayOf(1)       // Assertion passes
+ *  arrayOf(1) shouldNotContainOnly arrayOf(2, 2)          // Assertion passes
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
+ */
 @JvmName("shouldNotContainOnly_array")
 infix fun <T> Array<T>?.shouldNotContainOnly(expected: Array<T>) = this?.asList() shouldNot containOnly(*expected)
 
+/**
+ * Assert that a collection should not have only the given elements
+ * For example,
+ *  listOf(1, 1, 2) shouldNotContainOnly(1, 2)    // Assertion fails
+ *  listOf(1, 2, 1) shouldNotContainOnly(1)       // Assertion passes
+ *  listOf(1) shouldNotContainOnly(2, 2)          // Assertion passes
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
+ */
 fun <T> Iterable<T>?.shouldNotContainOnly(vararg expected: T) = this?.toList() shouldNot containOnly(*expected)
+
+/**
+ * Assert that an array should not have only the given elements
+ * For example,
+ *  arrayOf(1, 1, 2) shouldNotContainOnly(1, 2)    // Assertion fails
+ *  arrayOf(1, 2, 1) shouldNotContainOnly(1)       // Assertion passes
+ *  arrayOf(1) shouldNotContainOnly(2, 2)          // Assertion passes
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
+ */
 fun <T> Array<T>?.shouldNotContainOnly(vararg expected: T) = this?.asList() shouldNot containOnly(*expected)
 
+/**
+ * Assert that a collection should not have only the given elements
+ * For example,
+ *  listOf(1, 1, 2) shouldNotContainOnly listOf(1, 2)    // Assertion fails
+ *  listOf(1, 2, 1) shouldNotContainOnly listOf(1)       // Assertion passes
+ *  listOf(1) shouldNotContainOnly listOf(2, 2)          // Assertion passes
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
+ */
 infix fun <T, C : Collection<T>> C?.shouldNotContainOnly(expected: C) = this shouldNot containOnly(expected)
+
+/**
+ * Assert that a collection should not have only the given elements
+ * For example,
+ *  listOf(1, 1, 2) shouldNotContainOnly(1, 2)    // Assertion fails
+ *  listOf(1, 2, 1) shouldNotContainOnly(1)       // Assertion passes
+ *  listOf(1) shouldNotContainOnly(2, 2)          // Assertion passes
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
+ */
 fun <T> Collection<T>?.shouldNotContainOnly(vararg expected: T) = this shouldNot containOnly(*expected)
