6d64ab461b97baf8dfd032eb7933b5af95f16727
Matej Drobnic <matej.drobnic@externals.adidas.com>
2022-11-16 14:15:29 +0100

    Add runTest support to JS

--- kotest-framework/kotest-framework-engine/src/jsMain/kotlin/io/kotest/engine/test/interceptors/TestCoroutineInterceptor.kt
+++ kotest-framework/kotest-framework-engine/src/jsMain/kotlin/io/kotest/engine/test/interceptors/TestCoroutineInterceptor.kt
@@ -3,6 +3,10 @@ package io.kotest.engine.test.interceptors
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
 import io.kotest.core.test.TestScope
+import io.kotest.engine.test.scopes.withCoroutineTestScope
+import io.kotest.mpp.Logger
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 
 /**
  * A [TestExecutionInterceptor] that uses [runTest] from the coroutine library
@@ -10,10 +14,22 @@ import io.kotest.core.test.TestScope
  *
  * This setting cannot be nested.
  */
+@ExperimentalCoroutinesApi
+@ExperimentalStdlibApi
 actual class TestCoroutineInterceptor : TestExecutionInterceptor {
+
+   private val logger = Logger(TestCoroutineInterceptor::class)
+
    override suspend fun intercept(
       testCase: TestCase,
       scope: TestScope,
       test: suspend (TestCase, TestScope) -> TestResult
-   ): TestResult = test(testCase, scope)
+   ): TestResult {
+      var result: TestResult = TestResult.Ignored
+      logger.log { Pair(testCase.name.testName, "Switching context to coroutines runTest") }
+      runTest {
+         result = test(testCase, scope.withCoroutineTestScope(this))
+      }
+      return result
+   }
 }
