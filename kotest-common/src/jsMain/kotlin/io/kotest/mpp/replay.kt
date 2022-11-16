11e5300a02bd51914defeed1dcb2155cd67b5a22
Matej Drobnic <matej.drobnic@externals.adidas.com>
2022-11-16 12:48:38 +0100

    Use TestDispatcher inside beforeInvocation callbacks

--- kotest-common/src/jsMain/kotlin/io/kotest/mpp/replay.kt
+++ kotest-common/src/jsMain/kotlin/io/kotest/mpp/replay.kt
@@ -3,14 +3,10 @@ package io.kotest.mpp
 actual suspend fun replay(
    times: Int,
    threads: Int,
-   before: suspend (Int) -> Unit,
-   after: suspend (Int) -> Unit,
    action: suspend (Int) -> Unit
 ) {
    require(threads == 1) { "Cannot run JS tests on multiple threads. Use the built in kotlin function repeat(n)" }
    repeat(times) {
-      before(it)
       action(it)
-      after(it)
    }
 }
