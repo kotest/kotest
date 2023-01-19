6ffca150a89c54770d83284167eec4bdebc0eb78
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-01-19 12:59:52 +0300

    avoid quoting top-level strings to make outputs copy-paste friendly
    Fixes https://github.com/kotest/kotest/issues/3346

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/print/StringPrint.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/print/StringPrint.kt
@@ -14,9 +14,13 @@ object StringPrint : Print<String> {
       else -> a.printed()
    }
 
-   override fun print(a: String): Printed = when {
+   override fun print(a: String): Printed = print(a, 0)
+
+   override fun print(a: String, level: Int): Printed = when {
       a == "" -> "<empty string>".printed()
-      a.isBlank() -> a.replace(" ", "\\s").wrap().printed()
+      a.isBlank() -> "<blank string>".printed()
+      // Skip wrapping top-level strings with quotes. It makes easier copying the values to the code
+      level == 0 -> a.printed()
       else -> a.wrap().printed()
    }
 }
