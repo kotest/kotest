baf7861f06ad0664e38c8c4e6da0c38b184381de
aSemy <897017+aSemy@users.noreply.github.com>
2023-02-12 23:50:27 +0100

    re-implement language injection annotation (#3397)
    * re-implement language injection annotation
    
    * reduce build warnings - opt in to ExperimentalKotest
    
    * add android project integration test
    
    * match the kotlin version used by Kotest
    
    * revert Android Library integration test (moved to https://github.com/kotest/kotest/pull/3401)

--- kotest-common/src/desktopMain/kotlin/io/kotest/common/injectLanguage.kt
+++ kotest-common/src/desktopMain/kotlin/io/kotest/common/injectLanguage.kt
@@ -1,21 +1,4 @@
-@file:Suppress("PackageDirectoryMismatch")
-package org.intellij.lang.annotations
-
-import io.kotest.common.KotestInternal
+package io.kotest.common
 
 @KotestInternal
-@Retention(AnnotationRetention.BINARY)
-@Target(
-   AnnotationTarget.FUNCTION,
-   AnnotationTarget.PROPERTY_GETTER,
-   AnnotationTarget.PROPERTY_SETTER,
-   AnnotationTarget.FIELD,
-   AnnotationTarget.VALUE_PARAMETER,
-   AnnotationTarget.LOCAL_VARIABLE,
-   AnnotationTarget.ANNOTATION_CLASS,
-)
-actual annotation class Language actual constructor(
-   actual val value: String,
-   actual val prefix: String,
-   actual val suffix: String
-)
+actual typealias KotestLanguage = org.intellij.lang.annotations.Language
