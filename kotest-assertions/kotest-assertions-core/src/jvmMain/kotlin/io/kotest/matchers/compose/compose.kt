ca10e399bc614ec7d95b8fdd7f9939105ffb6792
Łukasz Pięta <pieta.lukasz98@gmail.com>
2023-02-13 08:48:21 +0100

    More flexible way to compose Matchers

---
+++ kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/compose/compose.kt
@@ -0,0 +1,55 @@
+package io.kotest.matchers.compose
+
+import io.kotest.assertions.fail
+import io.kotest.matchers.Matcher
+import io.kotest.matchers.MatcherResult
+import kotlin.reflect.KProperty1
+
+@Suppress("UNCHECKED_CAST")
+fun <T : Any?> Matcher.Companion.compose(
+   vararg pairs: Pair<Matcher<*>, KProperty1<T, *>>
+): Matcher<T> = object : Matcher<T> {
+   override fun test(value: T): MatcherResult {
+      val results = pairs.mapIndexed {i , (matcher, prop) ->
+         runCatching {
+            (matcher as Matcher<Any?>).test(prop(value))
+         }.onFailure {
+            fail("Mismatching types in argument ${i + 1} for composed matcher:\n\n${it.message}")
+         }.getOrThrow()
+      }
+
+      return MatcherResult(
+         results.all { it.passed() },
+         {
+            results.map { it.failureMessage() }.fold("") { acc: String, s: String -> acc + s + "\n" }
+               .trimIndent()
+         },
+         {
+            results.map { it.negatedFailureMessage() }.fold("") { acc: String, s: String -> acc + s + "\n" }
+               .trimIndent()
+         },
+      )
+   }
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <T : Any?> Matcher.Companion.compose(
+   vararg matchers: Matcher<T>
+): Matcher<T> = object : Matcher<T> {
+   override fun test(value: T): MatcherResult {
+      val results = matchers.mapIndexed { i, matcher  ->
+         runCatching {
+            (matcher as Matcher<Any?>).test(value)
+         }.onFailure {
+            fail("Mismatching types in argument ${i + 1} for composed matcher:\n\n${it.message}")
+         }.getOrThrow()
+      }
+
+      return MatcherResult(
+         results.all { it.passed() },
+         { results.joinToString(separator = "\n") { it.failureMessage() } },
+         { results.joinToString(separator = "\n") { it.negatedFailureMessage() } },
+      )
+   }
+}
+
