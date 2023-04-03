eb6267299909a7e0465eb7f358617e24d08d3fd0
Sam <sam@sksamuel.com>
2023-04-03 05:53:35 -0500

    Deprecate older json matchers (#3474)

--- kotest-assertions/kotest-assertions-json/src/commonMain/kotlin/io/kotest/assertions/json/matchers.kt
+++ kotest-assertions/kotest-assertions-json/src/commonMain/kotlin/io/kotest/assertions/json/matchers.kt
@@ -3,6 +3,8 @@ package io.kotest.assertions.json
 import io.kotest.matchers.ComparableMatcherResult
 import io.kotest.matchers.Matcher
 import io.kotest.matchers.MatcherResult
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldNot
 
 /**
  * Returns a [Matcher] that verifies json trees are equal.
@@ -46,44 +48,61 @@ fun equalJson(
 
 data class JsonTree(val root: JsonNode, val raw: String)
 
-infix fun String.shouldEqualJson(expected: String): Unit =
-   this.shouldEqualJson(expected, defaultCompareJsonOptions)
+infix fun String.shouldEqualJson(expected: String): String {
+   val (e, a) = parse(expected, this)
+   a should equalJson(e, CompareJsonOptions())
+   return this
+}
+
+/**
+ * Verifies that the recevier matches the json string returned by the given block [configureAndProvideExpected].
+ * The function allows configuration of [CompareJsonOptions] before returning the expected json.
+ */
+infix fun String.shouldEqualJson(configureAndProvideExpected: CompareJsonOptions.() -> String): String {
+   val options = CompareJsonOptions()
+   val expected = options.configureAndProvideExpected()
+   val (e, a) = parse(expected, this)
+   a should equalJson(e, options)
+   return this
+}
+
+infix fun String.shouldNotEqualJson(expected: String): String {
+   val (e, a) = parse(expected, this)
+   a shouldNot equalJson(e, CompareJsonOptions())
+   return this
+}
 
 /**
  * Configures [CompareJsonOptions] with the given block, which should also return the expected value
  */
-infix fun String.shouldEqualJson(configureAndProvideExpected: CompareJsonOptions.() -> String) {
+infix fun String.shouldNotEqualJson(configureAndProvideExpected: CompareJsonOptions.() -> String): String {
    val options = CompareJsonOptions()
    val expected = options.configureAndProvideExpected()
-   this.shouldEqualJson(expected, options)
+   val (e, a) = parse(expected, this)
+   a shouldNot equalJson(e, options)
+   return this
 }
 
-infix fun String.shouldNotEqualJson(expected: String): Unit =
-   this.shouldNotEqualJson(expected, defaultCompareJsonOptions)
-
-/**
- * Configures [CompareJsonOptions] with the given block, which should also return the expected value
- */
-infix fun String.shouldNotEqualJson(configureAndProvideExpected: CompareJsonOptions.() -> String) {
-   val options = CompareJsonOptions()
-   val expected = options.configureAndProvideExpected()
-   this.shouldNotEqualJson(expected, options)
-}
-
+@Deprecated("Use shouldEqualJson which uses a lambda. Deprecated since 5.6. Will be removed in 6.0")
 fun String.shouldEqualJson(expected: String, mode: CompareMode) =
    shouldEqualJson(expected, legacyOptions(mode, CompareOrder.Strict))
 
+@Deprecated("Use shouldEqualJson which uses a lambda. Deprecated since 5.6. Will be removed in 6.0")
 fun String.shouldNotEqualJson(expected: String, mode: CompareMode) =
    shouldNotEqualJson(expected, legacyOptions(mode, CompareOrder.Strict))
 
+@Deprecated("Use shouldEqualJson which uses a lambda. Deprecated since 5.6. Will be removed in 6.0")
 fun String.shouldEqualJson(expected: String, order: CompareOrder) =
    shouldEqualJson(expected, legacyOptions(CompareMode.Strict, order))
 
+@Deprecated("Use shouldNotEqualJson which uses a lambda. Deprecated since 5.6. Will be removed in 6.0")
 fun String.shouldNotEqualJson(expected: String, order: CompareOrder) =
    shouldNotEqualJson(expected, legacyOptions(CompareMode.Strict, order))
 
+@Deprecated("Use shouldNotEqualJson which uses a lambda. Deprecated since 5.6. Will be removed in 6.0")
 infix fun String.shouldEqualSpecifiedJson(expected: String) =
    shouldEqualJson(expected, compareJsonOptions { fieldComparison = FieldComparison.Lenient })
 
+@Deprecated("Use shouldNotEqualJson which uses a lambda. Deprecated since 5.6. Will be removed in 6.0")
 infix fun String.shouldNotEqualSpecifiedJson(expected: String) =
    shouldNotEqualJson(expected, compareJsonOptions { fieldComparison = FieldComparison.Lenient })
