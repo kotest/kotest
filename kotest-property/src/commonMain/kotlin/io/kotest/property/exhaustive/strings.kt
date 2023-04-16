cdf3eb466da1f4cd439f92f8fc8a9d2c314231a5
Emil Kantis <emil.kantis@protonmail.com>
2023-04-17 00:50:35 +0200

    Adding generators for upper/lower casing strings (#3402)
    * Adding generators for upper/lower casing strings
    
    * Adding warning for extreme permutations (1M+)
    
    ---------
    
    Co-authored-by: Sam <sam@sksamuel.com>

--- kotest-property/src/commonMain/kotlin/io/kotest/property/exhaustive/strings.kt
+++ kotest-property/src/commonMain/kotlin/io/kotest/property/exhaustive/strings.kt
@@ -1,6 +1,7 @@
 package io.kotest.property.exhaustive
 
 import io.kotest.property.Exhaustive
+import io.kotest.property.RandomSource
 
 fun Exhaustive.Companion.azstring(range: IntRange): Exhaustive<String> {
    fun az() = ('a'..'z').map { it.toString() }
@@ -9,3 +10,38 @@ fun Exhaustive.Companion.azstring(range: IntRange): Exhaustive<String> {
    }
    return values.exhaustive()
 }
+
+fun Exhaustive.Companion.upperLowerCase(s: String): Exhaustive<String> {
+   val caseSensitiveChars = s.filter(::isCaseSensitive).length
+
+   if (caseSensitiveChars > 20) {
+      println(
+         "[WARN] Generating exhaustive permutations for $s, which has $caseSensitiveChars case-sensitive characters. " +
+            "This will generate 2**$caseSensitiveChars permutations, which may take a while to run."
+      )
+   }
+
+   return Exhaustive.of(*s.upperLowerCases(RandomSource.default()).toList().toTypedArray())
+}
+
+internal fun String.upperLowerCases(rs: RandomSource): Sequence<String> = when {
+   isEmpty() -> sequenceOf("")
+   else -> first().let { c ->
+      val upper = c.uppercaseChar()
+      val lower = c.lowercaseChar()
+      val rest = substring(1).upperLowerCases(rs)
+
+      sequence {
+         if (upper == lower) rest.forEach { yield(buildString { append(c); append(it) }) }
+         else rest.forEach { substringCasing ->
+            // Otherwise we will guarantee to start with all upper, or all lower
+            val (first, second) = if (rs.random.nextBoolean()) upper to lower else lower to upper
+
+            yield(buildString { append(first); append(substringCasing) })
+            yield(buildString { append(second); append(substringCasing) })
+         }
+      }
+   }
+}
+
+private fun isCaseSensitive(c: Char) = c.uppercaseChar() != c.lowercaseChar()
