4c1bc92f503649f4fc38e0cb38f11fdf8eee0bd6
vladeekkk <vlad.krasss@yandex.ru>
2023-06-03 01:36:01 +0300

    add exhaustive enumsExcept

--- kotest-property/src/commonMain/kotlin/io/kotest/property/exhaustive/enum.kt
+++ kotest-property/src/commonMain/kotlin/io/kotest/property/exhaustive/enum.kt
@@ -6,3 +6,8 @@ inline fun <reified T : Enum<T>> Exhaustive.Companion.enum(): Exhaustive<T> {
    val constants = enumValues<T>().asList()
    return exhaustive(constants)
 }
+
+inline fun <reified T : Enum<T>> Exhaustive.Companion.enumsExcept(vararg elements: T): Exhaustive<T> {
+   val constants = enumValues<T>().toMutableList().apply { removeAll(elements.toSet()) }
+   return exhaustive(constants)
+}
