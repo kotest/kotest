db408424466272bca71d692c12b3113b2418a853
sksamuel <sam@sksamuel.com>
2023-08-28 03:03:06 -0500

    Updated NaN handling in assertions

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/doubles/Exactly.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/doubles/Exactly.kt
@@ -1,5 +1,6 @@
 package io.kotest.matchers.doubles
 
+import io.kotest.assertions.AssertionsConfig
 import io.kotest.matchers.Matcher
 import io.kotest.matchers.MatcherResult
 import io.kotest.matchers.shouldBe
@@ -16,9 +17,16 @@ import io.kotest.matchers.shouldNotBe
  * 0.1 shouldBeExactly 0.1   // Assertion passes
  * 0.1 shouldBeExactly 0.2   // Assertion fails
  * ```
+ *
+ * This matcher considers NaN to be equal to NaN.
+ * To disable this behavior, set the system property `kotest.assertions.nan.equality.disable` to true.
  */
 infix fun Double.shouldBeExactly(other: Double): Double {
-   this shouldBe exactly(other)
+   if (AssertionsConfig.disableNaNEquality) {
+      this shouldBe exactly(other)
+   } else {
+      this shouldBe exactlyByBits(other)
+   }
    return this
 }
 
@@ -33,16 +41,33 @@ infix fun Double.shouldBeExactly(other: Double): Double {
  * 0.1 shouldNotBeExactly 0.2   // Assertion passes
  * 0.1 shouldNotBeExactly 0.1   // Assertion fails
  * ```
+ *
+ * This matcher considers NaN to be equal to NaN.
+ * To disable this behavior, set the system property `kotest.assertions.nan.equality.disable` to true.
  */
 infix fun Double.shouldNotBeExactly(other: Double): Double {
-   this shouldNotBe exactly(other)
+   if (AssertionsConfig.disableNaNEquality) {
+      this shouldNotBe exactly(other)
+   } else {
+      this shouldNotBe exactlyByBits(other)
+   }
    return this
 }
 
 fun exactly(d: Double): Matcher<Double> = object : Matcher<Double> {
-  override fun test(value: Double) =
-     MatcherResult(
-        value == d,
-        { "$value is not equal to expected value $d" },
-        { "$value should not equal $d" })
+   override fun test(value: Double) =
+      MatcherResult(
+         value == d,
+         { "$value is not equal to expected value $d" },
+         { "$value should not equal $d" }
+      )
+}
+
+fun exactlyByBits(expected: Double): Matcher<Double> = object : Matcher<Double> {
+   override fun test(value: Double) =
+      MatcherResult(
+         value.toBits() == expected.toBits(),
+         { "$value is not equal to expected value $expected" },
+         { "$value should not equal $expected" }
+      )
 }
