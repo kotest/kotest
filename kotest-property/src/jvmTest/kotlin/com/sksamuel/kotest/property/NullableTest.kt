6e93382bfcbf6520f4abe535ffa329b8eac5e42c
kshired <shiroed1211@gmail.com>
2023-08-31 01:25:30 +0900

    Add test and fix typo

--- kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/NullableTest.kt
+++ kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/NullableTest.kt
@@ -5,6 +5,7 @@ import io.kotest.matchers.ints.shouldBeBetween
 import io.kotest.property.checkAll
 import io.kotest.property.forAll
 import io.kotest.property.forNone
+import io.kotest.property.withAssumptions
 
 class NullableTest : FunSpec({
    test("forAll with implicit nullable arbitaries") {
@@ -17,7 +18,7 @@ class NullableTest : FunSpec({
       classifications["non-null"]?.shouldBeBetween(300, 600)
    }
 
-   test("forNone with implicit nullable arbitaries") {
+   test("forNone with implicit nullable arbitraries") {
       val iterations = 1000
       val classifications = forNone<Int?>(iterations) { num ->
          classify(num == null, "null", "non-null")
@@ -27,12 +28,28 @@ class NullableTest : FunSpec({
       classifications["non-null"]?.shouldBeBetween(300, 600)
    }
 
-   test("checkAll with implicit nullable arbitaries") {
+   test("checkAll with implicit nullable arbitraries") {
       val iterations = 1000
       val classifications = checkAll<Int?>(iterations) { num ->
          classify(num == null, "null", "non-null")
       }.classifications()
       classifications["null"]?.shouldBeBetween(300, 600)
       classifications["non-null"]?.shouldBeBetween(300, 600)
+   }
+
+   test("checkAll with implicit nullable arbitraries with should not be null Assumption") {
+      checkAll<Int?> { num ->
+         withAssumptions(num != null) {
+            num != null
+         }
+      }
+   }
+
+   test("checkAll with implicit nullable arbitraries with should be null Assumption") {
+      checkAll<Int?> { num ->
+         withAssumptions(num == null) {
+            num == null
+         }
+      }
    }
 })
