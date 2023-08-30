e381b4bfeecabb489dded513995b135d958bc388
kshired <shiroed1211@gmail.com>
2023-08-31 02:30:31 +0900

    Add seed for test

--- kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/NullableTest.kt
+++ kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/NullableTest.kt
@@ -4,6 +4,7 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.ints.shouldBeBetween
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.kotest.property.PropTestConfig
 import io.kotest.property.checkAll
 import io.kotest.property.forAll
 import io.kotest.property.forNone
@@ -12,31 +13,31 @@ import io.kotest.property.withAssumptions
 class NullableTest : FunSpec({
    test("forAll with implicit nullable arbitaries") {
       val iterations = 1000
-      val classifications = forAll<Int?>(iterations) { num ->
+      val classifications = forAll<Int?>(iterations, PropTestConfig(seed = 1)) { num ->
          classify(num == null, "null", "non-null")
          true
       }.classifications()
-      classifications["null"]?.shouldBeBetween(300, 600)
-      classifications["non-null"]?.shouldBeBetween(300, 600)
+      classifications["null"] shouldBe 496
+      classifications["non-null"] shouldBe 504
    }
 
    test("forNone with implicit nullable arbitraries") {
       val iterations = 1000
-      val classifications = forNone<Int?>(iterations) { num ->
+      val classifications = forNone<Int?>(iterations, PropTestConfig(seed = 1)) { num ->
          classify(num == null, "null", "non-null")
          false
       }.classifications()
-      classifications["null"]?.shouldBeBetween(300, 600)
-      classifications["non-null"]?.shouldBeBetween(300, 600)
+      classifications["null"] shouldBe 496
+      classifications["non-null"] shouldBe 504
    }
 
    test("checkAll with implicit nullable arbitraries") {
       val iterations = 1000
-      val classifications = checkAll<Int?>(iterations) { num ->
+      val classifications = checkAll<Int?>(iterations, PropTestConfig(seed = 1)) { num ->
          classify(num == null, "null", "non-null")
       }.classifications()
-      classifications["null"]?.shouldBeBetween(300, 600)
-      classifications["non-null"]?.shouldBeBetween(300, 600)
+      classifications["null"] shouldBe 496
+      classifications["non-null"] shouldBe 504
    }
 
    test("checkAll with implicit nullable arbitraries with should not be null Assumption") {
