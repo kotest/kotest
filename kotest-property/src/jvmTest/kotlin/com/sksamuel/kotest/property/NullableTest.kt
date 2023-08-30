ec7281e445e8658b31eb3e651b442567892c2ede
kshired <shiroed1211@gmail.com>
2023-08-31 01:03:54 +0900

    Add test for forNone

--- kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/NullableTest.kt
+++ kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/NullableTest.kt
@@ -4,6 +4,7 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.ints.shouldBeBetween
 import io.kotest.property.checkAll
 import io.kotest.property.forAll
+import io.kotest.property.forNone
 
 class NullableTest : FunSpec({
    test("forAll with implicit nullable arbitaries") {
@@ -11,6 +12,16 @@ class NullableTest : FunSpec({
       val classifications = forAll<Int?>(iterations) { num ->
          classify(num == null, "null", "non-null")
          true
+      }.classifications()
+      classifications["null"]?.shouldBeBetween(300, 600)
+      classifications["non-null"]?.shouldBeBetween(300, 600)
+   }
+
+   test("forNone with implicit nullable arbitaries") {
+      val iterations = 1000
+      val classifications = forNone<Int?>(iterations) { num ->
+         classify(num == null, "null", "non-null")
+         false
       }.classifications()
       classifications["null"]?.shouldBeBetween(300, 600)
       classifications["non-null"]?.shouldBeBetween(300, 600)
