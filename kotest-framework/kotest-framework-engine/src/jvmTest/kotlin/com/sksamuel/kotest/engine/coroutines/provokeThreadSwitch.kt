63c927a9a4e52bb193a865e8995707d865cb63ae
OliverO2 <oliver.o456i@gmail.com>
2023-09-01 01:32:46 +0200

    Make provokeThreadSwitch internal and non-suspending

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/coroutines/provokeThreadSwitch.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/coroutines/provokeThreadSwitch.kt
@@ -2,9 +2,9 @@ package com.sksamuel.kotest.engine.coroutines
 
 /**
  * Provoke the dispatcher to switch coroutine threads by keeping the coroutine alive for a sufficient time period.
+ *
+ * This function is not suspending, but intended to be invoked in a coroutine exclusively.
  */
-@Suppress("RedundantSuspendModifier")
-suspend fun provokeThreadSwitch() {
-   @Suppress("BlockingMethodInNonBlockingContext")
+internal fun provokeThreadSwitch() {
    Thread.sleep(50)
 }
