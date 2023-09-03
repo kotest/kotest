3d34c117049c3fc18fabfec210495d0b03695a94
OliverO2 <Oliver.O456i@gmail.com>
2023-09-03 02:02:11 +0200

    Fix timeout detection when withTimeout wraps a blocking job (#3675)
    * Fix timeout detection when withTimeout wraps a blocking job
    
    This is a workaround for kotlinx.coroutines/issues/3875.
    
    Fixes #3672
    
    * Add timeout tests
    
    ---------
    
    Co-authored-by: Sam <sam@sksamuel.com>

--- kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/concurrency/DedicatedThreadCoroutineDispatcherFactory.kt
+++ kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/concurrency/DedicatedThreadCoroutineDispatcherFactory.kt
@@ -3,8 +3,9 @@ package io.kotest.engine.concurrency
 import io.kotest.core.concurrency.CoroutineDispatcherFactory
 import io.kotest.core.test.TestCase
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
 
 /**
  * A [CoroutineDispatcherFactory] that creates a dedicated thread for each test case.
@@ -14,7 +15,7 @@ object DedicatedThreadCoroutineDispatcherFactory : CoroutineDispatcherFactory {
 
    @OptIn(DelicateCoroutinesApi::class)
    override suspend fun <T> withDispatcher(testCase: TestCase, f: suspend () -> T): T =
-      newSingleThreadContext("dedicated").use { dispatcher ->
+      Executors.newSingleThreadExecutor().asCoroutineDispatcher().use { dispatcher ->
          withContext(dispatcher) {
             f()
          }
