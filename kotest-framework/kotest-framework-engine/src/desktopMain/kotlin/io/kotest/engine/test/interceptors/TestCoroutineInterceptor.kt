2ae389319778a0551dcda8c02f5334da29f130ab
sksamuel <sam@sksamuel.com>
2023-11-12 16:07:22 -0600

    tests

--- kotest-framework/kotest-framework-engine/src/desktopMain/kotlin/io/kotest/engine/test/interceptors/TestCoroutineInterceptor.kt
+++ kotest-framework/kotest-framework-engine/src/desktopMain/kotlin/io/kotest/engine/test/interceptors/TestCoroutineInterceptor.kt
@@ -1,12 +1,16 @@
 package io.kotest.engine.test.interceptors
 
+import io.kotest.core.coroutines.TestScopeContainer
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
 import io.kotest.core.test.TestScope
 import io.kotest.engine.test.scopes.withCoroutineContext
 import io.kotest.mpp.Logger
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withContext
 
+@ExperimentalCoroutinesApi
+@OptIn(KotestInternal::class)
 actual class TestCoroutineInterceptor : TestExecutionInterceptor {
 
    private val logger = Logger(TestCoroutineInterceptor::class)
@@ -19,7 +23,9 @@ actual class TestCoroutineInterceptor : TestExecutionInterceptor {
       var result: TestResult = TestResult.Ignored
       logger.log { Pair(testCase.name.testName, "Switching context to coroutines runTest") }
       runTest {
-         result = test(testCase, scope.withCoroutineContext(this.coroutineContext))
+         withContext(TestScopeContainer(this)) {
+            result = test(testCase, scope.withCoroutineContext(coroutineContext))
+         }
       }
       return result
    }
