c1083e6a7647640adef28acf83be4b1d5687cdc6
sksamuel <sam@sksamuel.com>
2023-11-12 15:00:38 -0600

    Expose the kotlin coroutineTestScope

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/coroutines/TestScopeContainer.kt
+++ kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/coroutines/TestScopeContainer.kt
@@ -1,15 +1,17 @@
 package io.kotest.core.coroutines
 
 import io.kotest.common.KotestInternal
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestScope
 import kotlin.coroutines.CoroutineContext
 
-@ExperimentalCoroutinesApi
 @KotestInternal
-class TestScopeContainer(val testScope: TestScope): CoroutineContext.Element {
-   companion object: CoroutineContext.Key<TestScopeContainer>
+class TestScopeContainer(val testScope: TestScope) : CoroutineContext.Element {
+   companion object : CoroutineContext.Key<TestScopeContainer>
 
    override val key: CoroutineContext.Key<*>
       get() = TestScopeContainer
 }
+
+val io.kotest.core.test.TestScope.coroutineTestScope: TestScope
+   get() = this.coroutineContext[TestScopeContainer]?.testScope
+      ?: error("kotlinx.coroutines.test.TestScope is not installed. Set coroutineTestScope = true to enable")
