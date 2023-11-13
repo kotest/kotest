689377e0110e76ff5c622012c9dbce7ef8363119
sksamuel <sam@sksamuel.com>
2023-11-12 18:12:27 -0600

    Added arm builds

--- buildSrc/src/main/kotlin/kotest-android-native-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotest-android-native-conventions.gradle.kts
@@ -17,11 +17,15 @@ kotlin {
          val desktopMain by getting { dependsOn(commonMain) }
          val androidNativeX86Main by getting { dependsOn(desktopMain) }
          val androidNativeX64Main by getting { dependsOn(desktopMain) }
+         val androidNativeArm32Main by getting { dependsOn(desktopMain) }
+         val androidNativeArm64Main by getting { dependsOn(desktopMain) }
 
          val commonTest by getting
          val nativeTest by getting { dependsOn(commonTest) }
          val androidNativeX86Test by getting { dependsOn(nativeTest) }
          val androidNativeX64Test by getting { dependsOn(nativeTest) }
+         val androidNativeArm32Test by getting { dependsOn(nativeTest) }
+         val androidNativeArm64Test by getting { dependsOn(nativeTest) }
       }
    } else {
       // Make sure every project has at least one valid target, otherwise Kotlin compiler will complain
