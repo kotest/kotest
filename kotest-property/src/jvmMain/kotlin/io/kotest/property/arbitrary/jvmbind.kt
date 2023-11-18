23c09ac1a4b417c6766ed2d7ca34d471af6bb4c8
Emil Kantis <emil.kantis@protonmail.com>
2023-11-18 10:19:39 +0100

    Support binding properties to specific Arbs when doing reflective binding (#3358)

--- kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/jvmbind.kt
+++ kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/jvmbind.kt
@@ -5,6 +5,7 @@ import io.kotest.property.Arb
 import io.kotest.property.resolution.GlobalArbResolver
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
+import kotlin.reflect.KProperty1
 import kotlin.reflect.KType
 import kotlin.reflect.KVisibility
 import kotlin.reflect.full.primaryConstructor
@@ -29,7 +30,12 @@ import kotlin.reflect.typeOf
  * - Classes for which an [Arb] has been provided through [providedArbs]
  */
 inline fun <reified T : Any> Arb.Companion.bind(providedArbs: Map<KClass<*>, Arb<*>> = emptyMap()): Arb<T> =
-   bind(providedArbs, T::class, typeOf<T>())
+   bind(providedArbs, emptyMap(), T::class, typeOf<T>())
+
+inline fun <reified T : Any> Arb.Companion.bind(builder: ProvidedArbsBuilder.() -> Unit): Arb<T> =
+   ProvidedArbsBuilder().apply(builder).run {
+      bind(classArbs, propertyArbs, T::class, typeOf<T>())
+   }
 
 /**
  * Alias for [Arb.Companion.bind]
@@ -54,13 +60,23 @@ inline fun <reified T : Any> Arb.Companion.bind(providedArbs: Map<KClass<*>, Arb
 inline fun <reified T : Any> Arb.Companion.data(providedArbs: Map<KClass<*>, Arb<*>> = emptyMap()): Arb<T> =
    Arb.bind(providedArbs)
 
+inline fun <reified T : Any> Arb.Companion.data(builder: ProvidedArbsBuilder.() -> Unit): Arb<T> =
+   ProvidedArbsBuilder().apply(builder).run {
+      bind(classArbs, propertyArbs, T::class, typeOf<T>())
+   }
+
 /**
  * **Do not call directly**
  *
  * Callers should use [Arb.Companion.bind] without [KClass] and [KType] parameters instead.
  */
-fun <T : Any> Arb.Companion.bind(providedArbs: Map<KClass<*>, Arb<*>>, kclass: KClass<T>, type: KType): Arb<T> {
-   val arb = Arb.forType(providedArbs, type)
+fun <T : Any> Arb.Companion.bind(
+   providedArbs: Map<KClass<*>, Arb<*>>,
+   arbsForProps: Map<KProperty1<*, *>, Arb<*>>,
+   kclass: KClass<T>,
+   type: KType
+): Arb<T> {
+   val arb = Arb.forType(providedArbs, arbsForProps, type)
       ?: error("Could not locate generator for ${kclass.simpleName}, consider making it a dataclass or provide an Arb for it.")
    return arb as Arb<T>
 }
@@ -72,12 +88,16 @@ fun <T : Any> Arb.Companion.bind(providedArbs: Map<KClass<*>, Arb<*>>, kclass: K
  * See [#3362](https://github.com/kotest/kotest/issues/3362) for an example of such usage.
  */
 @DelicateKotest
-fun <T : Any> Arb.Companion.bind(providedArbs: Map<KClass<*>, Arb<*>>, kclass: KClass<T>): Arb<T> {
-   return forClassUsingConstructor(providedArbs, kclass)
+fun <T : Any> Arb.Companion.bind(
+   providedArbs: Map<KClass<*>, Arb<*>>,
+   kclass: KClass<T>
+): Arb<T> {
+   return forClassUsingConstructor(providedArbs, emptyMap(), kclass)
 }
 
 internal fun <T : Any> Arb.Companion.forClassUsingConstructor(
    providedArbs: Map<KClass<*>, Arb<*>>,
+   arbsForProperties: Map<KProperty1<*, *>, Arb<*>>,
    kclass: KClass<T>
 ): Arb<T> {
    val className = kclass.qualifiedName ?: kclass.simpleName
@@ -89,26 +109,61 @@ internal fun <T : Any> Arb.Companion.forClassUsingConstructor(
       return Arb.constant(constructor.call())
    }
 
+   val relevantProps = arbsForProperties.filter { it.key.parameters[0].type == constructor.returnType }
+      .toList()
+
    val arbs: List<Arb<*>> = constructor.parameters.map { param ->
-      val arb = arbForParameter(providedArbs, className, param)
+      val arbForProp = relevantProps.firstOrNull { (key, _) -> key.name == param.name }?.second
+      val arb = when {
+         arbForProp != null -> arbForProp
+         else -> arbForParameter(providedArbs, arbsForProperties, className, param)
+      }
+
       if (param.type.isMarkedNullable) arb.orNull() else arb
    }
 
    return Arb.bind(arbs) { params -> constructor.call(*params.toTypedArray()) }
 }
 
-private fun arbForParameter(providedArbs: Map<KClass<*>, Arb<*>>, className: String?, param: KParameter): Arb<*> {
+private fun arbForParameter(
+   providedArbs: Map<KClass<*>, Arb<*>>,
+   arbsForProps: Map<KProperty1<*, *>, Arb<*>>,
+   className: String?,
+   param: KParameter
+): Arb<*> {
    val arb =
       try {
-         Arb.forType(providedArbs, param.type)
+         Arb.forType(providedArbs, arbsForProps, param.type)
       } catch (e: IllegalStateException) {
          throw IllegalStateException("Failed to create generator for parameter $className.${param.name}", e)
       }
    return arb ?: error("Could not locate generator for parameter $className.${param.name}")
 }
 
-internal fun Arb.Companion.forType(providedArbs: Map<KClass<*>, Arb<*>>, type: KType): Arb<*>? {
+internal fun Arb.Companion.forType(
+   providedArbs: Map<KClass<*>, Arb<*>>,
+   arbsForProperties: Map<KProperty1<*, *>, Arb<*>>,
+   type: KType
+): Arb<*>? {
    return (type.classifier as? KClass<*>)?.let { providedArbs[it] ?: defaultForClass(it) }
       ?: GlobalArbResolver.resolve(type)
-      ?: targetDefaultForType(providedArbs, type)
+      ?: targetDefaultForType(providedArbs, arbsForProperties, type)
+}
+
+class ProvidedArbsBuilder {
+   private val _classArbs = mutableMapOf<KClass<*>, Arb<*>>()
+   private val _propertyArbs = mutableMapOf<KProperty1<*, *>, Arb<*>>()
+
+   val classArbs: Map<KClass<*>, Arb<*>> get() = _classArbs
+   val propertyArbs: Map<KProperty1<*, *>, Arb<*>> get() = _propertyArbs
+
+   @JvmName("bindClass")
+   fun bind(mapping: Pair<KClass<*>, Arb<*>>) {
+      _classArbs[mapping.first] = mapping.second
+   }
+
+   @JvmName("bindProperty")
+   fun bind(mapping: Pair<KProperty1<*, *>, Arb<*>>) {
+      _propertyArbs[mapping.first] = mapping.second
+   }
 }
