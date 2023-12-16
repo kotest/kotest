b2dc09f7e0b305cc5525aa169449b4ab146a17c4
OliverO2 <oliver.o456i@gmail.com>
2023-12-16 01:06:00 +0100

    Add workaround for K/Wasm limitation in JS interop exception handling

--- kotest-common/src/wasmJsMain/kotlin/io/kotest/mpp/sysprop.wasmJs.kt
+++ kotest-common/src/wasmJsMain/kotlin/io/kotest/mpp/sysprop.wasmJs.kt
@@ -1,3 +1,15 @@
 package io.kotest.mpp
 
-actual fun jsProcessEnv(name: String): String? = js("process.env[name]")
+actual fun jsProcessEnv(name: String): String? = js(
+   // Work around K/Wasm limitation:
+   //     "The Kotlin try-catch expression can't catch JavaScript exceptions."
+   //     https://kotlinlang.org/docs/wasm-js-interop.html#exception-handling
+   """
+        (() => {
+          try {
+            return process.env[name];
+          } catch (e) {
+          }
+        })() ?? null
+    """
+)
