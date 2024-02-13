7bf12027c2c165dac3648dacae40422e0c7c49e0
Alex Kuznetsov <Alex.Cue.987@gmail.com>
2024-02-13 13:26:32 -0600

    ShouldExistInOrderTest.kt (#30)

---
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/collections/ShouldExistInOrderTest.kt
@@ -0,0 +1,27 @@
+package com.sksamuel.kotest.matchers.collections
+
+import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.shouldExistInOrder
+import io.kotest.matchers.shouldBe
+
+class ShouldExistInOrderTest: WordSpec() {
+   init {
+       "shouldExistInOrder" should {
+          "pass" {
+              listOf(1, 2).shouldExistInOrder(
+                 { i: Int -> i < 2 },
+             { i: Int -> i < 3 }
+              )
+          }
+          "fail with clear message" {
+             shouldThrowAny {
+                listOf(1, 2).shouldExistInOrder(
+                   { i: Int -> i < 2 },
+                   { i: Int -> i < 2 }
+                )
+             }.message shouldBe "[1, 2] did not match the predicates [(kotlin.Int) -> kotlin.Boolean, (kotlin.Int) -> kotlin.Boolean] in order. Predicate number 1 did not match."
+          }
+       }
+   }
+}
