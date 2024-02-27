7a85a6425b99ca0901d24032b1211773fd2e7af6
Emil Kantis <emil.kantis@protonmail.com>
2024-02-27 21:18:00 +0100

    Preserving original failure when short-circuiting eventually (#3864)
    Also made some minor tweaks to correct attempt counter and duration.
    
    Fixes #3863

--- kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/io/kotest/assertions/nondeterministic/EventuallyTest.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/io/kotest/assertions/nondeterministic/EventuallyTest.kt
@@ -4,6 +4,7 @@ package io.kotest.assertions.nondeterministic
 
 import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.fail
+import io.kotest.assertions.shouldFail
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.assertions.withClue
@@ -146,17 +147,46 @@ class EventuallyTest : FunSpec() {
          var count = 0
          val message = shouldThrow<AssertionError> {
             eventually(100.milliseconds) {
-               if (count == 0) {
-                  count = 1
+               if (count++ == 0) {
                   fail("first")
                } else {
                   fail("last")
                }
             }
          }.message
-         message.shouldContain("Block failed after 100ms; attempted \\d+ time\\(s\\)".toRegex())
-         message.shouldContain("The first error was caused by: first")
-         message.shouldContain("The last error was caused by: last")
+         message shouldContain """Block failed after \d+ms; attempted $count time\(s\)""".toRegex()
+         message shouldContain "The first error was caused by: first"
+         message shouldContain "The last error was caused by: last"
+      }
+
+      test("non-suppressible exception is not retried, but still printed with eventually-info") {
+         val config = eventuallyConfig {
+            duration = 100.milliseconds
+            expectedExceptionsFn = { it is AssertionError }
+         }
+
+         var count = 0
+         val message = shouldFail {
+            eventually(config) {
+               if (count++ == 0) {
+                  fail("first")
+               } else {
+                  error("last")
+               }
+            }
+         }.message
+         count shouldBe 2
+         message shouldContain """Block failed after \d+ms; attempted $count time\(s\)""".toRegex()
+         message shouldContain "The first error was caused by: first"
+         message shouldContain "The last error was caused by: last"
+      }
+
+      test("duration is displayed in whole seconds once past 1000ms") {
+         shouldFail {
+            eventually(1100.milliseconds) {
+               fail("")
+            }
+         }.message shouldContain "Block failed after 1s"
       }
 
       test("allow suspendable functions") {
@@ -246,8 +276,7 @@ class EventuallyTest : FunSpec() {
             }
          }.message
 
-         message.shouldContain("Block failed after 5s")
-         message.shouldContain("attempted 2 time(s)")
+         message shouldContain """Block failed after \d{1,3}ms; attempted 2 time\(s\)""".toRegex()
       }
 
       test("override assertion to hard assertion before executing assertion and reset it after executing") {
@@ -312,8 +341,8 @@ class EventuallyTest : FunSpec() {
          i shouldBe exceptions.size + 1
       }
 
-      test("short circuit happy path") {
-         shouldThrow<ShortCircuitControlException> {
+      test("short-circuited exceptions are not retried") {
+         shouldFail {
             val config = eventuallyConfig {
                duration = 5.seconds
                shortCircuit = { true }
@@ -321,20 +350,7 @@ class EventuallyTest : FunSpec() {
             eventually(config) {
                1 shouldBe 2
             }
-         }
-      }
-
-      test("short circuit exception cannot be suppressed") {
-         shouldThrow<ShortCircuitControlException> {
-            val config = eventuallyConfig {
-               duration = 5.seconds
-               expectedExceptions = setOf(ShortCircuitControlException::class)
-               shortCircuit = { true }
-            }
-            eventually(config) {
-               1 shouldBe 2
-            }
-         }
+         }.message shouldContain """Block failed after \d{1,3}ms; attempted 1 time""".toRegex()
       }
    }
 }
