6e60d1fb0f35473a92417f0273f6346b2091e795
Adam <897017+aSemy@users.noreply.github.com>
2024-03-09 14:01:35 +0100

    add more tests

--- kotest-tests/kotest-tests-js/src/jsTest/kotlin/io/kotest/engine/js/NestedTests.kt
+++ kotest-tests/kotest-tests-js/src/jsTest/kotlin/io/kotest/engine/js/NestedTests.kt
@@ -5,18 +5,38 @@ import io.kotest.matchers.shouldBe
 
 class NestedTests : FunSpec() {
    init {
-      test("a top level test") {
+      test("a top level successful test") {
          1 + 1 shouldBe 2
       }
 
+      test("a top level failing test") {
+         1 + 1 shouldBe 3
+      }
+
+      test("a top level skipped test") {}
+
       context("a context") {
-         test("a test") {
+         test("successful test") {
             1 + 1 shouldBe 2
          }
 
-         test("another test") {
+         test("failing test") {
             1 + 1 shouldBe 3
          }
+
+         xtest("skipped test") { }
+      }
+
+      xcontext("skipped context") {
+         test("successful test") {
+            1 + 1 shouldBe 2
+         }
+
+         test("failing test") {
+            1 + 1 shouldBe 3
+         }
+
+         xtest("skipped test") { }
       }
    }
 }
