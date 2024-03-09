0e0a6fef6426dc0a50cfb1b694a15e284b0e29b7
Adam <897017+aSemy@users.noreply.github.com>
2024-03-09 13:56:21 +0100

    bump io.kotest.multiplatform in kotest-tests-js

--- kotest-tests/kotest-tests-js/build.gradle.kts
+++ kotest-tests/kotest-tests-js/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
    id("kotest-js-conventions")
    // must be a published version and not one in the current build
-   id("io.kotest.multiplatform").version("5.5.4")
+   id("io.kotest.multiplatform").version("5.8.0")
 }
 
 kotlin {
