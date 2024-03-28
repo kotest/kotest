394924feda2a957237f148d91665a30394bca882
OliverO2 <oliver.o456i@gmail.com>
2024-03-28 19:03:54 +0100

    Integrate startUnitTests() into KotlinJsTestFramework.wasmJs.kt

--- kotest-framework/kotest-framework-engine/src/wasmJsMain/kotlin/io/kotest/engine/KotlinJsTestFramework.wasmJs.kt
+++ kotest-framework/kotest-framework-engine/src/wasmJsMain/kotlin/io/kotest/engine/KotlinJsTestFramework.wasmJs.kt
@@ -69,3 +69,11 @@ private fun describe(description: String, suiteFn: () -> Unit) {
 private external fun xdescribe(name: String, testFn: () -> Unit)
 private external fun it(name: String, testFn: () -> JsAny?)
 private external fun xit(name: String, testFn: () -> JsAny?)
+
+// The JS launcher set up by the Kotlin Gradle plugin calls this function in addition to `main()`.
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+internal fun startUnitTests() {
+   // The Kotlin compiler would insert test invocations here for kotlin-test.
+   // This mechanism is not used with Kotest.
+}
