ea65df0beca09eb6246ede6d585d254a0d9d8764
Emil Kantis <emil.kantis@protonmail.com>
2024-05-09 19:27:18 +0200

    Handling generic data classes when determining identifier stability
    By inspecting the stability of the actual values, we can see beyond the
    generics of the data class.
    
    Fixes #3947

--- kotest-common/src/jvmTest/kotlin/io/kotest/mpp/StableTest.kt
+++ kotest-common/src/jvmTest/kotlin/io/kotest/mpp/StableTest.kt
@@ -1,9 +1,10 @@
 package io.kotest.mpp
 
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.data.Row3
 import io.kotest.matchers.shouldBe
 
-class StableTest : StringSpec({
+class StableTest : FreeSpec({
    "class should be correctly identified as stable" {
       data class StableClass(
          val string: String,
@@ -28,5 +29,30 @@ class StableTest : StringSpec({
       isStable(StableClass::class) shouldBe true
       isStable(Unstable::class) shouldBe false
       isStable(UnstableDataClass::class) shouldBe false
+   }
+
+   "Given a data class with generics" - {
+      "When all members are stable, then the class should be considered stable" {
+         val x: Row3<String, Int, Double> = Row3("x", 1, 2.0)
+         isStable(x::class, x) shouldBe true
+      }
+
+      "When isStable does not have access to actual values, it cannot determine the stability of the class" {
+         val x: Row3<String, Int, Double> = Row3("x", 1, 2.0)
+         isStable(x::class, t = null) shouldBe false
+      }
+
+      "When an unstable type is included, the entire data class is considered unstable, even when including values" {
+         val y: Row3<String, Int, Array<Int>> = Row3("x", 1, arrayOf(1, 2))
+         isStable(y::class, y) shouldBe false
+      }
+
+      "When layering generic data classes, it should still work" {
+         val stable = Pair(Pair("a", "b"), Pair("c", "d"))
+         isStable(stable::class, stable) shouldBe true
+
+         val unstable = Pair(Pair("a", "b"), Pair("c", arrayOf(1, 2)))
+         isStable(unstable::class, unstable) shouldBe false
+      }
    }
 })
