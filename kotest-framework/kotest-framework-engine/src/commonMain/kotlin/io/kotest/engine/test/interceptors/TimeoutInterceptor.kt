036155fb0de34a5b19c584c43aad69a745e84c83
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-05-29 13:48:59 +0200

    Use wallclock timeout if TestCoroutineScheduler found

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/test/interceptors/TimeoutInterceptor.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/test/interceptors/TimeoutInterceptor.kt
@@ -5,7 +5,22 @@ import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
 import io.kotest.core.test.TestScope
 import io.kotest.engine.test.scopes.withCoroutineContext
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.selects.select
+import kotlinx.coroutines.test.TestCoroutineScheduler
 import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.yield
+import kotlin.coroutines.coroutineContext
 import kotlin.time.Duration
 import kotlin.time.TimeMark
 
@@ -31,15 +46,63 @@ internal class TimeoutInterceptor(
       logger.log { Pair(testCase.name.testName, "Switching context to add timeout $timeout") }
 
       return try {
-         withTimeout(timeout) {
+         withAppropriateTimeout(timeout) {
             test(testCase, scope.withCoroutineContext(coroutineContext))
          }
-      } catch (t: Throwable) {
-         logger.log { Pair(testCase.name.testName, "Caught timeout $t") }
-         TestResult.Error(mark.elapsedNow(), TestTimeoutException(timeout, testCase.name.testName))
+      } catch (t: CancellationException) {
+         if (t is WallclockTimeoutCancellationException || t is TimeoutCancellationException) {
+            logger.log { Pair(testCase.name.testName, "Caught timeout $t") }
+            TestResult.Error(mark.elapsedNow(), TestTimeoutException(timeout, testCase.name.testName))
+         } else {
+            throw t
+         }
+      }
+
+   }
+}
+
+// The implementation copied from Turbine:
+// https://github.com/cashapp/turbine/blob/1.1.0/src/commonMain/kotlin/app/cash/turbine/channel.kt#L93
+private suspend fun <T> withAppropriateTimeout(
+   timeout: Duration,
+   block: suspend CoroutineScope.() -> T,
+): T {
+   return if (coroutineContext[TestCoroutineScheduler] != null) {
+      // withTimeout uses virtual time, which will hang.
+      withWallclockTimeout(timeout, block)
+   } else {
+      withTimeout(timeout, block)
+   }
+}
+
+private suspend fun <T> withWallclockTimeout(
+   timeout: Duration,
+   block: suspend CoroutineScope.() -> T,
+): T = coroutineScope {
+   val blockDeferred = async(start = CoroutineStart.UNDISPATCHED) {
+      yield()
+      block()
+   }
+
+   // Run the timeout on a scope separate from the caller. This ensures that the use of the
+   // Default dispatcher doesn't affect the use of a TestScheduler and its fake time.
+   @OptIn(DelicateCoroutinesApi::class)
+   val timeoutJob = GlobalScope.launch(Dispatchers.Default) { delay(timeout) }
+
+   select {
+      blockDeferred.onAwait { result ->
+         timeoutJob.cancel()
+         result
+      }
+      timeoutJob.onJoin {
+         blockDeferred.cancel()
+         throw WallclockTimeoutCancellationException("Timed out waiting for $timeout")
       }
    }
 }
+
+// TimeoutCancellationException has an internal constructor, so we need a custom exception to indicate timeout
+private class WallclockTimeoutCancellationException(message: String) : CancellationException(message)
 
 /**
  * Exception used for when a test exceeds its timeout.
