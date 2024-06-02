3f8294badb0d8ae2e0ccef6b1e4d09a80adb5967
sksamuel <sam@sksamuel.com>
2024-06-02 11:19:00 -0500

    Refactor team city listener to extract the writing methods

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/listener/TeamCityTestEngineListener.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/listener/TeamCityTestEngineListener.kt
@@ -12,6 +12,7 @@ import io.kotest.engine.extensions.MultipleExceptions
 import io.kotest.engine.interceptors.EngineContext
 import io.kotest.engine.teamcity.Locations
 import io.kotest.engine.teamcity.TeamCityMessageBuilder
+import io.kotest.engine.teamcity.TeamCityWriter
 import io.kotest.engine.test.names.FallbackDisplayNameFormatter
 import io.kotest.engine.test.names.getFallbackDisplayNameFormatter
 import io.kotest.mpp.Logger
@@ -29,7 +30,7 @@ class TeamCityTestEngineListener(
 
    private val logger = Logger(TeamCityTestEngineListener::class)
 
-   private var formatter = FallbackDisplayNameFormatter.default(ProjectConfiguration())
+   private var writer = TeamCityWriter(prefix, FallbackDisplayNameFormatter.default(ProjectConfiguration()))
 
    // once a spec has completed, we want to be able to check whether any given test is
    // a container or a leaf test, and so this map contains all test that have children
@@ -45,57 +46,36 @@ class TeamCityTestEngineListener(
 
       val (name, cause) = ExtensionExceptionExtractor.resolve(t)
 
-      val msg1 = TeamCityMessageBuilder
-         .testStarted(prefix, name)
-         .id(name)
-         .parent(parent.path().value)
-         .build()
-      println(msg1)
-
+      writer.outputTestStarted(name, parent.path().value)
       // we must print out the stack trace in between the dummy, so it appears when you click on the test name
       //t?.printStackTrace()
-
-      val msg2 = TeamCityMessageBuilder
-         .testFailed(prefix, name)
-         .id(name)
-         .parent(parent.path().value)
-         .withException(cause, details)
-         .build()
-      println(msg2)
-
-      val msg3 = TeamCityMessageBuilder
-         .testFinished(prefix, name)
-         .id(name)
-         .parent(parent.path().value)
-         .build()
-      println(msg3)
+      writer.outputTestFailed(name, cause, details, parent.path().value)
+      writer.outputTestFinished(name, parent.path().value)
    }
 
    override suspend fun engineStarted() {}
 
    override suspend fun engineInitialized(context: EngineContext) {
-      formatter = getFallbackDisplayNameFormatter(context.configuration.registry, context.configuration)
+      writer = TeamCityWriter(
+         prefix,
+         getFallbackDisplayNameFormatter(context.configuration.registry, context.configuration)
+      )
    }
 
    override suspend fun engineFinished(t: List<Throwable>) {
       if (t.isNotEmpty()) {
          t.withIndex().forEach { (index, error) ->
             val testName = if (t.size == 1) "Engine exception" else "Engine exception ${index + 1}"
-            println(TeamCityMessageBuilder.testStarted(prefix, testName).build())
             val message = error.message ?: t::class.bestName()
-            println(TeamCityMessageBuilder.testFailed(prefix, testName).message(message).build())
-            println(TeamCityMessageBuilder.testFinished(prefix, testName).build())
+            writer.outputTestStarted(testName)
+            writer.outputTestFailed(testName, message)
+            writer.outputTestFinished(testName)
          }
       }
    }
 
    override suspend fun specStarted(kclass: KClass<*>) {
-      val msg = TeamCityMessageBuilder
-         .testSuiteStarted(prefix, formatter.format(kclass))
-         .id(kclass.toDescriptor().path().value)
-         .locationHint(Locations.location(kclass))
-         .build()
-      println(msg)
+      writer.outputTestSuiteStarted(kclass)
    }
 
    // ignored specs are completely hidden from output in team city
@@ -110,18 +90,9 @@ class TeamCityTestEngineListener(
          else -> insertPlaceholder(t, kclass.toDescriptor())
       }
 
-      finishSpec(kclass)
+      writer.outputTestSuiteFinished(kclass)
       results.clear()
       children.clear()
-   }
-
-   private fun finishSpec(kclass: KClass<*>) {
-      val msg = TeamCityMessageBuilder
-         .testSuiteFinished(prefix, formatter.format(kclass))
-         .id(kclass.toDescriptor().path().value)
-         .locationHint(Locations.location(kclass))
-         .build()
-      println(msg)
    }
 
    override suspend fun testStarted(testCase: TestCase) {
@@ -132,30 +103,43 @@ class TeamCityTestEngineListener(
             val p = testCase.parent
             // we might have a container inside a dynamic parent, in which case we need to start the dynamic parent
             if (p != null && p.type == TestType.Dynamic) {
-               if (!started.contains(p.descriptor)) startTestSuite(p)
+               if (!started.contains(p.descriptor)) {
+                  writer.outputTestSuiteStarted(p)
+                  started.add(p.descriptor)
+               }
             }
-            startTestSuite(testCase)
+            writer.outputTestSuiteStarted(testCase)
+            started.add(testCase.descriptor)
          }
+
          TestType.Test -> {
             val p = testCase.parent
             // we might have a container inside a dynamic parent, in which case we need to start it
             if (p != null && p.type == TestType.Dynamic) {
-               if (!started.contains(p.descriptor)) startTestSuite(p)
+               if (!started.contains(p.descriptor)) {
+                  writer.outputTestSuiteStarted(p)
+                  started.add(p.descriptor)
+               }
             }
-            startTest(testCase)
+            writer.outputTestStarted(testCase)
+            started.add(testCase.descriptor)
          }
+
          TestType.Dynamic -> {
             val p = testCase.parent
             // we might have a dynamic inside another dynamic parent, in which case we need to start the dynamic parent
             if (p != null && p.type == TestType.Dynamic) {
-               if (!started.contains(p.descriptor)) startTestSuite(p)
+               if (!started.contains(p.descriptor)) {
+                  writer.outputTestSuiteStarted(p)
+                  started.add(p.descriptor)
+               }
             }
          }
       }
    }
 
    override suspend fun testIgnored(testCase: TestCase, reason: String?) {
-      ignoreTest(testCase, TestResult.Ignored(reason))
+      writer.outputTestIgnored(testCase, TestResult.Ignored(reason))
    }
 
    private fun addChild(testCase: TestCase) {
@@ -168,22 +152,24 @@ class TeamCityTestEngineListener(
       when (testCase.type) {
          TestType.Container -> {
             failTestSuiteIfError(testCase, result)
-            finishTestSuite(testCase, result)
+            writer.outputTestSuiteFinished(testCase, result)
          }
+
          TestType.Test -> {
-            if (!started.contains(testCase.descriptor)) startTest(testCase)
-            if (result.isErrorOrFailure) failTest(testCase, result)
-            finishTest(testCase, result)
+            if (!started.contains(testCase.descriptor)) writer.outputTestStarted(testCase)
+            if (result.isErrorOrFailure) writer.outputTestFailed(testCase, result, details)
+            writer.outputTestFinished(testCase, result)
          }
+
          TestType.Dynamic -> {
             if (isParent(testCase)) {
-               if (!started.contains(testCase.descriptor)) startTestSuite(testCase)
+               if (!started.contains(testCase.descriptor)) writer.outputTestSuiteStarted(testCase)
                failTestSuiteIfError(testCase, result)
-               finishTestSuite(testCase, result)
+               writer.outputTestSuiteFinished(testCase, result)
             } else {
-               if (!started.contains(testCase.descriptor)) startTest(testCase)
-               if (result.isErrorOrFailure) failTest(testCase, result)
-               finishTest(testCase, result)
+               if (!started.contains(testCase.descriptor)) writer.outputTestStarted(testCase)
+               if (result.isErrorOrFailure) writer.outputTestFailed(testCase, result, details)
+               writer.outputTestFinished(testCase, result)
             }
          }
       }
@@ -201,96 +187,5 @@ class TeamCityTestEngineListener(
    // returns true if this test case contains child tests
    private fun isParent(testCase: TestCase) = children.getOrElse(testCase.descriptor) { mutableListOf() }.isNotEmpty()
 
-   /**
-    * For a given [TestCase] will output the "test ignored" message.
-    */
-   private fun ignoreTest(testCase: TestCase, result: TestResult.Ignored) {
-      val msg = TeamCityMessageBuilder
-         .testIgnored(prefix, formatter.format(testCase))
-         .id(testCase.descriptor.path().value)
-         .parent(testCase.descriptor.parent.path().value)
-         .locationHint(Locations.location(testCase.source))
-         .message(result.reason)
-         .result(result)
-         .build()
-      println(msg)
-   }
 
-   /**
-    * For a [TestCase] will output the "test started" message.
-    */
-   private fun startTest(testCase: TestCase) {
-      logger.log { Pair(testCase.name.testName, "startTest ${testCase.descriptor.path().value}") }
-      val msg = TeamCityMessageBuilder
-         .testStarted(prefix, formatter.format(testCase))
-         .id(testCase.descriptor.path().value)
-         .parent(testCase.descriptor.parent.path().value)
-         .locationHint(Locations.location(testCase.source))
-         .build()
-      println(msg)
-      started.add(testCase.descriptor)
-   }
-
-   /**
-    * For a given [TestCase] will output the "test failed" message.
-    */
-   private fun failTest(testCase: TestCase, result: TestResult) {
-      val msg = TeamCityMessageBuilder
-         .testFailed(prefix, formatter.format(testCase))
-         .id(testCase.descriptor.path().value)
-         .parent(testCase.descriptor.parent.path().value)
-         .duration(result.duration)
-         .locationHint(Locations.location(testCase.source))
-         .withException(result.errorOrNull, details)
-         .result(result)
-         .build()
-      println(msg)
-   }
-
-   /**
-    * For a given [TestCase] will output the "test finished" message.
-    */
-   private fun finishTest(testCase: TestCase, result: TestResult) {
-      logger.log { Pair(testCase.name.testName, "finishTest ${testCase.descriptor.path().value}") }
-      val msg = TeamCityMessageBuilder
-         .testFinished(prefix, formatter.format(testCase))
-         .id(testCase.descriptor.path().value)
-         .parent(testCase.descriptor.parent.path().value)
-         .duration(result.duration)
-         .locationHint(Locations.location(testCase.source))
-         .result(result)
-         .build()
-      println(msg)
-   }
-
-   /**
-    * For a given [TestCase] will output the "test suite started" message.
-    */
-   private fun startTestSuite(testCase: TestCase) {
-      logger.log { Pair(testCase.name.testName, "startTestSuite ${testCase.descriptor.path().value}") }
-      val msg = TeamCityMessageBuilder
-         .testSuiteStarted(prefix, formatter.format(testCase))
-         .id(testCase.descriptor.path().value)
-         .parent(testCase.descriptor.parent.path().value)
-         .locationHint(Locations.location(testCase.source))
-         .build()
-      println(msg)
-      started.add(testCase.descriptor)
-   }
-
-   /**
-    * For a given [TestCase] will output the "test suite finished" message.
-    */
-   private fun finishTestSuite(testCase: TestCase, result: TestResult) {
-      logger.log { Pair(testCase.name.testName, "finishTestSuite ${testCase.descriptor.path().value}") }
-      val msg = TeamCityMessageBuilder
-         .testSuiteFinished(prefix, formatter.format(testCase))
-         .id(testCase.descriptor.path().value)
-         .parent(testCase.descriptor.parent.path().value)
-         .duration(result.duration)
-         .locationHint(Locations.location(testCase.source))
-         .result(result)
-         .build()
-      println(msg)
-   }
 }
---
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/teamcity/TeamCityWriter.kt
@@ -0,0 +1,178 @@
+package io.kotest.engine.teamcity
+
+import io.kotest.core.descriptors.toDescriptor
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
+import io.kotest.engine.test.names.FallbackDisplayNameFormatter
+import io.kotest.mpp.Logger
+import kotlin.reflect.KClass
+
+/**
+ * [TeamCityWriter] handles outputting in the team city format using a [TeamCityMessageBuilder].
+ */
+class TeamCityWriter(
+   private val prefix: String,
+   private val formatter: FallbackDisplayNameFormatter
+) {
+
+   private val logger = Logger(TeamCityWriter::class)
+
+   /**
+    * For a given [TestCase] will output the "test ignored" message.
+    */
+   internal fun outputTestIgnored(testCase: TestCase, result: TestResult.Ignored) {
+      val msg = TeamCityMessageBuilder
+         .testIgnored(prefix, formatter.format(testCase))
+         .id(testCase.descriptor.path().value)
+         .parent(testCase.descriptor.parent.path().value)
+         .locationHint(Locations.location(testCase.source))
+         .message(result.reason)
+         .result(result)
+         .build()
+      println(msg)
+   }
+
+   /**
+    * For a [TestCase] will output the "test started" message.
+    */
+   internal fun outputTestStarted(testCase: TestCase) {
+      logger.log { Pair(testCase.name.testName, "startTest ${testCase.descriptor.path().value}") }
+      val msg = TeamCityMessageBuilder
+         .testStarted(prefix, formatter.format(testCase))
+         .id(testCase.descriptor.path().value)
+         .parent(testCase.descriptor.parent.path().value)
+         .locationHint(Locations.location(testCase.source))
+         .build()
+      println(msg)
+   }
+
+   /**
+    * For a [TestCase] will output the "test started" message.
+    */
+   internal fun outputTestStarted(name: String, parent: String) {
+      val msg1 = TeamCityMessageBuilder
+         .testStarted(prefix, name)
+         .id(name)
+         .parent(parent)
+         .build()
+      println(msg1)
+   }
+
+   internal fun outputTestStarted(testName: String) {
+      println(TeamCityMessageBuilder.testStarted(prefix, testName).build())
+   }
+
+   internal fun outputTestFailed(testName: String, message: String) {
+      println(TeamCityMessageBuilder.testFailed(prefix, testName).message(message).build())
+   }
+
+   internal fun outputTestFinished(testName: String) {
+      println(TeamCityMessageBuilder.testFinished(prefix, testName).build())
+   }
+
+   /**
+    * For a given [TestCase] will output the "test failed" message.
+    */
+   internal fun outputTestFailed(testCase: TestCase, result: TestResult, details: Boolean) {
+      val msg = TeamCityMessageBuilder
+         .testFailed(prefix, formatter.format(testCase))
+         .id(testCase.descriptor.path().value)
+         .parent(testCase.descriptor.parent.path().value)
+         .duration(result.duration)
+         .locationHint(Locations.location(testCase.source))
+         .withException(result.errorOrNull, details)
+         .result(result)
+         .build()
+      println(msg)
+   }
+
+   /**
+    * Outputs the "test failed" message for an arbitrary test name.
+    * This is used for placeholder tests.
+    */
+   internal fun outputTestFailed(name: String, cause: Throwable, details: Boolean, parent: String) {
+      val msg2 = TeamCityMessageBuilder
+         .testFailed(prefix, name)
+         .id(name)
+         .parent(parent)
+         .withException(cause, details)
+         .build()
+      println(msg2)
+   }
+
+   /**
+    * For a given [TestCase] will output the "test finished" message.
+    */
+   internal fun outputTestFinished(testCase: TestCase, result: TestResult) {
+      logger.log { Pair(testCase.name.testName, "finishTest ${testCase.descriptor.path().value}") }
+      val msg = TeamCityMessageBuilder
+         .testFinished(prefix, formatter.format(testCase))
+         .id(testCase.descriptor.path().value)
+         .parent(testCase.descriptor.parent.path().value)
+         .duration(result.duration)
+         .locationHint(Locations.location(testCase.source))
+         .result(result)
+         .build()
+      println(msg)
+   }
+
+   internal fun outputTestFinished(name: String, parent: String) {
+      val msg3 = TeamCityMessageBuilder
+         .testFinished(prefix, name)
+         .id(name)
+         .parent(parent)
+         .build()
+      println(msg3)
+   }
+
+   /**
+    * For a given [TestCase] will output the "test suite started" message.
+    */
+   internal fun outputTestSuiteStarted(testCase: TestCase) {
+      logger.log { Pair(testCase.name.testName, "startTestSuite ${testCase.descriptor.path().value}") }
+      val msg = TeamCityMessageBuilder
+         .testSuiteStarted(prefix, formatter.format(testCase))
+         .id(testCase.descriptor.path().value)
+         .parent(testCase.descriptor.parent.path().value)
+         .locationHint(Locations.location(testCase.source))
+         .build()
+      println(msg)
+   }
+
+   /**
+    * For a given [TestCase] will output the "test suite finished" message.
+    */
+   internal fun outputTestSuiteFinished(testCase: TestCase, result: TestResult) {
+      logger.log { Pair(testCase.name.testName, "finishTestSuite ${testCase.descriptor.path().value}") }
+      val msg = TeamCityMessageBuilder
+         .testSuiteFinished(prefix, formatter.format(testCase))
+         .id(testCase.descriptor.path().value)
+         .parent(testCase.descriptor.parent.path().value)
+         .duration(result.duration)
+         .locationHint(Locations.location(testCase.source))
+         .result(result)
+         .build()
+      println(msg)
+   }
+
+   /**
+    * For a given [KClass] spec will output the "test suite finished" message.
+    */
+   internal fun outputTestSuiteFinished(kclass: KClass<*>) {
+      val msg = TeamCityMessageBuilder
+         .testSuiteFinished(prefix, formatter.format(kclass))
+         .id(kclass.toDescriptor().path().value)
+         .locationHint(Locations.location(kclass))
+         .build()
+      println(msg)
+   }
+
+   internal fun outputTestSuiteStarted(kclass: KClass<*>) {
+      val msg = TeamCityMessageBuilder
+         .testSuiteStarted(prefix, formatter.format(kclass))
+         .id(kclass.toDescriptor().path().value)
+         .locationHint(Locations.location(kclass))
+         .build()
+      println(msg)
+   }
+}
