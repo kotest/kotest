f03557a50e0e4942f787a0bd98ff3cbd3c79f5f4
Adam <897017+aSemy@users.noreply.github.com>
2024-06-04 21:16:22 +0200

    update publishing build config
    - add workaround for Kotlin DSL accessors
    - use lazy configuration to configure javadocJar task, publications
    - move `publishPlatformArtifactsInRootModule()` to be alongside the other config, not above the plugins {} block

--- buildSrc/src/main/kotlin/kotest-publishing-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotest-publishing-conventions.gradle.kts
@@ -2,6 +2,91 @@ import groovy.util.Node
 import groovy.util.NodeList
 import org.gradle.configurationcache.extensions.capitalized
 
+plugins {
+   signing
+   `maven-publish`
+}
+
+group = "io.kotest"
+version = Ci.publishVersion
+
+val javadocJar by tasks.registering(Jar::class) {
+   group = JavaBasePlugin.DOCUMENTATION_GROUP
+   description = "Assembles java doc to jar"
+   archiveClassifier.set("javadoc")
+   val javadoc = tasks.named("javadoc")
+   from(javadoc)
+}
+
+val ossrhUsername: String by project
+val ossrhPassword: String by project
+val signingKey: String? by project
+val signingPassword: String? by project
+
+//region manually define accessors, because IntelliJ _still_ doesn't index them properly :(
+val Project.signing get() = extensions.getByType<SigningExtension>()
+fun Project.signing(configure: SigningExtension.() -> Unit = {}): Unit = signing.configure()
+val Project.publishing get() = extensions.getByType<PublishingExtension>()
+fun Project.publishing(configure: PublishingExtension.() -> Unit = {}): Unit = publishing.configure()
+//endregion
+
+signing {
+   useGpgCmd()
+   if (signingKey != null && signingPassword != null) {
+      @Suppress("UnstableApiUsage")
+      useInMemoryPgpKeys(signingKey, signingPassword)
+   }
+   if (Ci.isRelease) {
+      sign(publishing.publications)
+   }
+}
+
+publishing {
+   repositories {
+      maven {
+         val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+         val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+         name = "deploy"
+         url = if (Ci.isRelease) releasesRepoUrl else snapshotsRepoUrl
+         credentials {
+            username = System.getenv("OSSRH_USERNAME") ?: ossrhUsername
+            password = System.getenv("OSSRH_PASSWORD") ?: ossrhPassword
+         }
+      }
+   }
+
+   publications.withType<MavenPublication>().configureEach {
+      artifact(javadocJar)
+
+      pom {
+         name.set("Kotest")
+         description.set("Kotlin Test Framework")
+         url.set("https://github.com/kotest/kotest")
+
+         scm {
+            connection.set("scm:git:https://github.com/kotest/kotest/")
+            developerConnection.set("scm:git:https://github.com/sksamuel/")
+            url.set("https://github.com/kotest/kotest/")
+         }
+
+         licenses {
+            license {
+               name.set("Apache-2.0")
+               url.set("https://opensource.org/licenses/Apache-2.0")
+            }
+         }
+
+         developers {
+            developer {
+               id.set("sksamuel")
+               name.set("Stephen Samuel")
+               email.set("sam@sksamuel.com")
+            }
+         }
+      }
+   }
+}
+
 /**
  * Publish the platform JAR and POM so that consumers who depend on this module and can't read Gradle module
  * metadata can still get the platform artifact and transitive dependencies from the POM
@@ -10,7 +95,7 @@ import org.gradle.configurationcache.extensions.capitalized
 fun Project.publishPlatformArtifactsInRootModule() {
    val platformPublication: MavenPublication? =
       extensions
-         .findByType(PublishingExtension::class.java)
+         .findByType(PublishingExtension::class)
          ?.publications
          ?.getByName<MavenPublication>("jvm")
    if (platformPublication != null) {
@@ -19,12 +104,11 @@ fun Project.publishPlatformArtifactsInRootModule() {
       platformPublication.pom?.withXml { platformXml = this }
 
       extensions
-         .findByType(PublishingExtension::class.java)
+         .findByType(PublishingExtension::class)
          ?.publications
          ?.getByName("kotlinMultiplatform")
          ?.let { it as MavenPublication }
          ?.run {
-
             // replace pom
             pom.withXml {
                val xmlProvider = this
@@ -59,97 +143,4 @@ fun Project.publishPlatformArtifactsInRootModule() {
    }
 }
 
-plugins {
-   signing
-   `maven-publish`
-}
-
-val publications: PublicationContainer = (extensions.getByName("publishing") as PublishingExtension).publications
-
-val javadoc = tasks.named("javadoc")
-
-group = "io.kotest"
-version = Ci.publishVersion
-
-val javadocJar by tasks.creating(Jar::class) {
-   group = JavaBasePlugin.DOCUMENTATION_GROUP
-   description = "Assembles java doc to jar"
-   archiveClassifier.set("javadoc")
-   from(javadoc)
-}
-
-
-publishing {
-   publications.withType<MavenPublication>().forEach {
-      it.apply {
-         artifact(javadocJar)
-      }
-   }
-}
-
-val ossrhUsername: String by project
-val ossrhPassword: String by project
-val signingKey: String? by project
-val signingPassword: String? by project
-
-signing {
-   useGpgCmd()
-   if (signingKey != null && signingPassword != null) {
-      @Suppress("UnstableApiUsage")
-      useInMemoryPgpKeys(signingKey, signingPassword)
-   }
-   if (Ci.isRelease) {
-      sign(publications)
-   }
-}
-
 publishPlatformArtifactsInRootModule()
-
-publishing {
-   repositories {
-      maven {
-         val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-         val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-         name = "deploy"
-         url = if (Ci.isRelease) releasesRepoUrl else snapshotsRepoUrl
-         credentials {
-            username = System.getenv("OSSRH_USERNAME") ?: ossrhUsername
-            password = System.getenv("OSSRH_PASSWORD") ?: ossrhPassword
-         }
-      }
-   }
-
-   publications.withType<MavenPublication>().forEach {
-      it.apply {
-         //if (Ci.isRelease)
-         pom {
-            name.set("Kotest")
-            description.set("Kotlin Test Framework")
-            url.set("https://github.com/kotest/kotest")
-
-            scm {
-               connection.set("scm:git:https://github.com/kotest/kotest/")
-               developerConnection.set("scm:git:https://github.com/sksamuel/")
-               url.set("https://github.com/kotest/kotest/")
-            }
-
-            licenses {
-               license {
-                  name.set("Apache-2.0")
-                  url.set("https://opensource.org/licenses/Apache-2.0")
-               }
-            }
-
-            developers {
-               developer {
-                  id.set("sksamuel")
-                  name.set("Stephen Samuel")
-                  email.set("sam@sksamuel.com")
-               }
-            }
-         }
-      }
-   }
-}
-
-
