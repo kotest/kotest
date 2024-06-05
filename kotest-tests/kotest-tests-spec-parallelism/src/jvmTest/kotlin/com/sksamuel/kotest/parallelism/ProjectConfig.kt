49d09d4ca7e2a60615da6308a5ff66e281971a94
Adam <897017+aSemy@users.noreply.github.com>
2024-06-05 11:02:51 +0200

    update ProjectConfig to make test more stable

--- kotest-tests/kotest-tests-spec-parallelism/src/jvmTest/kotlin/com/sksamuel/kotest/parallelism/ProjectConfig.kt
+++ kotest-tests/kotest-tests-spec-parallelism/src/jvmTest/kotlin/com/sksamuel/kotest/parallelism/ProjectConfig.kt
@@ -2,13 +2,19 @@ package com.sksamuel.kotest.parallelism
 
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.config.ProjectConfiguration
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.TimeMark
+import kotlin.time.TimeSource
+import kotlinx.coroutines.delay
 
 object ProjectConfig : AbstractProjectConfig() {
 
-   private var start = 0L
+   private lateinit var start: TimeMark
 
    override suspend fun beforeProject() {
-      start = System.currentTimeMillis()
+      // Delay, so that tests can warm up (this is a wild guess - is it actually correct???)
+      delay(1.seconds)
+      start = TimeSource.Monotonic.markNow()
    }
 
    // set the number of threads so that each test runs in its own thread
@@ -17,10 +23,11 @@ object ProjectConfig : AbstractProjectConfig() {
    override val concurrentSpecs: Int = ProjectConfiguration.MaxConcurrency
 
    override suspend fun afterProject() {
-      val duration = System.currentTimeMillis() - start
-      // there are 8 specs, and each one has a delay
-      // if parallel is working they should all block at the same time
-      if (duration > 700)
+      val duration = start.elapsedNow()
+      // There are 8 specs, and each one has a 1-second delay.
+      // If parallel is working, they should all block at the same time.
+      if (duration > 7.seconds) {
          error("Parallel execution failure: Execution time was $duration")
+      }
    }
 }
