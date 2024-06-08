9302597075761d860679c051b6e4261813adb31a
Adam <897017+aSemy@users.noreply.github.com>
2024-06-08 14:33:22 +0200

    Create a BuildService to collect kotest-bom entries
    Replaces manual project filtering in kotest-bom with KotestBomService, which will automatically collect the dependencies from any project that has the `kotest-publishing-conventions` convention plugin. This focuses the logic of what should be added into a convention plugin.
    
    Subprojects can opt out of being included in the BOM by setting `includeInKotestBom = false`. `kotest-bom` is excluded by default.

--- kotest-bom/build.gradle.kts
+++ kotest-bom/build.gradle.kts
@@ -3,41 +3,18 @@ plugins {
    id("kotest-publishing-conventions")
 }
 
-val bomProject = project
-
-// Explicitly exclude subprojects that will never be published so that when configuring this project
-//   we don't force their configuration and do unnecessary work
-val excludeFromBom = listOf("kotest-examples", "kotest-tests")
-fun projectsFilter(candidateProject: Project) =
-   excludeFromBom.all { !candidateProject.name.contains(it) } &&
-      candidateProject.name != bomProject.name
-
-// Declare that this subproject depends on all subprojects matching the filter
-// When this subproject is configured, it will force configuration of all subprojects
-// so that we can declare dependencies on them
-rootProject.subprojects.filter(::projectsFilter).forEach { bomProject.evaluationDependsOn(it.path) }
-
-dependencies {
-   constraints {
-      rootProject.subprojects
-         .filter { project ->
-            // Only declare dependencies on projects that will have publications
-            projectsFilter(project) && project.tasks.findByName("publish")?.enabled == true
-         }
-         .forEach { project ->
-            project.publishing.publications
-               .withType<MavenPublication>()
-               // Skip platform artifacts (like *-linuxx64, *-macosx64)
-               // It leads to inconsistent bom when publishing from different platforms
-               // (e.g. on linux it will include only linuxx64 artifacts and no macosx64)
-               // It shouldn't be a problem as usually consumers need to use generic *-native artifact
-               // Gradle will choose correct variant by using metadata attributes
-               .matching { publication -> publication.artifacts.none { it.extension == "klib" } }
-               .all {
-                  api("${groupId}:${artifactId}:${version}")
-               }
-         }
-   }
+configurations.api.configure {
+   dependencyConstraints.addAllLater(
+      kotestBomService.coordinates.map { coords ->
+         logger.info("[$path] adding ${coords.size} coords: $coords")
+         coords
+            .distinct()
+            .sorted()
+            .map {
+               project.dependencies.constraints.create(it)
+            }
+      }
+   )
 }
 
 publishing {
