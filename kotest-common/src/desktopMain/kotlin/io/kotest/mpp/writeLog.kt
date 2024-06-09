b55bcf30a02e3ed98693e7a57fb474662fc0c102
Adam <897017+aSemy@users.noreply.github.com>
2024-06-09 23:03:30 +0200

    Tidy kotest-common logger (#4080)
    - Annotate vals/funs with `@KotestInternal` to avoid opt-in warnings
    - Remove `@OverloadResolutionByLambdaReturnType` from log function (it
    has no overloads)
    
    
    I saw that the failure in [this
    build](https://github.com/kotest/kotest/actions/runs/9431572967/job/25980529942?pr=4076)
    had an error relating to the logger functions, so maybe tidying it up
    will help.
    
    ```stacktrace
    org.gradle.api.internal.tasks.testing.TestSuiteExecutionException: Could not complete execution for Gradle Test Executor 1.
    	[...]
    Caused by: org.junit.platform.commons.JUnitException: TestEngine with ID 'kotest' failed to discover tests
    	[...]
    Caused by: java.lang.NoSuchMethodError: 'void io.kotest.mpp.Logger.logsimple(kotlin.jvm.functions.Function0)'
    	at io.kotest.runner.junit.platform.KotestJunitPlatformTestEngine.discover(KotestJunitPlatformTestEngine.kt:101)
    	at io.kotest.runner.junit.platform.KotestJunitPlatformTestEngine.discover(KotestJunitPlatformTestEngine.kt:35)
    	at org.junit.platform.launcher.core.EngineDiscoveryOrchestrator.discoverEngineRoot(EngineDiscoveryOrchestrator.java:152)
    	... 28 more
    ```
    
    ---------
    
    Co-authored-by: Sam <sam@sksamuel.com>

--- kotest-common/src/desktopMain/kotlin/io/kotest/mpp/writeLog.kt
+++ kotest-common/src/desktopMain/kotlin/io/kotest/mpp/writeLog.kt
@@ -1,6 +1,8 @@
 package io.kotest.mpp
 
+import io.kotest.common.KotestInternal
 import io.kotest.common.TimeMarkCompat
 
+@KotestInternal
 actual fun writeLog(start: TimeMarkCompat, t: Throwable?, f: () -> String) {
 }
