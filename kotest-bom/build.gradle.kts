ca5c34065efb4ba5ef6c6bb505c2ff1fc240811f
Adam <897017+aSemy@users.noreply.github.com>
2024-06-09 19:39:04 +0200

    Apply publishing conventions to kotest-bom (#4073)
    apply the publishing convention plugin in kotest-bom
    
    - update publishing-convention to only adds a Javadoc JAR for KMP
    subprojects
    - remove duplicated configuration in kotest-bom

--- kotest-bom/build.gradle.kts
+++ kotest-bom/build.gradle.kts
@@ -1,11 +1,7 @@
 plugins {
    `java-platform`
-   `maven-publish`
-   signing
+   id("kotest-publishing-conventions")
 }
-
-group = "io.kotest"
-version = Ci.publishVersion
 
 val bomProject = project
 
@@ -44,64 +40,10 @@ dependencies {
    }
 }
 
-val ossrhUsername: String by project
-val ossrhPassword: String by project
-val signingKey: String? by project
-val signingPassword: String? by project
-
 publishing {
-   repositories {
-      maven {
-         val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-         val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-         name = "deploy"
-         url = if (Ci.isRelease) releasesRepoUrl else snapshotsRepoUrl
-         credentials {
-            username = System.getenv("OSSRH_USERNAME") ?: ossrhUsername
-            password = System.getenv("OSSRH_PASSWORD") ?: ossrhPassword
-         }
-      }
-   }
-
    publications {
       create<MavenPublication>("KotestBom") {
          from(components["javaPlatform"])
-         pom {
-            name.set("Kotest")
-            description.set("Kotlin Test Framework")
-            url.set("https://github.com/kotest/kotest")
-
-            scm {
-               connection.set("scm:git:https://github.com/kotest/kotest/")
-               developerConnection.set("scm:git:https://github.com/sksamuel/")
-               url.set("https://github.com/kotest/kotest/")
-            }
-
-            licenses {
-               license {
-                  name.set("Apache-2.0")
-                  url.set("https://opensource.org/licenses/Apache-2.0")
-               }
-            }
-
-            developers {
-               developer {
-                  id.set("sksamuel")
-                  name.set("Stephen Samuel")
-                  email.set("sam@sksamuel.com")
-               }
-            }
-         }
       }
-   }
-}
-
-signing {
-   useGpgCmd()
-   if (signingKey != null && signingPassword != null) {
-      useInMemoryPgpKeys(signingKey, signingPassword)
-   }
-   if (Ci.isRelease) {
-      sign(publishing.publications)
    }
 }
