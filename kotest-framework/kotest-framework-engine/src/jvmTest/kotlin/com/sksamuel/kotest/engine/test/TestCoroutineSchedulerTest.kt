b343a5bbca520974b00684edebb2a47f55d94307
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-06-13 14:15:28 +0200

    Remove `@ExperimentalStdlibApi` annotation from testCoroutineScheduler (#4075)
    In addition to #4070
    
    Implementation of `testCoroutineScheduler` extension could be changed to
    avoid `@ExperimentalStdlibApi` annotation

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/TestCoroutineSchedulerTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/TestCoroutineSchedulerTest.kt
@@ -2,22 +2,19 @@ package com.sksamuel.kotest.engine.test
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.test.testCoroutineScheduler
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.days
 
-@ExperimentalCoroutinesApi
-@ExperimentalStdlibApi
 class TestCoroutineSchedulerTest : FunSpec() {
    init {
       test("delay controller should control time").config(testCoroutineDispatcher = true) {
          val duration = 1.days
          launch {
-            delay(duration.inWholeMilliseconds)
+            delay(duration)
          }
          // if this isn't working, the above test will just take forever
-         testCoroutineScheduler.advanceTimeBy(duration.inWholeMilliseconds)
+         testCoroutineScheduler.advanceTimeBy(duration)
       }
    }
 }
