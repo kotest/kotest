32e76ec381ded9a296483489b0cf1784e7408cf2
Adam <897017+aSemy@users.noreply.github.com>
2024-06-15 08:14:52 +0200

    replace deprecated getTimeMillis with posix clock_gettime

--- kotest-common/src/desktopMain/kotlin/io/kotest/mpp/time.kt
+++ kotest-common/src/desktopMain/kotlin/io/kotest/mpp/time.kt
@@ -1,5 +1,23 @@
 package io.kotest.mpp
 
-import kotlin.system.getTimeMillis
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.UnsafeNumber
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.convert
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.pointed
+import kotlinx.cinterop.ptr
+import kotlin.time.Duration.Companion.nanoseconds
+import kotlin.time.Duration.Companion.seconds
 
-actual fun timeInMillis(): Long = getTimeMillis()
+@OptIn(ExperimentalForeignApi::class)
+actual fun timeInMillis(): Long {
+   return memScoped {
+      val timespec = alloc<platform.posix.timespec>().ptr
+      platform.posix.clock_gettime(platform.posix.CLOCK_REALTIME.convert(), timespec)
+      @OptIn(UnsafeNumber::class)
+      timespec.pointed
+         .run { tv_sec.seconds + tv_nsec.nanoseconds }
+         .inWholeMilliseconds
+   }
+}
