7a50c6736a7538641005114fa72b9f411dd95cc8
Adam <897017+aSemy@users.noreply.github.com>
2024-06-16 10:04:17 +0200

    Use centralized repositories for build
    Centrally define the repositories required for the build. This encourages a stable build, as the repositories are defined consistently, in the same order of priority, for all subprojects.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,65 @@ pluginManagement {
    }
 }
 
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+   repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+
+   repositories {
+      google()
+      mavenCentral()
+      maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental") {
+         name = "JetBrainsKotlinWasm"
+      }
+      maven("https://oss.sonatype.org/content/repositories/snapshots/") {
+         name = "SonatypeSnapshots"
+         mavenContent { snapshotsOnly() }
+      }
+
+      gradlePluginPortal() // tvOS builds need to be able to fetch a kotlin gradle plugin
+
+      // workaround for https://youtrack.jetbrains.com/issue/KT-51379
+      // FIXME remove when updating to Kotlin 2.0
+      ivy("https://download.jetbrains.com/kotlin/native/builds") {
+         name = "KotlinNative"
+         patternLayout {
+            listOf(
+               "macos-x86_64",
+               "macos-aarch64",
+               "osx-x86_64",
+               "osx-aarch64",
+               "linux-x86_64",
+               "windows-x86_64",
+            ).forEach { os ->
+               listOf("dev", "releases").forEach { stage ->
+                  artifact("$stage/[revision]/$os/[artifact]-[revision].[ext]")
+               }
+            }
+         }
+         content { includeModuleByRegex(".*", ".*kotlin-native-prebuilt.*") }
+         metadataSources { artifact() }
+      }
+
+      //region Declare the Node.js & Yarn download repositories
+      // Workaround https://youtrack.jetbrains.com/issue/KT-68533/
+      ivy("https://cache-redirector.jetbrains.com/nodejs.org/dist/") {
+         name = "Node Distributions at $url"
+         patternLayout { artifact("v[revision]/[artifact](-v[revision]-[classifier]).[ext]") }
+         metadataSources { artifact() }
+         content { includeModule("org.nodejs", "node") }
+      }
+      ivy("https://github.com/yarnpkg/yarn/releases/download") {
+         name = "Yarn Distributions at $url"
+         patternLayout { artifact("v[revision]/[artifact](-v[revision]).[ext]") }
+         metadataSources { artifact() }
+         content { includeModule("com.yarnpkg", "yarn") }
+      }
+      //endregion
+
+      mavenLocal()
+   }
+}
+
 include(
    ":kotest-common",
 
