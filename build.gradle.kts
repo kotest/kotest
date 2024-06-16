3370cd92d30164ee9a3ce61964ddd069007c9060
Adam <897017+aSemy@users.noreply.github.com>
2024-06-16 23:11:25 +0200

    Use centralized repositories for build (#4110)
    Centrally define the repositories required for the build. This encourages a stable build, as the repositories are defined consistently, in the same order of priority, for all subprojects.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,6 @@ plugins {
    alias(libs.plugins.kotlinBinaryCompatibilityValidator)
 }
 
-repositories {
-   mavenCentral()
-   mavenLocal()
-   maven("https://oss.sonatype.org/content/repositories/snapshots/")
-   google()
-   gradlePluginPortal() // tvOS builds need to be able to fetch a kotlin gradle plugin
-}
-
 apiValidation {
    ignoredPackages.addAll(
       listOf(
