bf8c6fd193571f2d2b600e72e60f3ccb6518764b
Adam <897017+aSemy@users.noreply.github.com>
2024-06-18 19:46:34 +0200

    add additional assertions in ConcurrentTest

--- kotest-assertions/kotest-assertions-core/src/commonTest/kotlin/com/sksamuel/kotest/matchers/concurrent/suspension/ConcurrentTest.kt
+++ kotest-assertions/kotest-assertions-core/src/commonTest/kotlin/com/sksamuel/kotest/matchers/concurrent/suspension/ConcurrentTest.kt
@@ -19,14 +19,17 @@ class ConcurrentTest : FunSpec({
    coroutineTestScope = true
 
    test("should not fail when given lambda pass in given time") {
+      val start = testCoroutineScheduler.timeSource.markNow()
       shouldNotThrowAny {
          shouldCompleteWithin(2.seconds) {
             delay(1.seconds)
          }
       }
+      start.elapsedNow() shouldBe 1.seconds
    }
 
    test("should fail when given lambda does not complete in given time") {
+      val start = testCoroutineScheduler.timeSource.markNow()
       val message = shouldFail {
          shouldCompleteWithin(1.seconds) {
             delay(1.5.seconds)
@@ -34,9 +37,11 @@ class ConcurrentTest : FunSpec({
       }.message
 
       message shouldBe "Operation should have completed within 1s"
+      start.elapsedNow() shouldBe 1.seconds
    }
 
    test("should not fail when given lambda pass in given time range") {
+      val start = testCoroutineScheduler.timeSource.markNow()
       withContext(ShouldCompleteBetweenTimeSource(testCoroutineScheduler.timeSource)) {
          shouldNotThrowAny {
             shouldCompleteBetween(1.seconds..2.seconds) {
@@ -44,9 +49,11 @@ class ConcurrentTest : FunSpec({
             }
          }
       }
+      start.elapsedNow() shouldBe 1.5.seconds
    }
 
    test("should fail when given lambda pass before the given time range") {
+      val start = testCoroutineScheduler.timeSource.markNow()
       val message = shouldFail {
          shouldCompleteBetween(1.seconds..2.seconds) {
             delay(0.5.seconds)
@@ -54,9 +61,11 @@ class ConcurrentTest : FunSpec({
       }.message
 
       message shouldBe "Operation should not have completed before 1s"
+      start.elapsedNow() shouldBe 0.5.seconds
    }
 
    test("should fail when given lambda did not complete with in the given time range") {
+      val start = testCoroutineScheduler.timeSource.markNow()
       val message = shouldFail {
          shouldCompleteBetween(1.seconds..2.seconds) {
             delay(2.5.seconds)
@@ -64,22 +73,27 @@ class ConcurrentTest : FunSpec({
       }.message
 
       message shouldBe "Operation should have completed within 1s..2s"
+      start.elapsedNow() shouldBe 2.seconds
    }
 
    test("should not throw any if given lambda did not complete in given time") {
+      val start = testCoroutineScheduler.timeSource.markNow()
       shouldNotThrowAny {
          shouldTimeout(1.seconds) {
             delay(1.1.seconds)
          }
       }
+      start.elapsedNow() shouldBe 1.seconds
    }
 
    test("should fail if given lambda complete within given time") {
+      val start = testCoroutineScheduler.timeSource.markNow()
       val failure = shouldFail {
          shouldTimeout(1.seconds) {
             delay(0.1.seconds)
          }
       }
       failure.message shouldContain "Operation should not have completed before 1s"
+      start.elapsedNow() shouldBe 0.1.seconds
    }
 })
