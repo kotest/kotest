4c05414fa465d238a2fff2d18abffbd78a80baad
Emil Kantis <emil.kantis@protonmail.com>
2024-06-21 04:38:38 +0200

    Cleaning up IDE version differentiated files where files are identical (#303)

--- src/IC-232/kotlin/io/kotest/plugin/intellij/inspectionsuper.kt
+++
@@ -1,5 +0,0 @@
-package io.kotest.plugin.intellij
-
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.inspections.AbstractKotlinInspection
-
-abstract class AbstractInspection : AbstractKotlinInspection()
--- src/IC-233/kotlin/io/kotest/plugin/intellij/fqnames.kt
+++
@@ -1,7 +0,0 @@
-package io.kotest.plugin.intellij
-
-import org.jetbrains.kotlin.idea.base.psi.kotlinFqName
-import org.jetbrains.kotlin.name.FqName
-import org.jetbrains.kotlin.psi.KtClassOrObject
-
-fun KtClassOrObject.fqname(): FqName? = this.kotlinFqName
