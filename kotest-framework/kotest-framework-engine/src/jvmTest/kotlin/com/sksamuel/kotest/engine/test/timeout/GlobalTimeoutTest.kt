1343f16192b3dc1c8f9c87bc14241654c9637ad1
OliverO2 <oliver.o456i@gmail.com>
2024-06-22 22:12:40 +0200

    GlobalTimeoutTest: reformat, reestablish exceptions, increase timeout

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/timeout/GlobalTimeoutTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/timeout/GlobalTimeoutTest.kt
@@ -1,39 +1,41 @@
 package com.sksamuel.kotest.engine.test.timeout
 
+import io.kotest.assertions.asClue
 import io.kotest.core.config.ProjectConfiguration
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.engine.TestEngineLauncher
 import io.kotest.engine.listener.CollectingTestEngineListener
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.days
 
 class GlobalTimeoutTest : FunSpec() {
    init {
-      test("global timeouts should apply if no other timeout is set")
-      {
-         val c = ProjectConfiguration().apply { timeout = 2000 }
+      test("global timeouts should apply if no other timeout is set") {
+         val c = ProjectConfiguration().apply { timeout = 4000 }
          val collector = CollectingTestEngineListener()
          TestEngineLauncher(collector)
             .withClasses(TestTimeouts::class)
             .withConfiguration(c)
             .launch()
-         collector.tests.mapKeys { it.key.name.testName }["blocked"]?.isError shouldBe true
-         collector.tests.mapKeys { it.key.name.testName }["suspend"]?.isError shouldBe true
+
+         collector.names.shouldContainExactly("blocked", "suspend")
+
+         collector.result("blocked").asClue { result -> result?.isError shouldBe true }
+         collector.result("suspend").asClue { result -> result?.isError shouldBe true }
       }
    }
 }
 
-private class TestTimeouts : StringSpec() {
-   init {
-      "blocked".config(blockingTest = true) {
-         // high value to ensure its interrupted, we'd notice a test that runs for 10 weeks
-         Thread.sleep(1000000)
-      }
-
-      "suspend" {
-         // high value to ensure its interrupted, we'd notice a test that runs for 10 weeks
-         delay(1000000)
-      }
+private class TestTimeouts : StringSpec({
+   // Long delays, to ensure tests will be interrupted. We'd notice a test that runs for a month.
+   "blocked".config(blockingTest = true) {
+      Thread.sleep(28.days.inWholeMilliseconds)
    }
-}
+
+   "suspend" {
+      delay(28.days)
+   }
+})
