3421fa5908b3771d315815423941ba6e2e7937a7
Sam <sam@sksamuel.com>
2024-07-21 17:22:07 -0500

    Removed deprecated property test arbs and methods (#4202)
    <!-- 
    If this PR updates documentation, please update all relevant versions of
    the docs, see:
    https://github.com/kotest/kotest/tree/master/documentation/versioned_docs
    The documentation at
    https://github.com/kotest/kotest/tree/master/documentation/docs is the
    documentation for the next minor or major version _TO BE RELEASED_
    -->

--- kotest-property/src/commonMain/kotlin/io/kotest/property/arbitrary/emails.kt
+++ kotest-property/src/commonMain/kotlin/io/kotest/property/arbitrary/emails.kt
@@ -2,22 +2,7 @@ package io.kotest.property.arbitrary
 
 import io.kotest.property.Arb
 import io.kotest.property.Gen
-import io.kotest.property.azstring
 import kotlin.random.nextInt
-
-@Deprecated("This function is deprecated since 5.0. Use Arb.email(localPartGen, domainGen). This will be removed in 6.0")
-fun Arb.Companion.email(usernameSize: IntRange = 3..10, domainSize: IntRange): Arb<String> {
-   val tlds = listOf("com", "net", "gov", "co.uk", "jp", "nl", "ru", "de", "com.br", "it", "pl", "io")
-   return arbitrary {
-      val tld = tlds.random(it.random)
-      val username = it.random.azstring(size = it.random.nextInt(usernameSize))
-      val domain = it.random.azstring(size = it.random.nextInt(domainSize))
-      val usernamep = if (username.length > 5 && it.random.nextBoolean()) {
-         username.take(username.length / 2) + "." + username.drop(username.length / 2)
-      } else username
-      "$usernamep@$domain.$tld"
-   }
-}
 
 fun Arb.Companion.email(
    localPartGen: Gen<String> = emailLocalPart(),
