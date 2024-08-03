b1776652383025b608e7a9155c11fb2abcd8ddd7
sksamuel <sam@sksamuel.com>
2024-08-03 02:25:30 -0500

    Updated to kotlin 2.0

--- kotest-framework/kotest-framework-multiplatform-plugin-gradle/src/test/kotlin/io/kotest/framework/multiplatform/gradle/KotestMultiplatformCompilerGradlePluginSpec.kt
+++ kotest-framework/kotest-framework-multiplatform-plugin-gradle/src/test/kotlin/io/kotest/framework/multiplatform/gradle/KotestMultiplatformCompilerGradlePluginSpec.kt
@@ -27,7 +27,6 @@ import kotlin.io.path.name
 // See https://youtrack.jetbrains.com/issue/KT-24327 for one example.
 class KotestMultiplatformCompilerGradlePluginSpec : ShouldSpec({
    setOf(
-      "1.9.24",
       "2.0.0",
    ).forEach { kotlinVersion ->
       context("when the project targets Kotlin version $kotlinVersion") {
