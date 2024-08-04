d986e8fc733cd694141dfc3ed08947af914e0d73
sksamuel <sam@sksamuel.com>
2024-08-04 06:26:43 -0500

    tests

--- kotest-assertions/kotest-assertions-json/src/commonTest/kotlin/io.kotest.assertions.json/JsonAssertionsTest.kt
+++ kotest-assertions/kotest-assertions-json/src/commonTest/kotlin/io.kotest.assertions.json/JsonAssertionsTest.kt
@@ -12,15 +12,13 @@ class JsonAssertionsTest : StringSpec({
 
    "should return correct error message on failure" {
       shouldThrow<AssertionError> {
-         @Suppress("DEPRECATION")
-         json1 shouldMatchJson json3
+         json1 shouldEqualJson json3
       }.message shouldBe """expected json to match, but they differed
          |
          |expected:<{"location":"chicago","name":"sam"}> but was:<{"name":"sam","location":"london"}>""".trimMargin()
 
       shouldThrow<AssertionError> {
-         @Suppress("DEPRECATION")
-         json1 shouldNotMatchJson json2
+         json1 shouldNotEqualJson json2
       }.message shouldBe """expected not to match with: {"location":"london","name":"sam"} but match: {"name":"sam","location":"london"}"""
    }
 })
