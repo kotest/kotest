f3287cf812221798db4efa4c72d800ca46e27a41
sksamuel <sam@sksamuel.com>
2024-08-04 07:03:15 -0500

    fix

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/interceptors.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/interceptors.kt
@@ -1,14 +1,7 @@
 package io.kotest.engine
 
 import io.kotest.common.KotestInternal
-import io.kotest.engine.spec.interceptor.SpecInterceptor
 import io.kotest.engine.test.interceptors.TestExecutionInterceptor
-
-/**
- * Returns the [SpecInterceptor]s that should be used for this platform.
- */
-@KotestInternal
-internal expect fun specInterceptorsForPlatform(): List<SpecInterceptor>
 
 /**
  * Returns the [TestExecutionInterceptor]s that should be used for this platform.
--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/spec/interceptor/instance/AfterSpecListenerSpecInterceptor.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/spec/interceptor/instance/AfterSpecListenerSpecInterceptor.kt
@@ -13,21 +13,24 @@ import io.kotest.engine.spec.interceptor.SpecInterceptor
  * Invokes any [AfterSpecListener] callbacks for the given spec.
  * These listeners are only invoked if the spec has at least one defined test AND all defined tests are not ignored.
  */
-internal class AfterSpecCallbackInterceptor(private val registry: ExtensionRegistry) : SpecInterceptor {
+internal class AfterSpecListenerSpecInterceptor(private val registry: ExtensionRegistry) : SpecInterceptor {
    override suspend fun intercept(
       spec: Spec,
       fn: suspend (Spec) -> Result<Map<TestCase, TestResult>>
    ): Result<Map<TestCase, TestResult>> {
       return fn(spec).flatMap { results ->
-         if (noActiveTest(results)) Result.success(results) else {
+         if (hasActiveTest(results)) {
             SpecExtensionsExecutor(registry)
                .afterSpec(spec)
                .map { results }
+         } else {
+            Result.success(results)
          }
       }
    }
 
-   private fun noActiveTest(results: Map<TestCase, TestResult>): Boolean {
-      return results.isEmpty() || results.all { it.value is TestResult.Ignored }
+   // the spec results are considered active if at least test has a non ignored status
+   private fun hasActiveTest(results: Map<TestCase, TestResult>): Boolean {
+      return results.any { it.value !is TestResult.Ignored }
    }
 }
