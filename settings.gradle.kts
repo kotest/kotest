4a7be3b690f29b75d659931a75292286fcf29336
Adam <897017+aSemy@users.noreply.github.com>
2024-08-05 21:53:10 +0200

    Remove Kotlin Native repository
    Kotlin Native artifacts are now hosted in Maven Central, and the custom repositories can be removed.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,29 +18,6 @@ dependencyResolutionManagement {
          mavenContent { snapshotsOnly() }
       }
 
-      //region workaround for https://youtrack.jetbrains.com/issue/KT-51379
-      // FIXME remove when updating to Kotlin 2.0
-      ivy("https://download.jetbrains.com/kotlin/native/builds") {
-         name = "KotlinNative"
-         patternLayout {
-            listOf(
-               "macos-x86_64",
-               "macos-aarch64",
-               "osx-x86_64",
-               "osx-aarch64",
-               "linux-x86_64",
-               "windows-x86_64",
-            ).forEach { os ->
-               listOf("dev", "releases").forEach { stage ->
-                  artifact("$stage/[revision]/$os/[artifact]-[revision].[ext]")
-               }
-            }
-         }
-         content { includeModuleByRegex(".*", ".*kotlin-native-prebuilt.*") }
-         metadataSources { artifact() }
-      }
-      //endregion
-
       //region Declare the Node.js & Yarn download repositories
       // Workaround https://youtrack.jetbrains.com/issue/KT-68533/
       ivy("https://nodejs.org/dist/") {
