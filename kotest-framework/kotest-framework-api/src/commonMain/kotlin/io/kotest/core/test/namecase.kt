b02927ca2e70db1704d0f76930976cabab4b7806
sksamuel <sam@sksamuel.com>
2024-08-10 11:49:40 -0500

    Removed more deprecated elements

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/test/namecase.kt
+++
@@ -1,6 +0,0 @@
-package io.kotest.core.test
-
-import io.kotest.core.names.TestNameCase
-
-@Deprecated("Relocated to io.kotest.core.names.TestNameCase. This typealias will be removed in 6.0")
-typealias TestNameCase = TestNameCase
