ce06975c4bc9da2ececcbf0646b2e229a7882a06
Sam <sam@sksamuel.com>
2024-08-10 15:35:35 -0500

    Removed more deprecated elements (#4281)
    <!-- 
    If this PR updates documentation, please update all relevant versions of
    the docs, see:
    https://github.com/kotest/kotest/tree/master/documentation/versioned_docs
    The documentation at
    https://github.com/kotest/kotest/tree/master/documentation/docs is the
    documentation for the next minor or major version _TO BE RELEASED_
    -->

--- kotest-framework/kotest-framework-api/src/commonMain/kotlin/io/kotest/core/filter/Filter.kt
+++
@@ -1,6 +0,0 @@
-package io.kotest.core.filter
-
-import io.kotest.core.extensions.Extension
-
-@Deprecated("Filters have been subsumed by extensions. Deprecated since 5.0")
-interface Filter : Extension
