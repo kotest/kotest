47f35f93c2fecda800e25795ad4adbbdc9e86da8
Sam <sam@sksamuel.com>
2024-08-12 13:39:39 -0500

    Disable autoscan of config classes; Check for well known config FQN by default (#4282)
    Fixes #3895 caused by poor performance
    
    Fixes #4112 too possibly.
    
    Fixes #3746

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/config/loadProjectConfigFromClassname.kt
+++
@@ -1,9 +0,0 @@
-package io.kotest.engine.config
-
-import io.kotest.core.config.AbstractProjectConfig
-
-/**
- * Load an [AbstractProjectConfig] instance by using FQN from a system property.
- * Only applies on the JVM.
- */
-internal expect fun loadProjectConfigFromClassname(): AbstractProjectConfig?
