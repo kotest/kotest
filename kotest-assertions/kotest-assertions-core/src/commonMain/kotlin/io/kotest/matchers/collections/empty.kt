94a4a0b4fda51062b9a0e2e79e32fda823152e89
Ondřej Karmazín <ondrej.karmazin@outlook.com>
2024-09-22 17:38:59 +0200

    Reorder functions in empty.kt for easier human visual control
    Ordered by receiver, not by operation. Easier for the human eye to check.

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/empty.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/empty.kt
@@ -7,27 +7,9 @@ import io.kotest.matchers.invokeMatcher
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldNot
 
-fun <T, I : Iterable<T>> I?.shouldBeEmpty(): I {
-   if (this == null) fail("Iterable")
-   toList() should beEmpty(this.matcherName())
-   return this
-}
-
 fun <T> Array<T>?.shouldBeEmpty(): Array<T> {
    if (this == null) fail("Array")
    asList() should beEmpty("Array")
-   return this
-}
-
-fun <T, C : Collection<T>> C?.shouldBeEmpty(): C {
-   if (this == null) fail("Collection")
-   this should beEmpty("Collection")
-   return this
-}
-
-fun <T, I : Iterable<T>> I?.shouldNotBeEmpty(): I {
-   if (this == null) fail("Iterable")
-   toList() shouldNot beEmpty(this.matcherName())
    return this
 }
 
@@ -37,29 +19,35 @@ fun <T> Array<T>?.shouldNotBeEmpty(): Array<T> {
    return this
 }
 
+fun <T, C : Collection<T>> C?.shouldBeEmpty(): C {
+   if (this == null) fail("Collection")
+   this should beEmpty("Collection")
+   return this
+}
+
 fun <T, C : Collection<T>> C?.shouldNotBeEmpty(): C {
    if (this == null) fail("Collection")
    this shouldNot beEmpty("Collection")
    return this
 }
 
-@Deprecated("Use Collection.shouldBeEmpty() or Collection.shouldNotBeEmpty()", level = DeprecationLevel.ERROR)
-fun <T> beEmpty(): Matcher<Collection<T>> = beEmpty("Collection")
+fun <T, I : Iterable<T>> I?.shouldBeEmpty(): I {
+   if (this == null) fail("Iterable")
+   toList() should beEmpty(this.matcherName())
+   return this
+}
+
+fun <T, I : Iterable<T>> I?.shouldNotBeEmpty(): I {
+   if (this == null) fail("Iterable")
+   toList() shouldNot beEmpty(this.matcherName())
+   return this
+}
 
 private fun <T> beEmpty(name: String): Matcher<Collection<T>> = object : Matcher<Collection<T>> {
    override fun test(value: Collection<T>): MatcherResult = MatcherResult(
       value.isEmpty(),
       { "$name should be empty but contained ${value.first().print().value}" },
       { "$name should not be empty" }
-   )
-}
-
-@Deprecated("Use Array.shouldBeEmpty() or Array.shouldNotBeEmpty()", level = DeprecationLevel.ERROR)
-fun <T> beEmptyArray(): Matcher<Array<T>> = object : Matcher<Array<T>> {
-   override fun test(value: Array<T>): MatcherResult = MatcherResult(
-      value.isEmpty(),
-      { "Array should be empty but contained ${value.first().print().value}" },
-      { "Array should not be empty" }
    )
 }
 
@@ -71,4 +59,16 @@ private inline fun fail(name: String): Nothing {
 private inline fun Iterable<*>.matcherName() = when (this) {
    is ClosedRange<*>, is OpenEndRange<*> -> "Range"
    else -> "Iterable"
+}
+
+@Deprecated("Use Collection.shouldBeEmpty() or Collection.shouldNotBeEmpty()", level = DeprecationLevel.ERROR)
+fun <T> beEmpty(): Matcher<Collection<T>> = beEmpty("Collection")
+
+@Deprecated("Use Array.shouldBeEmpty() or Array.shouldNotBeEmpty()", level = DeprecationLevel.ERROR)
+fun <T> beEmptyArray(): Matcher<Array<T>> = object : Matcher<Array<T>> {
+   override fun test(value: Array<T>): MatcherResult = MatcherResult(
+      value.isEmpty(),
+      { "Array should be empty but contained ${value.first().print().value}" },
+      { "Array should not be empty" }
+   )
 }
