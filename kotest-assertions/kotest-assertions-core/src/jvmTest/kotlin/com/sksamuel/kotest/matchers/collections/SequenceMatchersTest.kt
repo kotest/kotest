c3e12c8b0d8c91e52ca1172fea3866b379abff65
Ondřej Karmazín <ondrej.karmazin@outlook.com>
2024-09-24 12:36:40 +0200

    shouldNotContainDuplicates: Report indexes of duplicate elements on failure

--- kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/collections/SequenceMatchersTest.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/collections/SequenceMatchersTest.kt
@@ -43,7 +43,6 @@ import io.kotest.matchers.sequences.shouldNotHaveCount
 import io.kotest.matchers.sequences.shouldNotHaveElementAt
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
-import io.kotest.matchers.string.shouldNotContain
 import io.kotest.matchers.throwable.shouldHaveMessage
 
 class SequenceMatchersTest : WordSpec() {
@@ -886,13 +885,13 @@ class SequenceMatchersTest : WordSpec() {
          "fail with repeated nulls" {
             shouldThrowAny {
                sampleData.sparse.shouldBeUnique()
-            }.shouldHaveMessage("Sequence should be Unique, but has duplicates: [<null>]")
+            }.shouldHaveMessage("Sequence should be Unique, but has duplicates:\n<null> at indexes: [0, 1, 2]")
          }
 
          "fail with repeats" {
             shouldThrowAny {
                sampleData.repeating.shouldBeUnique()
-            }.shouldHaveMessage("Sequence should be Unique, but has duplicates: [1, 2, 3]")
+            }.shouldHaveMessage("Sequence should be Unique, but has duplicates:\n1 at indexes: [0, 3]\n2 at indexes: [1, 4]\n3 at indexes: [2, 5]")
          }
 
          succeed("for multiple unique") {
@@ -946,7 +945,7 @@ class SequenceMatchersTest : WordSpec() {
          "fail with repeats" {
             shouldThrowAny {
                sampleData.repeating.shouldNotContainDuplicates()
-            }.shouldHaveMessage("Sequence should not contain duplicates, but has some: [1, 2, 3]")
+            }.shouldHaveMessage("Sequence should not contain duplicates, but has:\n1 at indexes: [0, 3]\n2 at indexes: [1, 4]\n3 at indexes: [2, 5]")
          }
       }
 
