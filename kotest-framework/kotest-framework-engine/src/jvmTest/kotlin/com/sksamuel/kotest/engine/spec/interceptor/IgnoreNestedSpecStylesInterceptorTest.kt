296eb14b61e4be54648b3708aa21d0ba7a3470e8
samsam <sam@sksamuel.com>
2024-10-01 11:27:15 -0500

    tests

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/spec/interceptor/IgnoreNestedSpecStylesInterceptorTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/spec/interceptor/IgnoreNestedSpecStylesInterceptorTest.kt
@@ -1,32 +1,42 @@
-//package com.sksamuel.kotest.engine.spec.interceptor
-//
-//import io.kotest.core.config.EmptyExtensionRegistry
-//import io.kotest.core.spec.style.BehaviorSpec
-//import io.kotest.core.spec.style.DescribeSpec
-//import io.kotest.core.spec.style.FreeSpec
-//import io.kotest.core.spec.style.FunSpec
-//import io.kotest.core.spec.style.WordSpec
-//import io.kotest.engine.listener.NoopTestEngineListener
-//import io.kotest.engine.spec.interceptor.instance.IgnoreNestedSpecStylesInterceptor
-//
-//class IgnoreNestedSpecStylesInterceptorTest : FunSpec({
-//   test("IgnoreNestedSpecStylesInterceptor should skip any nested spec style") {
-//
-//      IgnoreNestedSpecStylesInterceptor(NoopTestEngineListener, EmptyExtensionRegistry)
-//         .intercept(MyBehaviorSpec()) { error("boom") }
-//
-//      IgnoreNestedSpecStylesInterceptor(NoopTestEngineListener, EmptyExtensionRegistry)
-//         .intercept(MyWordSpec()) { error("boom") }
-//
-//      IgnoreNestedSpecStylesInterceptor(NoopTestEngineListener, EmptyExtensionRegistry)
-//         .intercept(MyFreeSpec()) { error("boom") }
-//
-//      IgnoreNestedSpecStylesInterceptor(NoopTestEngineListener, EmptyExtensionRegistry)
-//         .intercept(MyDescribeSpec()) { error("boom") }
-//   }
-//})
-//
-//private class MyBehaviorSpec : BehaviorSpec()
-//private class MyFreeSpec : FreeSpec()
-//private class MyWordSpec : WordSpec()
-//private class MyDescribeSpec : DescribeSpec()
+package com.sksamuel.kotest.engine.spec.interceptor
+
+import io.kotest.core.config.EmptyExtensionRegistry
+import io.kotest.core.spec.Spec
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
+import io.kotest.engine.listener.NoopTestEngineListener
+import io.kotest.engine.spec.interceptor.NextSpecInterceptor
+import io.kotest.engine.spec.interceptor.instance.IgnoreNestedSpecStylesInterceptor
+
+class IgnoreNestedSpecStylesInterceptorTest : FunSpec({
+   test("IgnoreNestedSpecStylesInterceptor should skip any nested spec style") {
+
+      val errorNext = object : NextSpecInterceptor {
+         override suspend fun invoke(spec: Spec): Result<Map<TestCase, TestResult>> {
+            error("boom")
+         }
+      }
+
+      IgnoreNestedSpecStylesInterceptor(NoopTestEngineListener, EmptyExtensionRegistry)
+         .intercept(MyBehaviorSpec(), errorNext)
+
+      IgnoreNestedSpecStylesInterceptor(NoopTestEngineListener, EmptyExtensionRegistry)
+         .intercept(MyWordSpec(), errorNext)
+
+      IgnoreNestedSpecStylesInterceptor(NoopTestEngineListener, EmptyExtensionRegistry)
+         .intercept(MyFreeSpec(), errorNext)
+
+      IgnoreNestedSpecStylesInterceptor(NoopTestEngineListener, EmptyExtensionRegistry)
+         .intercept(MyDescribeSpec(), errorNext)
+   }
+})
+
+private class MyBehaviorSpec : BehaviorSpec()
+private class MyFreeSpec : FreeSpec()
+private class MyWordSpec : WordSpec()
+private class MyDescribeSpec : DescribeSpec()
