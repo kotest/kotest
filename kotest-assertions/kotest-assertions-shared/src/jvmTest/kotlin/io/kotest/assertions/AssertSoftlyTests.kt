de47d965ec378a352a19fb8021d40c0058695ad0
Łukasz Pięta <pieta.lukasz98@gmail.com>
2024-10-13 18:08:31 +0200

    Fix AssertSoftlyTests run for LinuxCondition

--- kotest-assertions/kotest-assertions-shared/src/jvmTest/kotlin/io/kotest/assertions/AssertSoftlyTests.kt
+++ kotest-assertions/kotest-assertions-shared/src/jvmTest/kotlin/io/kotest/assertions/AssertSoftlyTests.kt
@@ -6,7 +6,6 @@ import io.kotest.core.annotation.enabledif.LinuxCondition
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.string.shouldContainInOrder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -46,14 +45,15 @@ class AssertSoftlyTests : FunSpec({
       )
    }
    test("adds an Exception to an empty collection of assertion failures") {
-      val thrown = shouldThrowExactly<AssertionError> {
+      val thrown = shouldThrowExactly<MultiAssertionError> {
          assertSoftly {
             bespokeDivision(1, 0) shouldBe 1
             "first assertion" shouldBe "First Assertion"
          }
       }
-      thrown.message.shouldContain(
-         "/ by zero"
+      thrown.message.shouldContainInOrder(
+         "The following assertion failed:",
+         "1) / by zero",
       )
    }
 })
