fcee605a3c68e881db8ee2b94975f15107a642d8
AlexKuznetsov <alexander.kuznetsov@target.com>
2024-10-15 10:37:07 -0500

    CustomComparison.kt

--- kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/equality/equalToComparingFields.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/equality/equalToComparingFields.kt
@@ -75,41 +75,6 @@ infix fun <T : Any> T.shouldNotBeEqualUsingFields(block: FieldEqualityConfig.() 
    return this
 }
 
-sealed interface CustomComparisonResult {
-   val comparable: Boolean
-   data object NotComparable: CustomComparisonResult {
-      override val comparable = false
-   }
-   data object Equal: CustomComparisonResult {
-      override val comparable = true
-   }
-   data class Different(val assertionError: AssertionError): CustomComparisonResult {
-      override val comparable = true
-   }
-}
-
-fun interface Assertable {
-   fun assert(expected: Any?, actual: Any?): CustomComparisonResult
-}
-
-inline fun<reified T: Any> customComparison(
-   expected: Any?,
-   actual: Any?,
-   assertion: (expected: T, actual: T) -> Unit
-): CustomComparisonResult = when {
-   expected == null -> CustomComparisonResult.NotComparable
-   actual == null -> CustomComparisonResult.NotComparable
-   expected is T && actual is T -> {
-      try {
-         assertion(expected, actual)
-         CustomComparisonResult.Equal
-      } catch (e: AssertionError) {
-         CustomComparisonResult.Different(e)
-      }
-   }
-   else -> CustomComparisonResult.NotComparable
-}
-
 /**
  * Config for controlling the way shouldBeEqualUsingFields compares fields.
  *
