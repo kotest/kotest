aba70bf9fbc520ee60000b5e37dcf774b6facf65
Alex Kuznetsov <Alex.Cue.987@gmail.com>
2024-10-21 13:50:06 -0500

    Alex/terser string mismatches draft3 (#4454)

--- kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/io/kotest/matchers/string/StringPartialMatchesTest.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/io/kotest/matchers/string/StringPartialMatchesTest.kt
@@ -5,7 +5,6 @@ import io.kotest.core.annotation.enabledif.LinuxCondition
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.submatching.PartialMatchesInCollectionDescription
-import io.kotest.submatching.describePartialMatchesInString
 import io.kotest.submatching.describePartialMatchesInStringForPrefix
 import io.kotest.submatching.describePartialMatchesInStringForSlice
 import io.kotest.submatching.describePartialMatchesInStringForSuffix
@@ -86,7 +85,6 @@ class StringPartialMatchesTest : WordSpec() {
             val actual = describePartialMatchesInStringForSlice(line, threeLines)
             actual.partialMatchesDescription.lines() shouldBe listOf(
                "Line[0] =\"What?\"",
-               "Match[0]= -----",
                "Line[1] =\"The quick brown fox jumps over the lazy dog.\"",
                "Match[0]= +++++++++++++++++++++++++++++++++++++++++++-",
                "Line[2] =\"And that's it.\"",
