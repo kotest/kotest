ee1be103995185b087380f43fd9d899d2f2bf0f9
AlexKuznetsov <alexander.kuznetsov@target.com>
2024-10-23 09:10:21 -0500

    PartialMatchesAreUnorderedTest.kt

--- kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/similarity/StringDistanceCalculator.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/similarity/StringDistanceCalculator.kt
@@ -1,5 +1,6 @@
 package io.kotest.similarity
 
+import io.kotest.submatching.PartialCollectionMatch
 import io.kotest.submatching.describePartialMatchesInStringForSlice
 import java.math.BigDecimal
 
@@ -9,9 +10,22 @@ internal fun matchNotNullStrings(field: String, expected: String, actual: String
       val orderedFields: List<String> = listOf(expected, actual).sortedBy { it.length }
       val shorter = orderedFields[0]
       val longer = orderedFields[1]
+      val maxLength = listOf(expected, actual).maxOf { it.length }
       val comparison = describePartialMatchesInStringForSlice(shorter, longer)
-//      val ordered =
+      val numberOfMatchedCharacters = comparison.partialMatches.sumOf { it.length }
+      val ratioOfPartialMatches = BigDecimal.valueOf(numberOfMatchedCharacters.toLong())
+         .divide(BigDecimal.valueOf(maxLength.toLong()))
+//      val partialMatchesAreUnordered = comparison.partialMatches.zipWithNext().any { (first, second) ->
+//         first.first > second.first
+//      }
       val distance = Distance(BigDecimal.ZERO)
       MismatchByField(field, expected, actual, emptyList(), distance)
+   }
+}
+
+internal fun partialMatchesAreUnordered(partialMatches: List<PartialCollectionMatch>): Boolean {
+   return partialMatches.sortedBy { it.matchedElement.startIndexInExpected }
+      .zipWithNext().any { (first, second) ->
+      first.matchedElement.startIndexInValue > second.matchedElement.startIndexInValue
    }
 }
