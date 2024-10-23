9fc8fdc9890593d9c6e7241f3a96e9302eece05f
AlexKuznetsov <alexander.kuznetsov@target.com>
2024-10-23 11:00:46 -0500

    MatchNotNullTest.kt

--- kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/io/kotest/similarity/MatchNotNullTest.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/io/kotest/similarity/MatchNotNullTest.kt
@@ -1,11 +1,14 @@
 package io.kotest.similarity
 
+import io.kotest.assertions.assertSoftly
 import io.kotest.core.annotation.EnabledIf
 import io.kotest.core.annotation.enabledif.LinuxCondition
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContainInOrder
+import java.math.BigDecimal
 
-@EnabledIf(LinuxCondition::class)
+//@EnabledIf(LinuxCondition::class)
 class MatchNotNullTest : StringSpec() {
    private val fieldName = "field"
 
@@ -15,7 +18,7 @@ class MatchNotNullTest : StringSpec() {
       }
 
       "mismatch for primitives" {
-         matchNotNull(fieldName, "this", "another") shouldBe AtomicMismatch(fieldName, "this", "another")
+         matchNotNull(fieldName, BigDecimal.ONE, BigDecimal.TEN) shouldBe AtomicMismatch(fieldName, BigDecimal.ONE, BigDecimal.TEN)
       }
 
       "match for identical data class instances" {
@@ -29,6 +32,21 @@ class MatchNotNullTest : StringSpec() {
             redCircle,
             otherRedCircle
          )
+      }
+
+      "mismatch for two different strings" {
+         val expectedStr = "0123456789abcdefghijklmnopqrstuvwxyz"
+         val actualStr = "$expectedStr?"
+         val actual = matchNotNull(fieldName, expectedStr, actualStr)
+         assertSoftly {
+            actual.match shouldBe false
+            actual.description().shouldContainInOrder(
+               "Match[0]: whole slice matched actual[0..35]",
+               """Line[0] ="0123456789abcdefghijklmnopqrstuvwxyz?"""",
+               """Match[0]= ++++++++++++++++++++++++++++++++++++-"""
+            )
+            (actual as StringMismatch).distance shouldBe Distance(BigDecimal("0.97"))
+         }
       }
    }
 }
