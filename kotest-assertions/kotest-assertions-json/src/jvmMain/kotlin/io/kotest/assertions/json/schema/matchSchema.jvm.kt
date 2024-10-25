dbc33ff55acb667b43307f7bf45dab93b73d7270
solonovamax <solonovamax@12oclockpoint.com>
2024-10-24 21:47:13 -0400

    Fix conflicting class names

--- kotest-assertions/kotest-assertions-json/src/jvmMain/kotlin/io/kotest/assertions/json/schema/matchSchema.jvm.kt
+++ kotest-assertions/kotest-assertions-json/src/jvmMain/kotlin/io/kotest/assertions/json/schema/matchSchema.jvm.kt
@@ -1,11 +1,7 @@
 package io.kotest.assertions.json.schema
 
-import io.kotest.matchers.paths.shouldBeAFile
-import io.kotest.matchers.paths.shouldExist
 import java.io.File
 import java.nio.file.Path
-import kotlin.io.path.exists
-import kotlin.io.path.isRegularFile
 import kotlin.io.path.readText
 
 infix fun Path.shouldMatchSchema(schema: JsonSchema): Path {
