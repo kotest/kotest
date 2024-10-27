a9126354fe8b2d4888cada3e2d307c2ac6100a89
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2024-10-27 13:00:22 -0300

    Add unit tests for containerName function in utils.kt
    Introduced comprehensive tests to validate that containerName accurately
     identifies different types of collections such as List, Set,
     Collection, and Iterable. Also removed classes that are not Iterable
     from being tested.
    
     Closes #4381
    
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/utils.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/utils.kt
@@ -3,15 +3,14 @@ package io.kotest.matchers.collections
 /**
  * Returns a more specific underlying type if this is one of the basic collections/iterables.
  *
- * "List" for lists, "Set" for sets, "Range" for ranges etc.
+ * "List" for lists, "Set" for sets, "Collection" for collections etc.
  */
 internal fun Iterable<*>.containerName(): String {
    return when (this) {
-      is List -> "List"
-      is Set -> "Set"
-      is Map<*, *> -> "Map"
-      is ClosedRange<*>, is OpenEndRange<*> -> "Range"
-      is Collection -> "Collection"
+      is List<*> -> "List"
+      is Set<*> -> "Set"
+      is Collection<*> -> "Collection"
       else -> "Iterable"
    }
 }
+
