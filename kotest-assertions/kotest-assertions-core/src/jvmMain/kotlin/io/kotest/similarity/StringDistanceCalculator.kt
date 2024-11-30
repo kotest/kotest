63ef004be1351d74b98c2e7c4a83a53fcbcba6f3
Alex Kuznetsov <Alex.Cue.987@gmail.com>
2024-11-30 14:27:56 -0600

    Alex/string similarity (#4518)
    next small step towards finding similar strings in collections and such
    - everywhere when we search for similar data classes.
    
    we are not plugging it in just yet, but creating the classes to plug in
    later.
    
    Also we are cleaning up three unused functions:
    `possibleMatchDescription` and its dependents

---
+++ kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/similarity/StringDistanceCalculator.kt
@@ -0,0 +1,45 @@
+package io.kotest.similarity
+
+import io.kotest.assertions.AssertionsConfig
+import io.kotest.submatching.PartialCollectionMatch
+import io.kotest.submatching.describePartialMatchesInStringForSlice
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+internal fun matchNotNullStrings(field: String, expected: String, actual: String): ComparisonResult = when {
+   expected == actual -> Match(field, expected)
+   else -> {
+      val comparison = describePartialMatchesInStringForSlice(expected, actual, forceComparison = true)
+      val ratioOfPartialMatches = ratioOfPartialMatchesInString(
+         comparison.partialMatches,
+         expected,
+         actual
+      )
+      val partialMatchesAreUnordered = partialMatchesAreUnordered(comparison.partialMatches)
+      val discountForUnorderedPartialMatches = if(partialMatchesAreUnordered) BigDecimal("0.8") else BigDecimal.ONE
+      val distance = Distance(ratioOfPartialMatches.multiply(discountForUnorderedPartialMatches))
+      if(distance.aboveThresholdForStrings()) {
+         StringMismatch(field, expected, actual, comparison.toString(), distance)
+      } else {
+         AtomicMismatch(field, expected, actual, distance)
+      }
+   }
+}
+
+internal fun partialMatchesAreUnordered(partialMatches: List<PartialCollectionMatch>): Boolean {
+   return partialMatches.sortedBy { it.matchedElement.startIndexInExpected }
+      .zipWithNext().any { (first, second) ->
+         first.matchedElement.startIndexInValue > second.matchedElement.startIndexInValue
+      }
+}
+
+internal fun ratioOfPartialMatchesInString(
+   partialMatches: List<PartialCollectionMatch>,
+   expected: String,
+   actual: String,
+): BigDecimal {
+   val maxLength = listOf(expected, actual).maxOf { it.length }
+   val numberOfMatchedCharacters = partialMatches.sumOf { it.length }
+   return BigDecimal.valueOf(numberOfMatchedCharacters.toLong())
+      .divide(BigDecimal.valueOf(maxLength.toLong()), 2, RoundingMode.HALF_DOWN)
+}
