dc31912cac3d2a260878f90a889d26ad46aa7439
sksamuel <sam@sksamuel.com>
2024-12-07 13:08:37 -0600

    Update EnabledIf to search parent classes #4495

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/spec/EnabledIfTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/spec/EnabledIfTest.kt
@@ -1,19 +1,30 @@
 package com.sksamuel.kotest.engine.spec
 
-import io.kotest.core.annotation.EnabledCondition
 import io.kotest.core.annotation.EnabledIf
-import io.kotest.core.spec.Spec
+import io.kotest.core.annotation.NeverEnabled
 import io.kotest.core.spec.style.FunSpec
-import kotlin.reflect.KClass
-
-class NeverEnabled : EnabledCondition {
-   override fun enabled(kclass: KClass<out Spec>): Boolean = false
-}
 
 @EnabledIf(NeverEnabled::class)
 class EnabledIfTest : FunSpec() {
    init {
       // this spec should not be created
       error("boom")
+   }
+}
+
+@EnabledIf(NeverEnabled::class)
+open class NeverEnabledBaseSpec : FunSpec() {
+   init {
+      // this spec should not be created
+      error("boom")
+   }
+}
+
+// this spec should not be created as its parent is disabled
+class InheritedEnabledIfTest : NeverEnabledBaseSpec() {
+   init {
+      test("whack!") {
+         error("whack!")
+      }
    }
 }
