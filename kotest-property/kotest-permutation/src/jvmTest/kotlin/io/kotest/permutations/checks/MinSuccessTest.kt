c0e0ec146a6eb9b4866634d60993728ea8105a8d
sksamuel <sam@sksamuel.com>
2024-12-07 06:15:08 -0600

    api dump

--- kotest-property/kotest-permutation/src/jvmTest/kotlin/io/kotest/permutations/checks/MinSuccessTest.kt
+++ kotest-property/kotest-permutation/src/jvmTest/kotlin/io/kotest/permutations/checks/MinSuccessTest.kt
@@ -1,4 +1,4 @@
-package io.kotest.permutations
+package io.kotest.permutations.checks
 
 import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.annotation.EnabledIf
@@ -6,6 +6,7 @@ import io.kotest.core.annotation.enabledif.LinuxCondition
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.ints.shouldBeLessThan
 import io.kotest.matchers.string.shouldContain
+import io.kotest.permutations.permutations
 import io.kotest.property.Exhaustive
 import io.kotest.property.exhaustive.ints
 
