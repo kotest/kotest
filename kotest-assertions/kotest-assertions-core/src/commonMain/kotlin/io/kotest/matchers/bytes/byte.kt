d276b5dfdd7f3614a2dbd4f10be8c18fd3f001ad
Sam <sam@sksamuel.com>
2024-12-08 03:58:22 -0600

    Adopt uniform wording and textual representation of ranges/intervals … (#4551)
    Closes #4362

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/bytes/byte.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/bytes/byte.kt
@@ -4,6 +4,9 @@ import io.kotest.matchers.Matcher
 import io.kotest.matchers.comparables.between
 import io.kotest.matchers.shouldBe
 
+/**
+ * Match that verifies a given [Byte] has a value between [lower, upper] (inclusive, inclusive).
+ */
 @Deprecated(
    "Byte-specific assertion is getting replaced with a new Comparable assertion of the same name.\nNote: If you perform the offered IDE autocorrection, you still need to remove the Byte import `io.kotest.matchers.bytes.shouldBeBetween` manually.",
    ReplaceWith("shouldBeBetween(lower, upper)", "io.kotest.matchers.comparables.shouldBeBetween")
@@ -13,6 +16,9 @@ fun Byte.shouldBeBetween(lower: Byte, upper: Byte): Byte {
    return this
 }
 
+/**
+ * Match that verifies a given [Byte] has a value between [lower, upper] (inclusive, inclusive).
+ */
 @Deprecated(
    "Byte-specific matcher is getting replaced with a new Comparable matcher of the same name.\nNote: If you perform the offered IDE autocorrection, you still need to remove the Byte import `io.kotest.matchers.bytes.between` manually.",
    ReplaceWith("between(lower, upper)", "io.kotest.matchers.comparables.between")
