c74adc5a913fd22795eba604e1d405259ce1a145
Sam <sam@sksamuel.com>
2024-12-14 17:08:37 -0600

    Migrate allure module back to main (#4572)
    <!-- 
    If this PR updates documentation, please update all relevant versions of
    the docs, see:
    https://github.com/kotest/kotest/tree/master/documentation/versioned_docs
    The documentation at
    https://github.com/kotest/kotest/tree/master/documentation/docs is the
    documentation for the next minor or major version _TO BE RELEASED_
    -->

---
+++ kotest-extensions/kotest-extensions-allure/src/jvmTest/kotlin/com/sksamuel/kotest/DummyShouldSpec.kt
@@ -0,0 +1,21 @@
+package com.sksamuel.kotest
+
+import io.kotest.core.spec.Order
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.booleans.shouldBeTrue
+
+@Order(0)
+class DummyShouldSpec : ShouldSpec() {
+   init {
+      context("a") {
+         should("work") {
+            true.shouldBeTrue()
+         }
+      }
+      context("b") {
+         should("work") {
+            true.shouldBeTrue()
+         }
+      }
+   }
+}
