37d13804b42762523a2060bb0b8cc196c2257765
sksamuel <sam@sksamuel.com>
2024-12-14 11:48:37 -0600

    more

--- kotest-framework/kotest-framework-api/src/jsHostedMain/kotlin/io/kotest/core/annotation/enabledif/Wrapper.kt
+++
@@ -1,8 +0,0 @@
-package io.kotest.core.annotation.enabledif
-
-import io.kotest.core.annotation.EnabledCondition
-import io.kotest.core.annotation.EnabledIf
-import kotlin.reflect.KClass
-
-actual val EnabledIf.wrapper: KClass<out EnabledCondition>
-   get() = enabledIf
