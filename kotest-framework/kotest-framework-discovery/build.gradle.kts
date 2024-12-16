ee9f5acf9bf50e001f4f6839d8b29980a7a34142
sksamuel <sam@sksamuel.com>
2024-12-15 20:59:30 -0600

    fix

--- kotest-framework/kotest-framework-discovery/build.gradle.kts
+++ kotest-framework/kotest-framework-discovery/build.gradle.kts
@@ -11,9 +11,7 @@ kotlin {
             implementation(kotlin("reflect"))
 
             api(projects.kotestCommon) // needs to be API so the domain objects are open
-            // needed to compile against Spec
-            // but runtime classes must be provided by modules using discovery
-            compileOnly(projects.kotestFramework.kotestFrameworkEngine)
+            api(projects.kotestFramework.kotestFrameworkEngine)
 
             // needed to scan the classpath for classes
             implementation(libs.classgraph)
