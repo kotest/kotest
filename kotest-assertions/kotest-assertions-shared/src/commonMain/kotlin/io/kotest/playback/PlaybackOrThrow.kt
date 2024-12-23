9fde4242de6397acedd2472ac5e5ceca6387c0c4
Alex Kuznetsov <Alex.Cue.987@gmail.com>
2024-12-23 15:21:06 -0600

    Alex/playback draft2 (#4608)

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/playback/PlaybackOrThrow.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/playback/PlaybackOrThrow.kt
@@ -3,10 +3,51 @@ package io.kotest.playback
 class PlaybackResults<T>(
    source: Sequence<Result<T>>
 ) {
-   val iterator = source.iterator()
+   private val iterator = source.iterator()
 
    fun next(): T = iterator.next().getOrThrow()
 }
 
-// syntactic  sugar
+/**
+ * Provides a fake function that returns elements or throw exceptions from a sequence.
+ *
+ * Example:
+ *
+ *          val fakeFunction = sequenceOf(
+ *             Result.success("yes"),
+ *             Result.failure(RuntimeException("bad request")),
+ *             Result.success("no")
+ *          ).toFunction()
+ *          fakeFunction.next() shouldBe "yes"
+ *          shouldThrow<RuntimeException> { fakeFunction.next() }
+ *          fakeFunction.next() shouldBe "no"
+ *
+ * It should be used in tests to replace a real dependency with a fake one, like this:
+ *
+ *    private fun interface HasAnswer<Q, T> {
+ *       fun answer(question: Q): T
+ *    }
+ *
+ *    private class DecisionEngine(
+ *       private val hasAnswers: HasAnswer<String, String>
+ *    ) {
+ *       fun answer(question: String): String {
+ *          return hasAnswers.answer(question)
+ *       }
+ *    }
+ *
+ *       "play back elements or throw exceptions, correctly plugged in" {
+ *          val fakeFunction = sequenceOf(
+ *             Result.success("yes"),
+ *             Result.failure(RuntimeException("bad request")),
+ *             Result.success("no")
+ *          ).toFunction()
+ *          val decisionEngine = DecisionEngine { fakeFunction.next() }
+ *          decisionEngine.answer("what") shouldBe "yes"
+ *          shouldThrow<RuntimeException> { decisionEngine.answer("when") }
+ *          decisionEngine.answer("where") shouldBe "no"
+ *       }
+ *
+ */
+
 fun<T> Sequence<Result<T>>.toFunction(): PlaybackResults<T> = PlaybackResults(this)
