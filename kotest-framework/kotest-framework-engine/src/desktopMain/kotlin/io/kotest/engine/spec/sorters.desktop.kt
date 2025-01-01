c4673aada8c58231295d2da786a8c0cd720e9b48
sksamuel <sam@sksamuel.com>
2024-12-31 22:09:49 -0600

    Simplify spec sorters

--- kotest-framework/kotest-framework-engine/src/desktopMain/kotlin/io/kotest/engine/spec/sorters.desktop.kt
+++ kotest-framework/kotest-framework-engine/src/desktopMain/kotlin/io/kotest/engine/spec/sorters.desktop.kt
@@ -1,12 +1,4 @@
 package io.kotest.engine.spec
 
-import io.kotest.core.spec.Spec
-import kotlin.reflect.KClass
-
-actual val FailureFirstSorter: SpecSorter = object : SpecSorter {
-   override fun compare(a: KClass<out Spec>, b: KClass<out Spec>): Int = 0
-}
-
-actual val AnnotatedSpecSorter: SpecSorter = object : SpecSorter {
-   override fun compare(a: KClass<out Spec>, b: KClass<out Spec>): Int = 0
-}
+actual val FailureFirstSorter: SpecSorter = NoopSpecSorter
+actual val AnnotatedSpecSorter: SpecSorter = NoopSpecSorter
--- kotest-framework/kotest-framework-engine/src/jsHostedMain/kotlin/io/kotest/engine/spec/sorters.js.kt
+++ kotest-framework/kotest-framework-engine/src/jsHostedMain/kotlin/io/kotest/engine/spec/sorters.js.kt
@@ -1,19 +1,12 @@
 package io.kotest.engine.spec
 
-import io.kotest.core.spec.Spec
-import kotlin.reflect.KClass
-
 /**
  * Note: No file system so this sort order is a no-op
  */
-actual val FailureFirstSorter: SpecSorter = object : SpecSorter {
-   override fun compare(a: KClass<out Spec>, b: KClass<out Spec>): Int = 0
-}
+actual val FailureFirstSorter: SpecSorter = NoopSpecSorter
 
 /**
  * Note: Runtime annotations are not supported on Native or JS so on those platforms
  * this sort order is a no-op.
  */
-actual val AnnotatedSpecSorter: SpecSorter = object : SpecSorter {
-   override fun compare(a: KClass<out Spec>, b: KClass<out Spec>): Int = 0
-}
+actual val AnnotatedSpecSorter: SpecSorter = NoopSpecSorter
