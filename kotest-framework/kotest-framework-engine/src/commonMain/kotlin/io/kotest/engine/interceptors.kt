432640f17c99bbf65c5dfae639ef063f326edc3d
sksamuel <sam@sksamuel.com>
2025-01-02 05:32:56 -0600

    Removed @KotestInternal from already internal objects

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/interceptors.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/interceptors.kt
@@ -1,10 +1,8 @@
 package io.kotest.engine
 
-import io.kotest.common.KotestInternal
 import io.kotest.engine.test.interceptors.TestExecutionInterceptor
 
 /**
  * Returns the [TestExecutionInterceptor]s that should be used for this platform.
  */
-@KotestInternal
 internal expect fun testInterceptorsForPlatform(): List<TestExecutionInterceptor>
