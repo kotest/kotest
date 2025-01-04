b0b88793c970f51d3395bac09604641b2a2b02e6
AlexKuznetsov <alexander.kuznetsov@target.com>
2025-01-04 15:23:21 -0600

    SampleIndexesTest.kt

--- kotest-property/src/commonMain/kotlin/io/kotest/property/exhaustive/collections.kt
+++ kotest-property/src/commonMain/kotlin/io/kotest/property/exhaustive/collections.kt
@@ -21,3 +21,21 @@ fun <A> Exhaustive.Companion.permutations(list: List<A>, length: Int = list.size
 
    return perms(list, length).exhaustive()
 }
+
+internal fun sampleIndexes(size: Int): Sequence<List<Int>> = sequence {
+   require(size > 0) { "Size should be positive, was: $size"}
+   val elementsIncluded = MutableList(size) { true }
+   val allIndexes = (0 until size).toList()
+   yield(allIndexes)
+   while(elementsIncluded.any { it }) {
+      for (index in 0 until size) {
+         if (elementsIncluded[index]) {
+            elementsIncluded[index] = false
+            yield(allIndexes.filterIndexed { i, _ -> elementsIncluded[i] })
+            break
+         } else {
+            elementsIncluded[index] = true
+         }
+      }
+   }
+}
