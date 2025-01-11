9abb03a468790bd166cd5db29be2ca8b80520c13
sksamuel <sam@sksamuel.com>
2025-01-11 15:41:14 -0600

    Renamed package config object

--- kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/config/configs.jvm.kt
+++ kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/config/configs.jvm.kt
@@ -5,7 +5,9 @@ import io.kotest.engine.instantiateOrObject
 
 actual fun loadPackageConfig(packageName: String): AbstractPackageConfig? {
    // ok to skip if the class doesn't exist
-   val kclass = runCatching { Class.forName("$packageName.KotestPackageConfig").kotlin }.getOrNull() ?: return null
+   val kclass = runCatching { Class.forName(packageConfigName(packageName)).kotlin }.getOrNull() ?: return null
    // but should fail if the class exists but cannot be instantiated
    return instantiateOrObject(kclass).getOrThrow() as AbstractPackageConfig
 }
+
+private fun packageConfigName(packageName: String) = "$packageName.PackageConfig"
