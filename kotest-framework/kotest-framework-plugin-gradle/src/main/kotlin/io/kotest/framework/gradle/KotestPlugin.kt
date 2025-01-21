deb2f9ad33cd76cb5c1e50df7bb51bf97eb351c9
Emil Kantis <emil.kantis@protonmail.com>
2025-01-21 07:06:35 +0100

    Kotest plugin creates a kotest target per Kotlin target

--- kotest-framework/kotest-framework-plugin-gradle/src/main/kotlin/io/kotest/framework/gradle/KotestPlugin.kt
+++ kotest-framework/kotest-framework-plugin-gradle/src/main/kotlin/io/kotest/framework/gradle/KotestPlugin.kt
@@ -3,8 +3,12 @@ package io.kotest.framework.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaBasePlugin
-import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.provider.Property
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import javax.inject.Inject
 
 // gradle requires the class be extendable
@@ -14,6 +18,12 @@ open class KotestPlugin : Plugin<Project> {
       const val DESCRIPTION = "Run Kotest"
       const val TASK_NAME = "kotest"
       const val EXTENSION_NAME = "kotest"
+
+      private const val KOTLIN_JVM_PLUGIN = "org.jetbrains.kotlin.jvm"
+      private const val KOTLIN_MULTIPLATFORM_PLUGIN = "org.jetbrains.kotlin.multiplatform"
+      private val unsupportedTargets = listOf(
+         "metadata"
+      )
    }
 
    override fun apply(project: Project) {
@@ -25,20 +35,37 @@ open class KotestPlugin : Plugin<Project> {
          /* ...constructionArguments = */ project
       )
 
-      // add a kotest task to all targets that have a test task
-//      project.plugins.withId("org.jetbrains.kotlin.multiplatform") {
-//         println("multiplatform=" + this::class.qualifiedName)
-//      }
+      // Configure Kotlin JVM projects
+      project.pluginManager.withPlugin(KOTLIN_JVM_PLUGIN) {
+         project.extensions.configure<KotlinJvmExtension> {
+            project.tasks.register("kotest", KotestTask::class.java) {
+               description = DESCRIPTION
+               group = JavaBasePlugin.VERIFICATION_GROUP
+               inputs.files(project.tasks.withType<KotlinCompile>().map { it.outputs.files })
+               // TODO: I think wiring dependencies,
+//               dependsOn(project.tasks.withType<KotlinCompile>())
+            }
+         }
+      }
 
-      // todo only add to projects which have a Test type already
-      // todo should only add to projects which have kotest on the classpath
-      project.afterEvaluate {
-         plugins.withId("org.jetbrains.kotlin.jvm") {
-            this@afterEvaluate.allprojects.forEach { subproject ->
-               subproject.tasks.register("kotest", KotestTask::class.java) {
-                  description = DESCRIPTION
-                  group = JavaBasePlugin.VERIFICATION_GROUP
-                  dependsOn(subproject.tasks.getByName(JavaPlugin.TEST_CLASSES_TASK_NAME))
+      // Configure Kotlin multiplatform projects
+      project.pluginManager.withPlugin(KOTLIN_MULTIPLATFORM_PLUGIN) {
+         project.extensions.configure<KotlinMultiplatformExtension> {
+            targets.configureEach {
+               println("Detected target: $name")
+               if (name in unsupportedTargets) {
+                  println("Skipping unsupported target: $name")
+               } else {
+                  val capitalTarget = name.replaceFirstChar { it.uppercase() }
+                  println("Creating task kotest$capitalTarget")
+                  project.tasks.register("kotest$capitalTarget", KotestTask::class.java) {
+                     description = DESCRIPTION
+                     group = JavaBasePlugin.VERIFICATION_GROUP
+                     println("Adding dependency on test classes")
+                     inputs.files(project.tasks.named("${name}TestClasses").map { it.outputs.files })
+//                     dependsOn(project.tasks.named("${targetName}TestClasses"))
+                     // TODO: Setup dependency on the compile task for this target's test source set
+                  }
                }
             }
          }
