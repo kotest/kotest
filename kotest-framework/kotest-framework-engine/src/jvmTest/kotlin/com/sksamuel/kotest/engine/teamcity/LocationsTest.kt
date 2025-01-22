9394fea2f9023321b17f63abf44ddeeadec8c28f
sksamuel <sam@sksamuel.com>
2025-01-21 22:35:32 -0600

    tests

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/teamcity/LocationsTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/teamcity/LocationsTest.kt
@@ -14,9 +14,4 @@ class LocationsTest : FunSpec({
       Locations.location(SourceRef.ClassSource("foo.bar", null)) shouldBe "kotest:class://foo.bar:1"
       Locations.location(SourceRef.ClassSource("foo.bar", 34)) shouldBe "kotest:class://foo.bar:34"
    }
-
-   test("FileSource hint") {
-      Locations.location(SourceRef.FileSource("foo.kt", null)) shouldBe "kotest:file://foo.kt:1"
-      Locations.location(SourceRef.FileSource("foo.kt", 34)) shouldBe "kotest:file://foo.kt:34"
-   }
 })
