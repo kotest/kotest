9687b0b88642e4fb2cfe44c3b502ecfd21261503
sksamuel <sam@sksamuel.com>
2025-01-21 23:06:35 -0600

    best name

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/teamcity/locations.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/teamcity/locations.kt
@@ -1,11 +1,12 @@
 package io.kotest.engine.teamcity
 
 import io.kotest.core.source.SourceRef
+import io.kotest.mpp.bestName
 import kotlin.reflect.KClass
 
 object Locations {
 
-   fun location(kclass: KClass<*>): String = classHint(kclass.qualifiedName ?: "", 1)
+   fun location(kclass: KClass<*>): String = classHint(kclass.bestName() ?: "", 1)
 
    fun location(sourceRef: SourceRef): String? = when (sourceRef) {
       is SourceRef.ClassSource -> classHint(sourceRef.fqn, sourceRef.lineNumber ?: 1)
