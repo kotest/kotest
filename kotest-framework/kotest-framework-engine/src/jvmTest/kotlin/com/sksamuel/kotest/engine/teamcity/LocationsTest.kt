5ee1e7cc96e7878c30cf9d33bcd2b1c79563a4e6
sksamuel <sam@sksamuel.com>
2025-01-21 22:37:00 -0600

    tests

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/teamcity/LocationsTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/teamcity/LocationsTest.kt
@@ -11,7 +11,7 @@ import io.kotest.matchers.shouldBe
 class LocationsTest : FunSpec({
 
    test("ClassSource hint") {
-      Locations.location(SourceRef.ClassSource("foo.bar", null)) shouldBe "kotest:class://foo.bar:1"
-      Locations.location(SourceRef.ClassSource("foo.bar", 34)) shouldBe "kotest:class://foo.bar:34"
+      Locations.location(SourceRef.ClassSource("foo.bar", null)) shouldBe "kotest://foo.bar:1"
+      Locations.location(SourceRef.ClassSource("foo.bar", 34)) shouldBe "kotest://foo.bar:34"
    }
 })
