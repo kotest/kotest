d5097801a77e19b57a1e0687f40513b55276a4a5
sksamuel <sam@sksamuel.com>
2025-01-25 00:01:05 -0600

    rebase

--- kotest-framework/kotest-framework-plugin-gradle/src/main/kotlin/io/kotest/framework/gradle/KotestPlugin.kt
+++ kotest-framework/kotest-framework-plugin-gradle/src/main/kotlin/io/kotest/framework/gradle/KotestPlugin.kt
@@ -3,10 +3,10 @@ package io.kotest.framework.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaBasePlugin
-import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.provider.Property
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -17,12 +17,12 @@ open class KotestPlugin : Plugin<Project> {
 
    companion object {
       const val DESCRIPTION = "Runs tests using Kotest"
-      const val TASK_NAME = "kotest"
+      const val JVM_TASK_NAME = "kotest"
       const val EXTENSION_NAME = "kotest"
 
       private const val KOTLIN_JVM_PLUGIN = "org.jetbrains.kotlin.jvm"
       private const val KOTLIN_MULTIPLATFORM_PLUGIN = "org.jetbrains.kotlin.multiplatform"
-      const val KOTLIN_ANDROID_PLUGIN = "org.jetbrains.kotlin.android"
+      private const val KOTLIN_ANDROID_PLUGIN = "org.jetbrains.kotlin.android"
 
       private val unsupportedTargets = listOf(
          "metadata"
@@ -43,7 +43,7 @@ open class KotestPlugin : Plugin<Project> {
          project.extensions.configure<KotlinJvmExtension> {
             // gradle best practice is to only apply to this project, and users add the plugin to each subproject
             // see https://docs.gradle.org/current/userguide/isolated_projects.html
-            project.tasks.register("kotest", KotestTask::class.java) {
+            project.tasks.register(JVM_TASK_NAME, KotestTask::class.java) {
                description = DESCRIPTION
                group = JavaBasePlugin.VERIFICATION_GROUP
                inputs.files(project.tasks.withType<KotlinCompile>().map { it.outputs.files })
@@ -57,6 +57,8 @@ open class KotestPlugin : Plugin<Project> {
             targets.configureEach {
                if (name !in unsupportedTargets) {
                   val capitalTarget = name.replaceFirstChar { it.uppercase() }
+                  // gradle best practice is to only apply to this project, and users add the plugin to each subproject
+                  // see https://docs.gradle.org/current/userguide/isolated_projects.html
                   project.tasks.register("kotest$capitalTarget", KotestTask::class.java) {
                      description = DESCRIPTION
                      group = JavaBasePlugin.VERIFICATION_GROUP
@@ -67,27 +69,19 @@ open class KotestPlugin : Plugin<Project> {
          }
       }
 
-
-
-      // this is for JVM projects
-      project.plugins.withId(KOTLIN_JVM_PLUGIN) {
-         // gradle best practice is to only apply to this project, and users add the plugin to each subproject
-         // see https://docs.gradle.org/current/userguide/isolated_projects.html
-         project.tasks.register(JVM_TASK_NAME, KotestTask::class.java) {
-            description = DESCRIPTION
-            group = JavaBasePlugin.VERIFICATION_GROUP
-            dependsOn(project.tasks.getByName("testClasses"))
-         }
-      }
-
-      // Android projects
-      project.plugins.withId(KOTLIN_ANDROID_PLUGIN) {
-         // gradle best practice is to only apply to this project, and users add the plugin to each subproject
-         // see https://docs.gradle.org/current/userguide/isolated_projects.html
-         project.tasks.register(JVM_TASK_NAME, KotestTask::class.java) {
-            description = DESCRIPTION
-            group = JavaBasePlugin.VERIFICATION_GROUP
-            dependsOn(project.tasks.getByName("processReleaseUnitTestJavaRes"))
+      // Configure Kotlin Android projects
+      project.pluginManager.withPlugin(KOTLIN_ANDROID_PLUGIN) {
+         project.extensions.configure<KotlinAndroidExtension> {
+            target.compilations.configureEach {
+               // gradle best practice is to only apply to this project, and users add the plugin to each subproject
+               // see https://docs.gradle.org/current/userguide/isolated_projects.html
+               val capitalTarget = name.replaceFirstChar { it.uppercase() }
+               project.tasks.register("kotest$capitalTarget", KotestTask::class.java) {
+                  description = DESCRIPTION
+                  group = JavaBasePlugin.VERIFICATION_GROUP
+                  inputs.files(project.tasks.withType<KotlinCompile>().map { it.outputs.files })
+               }
+            }
          }
       }
    }
