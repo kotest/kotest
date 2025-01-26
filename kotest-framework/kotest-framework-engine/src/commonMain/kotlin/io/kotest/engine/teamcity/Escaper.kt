004ae468a6eac9baad3dc5dbbd42beaf300af56d
sksamuel <sam@sksamuel.com>
2025-01-26 12:36:53 -0600

    tc docs

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/teamcity/Escaper.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/teamcity/Escaper.kt
@@ -1,9 +1,11 @@
 package io.kotest.engine.teamcity
 
 /**
- * Escapes strings for use when writing team city service messages
+ * Escapes strings for use when writing team city service messages.
+ * For escaped values, TeamCity uses a vertical bar | as an escape character
  *
- * See [Teamcity docs](https://www.jetbrains.com/help/teamcity/service-messages.html#Escaped+values)
+ * @see [Teamcity docs](https://www.jetbrains.com/help/teamcity/service-messages.html#Escaped+values)
+ * @see https://www.jetbrains.com/help/teamcity/6.5/build-script-interaction-with-teamcity.html?Build%2BScript%2BInteraction%2Bwith%2BTeamCity#BuildScriptInteractionwithTeamCity-servMsgsServiceMessages
  */
 object Escaper {
    fun escapeForTeamCity(str: String): String = StringBuilder(str.length).apply {
