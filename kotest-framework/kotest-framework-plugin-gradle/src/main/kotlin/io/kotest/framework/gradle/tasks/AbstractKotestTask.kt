bb190f0eea503d6e4f2961e54113aabae0d6f9b6
Adam <897017+aSemy@users.noreply.github.com>
2025-02-01 20:18:02 +0100

    add `@Input` to inputs

--- kotest-framework/kotest-framework-plugin-gradle/src/main/kotlin/io/kotest/framework/gradle/tasks/AbstractKotestTask.kt
+++ kotest-framework/kotest-framework-plugin-gradle/src/main/kotlin/io/kotest/framework/gradle/tasks/AbstractKotestTask.kt
@@ -4,28 +4,34 @@ import io.kotest.framework.gradle.TestClassDetector
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.FileCollection
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.options.Option
 
 abstract class AbstractKotestTask internal constructor() : DefaultTask() {
 
    @get:Option(option = "descriptor", description = "Filter to a single spec or test")
+   @get:Input
    @get:Optional
    abstract val descriptor: Property<String>
 
    @get:Option(option = "tests", description = "Filter to a test path expression")
+   @get:Input
    @get:Optional
    abstract val tests: Property<String>
 
    @get:Option(option = "candidates", description = "The candidates list to avoid scanning")
+   @get:Input
    @get:Optional
    abstract val candidates: Property<String>
 
    @get:Option(option = "packages", description = "Specify the packages to scan for tests")
+   @get:Input
    @get:Optional
    abstract val packages: Property<String>
 
    @get:Option(option = "tags", description = "Set tag expression to include or exclude tests")
+   @get:Input
    @get:Optional
    abstract val tags: Property<String>
 
