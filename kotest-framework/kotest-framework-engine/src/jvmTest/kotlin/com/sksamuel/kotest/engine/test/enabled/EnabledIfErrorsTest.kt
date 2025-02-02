b4b673908dd82334eaa68b51855a85f96249c7c3
sksamuel <sam@sksamuel.com>
2025-02-01 20:28:33 -0600

    Fixed for exceptions thrown in EnabledIf

---
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/enabled/EnabledIfErrorsTest.kt
@@ -0,0 +1,29 @@
+package com.sksamuel.kotest.engine.test.enabled
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.engine.TestEngineLauncher
+import io.kotest.engine.listener.CollectingTestEngineListener
+import io.kotest.matchers.shouldBe
+
+class EnabledIfErrorsTest : FunSpec() {
+   init {
+      test("error in enabledIf should disable test but run others") {
+         val collector = CollectingTestEngineListener()
+         TestEngineLauncher(collector)
+            .withClasses(EnabledIfFailues::class)
+            .launch()
+
+         collector.result("a")!!.isErrorOrFailure shouldBe true
+         collector.result("b")!!.isErrorOrFailure shouldBe true
+         collector.result("c")!!.isSuccess shouldBe true
+      }
+   }
+}
+
+private class EnabledIfFailues : FunSpec({
+   test("a").config(enabledIf = { error("foobar") }) {
+   }
+   test("b").config(enabledOrReasonIf = { error("foobar") }) {
+   }
+   test("c") {}
+})
