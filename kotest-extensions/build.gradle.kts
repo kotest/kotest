7fbd3cdedcac922980b32bcf3170d3dc0ef200ee
sksamuel <sam@sksamuel.com>
2025-02-01 19:22:04 -0600

    tests

--- kotest-extensions/build.gradle.kts
+++ kotest-extensions/build.gradle.kts
@@ -7,7 +7,7 @@ kotlin {
 
    sourceSets {
 
-      val jvmMain by getting {
+      jvmMain {
          dependencies {
             implementation(kotlin("reflect"))
             implementation(projects.kotestFramework.kotestFrameworkEngine)
@@ -15,7 +15,7 @@ kotlin {
          }
       }
 
-      val jvmTest by getting {
+      jvmTest {
          dependencies {
             implementation(projects.kotestAssertions.kotestAssertionsCore)
             implementation(libs.kotlinx.coroutines.core)
@@ -29,13 +29,13 @@ tasks.withType<Test>().configureEach {
    jvmArgumentProviders.add(CommandLineArgumentProvider {
       val javaLauncher = javaLauncher.orNull
       buildList {
-//         if (javaLauncher != null && javaLauncher.metadata.languageVersion >= JavaLanguageVersion.of(9)) {
+         if (javaLauncher != null) {
             // --add-opens is only available in Java 9+
             add("--add-opens=java.base/java.util=ALL-UNNAMED")
             add("--add-opens=java.base/java.lang=ALL-UNNAMED")
             // have to manually allow security.manager in Java 9+ https://github.com/kotest/kotest/issues/3841
             add("-Djava.security.manager=allow")
-//         }
+         }
       }
    })
 }
