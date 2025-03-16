ae584b36289dd0a85aabbc555abbf71d2759f621
belljun3395 <195850@jnu.ac.kr>
2025-03-16 11:43:11 +0900

    refactor: apply the createMessage function

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/ErrorCollector.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/assertions/ErrorCollector.kt
@@ -176,9 +176,9 @@ inline fun <reified T> ErrorCollector.runWithMode(mode: ErrorCollectionMode, blo
 internal fun List<Throwable>.toAssertionError(depth: Int, subject: Printed?): AssertionError? {
    return when {
       isEmpty() -> null
-      size == 1 && subject != null -> AssertionError("The following assertion for ${subject.value} failed:\n" + this[0].message)
+      size == 1 && subject != null -> AssertionError(createMessage(this, depth, subject))
       size == 1 && subject == null -> AssertionError(this[0].message)
-      else -> MultiAssertionError(this, depth, subject)
+      else -> MultiAssertionError(createMessage(this, depth, subject))
    }?.let {
       stacktraces.cleanStackTrace(it)
    }
