add607264a0ff80d97ae4ab4a1c5bac44da302df
0x746143 <176037012+0x746143@users.noreply.github.com>
2025-05-12 20:23:33 +0200

    Create additional tests for compilation matchers: - use a syntax highlighted code snippet - check a compilation error message - compile an inline function - fine-grained assertions on JvmCompilationResult - use a Kotlin compiler plugin

--- kotest-assertions/kotest-assertions-compiler/src/jvmTest/kotlin/io/kotest/matchers/compilation/CompilationsMatcherTest.kt
+++ kotest-assertions/kotest-assertions-compiler/src/jvmTest/kotlin/io/kotest/matchers/compilation/CompilationsMatcherTest.kt
@@ -1,10 +1,23 @@
 package io.kotest.matchers.compilation
 
+import com.tschuchort.compiletesting.KotlinCompilation.ExitCode
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.annotation.EnabledIf
 import io.kotest.core.annotation.LinuxOnlyGithubCondition
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
+import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
+import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+import org.jetbrains.kotlin.config.CommonConfigurationKeys
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import java.io.File
 
 @EnabledIf(LinuxOnlyGithubCondition::class)
@@ -22,13 +35,48 @@ class CompilationsMatcherTest : StringSpec() {
    init {
 
       "a code snippet of invalid variable assignment should not compile" {
-         val codeSnippet = """
+         val rawStringCodeSnippet = """
              val aString: String = 123
           """.trimIndent()
 
-         codeSnippet.shouldNotCompile()
-         file.writeText(codeSnippet)
+         val syntaxHighlightedCodeSnipped = codeSnippet("""
+             val aString: String = 123
+         """.trimIndent())
+
+         rawStringCodeSnippet.shouldNotCompile()
+         syntaxHighlightedCodeSnipped.shouldNotCompile()
+         file.writeText(rawStringCodeSnippet)
          file.shouldNotCompile()
+      }
+
+      "a code snippet should not compile and should produce the correct error message" {
+         val rawStringCodeSnippet = """
+             val aString: String = 123
+          """.trimIndent()
+
+         val syntaxHighlightedCodeSnipped = codeSnippet("""
+             val aString: String = 123
+         """.trimIndent())
+
+         val expectedErrorMsg = "Initializer type mismatch: expected 'String', actual 'Int'"
+         rawStringCodeSnippet.shouldNotCompile(expectedErrorMsg)
+         syntaxHighlightedCodeSnipped.shouldNotCompile(expectedErrorMsg)
+         file.writeText(rawStringCodeSnippet)
+         file.shouldNotCompile(expectedErrorMsg)
+      }
+
+      "shouldNotCompile() should throw AssertionError if the expected message is incorrect" {
+         val rawStringCodeSnippet = """
+             val aString: String = 123
+          """.trimIndent()
+
+         shouldThrowExactly<AssertionError> {
+            rawStringCodeSnippet.shouldNotCompile("Initializer type mismatch: expected 'Int', actual 'String'")
+         }
+         file.writeText(rawStringCodeSnippet)
+         shouldThrowExactly<AssertionError> {
+            file.shouldNotCompile("Initializer type mismatch: expected 'Int', actual 'String'")
+         }
       }
 
       "a code snippet with missing variable assignment should not compile" {
@@ -62,7 +110,7 @@ class CompilationsMatcherTest : StringSpec() {
          file.shouldCompile()
       }
 
-      "a code snippet with a invalid import statement should not compile" {
+      "a code snippet with an invalid import statement should not compile" {
          val codeSnippet = """
             package org.bar.foo
             import foo.time.LocalDate
@@ -76,6 +124,61 @@ class CompilationsMatcherTest : StringSpec() {
          codeSnippet.shouldNotCompile()
          file.writeText(codeSnippet)
          file.shouldNotCompile()
+      }
+
+      "a code snippet with an inline function should compile" {
+         codeSnippet("""
+            val aString = io.kotest.matchers.compilation.inlinedFunc(123)
+         """).shouldCompile()
+      }
+
+      @OptIn(ExperimentalCompilerApi::class)
+      "a code snippet should not compile and should produce several error messages" {
+         val codeSnippet = codeSnippet("""
+            fun foo() {
+               printDate(LocalDate.now())
+            }
+          """)
+
+         codeSnippet.compile {
+            exitCode shouldBe ExitCode.COMPILATION_ERROR
+            messages shouldContain "Unresolved reference 'LocalDate'"
+            messages shouldContain "Unresolved reference 'printDate'"
+         }
+      }
+
+      @OptIn(ExperimentalCompilerApi::class)
+      "compilation output should contain a message from the dummy compiler plugin" {
+         CompileConfig {
+            compilerPluginRegistrars = listOf(DummyCompilerPluginRegistrar())
+            verbose = true
+         }.codeSnippet("""
+            val a = 123
+         """).compile {
+            exitCode shouldBe ExitCode.OK
+            messages shouldContain "Hello from the dummy compiler plugin"
+         }
+      }
+   }
+}
+
+@Suppress("NOTHING_TO_INLINE", "unused")
+inline fun inlinedFunc(num: Int): String {
+   return num.toString()
+}
+
+@OptIn(ExperimentalCompilerApi::class)
+class DummyCompilerPluginRegistrar : CompilerPluginRegistrar() {
+   override val supportsK2: Boolean = true
+
+   override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
+      val dummyExtension = DummyIrGenerationExtension(configuration[CommonConfigurationKeys.MESSAGE_COLLECTOR_KEY])
+      IrGenerationExtension.registerExtension(dummyExtension)
+   }
+
+   class DummyIrGenerationExtension(private val messageCollector: MessageCollector?) : IrGenerationExtension {
+      override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
+         messageCollector?.report(CompilerMessageSeverity.LOGGING, "Hello from the dummy compiler plugin")
       }
    }
 }
