80efb7f43da1f144c56c370851c01d77390d775d
0x746143 <176037012+0x746143@users.noreply.github.com>
2025-05-12 11:49:20 +0200

    Improve test matcher to check compiler error messages on failure

--- kotest-assertions/kotest-assertions-compiler/src/jvmMain/kotlin/io/kotest/matchers/compilation/compileCodeSnippet.kt
+++ kotest-assertions/kotest-assertions-compiler/src/jvmMain/kotlin/io/kotest/matchers/compilation/compileCodeSnippet.kt
@@ -30,13 +30,45 @@ private fun compileCodeSnippet(sourceFile: SourceFile): JvmCompilationResult {
 }
 
 @OptIn(ExperimentalCompilerApi::class)
-private val compiles = object : Matcher<SourceFile> {
+private val compileMatcher = object : Matcher<SourceFile> {
    override fun test(sourceFile: SourceFile): MatcherResult {
       val compilationResult = compileCodeSnippet(sourceFile)
       return MatcherResult(
          compilationResult.exitCode == ExitCode.OK,
          { "Expected code to compile, but it failed to compile with error: \n${compilationResult.messages}" },
          { "Expected code to fail to compile, but it compile" }
+      )
+   }
+}
+
+@OptIn(ExperimentalCompilerApi::class)
+private class DoesNotCompileMatcher(private val expectedMessage: String) : Matcher<SourceFile> {
+   override fun test(value: SourceFile): MatcherResult {
+      val compilationResult = compileCodeSnippet(value)
+      val messages = compilationResult.messages
+      val exitCode = compilationResult.exitCode
+
+      val actuallyFailed = exitCode != ExitCode.OK
+      val messageMatched = expectedMessage in messages
+
+      return MatcherResult(
+         actuallyFailed && messageMatched,
+         {
+            when {
+               !actuallyFailed ->
+                  "Expected code to fail to compile, but it compiled successfully."
+
+               else -> buildString {
+                  appendLine("Expected error message containing:")
+                  appendLine(expectedMessage)
+                  appendLine("But actual messages were:")
+                  appendLine(messages)
+               }
+            }
+         },
+         {
+            "Expected compilation to not fail with message containing: $expectedMessage, but it did."
+         }
       )
    }
 }
@@ -51,47 +83,68 @@ fun codeSnippet(@Language("kotlin") sourceCode: String): SourceFile {
 /**
  * Assert that given codeSnippet [String] compiles successfully.
  * It includes the classpath of the calling process,
- * so that dependencies available for calling process will be available for code snippet as well.
+ * so that dependencies available to the calling process are also available to the code snippet.
  * @see [String.shouldNotCompile]
  * */
 fun String.shouldCompile() = codeSnippet(this).shouldCompile()
 
 /**
- * Assert that given codeSnippet [String] does not compiles successfully.
+ * Assert that given codeSnippet [String] does not compile successfully.
+ *
+ * If [expectedMessage] is provided, the test additionally verifies that the compilation fails
+ * with an error message containing the specified text. This helps ensure that the compilation
+ * fails for the expected reason, not due to an unrelated error.
+ *
  * It includes the classpath of the calling process,
- * so that dependencies available for calling process will be available for code snippet as well.
+ * so that dependencies available to the calling process are also available to the code snippet.
  * @see [String.shouldCompile]
  * */
-fun String.shouldNotCompile() = codeSnippet(this).shouldNotCompile()
+fun String.shouldNotCompile(expectedMessage: String? = null) = codeSnippet(this).shouldNotCompile(expectedMessage)
 
 /**
  * Assert that given file [File] compiles successfully.
  * It includes the classpath of the calling process,
- * so that dependencies available for calling process will be available for code snippet as well.
+ * so that dependencies available to the calling process are also available to the code snippet.
  * @see [File.shouldNotCompile]
  * */
 fun File.shouldCompile() = codeSnippet(readText()).shouldCompile()
 
 /**
- * Assert that given file [File] does not compiles successfully.
+ * Assert that given file [File] does not compile successfully.
+ *
+ * If [expectedMessage] is provided, the test additionally verifies that the compilation fails
+ * with an error message containing the specified text. This helps ensure that the compilation
+ * fails for the expected reason, not due to an unrelated error.
+ *
  * It includes the classpath of the calling process,
- * so that dependencies available for calling process will be available for code snippet as well.
+ * so that dependencies available to the calling process are also available to the code snippet.
  * @see [File.shouldCompile]
  * */
-fun File.shouldNotCompile() = codeSnippet(readText()).shouldNotCompile()
+fun File.shouldNotCompile(expectedMessage: String? = null) = codeSnippet(readText()).shouldNotCompile(expectedMessage)
 
 /**
  * Assert that given codeSnippet [SourceFile] compiles successfully.
  * It includes the classpath of the calling process,
- * so that dependencies available for calling process will be available for code snippet as well.
+ * so that dependencies available to the calling process are also available to the code snippet.
  * @see [SourceFile.shouldNotCompile]
  * */
-fun SourceFile.shouldCompile() = this should compiles
+fun SourceFile.shouldCompile() = this should compileMatcher
 
 /**
- * Assert that given codeSnippet [SourceFile] does not compiles successfully.
+ * Assert that given codeSnippet [SourceFile] does not compile successfully.
+ *
+ * If [expectedMessage] is provided, the test additionally verifies that the compilation fails
+ * with an error message containing the specified text. This helps ensure that the compilation
+ * fails for the expected reason, not due to an unrelated error.
+ *
  * It includes the classpath of the calling process,
- * so that dependencies available for calling process will be available for code snippet as well.
+ * so that dependencies available to the calling process are also available to the code snippet.
  * @see [SourceFile.shouldCompile]
  * */
-fun SourceFile.shouldNotCompile() = this shouldNot compiles
+fun SourceFile.shouldNotCompile(expectedMessage: String? = null) {
+   if (expectedMessage == null) {
+      this shouldNot compileMatcher
+   } else {
+      this should DoesNotCompileMatcher(expectedMessage)
+   }
+}
