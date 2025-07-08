a5072690a58e2745e22a5630d9a3f308aac0facb
Emil Kantis <emil.kantis@protonmail.com>
2025-07-08 22:10:20 +0200

    Dividing the Kotest release flow into multiple runs (#4930)
    Should make the publication process faster. Also setting the publication
    name so it's easier to see what's in Central.
    
    <!-- 
    If this PR updates documentation, please update all relevant versions of
    the docs, see:
    https://github.com/kotest/kotest/tree/master/documentation/versioned_docs
    The documentation at
    https://github.com/kotest/kotest/tree/master/documentation/docs is the
    documentation for the next minor or major version _TO BE RELEASED_
    -->

--- buildSrc/src/main/kotlin/kotest-publishing-aggregator.gradle.kts
+++ buildSrc/src/main/kotlin/kotest-publishing-aggregator.gradle.kts
@@ -2,12 +2,15 @@ plugins {
    id("com.gradleup.nmcp.aggregation")
 }
 
+val kotestSettings = extensions.getByType<KotestBuildLogicSettings>()
+
 nmcpAggregation {
    centralPortal {
       // New secrets need to be generated at https://central.sonatype.com/account w/ Generate User Token
       username.set(System.getenv("NEW_MAVEN_CENTRAL_USERNAME"))
       password.set(System.getenv("NEW_MAVEN_CENTRAL_PASSWORD"))
       publishingType = "USER_MANAGED"
+      publicationName = "Kotest - targets ${kotestSettings.enabledPublicationNamePrefixes.get()}"
    }
 }
 
