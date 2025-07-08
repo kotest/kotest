d01e4b08e4a3cf7461273abe0d43506db0c98332
Emil Kantis <emil.kantis@protonmail.com>
2025-07-08 18:26:41 +0200

    Fixing releases for wasmJs targets (#4931)
    wasmJs was missing as enabled prefix in all publication pipes. Adding it
    on the ubuntu runner.
    
    Also adding kotlin-js-conventions to kotlin-property module so it gets a
    wasmJs target defined

--- kotest-property/build.gradle.kts
+++ kotest-property/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
    id("kotest-android-native-conventions")
    id("kotest-watchos-device-conventions")
    id("kotest-native-conventions")
+   id("kotest-js-conventions")
    id("kotest-publishing-conventions")
 }
 
