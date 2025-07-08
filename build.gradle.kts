18c23171a68bc9c04e1ce53608bacf378373c799
Emil Kantis <emil.kantis@protonmail.com>
2025-07-08 15:54:23 +0200

    releasing kotest-property 6.0.0.M5 for js and wasm

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,54 +24,54 @@ apiValidation {
 
 // List all projects which should be included in publishing
 dependencies {
-   // Assertions
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsCore)
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsArrow)
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsArrowFxCoroutines)
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsArrowFxCoroutines)
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsCompiler)
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsJson)
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsKonform)
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsKotlinxDatetime)
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsKtor)
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsKtor)
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsShared)
-   nmcpAggregation(projects.kotestAssertions.kotestAssertionsYaml)
-
-   // bom
-   nmcpAggregation(projects.kotestBom)
-
-   nmcpAggregation(projects.kotestCommon)
-
-   // Extensions
-   nmcpAggregation(projects.kotestExtensions)
-   nmcpAggregation(projects.kotestExtensions.kotestExtensionsAllure)
-   nmcpAggregation(projects.kotestExtensions.kotestExtensionsBlockhound)
-   nmcpAggregation(projects.kotestExtensions.kotestExtensionsHtmlreporter)
-   nmcpAggregation(projects.kotestExtensions.kotestExtensionsJunit5)
-   nmcpAggregation(projects.kotestExtensions.kotestExtensionsJunitxml)
-   nmcpAggregation(projects.kotestExtensions.kotestExtensionsKoin)
-   nmcpAggregation(projects.kotestExtensions.kotestExtensionsMockserver)
-   nmcpAggregation(projects.kotestExtensions.kotestExtensionsNow)
-   nmcpAggregation(projects.kotestExtensions.kotestExtensionsSpring)
-   nmcpAggregation(projects.kotestExtensions.kotestExtensionsTestcontainers)
-   nmcpAggregation(projects.kotestExtensions.kotestExtensionsWiremock)
-
-   // Framework
-   nmcpAggregation(projects.kotestFramework.kotestFrameworkEngine)
-   nmcpAggregation(projects.kotestFramework.kotestFrameworkStandalone)
+//   // Assertions
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsCore)
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsArrow)
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsArrowFxCoroutines)
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsArrowFxCoroutines)
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsCompiler)
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsJson)
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsKonform)
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsKotlinxDatetime)
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsKtor)
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsKtor)
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsShared)
+//   nmcpAggregation(projects.kotestAssertions.kotestAssertionsYaml)
+//
+//   // bom
+//   nmcpAggregation(projects.kotestBom)
+//
+//   nmcpAggregation(projects.kotestCommon)
+//
+//   // Extensions
+//   nmcpAggregation(projects.kotestExtensions)
+//   nmcpAggregation(projects.kotestExtensions.kotestExtensionsAllure)
+//   nmcpAggregation(projects.kotestExtensions.kotestExtensionsBlockhound)
+//   nmcpAggregation(projects.kotestExtensions.kotestExtensionsHtmlreporter)
+//   nmcpAggregation(projects.kotestExtensions.kotestExtensionsJunit5)
+//   nmcpAggregation(projects.kotestExtensions.kotestExtensionsJunitxml)
+//   nmcpAggregation(projects.kotestExtensions.kotestExtensionsKoin)
+//   nmcpAggregation(projects.kotestExtensions.kotestExtensionsMockserver)
+//   nmcpAggregation(projects.kotestExtensions.kotestExtensionsNow)
+//   nmcpAggregation(projects.kotestExtensions.kotestExtensionsSpring)
+//   nmcpAggregation(projects.kotestExtensions.kotestExtensionsTestcontainers)
+//   nmcpAggregation(projects.kotestExtensions.kotestExtensionsWiremock)
+//
+//   // Framework
+//   nmcpAggregation(projects.kotestFramework.kotestFrameworkEngine)
+//   nmcpAggregation(projects.kotestFramework.kotestFrameworkStandalone)
 
    // Property
    nmcpAggregation(projects.kotestProperty)
-   nmcpAggregation(projects.kotestProperty.kotestPropertyArrow)
-   nmcpAggregation(projects.kotestProperty.kotestPropertyArrowOptics)
-   nmcpAggregation(projects.kotestProperty.kotestPropertyDatetime)
-   nmcpAggregation(projects.kotestProperty.kotestPropertyLifecycle)
-   nmcpAggregation(projects.kotestProperty.kotestPropertyPermutations)
-
-   // Runner
-   nmcpAggregation(projects.kotestRunner.kotestRunnerJunit5)
-   nmcpAggregation(projects.kotestRunner.kotestRunnerJunit4)
+//   nmcpAggregation(projects.kotestProperty.kotestPropertyArrow)
+//   nmcpAggregation(projects.kotestProperty.kotestPropertyArrowOptics)
+//   nmcpAggregation(projects.kotestProperty.kotestPropertyDatetime)
+//   nmcpAggregation(projects.kotestProperty.kotestPropertyLifecycle)
+//   nmcpAggregation(projects.kotestProperty.kotestPropertyPermutations)
+//
+//   // Runner
+//   nmcpAggregation(projects.kotestRunner.kotestRunnerJunit5)
+//   nmcpAggregation(projects.kotestRunner.kotestRunnerJunit4)
 }
 
 configureGradleDaemonJvm(
