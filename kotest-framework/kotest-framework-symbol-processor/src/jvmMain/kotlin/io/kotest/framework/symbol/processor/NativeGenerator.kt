8056a6ce0d91e68cf9827aba66d6400189f8ca42
Sam <sam@sksamuel.com>
2025-07-12 14:41:36 -0500

    Use kotlin poet for native generation (#4945)
    <!-- 
    If this PR updates documentation, please update all relevant versions of
    the docs, see:
    https://github.com/kotest/kotest/tree/master/documentation/versioned_docs
    The documentation at
    https://github.com/kotest/kotest/tree/master/documentation/docs is the
    documentation for the next minor or major version _TO BE RELEASED_
    -->

--- kotest-framework/kotest-framework-symbol-processor/src/jvmMain/kotlin/io/kotest/framework/symbol/processor/NativeGenerator.kt
+++ kotest-framework/kotest-framework-symbol-processor/src/jvmMain/kotlin/io/kotest/framework/symbol/processor/NativeGenerator.kt
@@ -4,8 +4,15 @@ import com.google.devtools.ksp.processing.Dependencies
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSFile
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.UNIT
 
 class NativeGenerator(private val environment: SymbolProcessorEnvironment) {
+
    fun generate(files: List<KSFile>, specs: List<KSClassDeclaration>) {
       val outputStream = environment.codeGenerator.createNewFile(
          dependencies = Dependencies(true, *files.toTypedArray()),
@@ -14,59 +21,68 @@ class NativeGenerator(private val environment: SymbolProcessorEnvironment) {
          extensionName = "kt"
       )
       outputStream.bufferedWriter().use { writer ->
-         writer.write(
-            buildString {
-               appendLine("""package io.kotest.framework.runtime.native""")
-               appendLine()
-               appendLine("""import io.kotest.core.descriptors.DescriptorPaths""")
-               appendLine("""import io.kotest.engine.extensions.ProvidedDescriptorFilter""")
-               appendLine("""import io.kotest.engine.TestEngineLauncher""")
-               appendLine("""import io.kotest.core.spec.SpecRef""")
-               appendLine("""import kotlinx.cinterop.ExperimentalForeignApi""")
-               appendLine("""import kotlinx.cinterop.toKString""")
-               appendLine("""import platform.posix.getenv""")
-
-               specs.forEach {
-                  appendLine("""import ${it.qualifiedName?.asString()}""")
-               }
-
-               appendLine(
-                  """
-// https://youtrack.jetbrains.com/issue/KT-63218/EagerInitialization-use-cases
-@Suppress("DEPRECATION", "unused")
-@OptIn(ExperimentalStdlibApi::class)
-@EagerInitialization
-val invoker = runKotest()
-
-@OptIn(ExperimentalForeignApi::class) // needed for getenv
-fun runKotest() {
-
-  val descriptorArg = getenv("kotest.framework.runtime.native.descriptor")?.toKString()
-  val listener = getenv("kotest.framework.runtime.native.listener")?.toKString()
-
-  val descriptor = descriptorArg?.let { DescriptorPaths.parse(it) }
-  val filter = descriptor?.let { ProvidedDescriptorFilter(descriptor) }
-
-  val launcher = TestEngineLauncher()
-   .withNative()
-   .addExtensions(listOfNotNull(filter))
-   .withSpecRefs("""
-               )
-
-               specs.forEach {
-                  appendLine("""       SpecRef.Function({ $it() }, $it::class),""")
-               }
-
-               appendLine(
-                  """   )
-   when (listener) {
-      "teamcity" -> launcher.withTeamCityListener().launch()
-      else -> launcher.launch()
-   }
-}"""
-               )
-            }
-         )
+         writer.write(createFileSpec(specs).toString())
       }
+   }
+
+   // https://youtrack.jetbrains.com/issue/KT-63218/EagerInitialization-use-cases
+
+   private fun createFileSpec(specs: List<KSClassDeclaration>): FileSpec {
+      val function = FunSpec.builder("runKotest")
+         .addModifiers(KModifier.PUBLIC)
+         .addAnnotation(ClassName("kotlinx.cinterop", "ExperimentalForeignApi"))
+         .addCode(
+            """
+val descriptorArg = getenv("kotest.framework.runtime.native.descriptor")?.toKString()
+val listenerType = getenv("kotest.framework.runtime.native.listener")?.toKString()
+
+val descriptor = descriptorArg?.let { DescriptorPaths.parse(it) }
+val filter = descriptor?.let { ProvidedDescriptorFilter(descriptor) }
+""".trim()
+         )
+         .addCode("\n")
+         .addCode(
+            """
+val launcher = TestEngineLauncher()
+ .withNative()
+ .addExtensions(listOfNotNull(filter))
+ .withSpecRefs(
+    """.trim()
+         ).addCode("\n")
+      specs.forEach {
+         function.addCode("""SpecRef.Function ({ `$it`() }, `$it`::class), """)
+         function.addCode("\n")
+      }
+      function
+         .addCode(""")""")
+         .addCode("\n")
+         .addCode(
+            """
+when (listenerType) {
+   "teamcity" -> launcher.withTeamCityListener().launch()
+   else -> launcher.launch()
+}
+""".trim()
+         ).addCode("\n")
+
+      val invoker = PropertySpec.builder("invoker", UNIT)
+         .addAnnotation(ClassName("kotlin.native", "EagerInitialization"))
+         .addAnnotation(ClassName("kotlin", "ExperimentalStdlibApi"))
+         .addAnnotation(ClassName("kotlinx.cinterop", "ExperimentalForeignApi"))
+         .initializer("""runKotest()""")
+
+      val file = FileSpec.builder("io.kotest.framework.runtime.native", "kotest.kt")
+         .addFunction(function.build())
+         .addProperty(invoker.build())
+         .addImport("io.kotest.core.descriptors", "DescriptorPaths")
+         .addImport("io.kotest.engine.extensions", "ProvidedDescriptorFilter")
+         .addImport("io.kotest.engine", "TestEngineLauncher")
+         .addImport("io.kotest.core.spec", "SpecRef")
+         .addImport("kotlinx.cinterop", "toKString")
+         .addImport("platform.posix", "getenv")
+      specs.forEach {
+         file.addImport(it.qualifiedName!!.asString().substringBeforeLast("."), it. simpleName.asString())
+      }
+      return file.build()
    }
 }
