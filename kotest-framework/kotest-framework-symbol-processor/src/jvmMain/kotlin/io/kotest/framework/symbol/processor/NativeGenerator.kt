3bc3ca26d8cb3856457457b17965dff8f406553c
sksamuel <sam@sksamuel.com>
2025-07-13 00:36:56 -0500

    api dump

--- kotest-framework/kotest-framework-symbol-processor/src/jvmMain/kotlin/io/kotest/framework/symbol/processor/NativeGenerator.kt
+++ kotest-framework/kotest-framework-symbol-processor/src/jvmMain/kotlin/io/kotest/framework/symbol/processor/NativeGenerator.kt
@@ -21,13 +21,13 @@ class NativeGenerator(private val environment: SymbolProcessorEnvironment) {
          extensionName = "kt"
       )
       outputStream.bufferedWriter().use { writer ->
-         writer.write(createFileSpec(specs).toString())
+         writer.write(createFileSpec(specs, configs).toString())
       }
    }
 
    // https://youtrack.jetbrains.com/issue/KT-63218/EagerInitialization-use-cases
 
-   private fun createFileSpec(specs: List<KSClassDeclaration>): FileSpec {
+   private fun createFileSpec(specs: List<KSClassDeclaration>, configs: MutableList<KSClassDeclaration>): FileSpec {
       val function = FunSpec.builder("runKotest")
          .addModifiers(KModifier.PUBLIC)
          .addAnnotation(ClassName("kotlinx.cinterop", "ExperimentalForeignApi"))
@@ -61,14 +61,14 @@ val launcher = TestEngineLauncher()
             .addCode(""".withProjectConfig(${configs.first().qualifiedName?.asString()}())""")
             .addCode("\n")
       }
-         .addCode(
-            """
+      function.addCode(
+         """
 when (listenerType) {
    "teamcity" -> launcher.withTeamCityListener().launch()
    else -> launcher.launch()
 }
 """.trim()
-         ).addCode("\n")
+      ).addCode("\n")
 
       val invoker = PropertySpec.builder("invoker", UNIT)
          .addAnnotation(ClassName("kotlin.native", "EagerInitialization"))
@@ -86,7 +86,7 @@ when (listenerType) {
          .addImport("kotlinx.cinterop", "toKString")
          .addImport("platform.posix", "getenv")
       specs.forEach {
-         file.addImport(it.qualifiedName!!.asString().substringBeforeLast("."), it. simpleName.asString())
+         file.addImport(it.qualifiedName!!.asString().substringBeforeLast("."), it.simpleName.asString())
       }
       return file.build()
    }
