8d0c18696c64603611e3dccd02193acaf82edd53
sksamuel <sam@sksamuel.com>
2025-07-25 00:15:18 -0500

    Remove junitxml

--- kotest-tests/kotest-tests-htmlreporter/src/jvmTest/kotlin/io/kotest/provided/ProjectConfig.kt
+++ kotest-tests/kotest-tests-htmlreporter/src/jvmTest/kotlin/io/kotest/provided/ProjectConfig.kt
@@ -4,17 +4,12 @@ import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.SpecExecutionOrder
 import io.kotest.extensions.htmlreporter.HtmlReporter
-import io.kotest.extensions.junitxml.JunitXmlReporter
 
 class ProjectConfig : AbstractProjectConfig() {
 
    override val specExecutionOrder = SpecExecutionOrder.Annotated
 
    override val extensions: List<Extension> = listOf(
-     JunitXmlReporter(
-       includeContainers = false,
-       useTestPathAsName = true
-     ),
      HtmlReporter()
    )
 }
