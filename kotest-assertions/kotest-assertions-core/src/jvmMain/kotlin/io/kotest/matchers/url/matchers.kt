c5069933572d15b4d489648fafd7938e69fb2768
sksamuel <sam@sksamuel.com>
2025-07-26 17:14:30 -0500

    Expanded tests for url matchers

--- kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/url/matchers.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmMain/kotlin/io/kotest/matchers/url/matchers.kt
@@ -12,9 +12,8 @@ fun beOpaque() = object : Matcher<URL> {
    override fun test(value: URL) = MatcherResult(
       value.toURI().isOpaque,
       { "URL $value should be opaque" },
-      {
-         "URL $value should not be opaque"
-      })
+      { "URL $value should not be opaque" }
+   )
 }
 
 infix fun URL.shouldHaveProtocol(protocol: String) = this should haveProtocol(protocol)
@@ -23,9 +22,8 @@ fun haveProtocol(protocol: String) = object : Matcher<URL> {
    override fun test(value: URL) = MatcherResult(
       value.protocol == protocol,
       { "URL $value should have protocol $protocol but was ${value.protocol}" },
-      {
-         "URL $value should not have protocol $protocol"
-      })
+      { "URL $value should not have protocol $protocol" }
+   )
 }
 
 infix fun URL.shouldHavePort(port: Int) = this should havePort(port)
@@ -34,9 +32,8 @@ fun havePort(port: Int) = object : Matcher<URL> {
    override fun test(value: URL) = MatcherResult(
       value.port == port,
       { "URL $value should have port $port but was ${value.port}" },
-      {
-         "URL $value should not have port $port"
-      })
+      { "URL $value should not have port $port" }
+   )
 }
 
 infix fun URL.shouldHaveHost(host: String) = this should haveHost(host)
@@ -45,9 +42,8 @@ fun haveHost(host: String) = object : Matcher<URL> {
    override fun test(value: URL) = MatcherResult(
       value.host == host,
       { "URL $value should have host $host but was ${value.host}" },
-      {
-         "URL $value should not have host $host"
-      })
+      { "URL $value should not have host $host" }
+   )
 }
 
 infix fun URL.shouldHaveQuery(q: String) = this should haveQuery(q)
@@ -56,9 +52,8 @@ fun haveQuery(q: String) = object : Matcher<URL> {
    override fun test(value: URL) = MatcherResult(
       value.query == q,
       { "URL $value should have query $q but was ${value.query}" },
-      {
-         "URL $value should not have query $q"
-      })
+      { "URL $value should not have query $q" }
+   )
 }
 
 infix fun URL.shouldHaveAuthority(authority: String) = this should haveAuthority(authority)
@@ -67,9 +62,8 @@ fun haveAuthority(authority: String) = object : Matcher<URL> {
    override fun test(value: URL) = MatcherResult(
       value.authority == authority,
       { "URL $value should have authority $authority but was ${value.authority}" },
-      {
-         "URL $value should not have authority $authority"
-      })
+      { "URL $value should not have authority $authority" }
+   )
 }
 
 infix fun URL.shouldHavePath(path: String) = this should havePath(path)
@@ -78,9 +72,8 @@ fun havePath(path: String) = object : Matcher<URL> {
    override fun test(value: URL) = MatcherResult(
       value.path == path,
       { "URL $value should have path $path but was ${value.path}" },
-      {
-         "URL $value should not have path $path"
-      })
+      { "URL $value should not have path $path" }
+   )
 }
 
 infix fun URL.shouldHaveParameter(key: String) = this should haveParameter(key)
@@ -89,20 +82,19 @@ fun haveParameter(key: String) = object : Matcher<URL> {
    override fun test(value: URL) = MatcherResult(
       value.query.split("&").any { it.split("=").first() == key },
       { "URL $value should have query parameter $key" },
-      {
-         "URL $value should not have query parameter $key"
-      })
+      { "URL $value should not have query parameter $key" }
+   )
 }
 
 fun URL.shouldHaveParameterValue(key: String, value: String) = this should haveParameterValue(key, value)
 fun URL.shouldNotHaveParameterValue(key: String, value: String) = this shouldNot haveParameterValue(key, value)
-fun haveParameterValue(key: String, value: String) = object : Matcher<URL> {
-   override fun test(url: URL) = MatcherResult(
-      url.query.split("&").find { it.split("=").first() == key } == "$key=$value",
-      { "URL $value should have query parameter $key=$value" },
-      {
-         "URL $value should not have query parameter $key=$value"
-      })
+
+fun haveParameterValue(key: String, v: String) = object : Matcher<URL> {
+   override fun test(value: URL) = MatcherResult(
+      value.query.split("&").find { it.split("=").first() == key } == "$key=$v",
+      { "URL $value should have query parameter $key=$v" },
+      { "URL $value should not have query parameter $key=$v" }
+   )
 }
 
 infix fun URL.shouldHaveRef(ref: String) = this should haveRef(ref)
@@ -111,7 +103,6 @@ fun haveRef(ref: String) = object : Matcher<URL> {
    override fun test(value: URL) = MatcherResult(
       value.ref == ref,
       { "URL $value should have ref $ref" },
-      {
-         "URL $value should not ref $ref"
-      })
+      { "URL $value should not ref $ref" }
+   )
 }
--- kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/url/UrlMatchersTest.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/url/UrlMatchersTest.kt
@@ -3,7 +3,6 @@ package com.sksamuel.kotest.matchers.url
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldNot
-import io.kotest.matchers.url.haveHost
 import io.kotest.matchers.url.haveParameter
 import io.kotest.matchers.url.haveParameterValue
 import io.kotest.matchers.url.havePath
@@ -11,83 +10,117 @@ import io.kotest.matchers.url.havePort
 import io.kotest.matchers.url.haveProtocol
 import io.kotest.matchers.url.haveRef
 import io.kotest.matchers.url.shouldBeOpaque
+import io.kotest.matchers.url.shouldHaveAuthority
+import io.kotest.matchers.url.shouldHaveHost
 import io.kotest.matchers.url.shouldHaveParameter
 import io.kotest.matchers.url.shouldHaveParameterValue
+import io.kotest.matchers.url.shouldHavePath
 import io.kotest.matchers.url.shouldHavePort
 import io.kotest.matchers.url.shouldHaveProtocol
+import io.kotest.matchers.url.shouldHaveQuery
 import io.kotest.matchers.url.shouldHaveRef
 import io.kotest.matchers.url.shouldNotBeOpaque
+import io.kotest.matchers.url.shouldNotHaveAuthority
+import io.kotest.matchers.url.shouldNotHaveHost
 import io.kotest.matchers.url.shouldNotHaveParameter
 import io.kotest.matchers.url.shouldNotHaveParameterValue
+import io.kotest.matchers.url.shouldNotHavePath
 import io.kotest.matchers.url.shouldNotHavePort
 import io.kotest.matchers.url.shouldNotHaveProtocol
+import io.kotest.matchers.url.shouldNotHaveQuery
+import io.kotest.matchers.url.shouldNotHaveRef
+import java.net.URI
 import java.net.URL
 
 class UrlMatchersTest : WordSpec() {
 
-  init {
+   init {
 
-    "beOpaque" should {
-      "test that a URL is opaque" {
-        URL("https:hostname:8080").shouldBeOpaque()
-        URL("https://path").shouldNotBeOpaque()
+      "beOpaque" should {
+         "test that a URL is opaque" {
+            URL("https:hostname:8080").shouldBeOpaque()
+            URL("https://path").shouldNotBeOpaque()
+         }
       }
-    }
 
-    "haveProtocol" should {
-      "test that a URL has the specified protocol" {
-        URL("https://hostname").shouldHaveProtocol("https")
-        URL("https://hostname") should haveProtocol("https")
-        URL("ftp://hostname").shouldNotHaveProtocol("https")
-        URL("ftp://hostname") shouldNot haveProtocol("https")
+      "haveProtocol" should {
+         "test that a URL has the specified protocol" {
+            URL("https://hostname").shouldHaveProtocol("https")
+            URL("https://hostname") should haveProtocol("https")
+            URL("ftp://hostname").shouldNotHaveProtocol("https")
+            URL("ftp://hostname") shouldNot haveProtocol("https")
+         }
       }
-    }
 
-    "havePort" should {
-      "test that a URL has the specified port" {
-        URL("https://hostname:90") should havePort(90)
-        URL("https://hostname:90").shouldHavePort(90)
-        URL("https://hostname") should havePort(-1)
-        URL("ftp://hostname:14") shouldNot havePort(80)
-        URL("ftp://hostname:14").shouldNotHavePort(80)
+      "havePort" should {
+         "test that a URL has the specified port" {
+            URL("https://hostname:90") should havePort(90)
+            URL("https://hostname:90").shouldHavePort(90)
+            URL("https://hostname") should havePort(-1)
+            URL("ftp://hostname:14") shouldNot havePort(80)
+            URL("ftp://hostname:14").shouldNotHavePort(80)
+         }
       }
-    }
 
-    "haveHost" should {
-      "test that a URL has the specified host" {
-        URL("https://hostname:90") should haveHost("hostname")
-        URL("https://wewqe") should haveHost("wewqe")
-        URL("ftp://hostname:14") shouldNot haveHost("qweqwe")
+      "haveQuery" should {
+         "test that a URL has a specified query" {
+            URL("https://hostname:90?f=t") shouldHaveQuery ("f=t")
+            URL("https://hostname:90?f=q") shouldNotHaveQuery ("f=t")
+         }
       }
-    }
 
-    "haveParameter" should {
-      "test that a URL has the specified host" {
-        URL("https://hostname:90?a=b&c=d") should haveParameter("a")
-        URL("https://hostname:90?a=b&c=d").shouldHaveParameter("a")
-        URL("https://hostname:90?a=b&c=d") should haveParameter("c")
-        URL("https://hostname:90?a=b&c=d") shouldNot haveParameter("b")
-        URL("https://hostname:90?a=b&c=d").shouldNotHaveParameter("b")
+      "haveHost" should {
+         "test that a URL has the specified host" {
+            URI.create("https://hostname:90").toURL() shouldHaveHost ("hostname")
+            URL("https://wewqe") shouldHaveHost ("wewqe")
+            URL("ftp://hostname:14") shouldNotHaveHost ("qweqwe")
+         }
       }
-      "support testing for the value" {
-        URL("https://hostname:90?key=value").shouldHaveParameterValue("key", "value")
-        URL("https://hostname:90?key=value") should haveParameterValue("key", "value")
-        URL("https://hostname:90?key=value").shouldNotHaveParameterValue("key", "wibble")
-      }
-    }
 
-    "havePath" should {
-      "test that a URL has the specified path" {
-        URL("https://hostname:90/index.html#qwerty") should havePath("/index.html")
+      "shouldHavePath" should {
+         "test that a URL has the specified path" {
+            URI.create("https://hostname:90/pathy").toURL() shouldHavePath ("/pathy")
+            URI.create("https://hostname:90/pathy2").toURL() shouldNotHavePath ("/pathy")
+         }
       }
-    }
 
-    "haveRef" should {
-      "test that a URL has the specified host" {
-        URL("https://hostname:90#qwerty") should haveRef("qwerty")
-        URL("https://hostname:90#qwerty").shouldHaveRef("qwerty")
-        URL("https://hostname:90#") should haveRef("")
+      "shouldHaveAuthority" should {
+         "test that a URL has the specified path" {
+            URI.create("https://hostname:90/pathy").toURL() shouldHaveAuthority ("hostname:90")
+            URI.create("https://hostname:91/pathy2").toURL() shouldNotHaveAuthority ("hostname:90")
+            URI.create("https://hostname:91/pathy2").toURL() shouldNotHaveAuthority ("h!stname:91")
+         }
       }
-    }
-  }
+
+      "haveParameter" should {
+         "test that a URL has the specified host" {
+            URL("https://hostname:90?a=b&c=d") should haveParameter("a")
+            URL("https://hostname:90?a=b&c=d").shouldHaveParameter("a")
+            URL("https://hostname:90?a=b&c=d") should haveParameter("c")
+            URL("https://hostname:90?a=b&c=d") shouldNot haveParameter("b")
+            URL("https://hostname:90?a=b&c=d").shouldNotHaveParameter("b")
+         }
+         "support testing for the value" {
+            URL("https://hostname:90?key=value").shouldHaveParameterValue("key", "value")
+            URL("https://hostname:90?key=value") should haveParameterValue("key", "value")
+            URL("https://hostname:90?key=value").shouldNotHaveParameterValue("key", "wibble")
+         }
+      }
+
+      "havePath" should {
+         "test that a URL has the specified path" {
+            URL("https://hostname:90/index.html#qwerty") should havePath("/index.html")
+         }
+      }
+
+      "haveRef" should {
+         "test that a URL has the specified host" {
+            URL("https://hostname:90#qwerty") should haveRef("qwerty")
+            URL("https://hostname:90#qwerty").shouldHaveRef("qwerty")
+            URL("https://hostname:90#") shouldHaveRef ("")
+            URL("https://hostname:90#") shouldNotHaveRef ("qwerty")
+            URL("https://hostname:90#qwerty") shouldNotHaveRef ("q")
+         }
+      }
+   }
 }
