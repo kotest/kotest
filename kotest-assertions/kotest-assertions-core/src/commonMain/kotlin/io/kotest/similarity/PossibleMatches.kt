5ae43db0bfb817cca26b405bb97cca62c31ada4b
sksamuel <sam@sksamuel.com>
2025-07-26 21:39:09 -0500

    Add nonjvm and remove jsHosted to simplify expect/actual implementations

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/similarity/PossibleMatches.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/similarity/PossibleMatches.kt
@@ -1,3 +1,13 @@
 package io.kotest.similarity
 
+import io.kotest.equals.Equality
+
 expect fun <T> possibleMatchesDescription(actual: Set<T>, expected: T): String
+
+expect fun <T> possibleMatchesForSet(
+   passed: Boolean,
+   expected: Set<T>,
+   actual: Set<T>,
+   verifier: Equality<T>?
+): String
+
--- kotest-assertions/kotest-assertions-core/src/wasmWasiMain/kotlin/io/kotest/similarity/PossibleMatches.kt
+++
@@ -1,3 +0,0 @@
-package io.kotest.similarity
-
-actual fun <T> possibleMatchesDescription(actual: Set<T>, expected: T): String = ""
