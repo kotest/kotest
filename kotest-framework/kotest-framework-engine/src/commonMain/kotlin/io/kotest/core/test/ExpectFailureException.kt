52e87ed5d78be52d757b1b3d83166fb6c6e664ed
sksamuel <sam@sksamuel.com>
2025-07-29 22:27:10 -0500

    Remove DisjointSet-related test classes and implementations
    This commit removes DisjointSet interface, its implementations, and associated test cases. The deleted code includes UnionFindImplTests, EmptyDisjointSetImplTests, and various property-based tests, indicating these functionalities are no longer needed or have been deprecated.

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/core/test/ExpectFailureException.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/core/test/ExpectFailureException.kt
@@ -1,10 +1,10 @@
 package io.kotest.core.test
 
-import io.kotest.engine.TestAbortedException
+import io.kotest.engine.IterationSkippedException
 
 // TestScope is added just to restrict where this function can be used.
 // It is not used in the function itself.
 @Suppress("UnusedReceiverParameter")
 fun TestScope.runIf(thunk: () -> Boolean) {
-   if (!thunk()) throw TestAbortedException()
+   if (!thunk()) throw IterationSkippedException()
 }
