d63aca532104386d36196c0e393cf913977becdd
Sam <sam@sksamuel.com>
2025-07-29 23:27:25 -0500

    Added IterationSkippedException to skip prop and non prop tests (#4976)
    Fixes #4883

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/core/test/ExpectFailureException.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/core/test/ExpectFailureException.kt
@@ -1,10 +1,10 @@
 package io.kotest.core.test
 
-/**
- * This exception is thrown when a test fails an expect block.
- */
-object ExpectFailureException : Exception()
+import io.kotest.engine.IterationSkippedException
 
+// TestScope is added just to restrict where this function can be used.
+// It is not used in the function itself.
+@Suppress("UnusedReceiverParameter")
 fun TestScope.runIf(thunk: () -> Boolean) {
-   if (!thunk()) throw ExpectFailureException
+   if (!thunk()) throw IterationSkippedException()
 }
