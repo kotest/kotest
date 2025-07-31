569fb2bfe1427d0d8879003f420ea3aff50c2793
Sam <sam@sksamuel.com>
2025-07-30 23:12:12 -0500

    Wire XML reports into JVM task (#4982)
    <!-- 
    If this PR updates documentation, please update all relevant versions of
    the docs, see:
    https://github.com/kotest/kotest/tree/master/documentation/versioned_docs
    The documentation at
    https://github.com/kotest/kotest/tree/master/documentation/docs is the
    documentation for the next minor or major version _TO BE RELEASED_
    -->

--- kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/io/kotest/assertions/print/PathPrintTest.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/io/kotest/assertions/print/PathPrintTest.kt
@@ -1,10 +1,13 @@
 package io.kotest.assertions.print
 
+import io.kotest.core.annotation.DisabledIf
+import io.kotest.core.annotation.WindowsCondition
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import java.nio.file.Path
 import java.nio.file.Paths
 
+@DisabledIf(WindowsCondition::class)
 class PathPrintTest : FunSpec({
    test("path print") {
       PathPrint.print(Paths.get("foo/bar.txt")) shouldBe Printed(value = "foo/bar.txt", type = Path::class)
---
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/reports/JUnitXmlWriter.kt
@@ -0,0 +1,92 @@
+package io.kotest.engine.reports
+
+import io.kotest.common.reflection.bestName
+import io.kotest.core.test.TestCase
+import io.kotest.engine.test.TestResult
+import nl.adaptivity.xmlutil.core.XmlVersion
+import nl.adaptivity.xmlutil.serialization.XML
+import kotlin.reflect.KClass
+import kotlin.time.Clock
+
+class JUnitXmlWriter(
+   private val clock: Clock,
+   private val includeStackTraces: Boolean,
+   private val hostname: String,
+) {
+
+   private val xml = XML {
+      indentString = "  "
+      xmlVersion = XmlVersion.XML10
+   }
+
+   private val target: String = "todo"
+
+   fun writeXml(spec: KClass<*>, tests: Map<TestCase, TestResult>): String {
+      val testsuite = generate(spec, tests)
+      return xml.encodeToString(TestSuite.serializer(), testsuite)
+   }
+
+   private fun generate(spec: KClass<*>, tests: Map<TestCase, TestResult>): TestSuite {
+      return TestSuite(
+         name = spec.bestName(),
+         tests = tests.size,
+         failures = tests.filter { it.value.isFailure }.count(),
+         errors = tests.filter { it.value.isError }.count(),
+         skipped = tests.filter { it.value.isIgnored }.count(),
+         timestamp = clock.now().toString().substringBeforeLast("."), // time without nanos
+         hostname = hostname,
+         time = tests.map { it.value.duration.inWholeMilliseconds / 1_000.0 }.sum(),
+         cases = tests.map { (test, result) ->
+            TestCaseElement(
+               classname = spec::class.bestName(),
+               name = "[${target}] ${test.descriptor.path().value}",
+               time = result.duration.inWholeMilliseconds / 1_000.0,
+               error = errorElementOrNull(result),
+               failure = failureElementOrNull(result),
+               skipped = skippedElementOrNull(result),
+            )
+         }
+      )
+   }
+
+   private fun failureElementOrNull(result: TestResult): FailureElement? {
+      return when (result) {
+         is TestResult.Failure -> FailureElement(
+            message = result.cause.message ?: "",
+            type = result.cause::class.simpleName ?: "",
+            stack = if (includeStackTraces) result.cause.stackTraceToString() else null,
+         )
+
+         else -> null
+      }
+   }
+
+   private fun errorElementOrNull(result: TestResult): ErrorElement? {
+      return when (result) {
+         is TestResult.Error -> ErrorElement(
+            message = result.cause.message ?: "",
+            type = result.cause::class.simpleName ?: "",
+            stack = if (includeStackTraces) result.cause.stackTraceToString() else null,
+         )
+
+         else -> null
+      }
+   }
+
+   private fun skippedElementOrNull(result: TestResult): SkippedElement? {
+      return when (result) {
+         is TestResult.Ignored -> SkippedElement(message = result.reason)
+         else -> null
+      }
+   }
+
+   /* ---------- platform hook ---------- */
+//   private fun writeXmlFile(xml: String, filename: String) {
+//      val path = Path(tempPath, filename)
+//      SystemFileSystem.createDirectories(tempPath)
+//      println(" >> Test report will be written to $path")
+//      val sink = SystemFileSystem.sink(path, append = false).buffered()
+//      sink.writeString(xml)
+//      sink.close()
+//   }
+}
