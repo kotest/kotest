1e1b49d77f66d89ccb38468b4a2b08f366577fb8
Peter Laggner <pmlaggner@gmail.com>
2025-08-13 17:35:59 +0200

    fix: documentation of any composer

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/compose/any.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/compose/any.kt
@@ -4,7 +4,7 @@ import io.kotest.matchers.Matcher
 import io.kotest.matchers.MatcherResult
 
 /**
- * Compose matchers. All matchers must be the same type `T`. If any matcher fails, composed matcher fails, too.
+ * Compose matchers. All matchers must be the same type `T`. If all matchers fail, composed matcher fails, too.
  */
 @Suppress("UNCHECKED_CAST")
 fun <T : Any?> Matcher.Companion.any(
