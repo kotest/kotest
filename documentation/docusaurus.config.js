2190a256ffecfe82a8d1b7acff2c9b97283b141d
sksamuel <sam@sksamuel.com>
2025-08-20 16:41:15 -0500

    Update Docusaurus version label to `6.1 🚧` in config

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -154,7 +154,7 @@ module.exports = {
             docs: {
                versions: {
                   current: {
-                     label: `6.1`,
+                     label: `6.1 🚧`,
                   },
                },
                sidebarPath: require.resolve('./sidebars.js'),
