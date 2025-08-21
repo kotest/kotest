12ad464a342df08661d5665b18abd6239c3a5e75
sksamuel <sam@sksamuel.com>
2025-08-21 11:29:38 -0500

    Simplify test configuration by removing `withSystemProperty` usage and updating Gradle tasks and test launcher setup.

--- kotest-tests/kotest-tests-config-classname/src/jvmTest/kotlin/com/sksamuel/kotest/config/classname/SystemPropertyConfigClassTest.kt
+++ kotest-tests/kotest-tests-config-classname/src/jvmTest/kotlin/com/sksamuel/kotest/config/classname/SystemPropertyConfigClassTest.kt
@@ -3,27 +3,20 @@ package com.sksamuel.kotest.config.classname
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.engine.TestEngineLauncher
-import io.kotest.engine.config.KotestEngineProperties
 import io.kotest.engine.listener.CollectingTestEngineListener
-import io.kotest.extensions.system.withSystemProperty
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.delay
 import kotlin.time.Duration.Companion.milliseconds
 
 class SystemPropertyConfigClassTest : FunSpec() {
    init {
-
       test("system property override should be used for config classname") {
-         withSystemProperty(
-            KotestEngineProperties.PROJECT_CONFIGURATION_FQN,
-            "com.sksamuel.kotest.config.classname.WibbleConfig"
-         ) {
-            val collector = CollectingTestEngineListener()
-            TestEngineLauncher(collector)
-               .withClasses(FooTest::class)
-               .launch()
-            collector.result("a")?.errorOrNull?.message shouldBe "Test 'a' did not complete within 1ms"
-         }
+         val collector = CollectingTestEngineListener()
+         TestEngineLauncher()
+            .withListener(collector)
+            .withClasses(FooTest::class)
+            .launch()
+         collector.result("a")?.errorOrNull?.message shouldBe "Test 'a' did not complete within 1ms"
       }
    }
 }
