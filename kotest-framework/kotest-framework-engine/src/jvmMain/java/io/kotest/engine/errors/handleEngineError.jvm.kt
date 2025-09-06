3b073a1872194f9bfb796d65b157e785fdf136bd
sksamuel <sam@sksamuel.com>
2025-09-06 05:37:11 -0500

    Throw `MultipleExceptions` for engine errors in JVM implementation.

--- kotest-framework/kotest-framework-engine/src/jvmMain/java/io/kotest/engine/errors/handleEngineError.jvm.kt
+++ kotest-framework/kotest-framework-engine/src/jvmMain/java/io/kotest/engine/errors/handleEngineError.jvm.kt
@@ -3,9 +3,13 @@
 package io.kotest.engine.errors
 
 import io.kotest.engine.EngineResult
+import io.kotest.engine.extensions.MultipleExceptions
 
 actual fun handleEngineResult(result: EngineResult) {
    if (result.testFailures) {
       error("Tests failed")
+   }
+   if (result.errors.isNotEmpty()) {
+      throw MultipleExceptions(result.errors)
    }
 }
