7c0ee6d46f258db65dab95b8e666affe121ef620
samsam <sam@sksamuel.com>
2025-09-09 15:05:16 -0500

    Fix errors in the jvm error handling

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/errors/handleEngineError.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/errors/handleEngineError.kt
@@ -2,4 +2,4 @@ package io.kotest.engine.errors
 
 import io.kotest.engine.EngineResult
 
-expect fun handleEngineResult(result: EngineResult)
+expect fun handleEngineResult(result: EngineResult): Unit
--- kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/errors/handleEngineError.jvm.kt
+++ kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/errors/handleEngineError.jvm.kt
@@ -4,10 +4,12 @@ package io.kotest.engine.errors
 
 import io.kotest.engine.EngineResult
 import io.kotest.engine.extensions.MultipleExceptions
+import kotlin.system.exitProcess
 
-actual fun handleEngineResult(result: EngineResult) {
+actual fun handleEngineResult(result: EngineResult): Unit {
    if (result.testFailures) {
-      error("Tests failed")
+      // the kotest task test will pick up return code as 1 as failed errors
+      exitProcess(1)
    }
    if (result.errors.isNotEmpty()) {
       throw MultipleExceptions(result.errors)
