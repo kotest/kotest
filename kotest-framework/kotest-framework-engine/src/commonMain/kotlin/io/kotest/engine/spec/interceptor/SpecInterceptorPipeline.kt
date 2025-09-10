76856a9e27c747fed76d3e4fde85ed1adf657923
sksamuel <sam@sksamuel.com>
2025-09-09 21:58:03 -0500

    Fixed duplicate application of @ApplyExtension
    Fixes #5056

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/spec/interceptor/SpecInterceptorPipeline.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/spec/interceptor/SpecInterceptorPipeline.kt
@@ -43,11 +43,7 @@ internal class SpecInterceptorPipeline(
       val interceptors = createPipeline(context)
       logger.log { Pair(spec::class.bestName(), "Executing ${interceptors.size} spec interceptors") }
       return interceptors.foldRight(initial) { ext, next ->
-         object : NextSpecInterceptor {
-            override suspend fun invoke(spec: Spec): Result<Map<TestCase, TestResult>> {
-               return ext.intercept(spec, next)
-            }
-         }
+         NextSpecInterceptor { spec -> ext.intercept(spec, next) }
       }.invoke(spec)
    }
 
