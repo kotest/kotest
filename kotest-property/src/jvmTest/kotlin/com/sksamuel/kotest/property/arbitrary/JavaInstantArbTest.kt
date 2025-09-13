acbe29f7a261de9e928ccd5c3647843d772c0c10
Phil Davies <224875+tKe@users.noreply.github.com>
2025-09-13 07:00:50 +0100

    feat: add Arb for kotlin.time.Instant (#5085)
    <!-- 
    If this PR updates documentation, please update all relevant versions of
    the docs, see:
    https://github.com/kotest/kotest/tree/master/documentation/versioned_docs
    The documentation at
    https://github.com/kotest/kotest/tree/master/documentation/docs is the
    documentation for the next minor or major version _TO BE RELEASED_
    -->
    
    This also deprecates and renames the current
    `io.kotest.property.arbitrary.instant` to
    `io.kotest.property.arbitrary.javaInstant` to avoid confusion.

--- kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/arbitrary/JavaInstantArbTest.kt
+++ kotest-property/src/jvmTest/kotlin/com/sksamuel/kotest/property/arbitrary/JavaInstantArbTest.kt
@@ -6,13 +6,13 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.RandomSource
-import io.kotest.property.arbitrary.instant
+import io.kotest.property.arbitrary.javaInstant
 
 @EnabledIf(LinuxOnlyGithubCondition::class)
-class InstantArbTest : FunSpec() {
+class JavaInstantArbTest : FunSpec() {
    init {
-      test("Arb.instant() should generate N valid Instants(no exceptions)") {
-         Arb.instant().generate(RandomSource.default()).take(8656).toList().size shouldBe 8656
+      test("Arb.javaInstant() should generate N valid Instants(no exceptions)") {
+         Arb.javaInstant().generate(RandomSource.default()).take(8656).toList().size shouldBe 8656
       }
    }
 }
