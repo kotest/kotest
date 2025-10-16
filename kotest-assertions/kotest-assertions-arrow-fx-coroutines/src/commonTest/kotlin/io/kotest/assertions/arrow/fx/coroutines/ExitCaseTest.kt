2a953d7c34c3aa00e3c3e60909ce26dae4df77e7
rafearnold <rafe.arnold@gmail.com>
2025-10-16 18:22:58 +0100

    test(arrow-fx): assert unhappy paths for ExitCase.

--- kotest-assertions/kotest-assertions-arrow-fx-coroutines/src/commonTest/kotlin/io/kotest/assertions/arrow/fx/coroutines/ExitCaseTest.kt
+++ kotest-assertions/kotest-assertions-arrow-fx-coroutines/src/commonTest/kotlin/io/kotest/assertions/arrow/fx/coroutines/ExitCaseTest.kt
@@ -1,6 +1,7 @@
 package io.kotest.assertions.arrow.fx.coroutines
 
 import arrow.fx.coroutines.ExitCase
+import io.kotest.assertions.throwables.shouldThrowWithMessage
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.map
@@ -11,6 +12,18 @@ import kotlinx.coroutines.CancellationException
 class ExitCaseTest : StringSpec({
   "shouldBeCancelled" {
     ExitCase.Cancelled(CancellationException("")).shouldBeCancelled()
+
+     shouldThrowWithMessage<AssertionError>("Expected ExitCase.Cancelled, but found ExitCase.Completed") {
+        ExitCase.Completed.shouldBeCancelled()
+     }
+
+     checkAll(Arb.string()) { s ->
+        shouldThrowWithMessage<AssertionError>(
+           "Expected ExitCase.Cancelled, but found Failure(failure=java.lang.RuntimeException: $s)",
+        ) {
+           ExitCase.Failure(RuntimeException(s)).shouldBeCancelled()
+        }
+     }
   }
 
   "shouldBeCancelled(e)" {
@@ -21,12 +34,38 @@ class ExitCaseTest : StringSpec({
 
   "shouldBeCompleted" {
     ExitCase.Completed.shouldBeCompleted()
+
+     checkAll(Arb.string()) { s ->
+        shouldThrowWithMessage<AssertionError>(
+           "Expected ExitCase.Completed, but found Cancelled(exception=java.util.concurrent.CancellationException: $s)",
+        ) {
+           ExitCase.Cancelled(CancellationException(s)).shouldBeCompleted()
+        }
+
+        shouldThrowWithMessage<AssertionError>(
+           "Expected ExitCase.Cancelled, but found Failure(failure=java.lang.RuntimeException: $s)",
+        ) {
+           ExitCase.Failure(RuntimeException(s)).shouldBeCancelled()
+        }
+     }
   }
 
   "shouldBeFailure" {
     checkAll(Arb.string().map(::RuntimeException)) { e ->
       ExitCase.Failure(e).shouldBeFailure()
     }
+
+     shouldThrowWithMessage<AssertionError>("Expected ExitCase.Failure, but found ExitCase.Completed") {
+        ExitCase.Completed.shouldBeFailure()
+     }
+
+     checkAll(Arb.string()) { s ->
+        shouldThrowWithMessage<AssertionError>(
+           "Expected ExitCase.Failure, but found Cancelled(exception=java.util.concurrent.CancellationException: $s)",
+        ) {
+           ExitCase.Cancelled(CancellationException(s)).shouldBeFailure()
+        }
+     }
   }
 
   "shouldBeFailure(e)" {
