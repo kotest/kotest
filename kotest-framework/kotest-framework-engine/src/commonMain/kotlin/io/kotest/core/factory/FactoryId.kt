0545d0ea125edf7cbced65f21913f57858813db3
sksamuel <sam@sksamuel.com>
2025-10-20 07:33:34 -0500

    api dump, private

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/core/factory/FactoryId.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/core/factory/FactoryId.kt
@@ -7,7 +7,7 @@ import kotlin.concurrent.atomics.incrementAndFetch
 data class FactoryId(val value: String) {
    @OptIn(ExperimentalAtomicApi::class)
    companion object {
-      val counter = AtomicLong(1)
+      private val counter = AtomicLong(1)
       // Kotlin uuids are broken in kotlin currently for some versions of node (they use a javascript method not available on all versions)
       // we don't actually care that id value is here, as long as its unique, so a counter will do just as well
       // we can switch to a uuid later if/when they come out of experimental mode
