9eb81b46157b599960b033c97b57c246b119b9e4
sksamuel <sam@sksamuel.com>
2025-10-20 17:46:49 -0500

    Enable commented-out platform-specific conventions in kotest-common build script

--- kotest-common/build.gradle.kts
+++ kotest-common/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
    id("kotest-jvm-conventions")
-//   id("kotest-js-wasm-conventions")
-//   id("kotest-native-conventions")
-//   id("kotest-android-native-conventions")
-//   id("kotest-watchos-device-conventions")
+   id("kotest-js-wasm-conventions")
+   id("kotest-native-conventions")
+   id("kotest-android-native-conventions")
+   id("kotest-watchos-device-conventions")
    id("kotest-publishing-conventions")
 }
 
