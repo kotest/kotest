182698dd8660a7140bd8dfa09c31e6aa5d0cbfed
sksamuel <sam@sksamuel.com>
2025-10-20 07:17:49 -0500

    Remove uuid usage to fix JS on certain version of node
    Fixes #5109

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/core/factory/FactoryId.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/core/factory/FactoryId.kt
@@ -1,11 +1,17 @@
 package io.kotest.core.factory
 
-import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
+import kotlin.concurrent.atomics.AtomicLong
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
+import kotlin.concurrent.atomics.incrementAndFetch
 
 data class FactoryId(val value: String) {
+   @OptIn(ExperimentalAtomicApi::class)
    companion object {
-      @OptIn(ExperimentalUuidApi::class)
-      fun next(): FactoryId = FactoryId(Uuid.random().toString())
+      val counter = AtomicLong(1)
+      // Kotlin uuids are broken in kotlin currently for some versions of node (they use a javascript method not available on all versions)
+      // we don't actually care that id value is here, as long as its unique, so a counter will do just as well
+      // we can switch to a uuid later if/when they come out of experimental mode
+      // see https://github.com/kotest/kotest/issues/5109
+      fun next(): FactoryId = FactoryId("factory-id-" + counter.incrementAndFetch().toString())
    }
 }
