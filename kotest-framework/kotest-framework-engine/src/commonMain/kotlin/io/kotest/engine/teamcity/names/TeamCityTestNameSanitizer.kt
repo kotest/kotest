b19dacf63635c45150c5d4f22adf35ebd7ca1133
Sam <sam@sksamuel.com>
2025-10-20 08:33:01 -0500

    Remove periods from test names only at TCSM stage (#5149)
    Fixes #5064
    
    <!-- 
    If this PR updates documentation, please update all relevant versions of
    the docs, see:
    https://github.com/kotest/kotest/tree/master/documentation/versioned_docs
    The documentation at
    https://github.com/kotest/kotest/tree/master/documentation/docs is the
    documentation for the next minor or major version _TO BE RELEASED_
    -->

---
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/teamcity/names/TeamCityTestNameSanitizer.kt
@@ -0,0 +1,15 @@
+package io.kotest.engine.teamcity.names
+
+import io.kotest.core.names.TestName
+
+/**
+ * Sanitizes test names to work around platform/framework limitations.
+ * Delegates to various specific workaround implementations.
+ */
+internal object TeamCityTestNameSanitizer {
+
+   fun sanitize(name: TestName, parent: TestName?): TestName {
+      val parentStripped = ParentNameStripper.strip(name, parent)
+      return name.copy(name = TeamCityTestNameEscaper.escape(parentStripped))
+   }
+}
--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/io/kotest/engine/test/names/TeamCityTestNameEscaperTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/io/kotest/engine/test/names/TeamCityTestNameEscaperTest.kt
@@ -1,6 +1,7 @@
 package io.kotest.engine.test.names
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.engine.teamcity.names.TeamCityTestNameEscaper
 import io.kotest.matchers.shouldBe
 
 class TeamCityTestNameEscaperTest : FunSpec() {
