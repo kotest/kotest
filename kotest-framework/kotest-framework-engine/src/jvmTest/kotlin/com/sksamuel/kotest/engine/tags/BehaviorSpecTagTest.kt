3e38472f73d0d424aed17f799bbf2067152f1451
sksamuel <sam@sksamuel.com>
2025-10-20 09:41:47 -0500

    Expand behavior spec tags to given and when
    Fixes #5127

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/tags/BehaviorSpecTagTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/tags/BehaviorSpecTagTest.kt
@@ -15,25 +15,100 @@ class BehaviorSpecTagTest : FunSpec() {
          withSystemProperty("kotest.tags", "!MyTagABC") {
             TestEngineLauncher()
                .withListener(listener)
-               .withClasses(ImportantTest::class)
+               .withClasses(ThenTagTest::class)
                .launch()
          }
          listener.tests.toList().first { it.first.name.name == "a" }.second.isSuccess shouldBe true
          listener.tests.toList().first { it.first.name.name == "b" }.second.isSuccess shouldBe true
          listener.tests.toList().first { it.first.name.name == "c" }.second.isIgnored shouldBe true
+         listener.tests.toList().first { it.first.name.name == "d" }.second.isSuccess shouldBe true
+      }
+
+      test("when including a tag, should work at the given level") {
+         val listener = CollectingTestEngineListener()
+         withSystemProperty("kotest.tags", "MyTagABC") {
+            TestEngineLauncher()
+               .withListener(listener)
+               .withClasses(GivenTagTest::class)
+               .launch()
+         }
+         listener.tests.toList().first { it.first.name.name == "a" }.second.isSuccess shouldBe true
+         listener.tests.toList().first { it.first.name.name == "b" }.second.isSuccess shouldBe true
+         listener.tests.toList().first { it.first.name.name == "c" }.second.isSuccess shouldBe true
+         listener.tests.toList().first { it.first.name.name == "d" }.second.isIgnored shouldBe true
+      }
+
+      test("when excluding a tag, should work at the given level") {
+         val listener = CollectingTestEngineListener()
+         withSystemProperty("kotest.tags", "!MyTagABC") {
+            TestEngineLauncher()
+               .withListener(listener)
+               .withClasses(GivenTagTest::class)
+               .launch()
+         }
+         listener.tests.toList().first { it.first.name.name == "a" }.second.isIgnored shouldBe true
+         listener.tests.toList().first { it.first.name.name == "d" }.second.isSuccess shouldBe true
+         listener.tests.toList().first { it.first.name.name == "e" }.second.isSuccess shouldBe true
+         listener.tests.toList().first { it.first.name.name == "f" }.second.isSuccess shouldBe true
+      }
+
+      test("when excluding a tag, should work at the when level") {
+         val listener = CollectingTestEngineListener()
+         withSystemProperty("kotest.tags", "!MyTagABC") {
+            TestEngineLauncher()
+               .withListener(listener)
+               .withClasses(WhenTagTest::class)
+               .launch()
+         }
+         listener.tests.toList().first { it.first.name.name == "a" }.second.isSuccess shouldBe true
+         listener.tests.toList().first { it.first.name.name == "b" }.second.isIgnored shouldBe true
+         listener.tests.toList().first { it.first.name.name == "c" }.second.isSuccess shouldBe true
+         listener.tests.toList().first { it.first.name.name == "d" }.second.isSuccess shouldBe true
       }
    }
 }
 
 object MyTagABC : Tag()
 
-class ImportantTest : BehaviorSpec({
+private class GivenTagTest : BehaviorSpec({
+
+   given("a").config(tags = setOf(MyTagABC)) {
+      `when`("b") {
+         then("c") {
+         }
+      }
+   }
+
+   given("d") {
+      `when`("e") {
+         then("f") {
+         }
+      }
+   }
+})
+
+private class WhenTagTest : BehaviorSpec({
+
+   given("a") {
+      `when`("b").config(tags = setOf(MyTagABC)) {
+      }
+      `when`("c") {
+         then("d") {
+         }
+      }
+   }
+})
+
+private class ThenTagTest : BehaviorSpec({
 
    given("a") {
       `when`("b") {
          then("c").config(tags = setOf(MyTagABC)) {
          }
+         then("d") {
+         }
       }
    }
 
 })
+
