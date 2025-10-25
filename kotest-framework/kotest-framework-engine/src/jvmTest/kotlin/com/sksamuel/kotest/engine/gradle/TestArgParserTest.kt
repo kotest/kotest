80f82511a59c32558d22dd99d14b83abb34f9568
sksamuel <sam@sksamuel.com>
2025-10-25 15:21:05 -0500

    Refactor `TestArgParser` to replace `kotest_intellij_plugin` with `kotest`, rename constants, and improve wildcard handling in test names and nested contexts.

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/gradle/TestArgParserTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/gradle/TestArgParserTest.kt
@@ -9,31 +9,36 @@ class TestArgParserTest : FunSpec() {
    init {
 
       test("support packages") {
-         TestArgParser.parse("\\Qkotest_intellij_plugin.io.kotest\\E") shouldBe TestArg.Package("io.kotest")
+         TestArgParser.parse("\\Qkotest.io.kotest\\E") shouldBe TestArg.Package("io.kotest")
       }
 
       test("support classes") {
-         TestArgParser.parse("\\Qkotest_intellij_plugin.io.kotest.SomeTest\\E") shouldBe TestArg.Class("io.kotest.SomeTest")
+         TestArgParser.parse("\\Qkotest.io.kotest.SomeTest\\E") shouldBe TestArg.Class("io.kotest.SomeTest")
       }
 
       test("support root tests") {
-         TestArgParser.parse("kotest_intellij_plugin.io.kotest.SomeTest.test\\E") shouldBe
+         TestArgParser.parse("kotest.io.kotest.SomeTest.test\\E") shouldBe
             TestArg.Test("io.kotest.SomeTest", listOf("test"))
       }
 
       test("support nested tests") {
-         TestArgParser.parse("\\Qkotest_intellij_plugin.io.kotest.SomeTest.test__context__nested\\E") shouldBe
+         TestArgParser.parse("\\Qkotest.io.kotest.SomeTest.test__context__nested\\E") shouldBe
             TestArg.Test("io.kotest.SomeTest", listOf("test", "nested"))
       }
 
       test("support spaces in test names") {
-         TestArgParser.parse("\\Qkotest_intellij_plugin.io.kotest.SomeTest.some method name\\E") shouldBe
+         TestArgParser.parse("\\Qkotest.io.kotest.SomeTest.some method name\\E") shouldBe
             TestArg.Test("io.kotest.SomeTest", listOf("some method name"))
       }
 
-      test("f:support regex in in test names") {
-         TestArgParser.parse("\\Qkotest_intellij_plugin.com.sksamuel.kotest.engine.gradle.TestArgParserTest.some method name \\E.*\\Q with a period\\E") shouldBe
-            TestArg.Test("com.sksamuel.kotest.engine.gradle.TestArgParserTest", listOf("some method name . with a period"))
+      test("support wildcards in test names") {
+         TestArgParser.parse("\\Qkotest.com.sksamuel.kotest.engine.gradle.TestArgParserTest.some method name \\E.*\\Q with a wildcard\\E") shouldBe
+            TestArg.Test("com.sksamuel.kotest.engine.gradle.TestArgParserTest", listOf("some method name * with a wildcard"))
+      }
+
+      test("support wildcards in nested test names") {
+         TestArgParser.parse("\\Qkotest.com.sksamuel.kotest.engine.gradle.TestArgParserTest.some context__context__some method name \\E.*\\Q with a wildcard\\E") shouldBe
+            TestArg.Test("com.sksamuel.kotest.engine.gradle.TestArgParserTest", listOf("some context", "some method name * with a wildcard"))
       }
    }
 }
