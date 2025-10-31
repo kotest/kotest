58297333e3471777967d74fda68361e96b6c2a79
alfonsoristorato <alfonsoristorato@gmail.com>
2025-10-31 08:58:09 +0000

    allow to verify if json schema prop is null

--- kotest-assertions/kotest-assertions-json/src/commonMain/kotlin/io/kotest/assertions/json/schema/matchSchema.kt
+++ kotest-assertions/kotest-assertions-json/src/commonMain/kotlin/io/kotest/assertions/json/schema/matchSchema.kt
@@ -146,7 +146,12 @@ private fun validate(
          } else violation("Expected ${expected.typeName()}, but was object")
       }
 
-      is JsonNode.NullNode -> TODO("Check how Json schema handles null")
+      is JsonNode.NullNode -> {
+         if (!isCompatible(tree, expected))
+            violation("Expected ${expected.typeName()}, but was ${tree.type()}")
+         else
+            emptyList()
+      }
 
       is JsonNode.NumberNode ->
          when (expected) {
@@ -183,4 +188,5 @@ private class SchemaViolation(
 private fun isCompatible(actual: JsonNode, schema: JsonSchemaElement) =
    (actual is JsonNode.BooleanNode && schema is JsonSchema.JsonBoolean) ||
       (actual is JsonNode.StringNode && schema is JsonSchema.JsonString) ||
-      (actual is JsonNode.NumberNode && schema is JsonSchema.JsonNumber)
+      (actual is JsonNode.NumberNode && schema is JsonSchema.JsonNumber) ||
+      (actual is JsonNode.NullNode && schema is JsonSchema.Null)
