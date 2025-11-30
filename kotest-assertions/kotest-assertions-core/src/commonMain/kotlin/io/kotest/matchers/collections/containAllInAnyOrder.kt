b87972752604d1ad508ef463b93a358d4aee56b6
sksamuel <sam@sksamuel.com>
2025-11-30 01:45:01 -0600

    Add `shouldContainAllInAnyOrder` matchers for collections

---
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/containAllInAnyOrder.kt
@@ -0,0 +1,37 @@
+@file:Suppress("unused")
+
+package io.kotest.matchers.collections
+
+import io.kotest.matchers.Matcher
+import io.kotest.matchers.MatcherResult
+import io.kotest.matchers.neverNullMatcher
+import io.kotest.matchers.sequences.UnorderedCollectionsDifference
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldNot
+
+infix fun <T, C : Collection<T>> C?.shouldNotContainAllInAnyOrder(expected: C) =
+   this shouldNot containAllInAnyOrder(expected)
+
+fun <T, C : Collection<T>> C?.shouldNotContainAllInAnyOrder(vararg expected: T) =
+   this shouldNot containAllInAnyOrder(*expected)
+
+infix fun <T, C : Collection<T>> C?.shouldContainAllInAnyOrder(expected: C) =
+   this should containAllInAnyOrder(expected)
+
+fun <T, C : Collection<T>> C?.shouldContainAllInAnyOrder(vararg expected: T) =
+   this should containAllInAnyOrder(*expected)
+
+fun <T> containAllInAnyOrder(vararg expected: T): Matcher<Collection<T>?> =
+   containAllInAnyOrder(expected.asList())
+
+/** Assert that a collection contains all the given values and nothing else, in any order. */
+fun <T, C : Collection<T>> containAllInAnyOrder(expected: C): Matcher<C?> = neverNullMatcher { value ->
+   val valueAsList = value.toList()
+   val expectedAsList = expected.toList()
+   val comparison = UnorderedCollectionsDifference.of(expectedAsList, valueAsList)
+   MatcherResult(
+      comparison.isMatch(),
+      { "Collection should contain the values of $expectedAsList in any order, but was $valueAsList.${comparison}" },
+      { "Collection should not contain the values of $expectedAsList in any order" }
+   )
+}
