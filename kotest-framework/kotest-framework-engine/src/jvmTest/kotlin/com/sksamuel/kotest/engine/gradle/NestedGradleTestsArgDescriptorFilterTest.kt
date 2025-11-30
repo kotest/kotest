d26425055cc273f9dfbcbfce723b0a20f81aeb4e
sksamuel <sam@sksamuel.com>
2025-11-30 02:43:47 -0600

    tests

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/gradle/NestedGradleTestsArgDescriptorFilterTest.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/gradle/NestedGradleTestsArgDescriptorFilterTest.kt
@@ -14,38 +14,32 @@ class NestedGradleTestsArgDescriptorFilterTest : FunSpec({
 
    test("include packages") {
       val spec = NestedGradleTestsArgDescriptorFilterTest::class.toDescriptor()
-      val args1 = NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.gradle\\E")!!
+      val args1 = NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.gradle.NestedGradleTestsArgDescriptorFilterTest.a__--__b\\E")!!
       NestedGradleTestsArgDescriptorFilter(setOf(args1)).filter(spec) shouldBe DescriptorFilterResult.Include
 
-      val args2 = NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.xxxxx\\E")!!
+      val args2 = NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.xxxxxx.NestedGradleTestsArgDescriptorFilterTest.a__--__b\\E")!!
       NestedGradleTestsArgDescriptorFilter(setOf(args2)).filter(spec) shouldBe DescriptorFilterResult.Exclude(null)
    }
 
    test("include classes") {
       val spec = NestedGradleTestsArgDescriptorFilterTest::class.toDescriptor()
-      val args1 =
-         NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.gradle.IntellijGradleTestsArgDescriptorFilterTest\\E")!!
+      val args1 = NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.gradle.NestedGradleTestsArgDescriptorFilterTest.a__--__b\\E")!!
       NestedGradleTestsArgDescriptorFilter(setOf(args1)).filter(spec) shouldBe DescriptorFilterResult.Include
 
-      val args2 =
-         NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.gradle.XxxGradleTestsArgDescriptorFilterTest\\E")!!
+      val args2 = NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.xxxxxx.Y.a__--__b\\E")!!
       NestedGradleTestsArgDescriptorFilter(setOf(args2)).filter(spec) shouldBe DescriptorFilterResult.Exclude(null)
    }
 
-   test("includes with test paths") {
+   test("include matching nested tests") {
       val spec = NestedGradleTestsArgDescriptorFilterTest::class.toDescriptor()
-      val test1 = spec.append("foo")
-      val args1 =
-         NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.gradle.IntellijGradleTestsArgDescriptorFilterTest.foo\\E")!!
+      val test1 = spec.append("a")
+      val test2 = test1.append("b")
+      val test3 = spec.append("c")
+      val test4 = test1.append("d")
+      val args1 = NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.gradle.NestedGradleTestsArgDescriptorFilterTest.a__--__b\\E")!!
       NestedGradleTestsArgDescriptorFilter(setOf(args1)).filter(test1) shouldBe DescriptorFilterResult.Include
-
-      val test2 = test1.append("bar")
-      val args2 =
-         NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.gradle.IntellijGradleTestsArgDescriptorFilterTest.foo__context__bar\\E")!!
-      NestedGradleTestsArgDescriptorFilter(setOf(args2)).filter(test2) shouldBe DescriptorFilterResult.Include
-
-      val args3 =
-         NestedGradleTestsArgParser.parse("\\Qcom.sksamuel.kotest.engine.gradle.IntellijGradleTestsArgDescriptorFilterTest.bar\\E")!!
-      NestedGradleTestsArgDescriptorFilter(setOf(args3)).filter(test1) shouldBe DescriptorFilterResult.Exclude(null)
+      NestedGradleTestsArgDescriptorFilter(setOf(args1)).filter(test2) shouldBe DescriptorFilterResult.Include
+      NestedGradleTestsArgDescriptorFilter(setOf(args1)).filter(test3) shouldBe DescriptorFilterResult.Exclude(null)
+      NestedGradleTestsArgDescriptorFilter(setOf(args1)).filter(test4) shouldBe DescriptorFilterResult.Exclude(null)
    }
 })
