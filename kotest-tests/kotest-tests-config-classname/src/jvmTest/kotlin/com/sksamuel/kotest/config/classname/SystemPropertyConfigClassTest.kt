be87fc0505ee2dc6370cc6bc1bc2bf90c0c3bd2c
sksamuel <sam@sksamuel.com>
2025-11-30 05:11:31 -0600

    Update timeout properties and test logic in `SystemPropertyConfigClassTest` for consistency with configuration changes.

--- kotest-tests/kotest-tests-config-classname/src/jvmTest/kotlin/com/sksamuel/kotest/config/classname/SystemPropertyConfigClassTest.kt
+++ kotest-tests/kotest-tests-config-classname/src/jvmTest/kotlin/com/sksamuel/kotest/config/classname/SystemPropertyConfigClassTest.kt
@@ -7,9 +7,11 @@ import io.kotest.engine.listener.CollectingTestEngineListener
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.delay
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 class SystemPropertyConfigClassTest : FunSpec() {
    init {
+      // if the system property isn't picked up then this test will not time out because it won't have the lower timeout setting applied
       test("system property override should be used for config classname") {
          val collector = CollectingTestEngineListener()
          TestEngineLauncher()
@@ -22,11 +24,11 @@ class SystemPropertyConfigClassTest : FunSpec() {
 }
 
 class WibbleConfig : AbstractProjectConfig() {
-   override val invocationTimeout = 15.milliseconds
+   override val timeout = 15.milliseconds
 }
 
 private class FooTest : FunSpec({
    test("a") {
-      delay(10000000)
+      delay(5.seconds)
    }
 })
