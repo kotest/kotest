a23d8f3047f04ed2279ce35f0877c9b7bb85f6ae
Sam <sam@sksamuel.com>
2025-12-23 17:51:00 -0600

    Add IncludeTestPatternDescriptorFilter to allow filtering via env var (#5280)
    Also support wasi wasm env vars

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/extensions/DescriptorFilter.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/extensions/DescriptorFilter.kt
@@ -4,13 +4,15 @@ import io.kotest.core.descriptors.Descriptor
 import io.kotest.core.extensions.Extension
 
 /**
- * A [DescriptorFilter] can be used to filter specs and tests before they are executed.
+ * A [DescriptorFilter] is an [Extension] that is used to filter specs and tests before they are executed.
  *
- * A given [Descriptor] must be included by all filters for it to be considered enabled at runtime,
- * or in other words, if any filter returns [DescriptorFilterResult.Exclude], then the spec or test
- * will not be executed.
+ * A given [Descriptor] must be included by ALL filters for it to be considered enabled at runtime,
+ * or in other words, if any filter returns [DescriptorFilterResult.Exclude], then matching specs and tests
+ * will not be executed, even if other filters return [DescriptorFilterResult.Include].
  *
  * If no filters are registered, then all specs and tests will be executed.
+ *
+ * See [IncludeTestPatternDescriptorFilter].
  */
 interface DescriptorFilter : Extension {
 
