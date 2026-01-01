454ab9c63ca4b3426c9001c98822e33920575170
sksamuel <sam@sksamuel.com>
2026-01-01 06:25:31 -0600

    feedback

--- kotest-tests/kotest-tests-js/build.gradle.kts
+++ kotest-tests/kotest-tests-js/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
    id("kotest-js-conventions")
    id("com.google.devtools.ksp") version "2.2.21-2.0.4"
    // must be a published version and not one in the current build
-   id("io.kotest").version("6.1.0-LOCAL")
+   // this module is commented out from settings.gradle.kts because it needs a published plugin to work
+   // so once 6.1 is out I will come back and uncomment this module and get the JS tests working.
+   id("io.kotest").version("TODO")
 }
 
 kotlin {
