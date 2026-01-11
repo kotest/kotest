86d96d38555865d890b714ba1896928bdf53bfed
alfonsoristorato <alfonsoristorato@gmail.com>
2026-01-11 12:03:06 +0000

    fix format list error test

--- kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/MatchersTest.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/MatchersTest.kt
@@ -147,7 +147,7 @@ class MatchersTest : FreeSpec({
             l.add(l)
             l shouldBe emptyList()
          }.message shouldBe """Unexpected elements from index 0
-                                                              |expected:<[]> but was:<[[(this ArrayList)]]>""".trimMargin()
+                                                              |expected:<[]> but was:<[(this ArrayList)]>""".trimMargin()
       }
    }
 
