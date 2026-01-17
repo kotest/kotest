c6a174850db6b964c333e0a2a497f1ec0630a8f2
AlexKuznetsov <alexander.kuznetsov@target.com>
2026-01-16 20:02:39 -0600

    change-signature

--- kotest-assertions/kotest-assertions-json/src/commonMain/kotlin/io/kotest/assertions/json/comparisons/CompareBooleans.kt
+++ kotest-assertions/kotest-assertions-json/src/commonMain/kotlin/io/kotest/assertions/json/comparisons/CompareBooleans.kt
@@ -2,9 +2,9 @@ package io.kotest.assertions.json.comparisons
 
 import io.kotest.assertions.json.JsonError
 
-internal fun compareBooleans(path: List<String>, expected: Boolean, actual: Boolean): JsonError? {
+internal fun compareBooleans(path: List<String>, expected: Boolean, actual: Boolean): List<JsonError> {
    return when (expected) {
-      actual -> null
-      else -> JsonError.UnequalBooleans(path, expected, actual)
+      actual -> emptyList()
+      else -> listOf(JsonError.UnequalBooleans(path, expected, actual))
    }
 }
