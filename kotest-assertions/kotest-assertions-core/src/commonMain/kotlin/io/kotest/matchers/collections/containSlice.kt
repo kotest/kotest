34e1a788c5eb5ef2dd22681280ef42d73d2950cf
AlexKuznetsov <alexander.kuznetsov@target.com>
2026-01-20 12:45:34 -0600

    ShouldContainSliceTest

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/containSlice.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/containSlice.kt
@@ -1,13 +1,49 @@
 package io.kotest.matchers.collections
 
-//fun<T> sliceStart(list: List<T>, listOffset: Int, slice: List<T>, sliceOffset: Int): Int? {
-//   val sliceLength = slice.size - sliceOffset
-//   if((list.size - listOffset) < sliceLength) return null
-//   (listOffset until list.size - sliceLength + 1).forEach { index ->
-//      if ()
-//   }
-//   return null
-//}
+import io.kotest.assertions.print.print
+import io.kotest.matchers.Matcher
+import io.kotest.matchers.MatcherResult
+import io.kotest.matchers.should
+import kotlin.jvm.JvmName
+
+/**
+ * Assert that a collection contains the given slice.
+ * For example,
+ *  listOf(1, 1, 2) shouldContainSlice listOf(1, 2)    // Assertion passes
+ *  listOf(1, 2, 1) shouldContainSlice listOf(1)       // Assertion passes
+ *  listOf(1) shouldContainSlice listOf(1, 2)          // Assertion fails
+ *
+ *  Note: Comparison is via the standard Java equals and hash code methods.
+ */
+@JvmName("shouldContainSlice_iterable")
+infix fun <T> Iterable<T>.shouldContainSlice(expected: Iterable<T>) =
+   this.toList() should containSlice(expected.toList())
+
+fun <T> containSlice(slice: List<T>) = object : Matcher<List<T>> {
+   override fun test(value: List<T>): MatcherResult {
+      val contains = sliceStart(value, slice.toList()) != null
+      val partialMatchesDescription = { describePartialMatchesInCollection(slice, value) }
+//      val elementsToSearchForSimilar = partialMatchesDescription
+      return MatcherResult(
+         contains,
+         { "List should contain slice ${slice.print().value} but was ${value.print().value}\n${partialMatchesDescription()}" },
+         { "List should not contain slice ${slice.print().value}" }
+      )
+   }
+}
+
+internal fun<T> describePartialMatchesAndSimilarityInCollection(expectedSlice: Collection<T>, value: List<T>): SliceMatchDescription {
+   val partialMatchesInCollectionDescription = describePartialMatchesInCollection(expectedSlice, value)
+   return SliceMatchDescription(
+      partialMatchesInCollectionDescription,
+      similarElementsDescription = "",
+   )
+}
+
+internal data class SliceMatchDescription(
+   val partialMatchesInCollectionDescription: PartialMatchesInCollectionDescription,
+   val similarElementsDescription: String,
+)
 
 internal fun<T> sliceStart(list: List<T>, slice: List<T>): Int? {
    if(list.size  < slice.size) return null
---
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/collections/ShouldContainSliceTest.kt
@@ -0,0 +1,34 @@
+package com.sksamuel.kotest.matchers.collections
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContainSlice
+import io.kotest.matchers.string.shouldContainInOrder
+
+class ShouldContainSliceTest : StringSpec() {
+   init {
+      "pass when slice is at the beginning" {
+         listOf(1, 2, 3, 4, 5).shouldContainSlice(listOf(1, 2, 3))
+      }
+      "pass when slice is in the middle" {
+         listOf(1, 2, 3, 4, 5).shouldContainSlice(listOf(2, 3, 4))
+      }
+      "pass when slice is at the end" {
+         listOf(1, 2, 3, 4, 5).shouldContainSlice(listOf(3, 4, 5))
+      }
+      "fail when slice not found" {
+         val thrown = shouldThrow<AssertionError> {
+            listOf(1, 2, 3, 4, 5).shouldContainSlice(listOf(1, 2, 4, 5))
+         }
+         thrown.message.shouldContainInOrder(
+            "List should contain slice [1, 2, 4, 5] but was [1, 2, 3, 4, 5]",
+            "Slice[0] of expected with indexes: 0..1 matched a slice of actual values with indexes: 0..1",
+            "Slice[1] of expected with indexes: 2..3 matched a slice of actual values with indexes: 3..4",
+            "[0] 1 => slice 0",
+            "[1] 2 => slice 0",
+            "[3] 4 => slice 1",
+            "[4] 5 => slice 1",
+         )
+      }
+   }
+}
