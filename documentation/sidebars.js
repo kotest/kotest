a8c9fc030e581106b2f8e8526cd270f154c5c1ea
alfonsoristorato <alfonsoristorato@gmail.com>
2026-01-20 09:52:35 +0000

    re-add koin and fix current docs version

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -35,6 +35,7 @@ module.exports = {
      "extensions/decoroutinator",
      "extensions/instant",
      "extensions/junit_xml",
+     "extensions/koin",
      "extensions/ktor",
      "extensions/html_reporter",
      "extensions/mockserver",
