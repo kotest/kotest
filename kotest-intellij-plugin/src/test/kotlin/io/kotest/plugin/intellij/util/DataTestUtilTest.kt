00d59f43da7c86934f8bee7d9213d5a17e10e635
alfonsoristorato <alfonsoristorato@gmail.com>
2026-01-21 19:25:19 +0000

    add a behavior spec example and add more lines to run all tests with the same structure

--- kotest-intellij-plugin/src/test/kotlin/io/kotest/plugin/intellij/util/DataTestUtilTest.kt
+++ kotest-intellij-plugin/src/test/kotlin/io/kotest/plugin/intellij/util/DataTestUtilTest.kt
@@ -20,7 +20,8 @@ class DataTestUtilTest : LightJavaCodeInsightFixtureTestCase() {
 
       val testFiles = listOf(
          "/data-test-tags/DataTestTagsFunSpec.kt",
-         "/data-test-tags/DataTestTagsFreeSpec.kt"
+         "/data-test-tags/DataTestTagsFreeSpec.kt",
+         "/data-test-tags/DataTestTagsBehaviorSpec.kt"
       )
 
       testFiles.forEach { testFile ->
@@ -33,124 +34,124 @@ class DataTestUtilTest : LightJavaCodeInsightFixtureTestCase() {
 
          allDataTestCalls.size shouldBe 16
 
-         // Data test inside "child context" at line 13 (withData - firstChildOfChildContext)
-         val firstChildOfChildContext = allDataTestCalls.find { getLineNumber(it) == 13 }.shouldNotBeNull()
+         // Data test inside "child context" at line 16 (withData - firstChildOfChildContext)
+         val firstChildOfChildContext = allDataTestCalls.find { getLineNumber(it) == 16 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(isDataTest = true, firstChildOfChildContext).apply {
             this.shouldNotBeNull()
-            tag shouldBe "(kotest.data.13) | !kotest.data"
+            tag shouldBe "(kotest.data.16) | !kotest.data"
             ancestorTestPath shouldBe "parent context -- child context"
          }
 
-         // Data test inside firstChildOfChildContext at line 14 (withTests - firstChildOfFirstChildOfChildContext)
-         // Exclude sibling at line 17
-         val firstChildOfFirstChildOfChildContext = allDataTestCalls.find { getLineNumber(it) == 14 }.shouldNotBeNull()
+         // Data test inside firstChildOfChildContext at line 17 (withTests - firstChildOfFirstChildOfChildContext)
+         // Exclude sibling at line 20
+         val firstChildOfFirstChildOfChildContext = allDataTestCalls.find { getLineNumber(it) == 17 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(isDataTest = true, firstChildOfFirstChildOfChildContext).apply {
             this.shouldNotBeNull()
-            tag shouldBe "(kotest.data.13 & !kotest.data.17) | !kotest.data"
+            tag shouldBe "(kotest.data.16 & !kotest.data.20) | !kotest.data"
             ancestorTestPath shouldBe "parent context -- child context"
          }
 
-         // Data test inside firstChildOfChildContext at line 17 (withData - secondChildOfFirstChildOfChildContext)
-         // Exclude sibling at line 14
-         val secondChildOfFirstChildOfChildContext = allDataTestCalls.find { getLineNumber(it) == 17 }.shouldNotBeNull()
+         // Data test inside firstChildOfChildContext at line 20 (withData - secondChildOfFirstChildOfChildContext)
+         // Exclude sibling at line 17
+         val secondChildOfFirstChildOfChildContext = allDataTestCalls.find { getLineNumber(it) == 20 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(isDataTest = true, secondChildOfFirstChildOfChildContext).apply {
             this.shouldNotBeNull()
-            tag shouldBe "(kotest.data.13 & !kotest.data.14) | !kotest.data"
+            tag shouldBe "(kotest.data.16 & !kotest.data.17) | !kotest.data"
             ancestorTestPath shouldBe "parent context -- child context"
          }
 
-         // Data test inside secondChildOfFirstChildOfChildContext at line 18 (withTests - firstChildOfsecondChildOfFirstChildOfChildContext)
+         // Data test inside secondChildOfFirstChildOfChildContext at line 21 (withTests - firstChildOfsecondChildOfFirstChildOfChildContext)
          // Same as parent (no siblings)
          val firstChildOfsecondChildOfFirstChildOfChildContext =
-            allDataTestCalls.find { getLineNumber(it) == 18 }.shouldNotBeNull()
+            allDataTestCalls.find { getLineNumber(it) == 21 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(isDataTest = true, firstChildOfsecondChildOfFirstChildOfChildContext).apply {
             this.shouldNotBeNull()
-            tag shouldBe "(kotest.data.13 & !kotest.data.14) | !kotest.data"
+            tag shouldBe "(kotest.data.16 & !kotest.data.17) | !kotest.data"
             ancestorTestPath shouldBe "parent context -- child context"
          }
 
-         // Data test inside "child context" at line 23 (withTests - secondChildOfChildContext)
-         val secondChildOfChildContext = allDataTestCalls.find { getLineNumber(it) == 23 }.shouldNotBeNull()
+         // Data test inside "child context" at line 26 (withTests - secondChildOfChildContext)
+         val secondChildOfChildContext = allDataTestCalls.find { getLineNumber(it) == 26 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(isDataTest = true, secondChildOfChildContext).apply {
             this.shouldNotBeNull()
-            tag shouldBe "(kotest.data.23) | !kotest.data"
+            tag shouldBe "(kotest.data.26) | !kotest.data"
             ancestorTestPath shouldBe "parent context -- child context"
          }
 
-         // Data test inside "parent context" at line 27 (withContexts - firstChildOfParentContext)
-         val firstChildOfParentContext = allDataTestCalls.find { getLineNumber(it) == 27 }.shouldNotBeNull()
+         // Data test inside "parent context" at line 30 (withContexts - firstChildOfParentContext)
+         val firstChildOfParentContext = allDataTestCalls.find { getLineNumber(it) == 30 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(isDataTest = true, firstChildOfParentContext).apply {
             this.shouldNotBeNull()
-            tag shouldBe "(kotest.data.27) | !kotest.data"
+            tag shouldBe "(kotest.data.30) | !kotest.data"
             ancestorTestPath shouldBe "parent context"
          }
 
-         // Root withData at line 33
-         val rootWithData = allDataTestCalls.find { getLineNumber(it) == 33 }.shouldNotBeNull()
-         DataTestUtil.dataTestInfoMaybe(isDataTest = true, rootWithData)?.tag shouldBe "kotest.data.33"
+         // Root withData at line 36
+         val rootWithData = allDataTestCalls.find { getLineNumber(it) == 36 }.shouldNotBeNull()
+         DataTestUtil.dataTestInfoMaybe(isDataTest = true, rootWithData)?.tag shouldBe "kotest.data.36"
 
-         // firstChild (withTests) at line 34 - exclude siblings at lines 37 and 55
-         val firstChild = allDataTestCalls.find { getLineNumber(it) == 34 }.shouldNotBeNull()
+         // firstChild (withTests) at line 37 - exclude siblings at lines 40 and 58
+         val firstChild = allDataTestCalls.find { getLineNumber(it) == 37 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(
             isDataTest = true,
             firstChild
-         )?.tag shouldBe "kotest.data.33 & !kotest.data.37 & !kotest.data.55"
+         )?.tag shouldBe "kotest.data.36 & !kotest.data.40 & !kotest.data.58"
 
-         // secondChild (withData) at line 37 - exclude siblings at lines 34 and 55
-         val secondChild = allDataTestCalls.find { getLineNumber(it) == 37 }.shouldNotBeNull()
+         // secondChild (withData) at line 40 - exclude siblings at lines 37 and 58
+         val secondChild = allDataTestCalls.find { getLineNumber(it) == 40 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(
             isDataTest = true,
             secondChild
-         )?.tag shouldBe "kotest.data.33 & !kotest.data.34 & !kotest.data.55"
+         )?.tag shouldBe "kotest.data.36 & !kotest.data.37 & !kotest.data.58"
 
-         // firstChildOfSecondChild (withContexts) at line 38 - exclude siblings at lines 46 and 49, plus parent's siblings
-         val firstChildOfSecondChild = allDataTestCalls.find { getLineNumber(it) == 38 }.shouldNotBeNull()
+         // firstChildOfSecondChild (withContexts) at line 41 - exclude siblings at lines 49 and 52, plus parent's siblings
+         val firstChildOfSecondChild = allDataTestCalls.find { getLineNumber(it) == 41 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(
             isDataTest = true,
             firstChildOfSecondChild
-         )?.tag shouldBe "kotest.data.33 & !kotest.data.34 & !kotest.data.55 & !kotest.data.46 & !kotest.data.49"
+         )?.tag shouldBe "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.49 & !kotest.data.52"
 
-         // firstChildOfFirstChildOfSecondChild (withTests) at line 39 - exclude sibling at line 42
-         val firstChildOfFirstChildOfSecondChild = allDataTestCalls.find { getLineNumber(it) == 39 }.shouldNotBeNull()
+         // firstChildOfFirstChildOfSecondChild (withTests) at line 42 - exclude sibling at line 45
+         val firstChildOfFirstChildOfSecondChild = allDataTestCalls.find { getLineNumber(it) == 42 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(
             isDataTest = true,
             firstChildOfFirstChildOfSecondChild
-         )?.tag shouldBe "kotest.data.33 & !kotest.data.34 & !kotest.data.55 & !kotest.data.46 & !kotest.data.49 & !kotest.data.42"
+         )?.tag shouldBe "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.49 & !kotest.data.52 & !kotest.data.45"
 
-         // secondChildOfFirstChildOfSecondChild (withTests) at line 42 - exclude sibling at line 39
-         val secondChildOfFirstChildOfSecondChild = allDataTestCalls.find { getLineNumber(it) == 42 }.shouldNotBeNull()
+         // secondChildOfFirstChildOfSecondChild (withTests) at line 45 - exclude sibling at line 42
+         val secondChildOfFirstChildOfSecondChild = allDataTestCalls.find { getLineNumber(it) == 45 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(
             isDataTest = true,
             secondChildOfFirstChildOfSecondChild
-         )?.tag shouldBe "kotest.data.33 & !kotest.data.34 & !kotest.data.55 & !kotest.data.46 & !kotest.data.49 & !kotest.data.39"
+         )?.tag shouldBe "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.49 & !kotest.data.52 & !kotest.data.42"
 
-         // secondChildOfSecondChild (withTests) at line 46 - exclude siblings at lines 38 and 49
-         val secondChildOfSecondChild = allDataTestCalls.find { getLineNumber(it) == 46 }.shouldNotBeNull()
+         // secondChildOfSecondChild (withTests) at line 49 - exclude siblings at lines 41 and 52
+         val secondChildOfSecondChild = allDataTestCalls.find { getLineNumber(it) == 49 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(
             isDataTest = true,
             secondChildOfSecondChild
-         )?.tag shouldBe "kotest.data.33 & !kotest.data.34 & !kotest.data.55 & !kotest.data.38 & !kotest.data.49"
+         )?.tag shouldBe "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.41 & !kotest.data.52"
 
-         // thirdChildOfSecondChild (withData) at line 49 - exclude siblings at lines 38 and 46
-         val thirdChildOfSecondChild = allDataTestCalls.find { getLineNumber(it) == 49 }.shouldNotBeNull()
+         // thirdChildOfSecondChild (withData) at line 52 - exclude siblings at lines 41 and 49
+         val thirdChildOfSecondChild = allDataTestCalls.find { getLineNumber(it) == 52 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(
             isDataTest = true,
             thirdChildOfSecondChild
-         )?.tag shouldBe "kotest.data.33 & !kotest.data.34 & !kotest.data.55 & !kotest.data.38 & !kotest.data.46"
+         )?.tag shouldBe "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.41 & !kotest.data.49"
 
-         // firstAndOnlyChildOfThirdChildOfSecondChild (withTests) at line 50 - same as parent (no siblings)
-         val firstAndOnlyChild = allDataTestCalls.find { getLineNumber(it) == 50 }.shouldNotBeNull()
+         // firstAndOnlyChildOfThirdChildOfSecondChild (withTests) at line 53 - same as parent (no siblings)
+         val firstAndOnlyChild = allDataTestCalls.find { getLineNumber(it) == 53 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(
             isDataTest = true,
             firstAndOnlyChild
-         )?.tag shouldBe "kotest.data.33 & !kotest.data.34 & !kotest.data.55 & !kotest.data.38 & !kotest.data.46"
+         )?.tag shouldBe "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.41 & !kotest.data.49"
 
-         // thirdChild (withTests) at line 55 - exclude siblings at lines 34 and 37
-         val thirdChild = allDataTestCalls.find { getLineNumber(it) == 55 }.shouldNotBeNull()
+         // thirdChild (withTests) at line 58 - exclude siblings at lines 37 and 40
+         val thirdChild = allDataTestCalls.find { getLineNumber(it) == 58 }.shouldNotBeNull()
          DataTestUtil.dataTestInfoMaybe(
             isDataTest = true,
             thirdChild
-         )?.tag shouldBe "kotest.data.33 & !kotest.data.34 & !kotest.data.37"
+         )?.tag shouldBe "kotest.data.36 & !kotest.data.37 & !kotest.data.40"
       }
    }
 
--- kotest-intellij-plugin/src/test/resources/data-test-tags/DataTestTagsFunSpec.kt
+++ kotest-intellij-plugin/src/test/resources/data-test-tags/DataTestTagsFunSpec.kt
@@ -1,58 +1,61 @@
 package kotest.datatesttags
-
+// tests in this package all have the same structure but different style (FunSpec, FreeSpec, BehaviorSpec) - keep it that way so they can all be tested at once
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withContexts
 import io.kotest.datatest.withData
 import io.kotest.datatest.withTests
 import io.kotest.matchers.shouldBe
 
+
+
+
 class DataTestTagsFunSpec : FunSpec({
 
     context("parent context") {
         context("child context") {
-            withData("firstChildOfChildContext1", "firstChildOfChildContext2") { // tags: "(kotest.data.13) | !kotest.data" - ancestorTestPath: "parent context -- child context"
-               withTests("firstChildOfFirstChildOfChildContext1", "firstChildOfFirstChildOfChildContext2") { // tags: "(kotest.data.13 & !kotest.data.17) | !kotest.data" - ancestorTestPath: "parent context -- child context"
+            withData("firstChildOfChildContext1", "firstChildOfChildContext2") { // tags: "(kotest.data.16) | !kotest.data" - ancestorTestPath: "parent context -- child context"
+               withTests("firstChildOfFirstChildOfChildContext1", "firstChildOfFirstChildOfChildContext2") { // tags: "(kotest.data.16 & !kotest.data.20) | !kotest.data" - ancestorTestPath: "parent context -- child context"
                   1 + 1 shouldBe 2
                }
-               withData("secondChildOfFirstChildOfChildContext1", "secondChildOfFirstChildOfChildContext2") { // tags: "(kotest.data.13 & !kotest.data.14) | !kotest.data" - ancestorTestPath: "parent context -- child context"
-                  withTests("firstChildOfsecondChildOfFirstChildOfChildContext1", "firstChildOfsecondChildOfFirstChildOfChildContext2") { // tags: "(kotest.data.13 & !kotest.data.14) | !kotest.data" - ancestorTestPath: "parent context -- child context" (same as parent, no siblings)
+               withData("secondChildOfFirstChildOfChildContext1", "secondChildOfFirstChildOfChildContext2") { // tags: "(kotest.data.16 & !kotest.data.17) | !kotest.data" - ancestorTestPath: "parent context -- child context"
+                  withTests("firstChildOfsecondChildOfFirstChildOfChildContext1", "firstChildOfsecondChildOfFirstChildOfChildContext2") { // tags: "(kotest.data.16 & !kotest.data.17) | !kotest.data" - ancestorTestPath: "parent context -- child context" (same as parent, no siblings)
                      1 + 1 shouldBe 2
                   }
                }
             }
-            withTests("secondChildOfChildContext1", "secondChildOfChildContext2") { // tags: "(kotest.data.23) | !kotest.data" - ancestorTestPath: "parent context -- child context"
+            withTests("secondChildOfChildContext1", "secondChildOfChildContext2") { // tags: "(kotest.data.26) | !kotest.data" - ancestorTestPath: "parent context -- child context"
                 1 + 1 shouldBe 2
             }
         }
-        withContexts("firstChildOfParentContext1", "firstChildOfParentContext2") { // tags: "(kotest.data.27) | !kotest.data" - ancestorTestPath: "parent context"
+        withContexts("firstChildOfParentContext1", "firstChildOfParentContext2") { // tags: "(kotest.data.30) | !kotest.data" - ancestorTestPath: "parent context"
             1 + 1 shouldBe 2
         }
     }
 
    // ancestorTestPath: null for all below as they are not wrapped in a container
-   withData("parent1", "parent2", "parent3") { // tag: "kotest.data.32"
-      withTests("firstChild1", "firstChild2") { // tag: "kotest.data.32 & !kotest.data.36 & !kotest.data.54"
+   withData("parent1", "parent2", "parent3") { // tag: "kotest.data.36"
+      withTests("firstChild1", "firstChild2") { // tag: "kotest.data.36 & !kotest.data.40 & !kotest.data.58"
          1 + 1 shouldBe 2
       }
-      withData("secondChild1", "secondChild2") { // tag: "kotest.data.32 & !kotest.data.33 & !kotest.data.54"
-         withContexts("firstChildOfSecondChild1", "firstChildOfSecondChild2") { // tag: "kotest.data.32 & !kotest.data.33 & !kotest.data.54 & !kotest.data.45 & !kotest.data.48"
-            withTests("firstChildOfFirstChildOfSecondChild1", "firstChildOfFirstChildOfSecondChild2") { // tags: "kotest.data.32 & !kotest.data.33 & !kotest.data.54 & !kotest.data.45 & !kotest.data.48 & !kotest.data.40 & !kotest.data.43"
+      withData("secondChild1", "secondChild2") { // tag: "kotest.data.36 & !kotest.data.37 & !kotest.data.58"
+         withContexts("firstChildOfSecondChild1", "firstChildOfSecondChild2") { // tag: "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.49 & !kotest.data.52"
+            withTests("firstChildOfFirstChildOfSecondChild1", "firstChildOfFirstChildOfSecondChild2") { // tags: "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.49 & !kotest.data.52 & !kotest.data.45"
                1 + 1 shouldBe 2
             }
-            withTests("secondChildOfFirstChildOfSecondChild1", "secondChildOfFirstChildOfSecondChild2") { // tags: "kotest.data.32 & !kotest.data.33 & !kotest.data.54 & !kotest.data.45 & !kotest.data.48 & !kotest.data.40 & !kotest.data.43"
+            withTests("secondChildOfFirstChildOfSecondChild1", "secondChildOfFirstChildOfSecondChild2") { // tags: "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.49 & !kotest.data.52 & !kotest.data.42"
                1 + 1 shouldBe 2
             }
          }
-         withTests("secondChildOfSecondChild1", "secondChildOfSecondChild2") { // tags: "kotest.data.32 & !kotest.data.33 & !kotest.data.54 & !kotest.data.37 & !kotest.data.48"
+         withTests("secondChildOfSecondChild1", "secondChildOfSecondChild2") { // tags: "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.41 & !kotest.data.52"
             1 + 1 shouldBe 2
          }
-         withData("thirdChildOfSecondChild1", "thirdChildOfSecondChild2") { // tags: "kotest.data.32 & !kotest.data.33 & !kotest.data.54 & !kotest.data.37 & !kotest.data.45"
-            withTests("firstAndOnlyChildOfThirdChildOfSecondChild1", "firstAndOnlyChildOfThirdChildOfSecondChild2") { // tags: "kotest.data.32 & !kotest.data.33 & !kotest.data.54 & !kotest.data.37 & !kotest.data.45" - same as parent as no siblings
+         withData("thirdChildOfSecondChild1", "thirdChildOfSecondChild2") { // tags: "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.41 & !kotest.data.49"
+            withTests("firstAndOnlyChildOfThirdChildOfSecondChild1", "firstAndOnlyChildOfThirdChildOfSecondChild2") { // tags: "kotest.data.36 & !kotest.data.37 & !kotest.data.58 & !kotest.data.41 & !kotest.data.49" - same as parent as no siblings
                1 + 1 shouldBe 2
             }
          }
       }
-      withTests("thirdChild1", "thirdChild2") { // tags: "kotest.data.32 & !kotest.data.33 & !kotest.data.36"
+      withTests("thirdChild1", "thirdChild2") { // tags: "kotest.data.36 & !kotest.data.37 & !kotest.data.40"
          1 + 1 shouldBe 2
       }
    }
