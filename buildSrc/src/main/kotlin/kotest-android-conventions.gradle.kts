76dad023acf1271edd5e874861ebddeb4c4ba6dd
sksamuel <sam@sksamuel.com>
2026-01-26 15:12:25 -0600

    Remove unused `kotlin` block from Android conventions gradle script

--- buildSrc/src/main/kotlin/kotest-android-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotest-android-conventions.gradle.kts
@@ -1,10 +1,4 @@
-import gradle.kotlin.dsl.accessors._069379b19141ff0fd15bffdbd97c6fe9.kotlin
-
 plugins {
    id("kotlin-conventions")
    id("com.android.kotlin.multiplatform.library")
-}
-
-kotlin {
-//   androidTarget()
 }
