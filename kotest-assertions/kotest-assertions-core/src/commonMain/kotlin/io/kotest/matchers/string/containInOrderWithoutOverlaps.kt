f3a811965a1c56375e8bf8089379b360d656c4a1
Alex Kuznetsov <Alex.Cue.987@gmail.com>
2026-01-30 16:25:53 -0600

    add shouldContainInOrderWithoutOverlaps (#5649)
    add `shouldContainInOrderWithoutOverlaps`

---
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/string/containInOrderWithoutOverlaps.kt
@@ -0,0 +1,37 @@
+package io.kotest.matchers.string
+
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldNot
+
+
+/**
+ * Verifies that the given [String] contains all the specified substrings in given order,
+ * with any characters before, after, or in between.
+ *
+ * For example, each of the following examples would pass:
+ *
+ * val value = "The quick brown fox jumps over the lazy dog"
+ * value.shouldContainInOrderWithoutOverlaps("The", "quick", "fox", "jumps", "over", "dog")
+ * value.shouldContainInOrderWithoutOverlaps("The quick", "fox jump", "over", "dog")
+ * "superstar".shouldContainInOrderWithoutOverlaps("super", "star")
+ *
+ * Note: unlike [shouldContainInOrder], consecutive substrings cannot overlap.
+ * So the following tests would fail:
+ *
+ * "sourdough bread".shouldContainInOrderWithoutOverlaps("bread", "read")
+ * "superstar".shouldContainInOrderWithoutOverlaps("supers", "star")
+ */
+fun String.shouldContainInOrderWithoutOverlaps(vararg substrings: String): String {
+   this should containInOrderWithoutOverlaps(*substrings)
+   return this
+}
+
+fun String.shouldNotContainInOrderWithoutOverlaps(vararg substrings: String): String {
+   this shouldNot containInOrderWithoutOverlaps(*substrings)
+   return this
+}
+
+fun containInOrderWithoutOverlaps(vararg substrings: String) = containSubstringsInOrder(
+   { substr -> substr.length },
+   *substrings)
+
