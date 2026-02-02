22485c1481bdbf08e726b2844d4265da1555dc99
sksamuel <sam@sksamuel.com>
2026-02-02 10:34:22 -0600

    Migrate to `kotest-js-wasm-conventions` and update PR workflow for JS/Wasm configuration.

--- kotest-tests/kotest-tests-wasm-wasi/build.gradle.kts
+++ kotest-tests/kotest-tests-wasm-wasi/build.gradle.kts
@@ -3,16 +3,13 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
-   id("kotlin-conventions")
+   id("kotest-js-wasm-conventions")
    id("com.google.devtools.ksp").version("2.3.4")
    // the Kotest plugin must be a published version and not one in the current build
    id("io.kotest").version("6.1.2")
 }
 
 kotlin {
-   wasmWasi {
-      nodejs()
-   }
    sourceSets {
       commonTest {
          dependencies {
