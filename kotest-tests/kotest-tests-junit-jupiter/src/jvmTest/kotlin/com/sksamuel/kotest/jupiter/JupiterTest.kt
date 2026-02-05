a6bf1adb9c6b57fc7deaedb64115bfb869f4416e
sksamuel <sam@sksamuel.com>
2026-02-04 23:03:39 -0600

    tests

--- kotest-tests/kotest-tests-junit-jupiter/src/jvmTest/kotlin/com/sksamuel/kotest/jupiter/JupiterTest.kt
+++ kotest-tests/kotest-tests-junit-jupiter/src/jvmTest/kotlin/com/sksamuel/kotest/jupiter/JupiterTest.kt
@@ -1,7 +1,15 @@
 package com.sksamuel.kotest.jupiter
 
+import io.kotest.core.spec.style.FunSpec
+
 class JupiterTest {
    @org.junit.jupiter.api.Test
    fun myTest() {
+   }
+}
+
+class KotestTest : FunSpec() {
+   init {
+      test("foo") {}
    }
 }
