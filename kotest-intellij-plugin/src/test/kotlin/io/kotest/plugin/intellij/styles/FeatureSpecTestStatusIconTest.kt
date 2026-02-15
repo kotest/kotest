97364e3f120b8c58a9275bd385c5aa45c69ae7ab
alfonsoristorato <alfonsoristorato@gmail.com>
2026-02-15 19:16:45 +0000

    add tests

---
+++ kotest-intellij-plugin/src/test/kotlin/io/kotest/plugin/intellij/styles/FeatureSpecTestStatusIconTest.kt
@@ -0,0 +1,111 @@
+package io.kotest.plugin.intellij.styles
+
+import com.intellij.execution.TestStateStorage
+import com.intellij.execution.testframework.sm.runner.states.TestStateInfo
+import com.intellij.icons.AllIcons
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
+import io.kotest.matchers.shouldBe
+import io.kotest.plugin.intellij.testMode
+import java.nio.file.Paths
+import java.util.Date
+
+class FeatureSpecTestStatusIconTest : LightJavaCodeInsightFixtureTestCase() {
+
+   override fun getTestDataPath(): String {
+      val path = Paths.get("./src/test/resources/").toAbsolutePath()
+      return path.toString()
+   }
+
+   override fun setUp() {
+      super.setUp()
+      testMode = true
+   }
+
+   override fun tearDown() {
+      try {
+         val storage = TestStateStorage.getInstance(project)
+         storage.keys.forEach { storage.removeState(it) }
+      } finally {
+         super.tearDown()
+      }
+   }
+
+   fun testIconShowsFailedForFailedTest() {
+      myFixture.configureByFiles(
+         "/featurespec.kt",
+         "/io/kotest/core/spec/style/specs.kt"
+      )
+
+      val storage = TestStateStorage.getInstance(project)
+      val specFqn = "com.sksamuel.kotest.specs.feature.FeatureSpecExample"
+
+      // failing test: feature("some feature") { scenario("some scenario")
+      val testPath = "$specFqn/some feature -- some scenario"
+      val url = "java:test://<kotest>$testPath</kotest>some scenario"
+      storage.writeState(url, TestStateStorage.Record(TestStateInfo.Magnitude.FAILED_INDEX.value, Date(), 0, 0, "", "", ""))
+
+      val gutters = myFixture.findAllGutters()
+
+      // index 3 is "some feature some scenario"
+      gutters[3].icon shouldBe AllIcons.RunConfigurations.TestState.Red2
+      gutters[3].tooltipText shouldBe "Run some feature some scenario"
+   }
+
+   fun testIconShowsPassedForPassedContainerTest() {
+      myFixture.configureByFiles(
+         "/featurespec.kt",
+         "/io/kotest/core/spec/style/specs.kt"
+      )
+
+      val storage = TestStateStorage.getInstance(project)
+      val specFqn = "com.sksamuel.kotest.specs.feature.FeatureSpecExample"
+
+      // passing container: feature("some feature")
+      val testPath = "$specFqn/some feature"
+      val url = "java:suite://<kotest>$testPath</kotest>"
+      storage.writeState(url, TestStateStorage.Record(TestStateInfo.Magnitude.PASSED_INDEX.value, Date(), 0, 0, "", "", ""))
+
+      val gutters = myFixture.findAllGutters()
+
+      // index 2 is "some feature"
+      gutters[2].icon shouldBe AllIcons.RunConfigurations.TestState.Green2
+      gutters[2].tooltipText shouldBe "Run some feature"
+   }
+
+   fun testIconShowsFailedForFailedSpec() {
+      myFixture.configureByFiles(
+         "/featurespec.kt",
+         "/io/kotest/core/spec/style/specs.kt"
+      )
+
+      val storage = TestStateStorage.getInstance(project)
+      val specFqn = "com.sksamuel.kotest.specs.feature.FeatureSpecExample"
+
+      val url = "java:suite://$specFqn"
+      storage.writeState(url, TestStateStorage.Record(TestStateInfo.Magnitude.FAILED_INDEX.value, Date(), 0, 0, "", "", ""))
+
+      val gutters = myFixture.findAllGutters()
+
+      gutters[0].icon shouldBe AllIcons.RunConfigurations.TestState.Red2
+      gutters[0].tooltipText shouldBe "Run FeatureSpecExample"
+   }
+
+   fun testIconShowsPassedForPassedSpec() {
+      myFixture.configureByFiles(
+         "/featurespec.kt",
+         "/io/kotest/core/spec/style/specs.kt"
+      )
+
+      val storage = TestStateStorage.getInstance(project)
+      val specFqn = "com.sksamuel.kotest.specs.feature.FeatureSpecExample"
+
+      val url = "java:suite://$specFqn"
+      storage.writeState(url, TestStateStorage.Record(TestStateInfo.Magnitude.PASSED_INDEX.value, Date(), 0, 0, "", "", ""))
+
+      val gutters = myFixture.findAllGutters()
+
+      gutters[0].icon shouldBe AllIcons.RunConfigurations.TestState.Green2
+      gutters[0].tooltipText shouldBe "Run FeatureSpecExample"
+   }
+}
+
---
+++ kotest-intellij-plugin/src/test/kotlin/io/kotest/plugin/intellij/styles/ShouldSpecTestStatusIconTest.kt
@@ -0,0 +1,111 @@
+package io.kotest.plugin.intellij.styles
+
+import com.intellij.execution.TestStateStorage
+import com.intellij.execution.testframework.sm.runner.states.TestStateInfo
+import com.intellij.icons.AllIcons
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
+import io.kotest.matchers.shouldBe
+import io.kotest.plugin.intellij.testMode
+import java.nio.file.Paths
+import java.util.Date
+
+class ShouldSpecTestStatusIconTest : LightJavaCodeInsightFixtureTestCase() {
+
+   override fun getTestDataPath(): String {
+      val path = Paths.get("./src/test/resources/").toAbsolutePath()
+      return path.toString()
+   }
+
+   override fun setUp() {
+      super.setUp()
+      testMode = true
+   }
+
+   override fun tearDown() {
+      try {
+         val storage = TestStateStorage.getInstance(project)
+         storage.keys.forEach { storage.removeState(it) }
+      } finally {
+         super.tearDown()
+      }
+   }
+
+   fun testIconShowsFailedForFailedTest() {
+      myFixture.configureByFiles(
+         "/shouldspec.kt",
+         "/io/kotest/core/spec/style/specs.kt"
+      )
+
+      val storage = TestStateStorage.getInstance(project)
+      val specFqn = "com.sksamuel.kotest.specs.shouldspec.ShouldSpecExample"
+
+      // failing test: should("top level test")
+      val testPath = "$specFqn/top level test"
+      val url = "java:test://<kotest>$testPath</kotest>top level test"
+      storage.writeState(url, TestStateStorage.Record(TestStateInfo.Magnitude.FAILED_INDEX.value, Date(), 0, 0, "", "", ""))
+
+      val gutters = myFixture.findAllGutters()
+
+      // index 1 is "top level test"
+      gutters[1].icon shouldBe AllIcons.RunConfigurations.TestState.Red2
+      gutters[1].tooltipText shouldBe "Run top level test"
+   }
+
+   fun testIconShowsPassedForPassedContainerTest() {
+      myFixture.configureByFiles(
+         "/shouldspec.kt",
+         "/io/kotest/core/spec/style/specs.kt"
+      )
+
+      val storage = TestStateStorage.getInstance(project)
+      val specFqn = "com.sksamuel.kotest.specs.shouldspec.ShouldSpecExample"
+
+      // passing container: context("some context")
+      val testPath = "$specFqn/some context"
+      val url = "java:suite://<kotest>$testPath</kotest>"
+      storage.writeState(url, TestStateStorage.Record(TestStateInfo.Magnitude.PASSED_INDEX.value, Date(), 0, 0, "", "", ""))
+
+      val gutters = myFixture.findAllGutters()
+
+      // index 3 is "some context"
+      gutters[3].icon shouldBe AllIcons.RunConfigurations.TestState.Green2
+      gutters[3].tooltipText shouldBe "Run some context"
+   }
+
+   fun testIconShowsFailedForFailedSpec() {
+      myFixture.configureByFiles(
+         "/shouldspec.kt",
+         "/io/kotest/core/spec/style/specs.kt"
+      )
+
+      val storage = TestStateStorage.getInstance(project)
+      val specFqn = "com.sksamuel.kotest.specs.shouldspec.ShouldSpecExample"
+
+      val url = "java:suite://$specFqn"
+      storage.writeState(url, TestStateStorage.Record(TestStateInfo.Magnitude.FAILED_INDEX.value, Date(), 0, 0, "", "", ""))
+
+      val gutters = myFixture.findAllGutters()
+
+      gutters[0].icon shouldBe AllIcons.RunConfigurations.TestState.Red2
+      gutters[0].tooltipText shouldBe "Run ShouldSpecExample"
+   }
+
+   fun testIconShowsPassedForPassedSpec() {
+      myFixture.configureByFiles(
+         "/shouldspec.kt",
+         "/io/kotest/core/spec/style/specs.kt"
+      )
+
+      val storage = TestStateStorage.getInstance(project)
+      val specFqn = "com.sksamuel.kotest.specs.shouldspec.ShouldSpecExample"
+
+      val url = "java:suite://$specFqn"
+      storage.writeState(url, TestStateStorage.Record(TestStateInfo.Magnitude.PASSED_INDEX.value, Date(), 0, 0, "", "", ""))
+
+      val gutters = myFixture.findAllGutters()
+
+      gutters[0].icon shouldBe AllIcons.RunConfigurations.TestState.Green2
+      gutters[0].tooltipText shouldBe "Run ShouldSpecExample"
+   }
+}
+
