6ce781267bf5d5474199552d324169c62e8a6080
sksamuel <sam@sksamuel.com>
2026-02-19 22:46:26 -0600

    Update fallback `productName` to `IC-253` and replace deprecated annotation in tests

--- kotest-intellij-plugin/build.gradle.kts
+++ kotest-intellij-plugin/build.gradle.kts
@@ -75,7 +75,7 @@ val descriptors = listOf(
    ),
 )
 
-val productName = System.getenv("PRODUCT_NAME") ?: "IC-261"
+val productName = System.getenv("PRODUCT_NAME") ?: "IC-253"
 val descriptor: PluginDescriptor = descriptors.first { it.sourceFolder == productName }
 val jvmTargetVersion: String = System.getenv("JVM_TARGET") ?: descriptor.jdkTarget.majorVersion
 
