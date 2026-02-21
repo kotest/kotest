8b0bf4a5bc18971b2c69d723973a1d4b9c2bd933
sksamuel <sam@sksamuel.com>
2026-02-20 21:16:18 -0600

    Update `io.kotest` to 6.1.3 and pin `com.google.devtools.ksp` to 2.3.6 in WASM Gradle scripts

--- kotest-tests/kotest-tests-wasm-js/build.gradle.kts
+++ kotest-tests/kotest-tests-wasm-js/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
    id("kotest-js-conventions")
    id("com.google.devtools.ksp").version("2.3.6")
    // the Kotest plugin must be a published version and not one in the current build
-   id("io.kotest").version("6.1.2")
+   id("io.kotest").version("6.1.3")
 }
 
 kotlin {
--- kotest-tests/kotest-tests-wasm-wasi/build.gradle.kts
+++ kotest-tests/kotest-tests-wasm-wasi/build.gradle.kts
@@ -4,9 +4,9 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
    id("kotest-wasi-conventions")
-   id("com.google.devtools.ksp")
+   id("com.google.devtools.ksp").version("2.3.6")
    // the Kotest plugin must be a published version and not one in the current build
-   id("io.kotest")
+   id("io.kotest").version("6.1.3")
 }
 
 kotlin {
