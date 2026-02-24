c2a7f2fb789751ae83a1f455e4d82d4189fdbe73
alfonsoristorato <alfonsoristorato@gmail.com>
2026-02-24 17:09:13 +0000

    re-add-junit4-module

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,7 +99,7 @@ dependencies {
 
    // Runners
    nmcpAggregation(projects.kotestRunner.kotestRunnerJunitPlatform)
-//   nmcpAggregation(projects.kotestRunner.kotestRunnerJunit4)
+   nmcpAggregation(projects.kotestRunner.kotestRunnerJunit4)
    nmcpAggregation(projects.kotestRunner.kotestRunnerJunit5)
    nmcpAggregation(projects.kotestRunner.kotestRunnerJunit6)
 
