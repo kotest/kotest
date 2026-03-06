6cbe5c36212ab13d474d7bc1e825b4b4d55c65bc
alfonsoristorato <alfonsoristorato@gmail.com>
2026-03-06 17:25:55 +0000

    replace typealias with deprecated interfaces/classes in io.kotest.matchers

--- kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/matchers/aliases.kt
+++ kotest-assertions/kotest-assertions-shared/src/commonMain/kotlin/io/kotest/matchers/aliases.kt
@@ -3,7 +3,7 @@ package io.kotest.matchers
 import io.kotest.assertions.BasicErrorCollector
 
 @Deprecated("Use io.kotest.assertions.AssertionCounter. Will be removed in a future release.")
-typealias AssertionCounter = io.kotest.assertions.AssertionCounter
+interface AssertionCounter : io.kotest.assertions.AssertionCounter
 
 @Deprecated("Use io.kotest.assertions.assertionCounter. Will be removed in a future release.")
 val assertionCounter: io.kotest.assertions.AssertionCounter get() = io.kotest.assertions.assertionCounter
@@ -12,10 +12,10 @@ val assertionCounter: io.kotest.assertions.AssertionCounter get() = io.kotest.as
 typealias ErrorCollectionMode = io.kotest.assertions.ErrorCollectionMode
 
 @Deprecated("Use io.kotest.assertions.ErrorCollector. Will be removed in a future release.")
-typealias ErrorCollector = io.kotest.assertions.ErrorCollector
+interface ErrorCollector : io.kotest.assertions.ErrorCollector
 
 @Deprecated("Use io.kotest.assertions.errorCollector. Will be removed in a future release.")
 val errorCollector: io.kotest.assertions.ErrorCollector get() = io.kotest.assertions.errorCollector
 
 @Deprecated("Use io.kotest.assertions.BasicErrorCollector. Will be removed in a future release.")
-typealias BasicErrorCollector = BasicErrorCollector
+open class BasicErrorCollector : BasicErrorCollector()
