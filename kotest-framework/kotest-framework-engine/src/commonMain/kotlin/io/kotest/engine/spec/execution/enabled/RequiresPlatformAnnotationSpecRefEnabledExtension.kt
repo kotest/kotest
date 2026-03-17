04b1309747523df3dd2289809f892444acfd3de3
sksamuel <sam@sksamuel.com>
2026-03-17 11:48:59 -0500

    Use `KClass.annotation<T>()` in RequiresPlatformAnnotationSpecRefEnabledExtension

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/spec/execution/enabled/RequiresPlatformAnnotationSpecRefEnabledExtension.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io/kotest/engine/spec/execution/enabled/RequiresPlatformAnnotationSpecRefEnabledExtension.kt
@@ -1,6 +1,7 @@
 package io.kotest.engine.spec.execution.enabled
 
 import io.kotest.common.platform
+import io.kotest.common.reflection.annotation
 import io.kotest.core.annotation.RequiresPlatform
 import io.kotest.core.spec.SpecRef
 
@@ -11,7 +12,7 @@ import io.kotest.core.spec.SpecRef
 internal object RequiresPlatformAnnotationSpecRefEnabledExtension : SpecRefEnabledExtension {
 
    override fun isEnabled(ref: SpecRef): EnabledOrDisabled {
-      val annotation = ref.kclass.annotations.filterIsInstance<RequiresPlatform>().firstOrNull()
+      val annotation = ref.kclass.annotation<RequiresPlatform>()
          ?: return EnabledOrDisabled.Enabled
       return if (annotation.values.contains(platform))
          EnabledOrDisabled.Enabled
