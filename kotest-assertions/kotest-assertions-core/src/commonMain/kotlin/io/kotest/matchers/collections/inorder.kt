1e9d0d4b0d2bcc08747f0ef5137adbffdb35b6ba
sksamuel <sam@sksamuel.com>
2026-03-23 21:23:24 -0500

    Add infix forms for primitive array `shouldContainInOrder`/`shouldNotContainInOrder` matchers with `@JvmName` annotations

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/inorder.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/collections/inorder.kt
@@ -6,6 +6,7 @@ import io.kotest.matchers.MatcherResult
 import io.kotest.matchers.neverNullMatcher
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldNot
+import kotlin.jvm.JvmName
 
 /** Assert that a collection contains a given subsequence, possibly with values in between. */
 fun <T> containsInOrder(vararg ts: T): Matcher<Collection<T>?> = containsInOrder(ts.asList())
@@ -56,32 +57,64 @@ infix fun <T> List<T>.shouldNotContainInOrder(expected: List<T>) = this shouldNo
 
 // BooleanArray
 fun BooleanArray.shouldContainInOrder(vararg ts: Boolean): BooleanArray = apply { asList().shouldContainInOrder(ts.asList()) }
+@JvmName("shouldContainInOrderBooleanArray_infix")
+infix fun BooleanArray.shouldContainInOrder(expected: BooleanArray): BooleanArray = apply { asList().shouldContainInOrder(expected.asList()) }
 fun BooleanArray.shouldNotContainInOrder(vararg ts: Boolean): BooleanArray = apply { asList().shouldNotContainInOrder(ts.asList()) }
+@JvmName("shouldNotContainInOrderBooleanArray_infix")
+infix fun BooleanArray.shouldNotContainInOrder(expected: BooleanArray): BooleanArray = apply { asList().shouldNotContainInOrder(expected.asList()) }
 
 // ByteArray
 fun ByteArray.shouldContainInOrder(vararg ts: Byte): ByteArray = apply { asList().shouldContainInOrder(ts.asList()) }
+@JvmName("shouldContainInOrderByteArray_infix")
+infix fun ByteArray.shouldContainInOrder(expected: ByteArray): ByteArray = apply { asList().shouldContainInOrder(expected.asList()) }
 fun ByteArray.shouldNotContainInOrder(vararg ts: Byte): ByteArray = apply { asList().shouldNotContainInOrder(ts.asList()) }
+@JvmName("shouldNotContainInOrderByteArray_infix")
+infix fun ByteArray.shouldNotContainInOrder(expected: ByteArray): ByteArray = apply { asList().shouldNotContainInOrder(expected.asList()) }
 
 // ShortArray
 fun ShortArray.shouldContainInOrder(vararg ts: Short): ShortArray = apply { asList().shouldContainInOrder(ts.asList()) }
+@JvmName("shouldContainInOrderShortArray_infix")
+infix fun ShortArray.shouldContainInOrder(expected: ShortArray): ShortArray = apply { asList().shouldContainInOrder(expected.asList()) }
 fun ShortArray.shouldNotContainInOrder(vararg ts: Short): ShortArray = apply { asList().shouldNotContainInOrder(ts.asList()) }
+@JvmName("shouldNotContainInOrderShortArray_infix")
+infix fun ShortArray.shouldNotContainInOrder(expected: ShortArray): ShortArray = apply { asList().shouldNotContainInOrder(expected.asList()) }
 
 // CharArray
 fun CharArray.shouldContainInOrder(vararg ts: Char): CharArray = apply { asList().shouldContainInOrder(ts.asList()) }
+@JvmName("shouldContainInOrderCharArray_infix")
+infix fun CharArray.shouldContainInOrder(expected: CharArray): CharArray = apply { asList().shouldContainInOrder(expected.asList()) }
 fun CharArray.shouldNotContainInOrder(vararg ts: Char): CharArray = apply { asList().shouldNotContainInOrder(ts.asList()) }
+@JvmName("shouldNotContainInOrderCharArray_infix")
+infix fun CharArray.shouldNotContainInOrder(expected: CharArray): CharArray = apply { asList().shouldNotContainInOrder(expected.asList()) }
 
 // IntArray
 fun IntArray.shouldContainInOrder(vararg ts: Int): IntArray = apply { asList().shouldContainInOrder(ts.asList()) }
+@JvmName("shouldContainInOrderIntArray_infix")
+infix fun IntArray.shouldContainInOrder(expected: IntArray): IntArray = apply { asList().shouldContainInOrder(expected.asList()) }
 fun IntArray.shouldNotContainInOrder(vararg ts: Int): IntArray = apply { asList().shouldNotContainInOrder(ts.asList()) }
+@JvmName("shouldNotContainInOrderIntArray_infix")
+infix fun IntArray.shouldNotContainInOrder(expected: IntArray): IntArray = apply { asList().shouldNotContainInOrder(expected.asList()) }
 
 // LongArray
 fun LongArray.shouldContainInOrder(vararg ts: Long): LongArray = apply { asList().shouldContainInOrder(ts.asList()) }
+@JvmName("shouldContainInOrderLongArray_infix")
+infix fun LongArray.shouldContainInOrder(expected: LongArray): LongArray = apply { asList().shouldContainInOrder(expected.asList()) }
 fun LongArray.shouldNotContainInOrder(vararg ts: Long): LongArray = apply { asList().shouldNotContainInOrder(ts.asList()) }
+@JvmName("shouldNotContainInOrderLongArray_infix")
+infix fun LongArray.shouldNotContainInOrder(expected: LongArray): LongArray = apply { asList().shouldNotContainInOrder(expected.asList()) }
 
 // FloatArray
 fun FloatArray.shouldContainInOrder(vararg ts: Float): FloatArray = apply { asList().shouldContainInOrder(ts.asList()) }
+@JvmName("shouldContainInOrderFloatArray_infix")
+infix fun FloatArray.shouldContainInOrder(expected: FloatArray): FloatArray = apply { asList().shouldContainInOrder(expected.asList()) }
 fun FloatArray.shouldNotContainInOrder(vararg ts: Float): FloatArray = apply { asList().shouldNotContainInOrder(ts.asList()) }
+@JvmName("shouldNotContainInOrderFloatArray_infix")
+infix fun FloatArray.shouldNotContainInOrder(expected: FloatArray): FloatArray = apply { asList().shouldNotContainInOrder(expected.asList()) }
 
 // DoubleArray
 fun DoubleArray.shouldContainInOrder(vararg ts: Double): DoubleArray = apply { asList().shouldContainInOrder(ts.asList()) }
+@JvmName("shouldContainInOrderDoubleArray_infix")
+infix fun DoubleArray.shouldContainInOrder(expected: DoubleArray): DoubleArray = apply { asList().shouldContainInOrder(expected.asList()) }
 fun DoubleArray.shouldNotContainInOrder(vararg ts: Double): DoubleArray = apply { asList().shouldNotContainInOrder(ts.asList()) }
+@JvmName("shouldNotContainInOrderDoubleArray_infix")
+infix fun DoubleArray.shouldNotContainInOrder(expected: DoubleArray): DoubleArray = apply { asList().shouldNotContainInOrder(expected.asList()) }
