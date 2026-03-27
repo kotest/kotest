110128a9090b1b7da0ede90726f3f6057642e8b4
Meyon Soo  Kim <39788337+PreAgile@users.noreply.github.com>
2026-03-27 16:40:46 +0900

    Rename shouldEq/shouldNotEq to shouldEqual/shouldNotEqual
    Per review consensus, rename the type-safe assertions to avoid
    Eq typeclass confusion and align with existing naming conventions.
    
    - Merge shouldEqual/shouldNotEqual into shouldEqual.kt alongside
      existing shouldBeEqual/shouldNotBeEqual
    - Remove standalone shouldEq.kt
    - Rename test class and add shouldNotEqual coverage for nullable
      types and error messages
    - Update API dump accordingly

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/equals/shouldEqual.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/equals/shouldEqual.kt
@@ -17,6 +17,38 @@ infix fun <A : Any> A.shouldNotBeEqual(expected: A): A {
 }
 
 /**
+ * Verifies that this value is equal to [expected] using [equals].
+ *
+ * Unlike [io.kotest.matchers.shouldBe], this function restricts the type parameter
+ * so that only values of the same type can be compared.
+ *
+ * ```
+ * 1 shouldEqual 1       // compiles
+ * 1 shouldEqual "1"     // compile error - Int vs String
+ * ```
+ */
+infix fun <@kotlin.internal.OnlyInputTypes T> T.shouldEqual(expected: T): T {
+   this should beEqual(expected)
+   return this
+}
+
+/**
+ * Verifies that this value is **not** equal to [expected] using [equals].
+ *
+ * Unlike [io.kotest.matchers.shouldNotBe], this function restricts the type parameter
+ * so that only values of the same type can be compared.
+ *
+ * ```
+ * 1 shouldNotEqual 2       // compiles
+ * 1 shouldNotEqual "1"     // compile error - Int vs String
+ * ```
+ */
+infix fun <@kotlin.internal.OnlyInputTypes T> T.shouldNotEqual(expected: T): T {
+   this shouldNot beEqual(expected)
+   return this
+}
+
+/**
  * Verifies that two values are the same using [equals].
  */
 fun <A> beEqual(expected: A): Matcher<A> = object : Matcher<A> {
