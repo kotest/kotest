ff34ed9ae6d18b815fd15ac938c79a1fdff40b72
Alex Kuznetsov <Alex.Cue.987@gmail.com>
2026-03-28 14:48:17 -0500

    Alex/improve contains in order  (#5816)

---
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/string/SubsetFitsInOrderTest.kt
@@ -0,0 +1,111 @@
+package com.sksamuel.kotest.matchers.string
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.subsetFitsInOrder
+
+class SubsetFitsInOrderTest : StringSpec() {
+   init {
+       "returns true in the simplest case of one match per substring" {
+            val indexesOfMatches = listOf(
+               listOf(0),
+               listOf(1),
+               listOf(2),
+            )
+            subsetFitsInOrder(
+               indexesOfMatches,
+               listOf(0, 1, 2),
+               listOf("a", "b", "c"),
+               { 1 },
+               ) shouldBe true
+       }
+      "returns true when multiple matches per substring and an increasing sequence is found" {
+         val indexesOfMatches = listOf(
+            listOf(10, 15, 20,),
+            listOf(8, 16, 24,),
+            listOf(6, 12, 18, ),
+         )
+         subsetFitsInOrder(
+            indexesOfMatches,
+            listOf(0, 1, 2),
+            listOf("a", "b", "c"),
+            { 1 },
+            ) shouldBe true
+      }
+      "returns false when multiple matches per substring and an increasing sequence cannot be found" {
+         val indexesOfMatches = listOf(
+            listOf(10, 15, 20,),
+            listOf(8, 16, 24,),
+            listOf(6, 12, 16, ),
+         )
+         subsetFitsInOrder(
+            indexesOfMatches,
+            listOf(0, 1, 2),
+            listOf("a", "b", "c"),
+            { 1 },
+            ) shouldBe false
+      }
+      "returns false when first substring has no matches" {
+         val indexesOfMatches = listOf(
+            emptyList(),
+            listOf(8, 16, 24,),
+            listOf(6, 12, 16, ),
+         )
+         subsetFitsInOrder(
+            indexesOfMatches,
+            listOf(0, 1, 2),
+            listOf("a", "b", "c"),
+            { 1 },
+            ) shouldBe false
+      }
+       "returns false when substring in the middle has no matches" {
+          val indexesOfMatches = listOf(
+             listOf(10, 15, 20,),
+             emptyList(),
+             listOf(6, 12, 16, ),
+          )
+          subsetFitsInOrder(
+             indexesOfMatches,
+             listOf(0, 1, 2),
+             listOf("a", "b", "c"),
+             { 1 },
+             ) shouldBe false
+       }
+       "returns false when last substring has no matches" {
+          val indexesOfMatches = listOf(
+             listOf(10, 15, 20,),
+             listOf(8, 16, 24,),
+             emptyList(),
+          )
+          subsetFitsInOrder(
+             indexesOfMatches,
+             listOf(0, 1, 2),
+             listOf("a", "b", "c"),
+             { 1 },
+             ) shouldBe false
+       }
+      "return true if overlaps allowed" {
+         subsetFitsInOrder(
+            listOf(
+               listOf(0),
+               listOf(1),
+            ),
+            listOf(0, 1),
+            listOf("bread", "read"),
+            { 1 },
+         ) shouldBe true
+      }
+      "return false if overlaps allowed" {
+         subsetFitsInOrder(
+            listOf(
+               listOf(0),
+               listOf(1),
+            ),
+            listOf(0, 1),
+            listOf("bread", "read"),
+            { value -> value.length },
+         ) shouldBe false
+      }
+
+   }
+}
