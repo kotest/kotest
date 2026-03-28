5c7b8b1749627d7f3cff2dccb5ae439527b63fbe
alfonsoristorato <alfonsoristorato@gmail.com>
2026-03-28 15:12:45 +0000

    add tests for InvocationCountExtension

--- kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/interceptors/InvocationCountCheckInterceptor.kt
+++ kotest-framework/kotest-framework-engine/src/jvmTest/kotlin/com/sksamuel/kotest/engine/test/interceptors/InvocationCountCheckInterceptor.kt
@@ -2,6 +2,7 @@ package com.sksamuel.kotest.engine.test.interceptors
 
 import io.kotest.core.annotation.EnabledIf
 import io.kotest.core.annotation.LinuxOnlyGithubCondition
+import io.kotest.core.extensions.InvocationCountExtension
 import io.kotest.core.names.TestNameBuilder
 import io.kotest.core.source.SourceRef
 import io.kotest.core.spec.style.DescribeSpec
@@ -9,10 +10,15 @@ import io.kotest.core.test.TestCase
 import io.kotest.engine.test.TestResult
 import io.kotest.core.test.TestType
 import io.kotest.core.test.config.TestConfig
+import io.kotest.engine.config.ProjectConfigResolver
+import io.kotest.engine.config.SpecConfigResolver
 import io.kotest.engine.config.TestConfigResolver
+import io.kotest.engine.extensions.DefaultExtensionRegistry
 import io.kotest.core.descriptors.toDescriptor
 import io.kotest.engine.test.interceptors.InvocationCountCheckInterceptor
+import io.kotest.engine.test.interceptors.TestEnabledCheckInterceptor
 import io.kotest.engine.test.scopes.NoopTestScope
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 import kotlin.time.Duration.Companion.milliseconds
@@ -76,6 +82,134 @@ class InvocationCountCheckInterceptorTest : DescribeSpec() {
                tc.copy(config = TestConfig(invocations = 4)),
                NoopTestScope(tc, coroutineContext)
             ) { _, _ -> TestResult.Success(0.milliseconds) }.isError shouldBe true
+         }
+      }
+
+      describe("TestConfigResolver.invocations with InvocationCountExtension") {
+
+         it("should use invocation count from extension when no test config sets invocations") {
+            val registry = DefaultExtensionRegistry()
+            registry.add(object : InvocationCountExtension {
+               override fun getInvocationCount(): Int = 5
+            })
+            val resolver = TestConfigResolver(null, registry)
+
+            val tc = TestCase(
+               InvocationCountCheckInterceptorTest::class.toDescriptor().append("foo"),
+               TestNameBuilder.builder("foo").build(),
+               InvocationCountCheckInterceptorTest(),
+               {},
+               SourceRef.None,
+               TestType.Test,
+            )
+
+            resolver.invocations(tc) shouldBe 5
+         }
+
+         it("should prefer test config invocations over extension") {
+            val registry = DefaultExtensionRegistry()
+            registry.add(object : InvocationCountExtension {
+               override fun getInvocationCount(): Int = 5
+            })
+            val resolver = TestConfigResolver(null, registry)
+
+            val tc = TestCase(
+               InvocationCountCheckInterceptorTest::class.toDescriptor().append("foo"),
+               TestNameBuilder.builder("foo").build(),
+               InvocationCountCheckInterceptorTest(),
+               {},
+               SourceRef.None,
+               TestType.Test,
+            )
+
+            resolver.invocations(tc.copy(config = TestConfig(invocations = 3))) shouldBe 3
+         }
+
+         it("should return default invocations when no extension is registered") {
+            val resolver = TestConfigResolver()
+
+            val tc = TestCase(
+               InvocationCountCheckInterceptorTest::class.toDescriptor().append("foo"),
+               TestNameBuilder.builder("foo").build(),
+               InvocationCountCheckInterceptorTest(),
+               {},
+               SourceRef.None,
+               TestType.Test,
+            )
+
+            resolver.invocations(tc) shouldBe 1
+         }
+      }
+
+      describe("interceptor ordering: enabled check before invocation count check") {
+
+         it("should skip a disabled container with invocations > 1 without error") {
+            // A container test that is disabled (name starts with !) and has invocations > 1.
+            // With correct ordering, TestEnabledCheckInterceptor returns Ignored before
+            // InvocationCountCheckInterceptor sees it, so no error is produced.
+            val registry = DefaultExtensionRegistry()
+            registry.add(object : InvocationCountExtension {
+               override fun getInvocationCount(): Int = 5
+            })
+            val testConfigResolver = TestConfigResolver(null, registry)
+
+            val tc = TestCase(
+               InvocationCountCheckInterceptorTest::class.toDescriptor().append("!disabled container"),
+               TestNameBuilder.builder("!disabled container").build(),
+               InvocationCountCheckInterceptorTest(),
+               {},
+               SourceRef.None,
+               TestType.Container,
+            )
+
+            // Chain: TestEnabledCheckInterceptor -> InvocationCountCheckInterceptor -> downstream
+            val enabledCheck = TestEnabledCheckInterceptor(
+               ProjectConfigResolver(), SpecConfigResolver(), testConfigResolver
+            )
+            val invocationCheck = InvocationCountCheckInterceptor(testConfigResolver)
+
+            var downstreamFired = false
+            val result = enabledCheck.intercept(tc, NoopTestScope(tc, coroutineContext)) { tc2, scope2 ->
+               invocationCheck.intercept(tc2, scope2) { _, _ ->
+                  downstreamFired = true
+                  TestResult.Success(0.milliseconds)
+               }
+            }
+
+            // The test should be ignored by the enabled check, never reaching the invocation check
+            downstreamFired.shouldBeFalse()
+            result.isIgnored shouldBe true
+            result.isError shouldBe false
+         }
+
+         it("should still error for an enabled container with invocations > 1") {
+            val registry = DefaultExtensionRegistry()
+            registry.add(object : InvocationCountExtension {
+               override fun getInvocationCount(): Int = 5
+            })
+            val testConfigResolver = TestConfigResolver(null, registry)
+
+            val tc = TestCase(
+               InvocationCountCheckInterceptorTest::class.toDescriptor().append("enabled container"),
+               TestNameBuilder.builder("enabled container").build(),
+               InvocationCountCheckInterceptorTest(),
+               {},
+               SourceRef.None,
+               TestType.Container,
+            )
+
+            val enabledCheck = TestEnabledCheckInterceptor(
+               ProjectConfigResolver(), SpecConfigResolver(), testConfigResolver
+            )
+            val invocationCheck = InvocationCountCheckInterceptor(testConfigResolver)
+
+            val result = enabledCheck.intercept(tc, NoopTestScope(tc, coroutineContext)) { tc2, scope2 ->
+               invocationCheck.intercept(tc2, scope2) { _, _ ->
+                  TestResult.Success(0.milliseconds)
+               }
+            }
+
+            result.isError shouldBe true
          }
       }
    }
