667f1140758091b3546b11e0d942f0a057b3f2b4
Alexander.Kuznetsov <alexander.kuznetsov@target.com>
2026-03-29 13:55:10 -0500

    apiDump

--- kotest-assertions/kotest-assertions-core/src/jsMain/kotlin/io/kotest/matchers/string/JsUtils.kt
+++
@@ -1,7 +0,0 @@
-package io.kotest.matchers.string
-
-internal actual fun describeBestFitForSubstringsInOrder(
-   value: String,
-   substrings: List<String>,
-   matchOffset: MatchOffset,
-) : BestFitForSubstringsInOrderOutcome = BestFitForSubstringsInOrderOutcome.Ineligible("JVM only")
