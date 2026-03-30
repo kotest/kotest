d66240102cbb1da1befbc50ab245e5caf6232a0b
sksamuel <sam@sksamuel.com>
2026-03-30 11:43:12 -0500

    Remove redundant copyright comment from KotestBreadcrumbsCollector file

--- kotest-intellij-plugin/src/main/kotlin/io/kotest/plugin/intellij/breadcrumbs/KotestBreadcrumbsCollector.kt
+++ kotest-intellij-plugin/src/main/kotlin/io/kotest/plugin/intellij/breadcrumbs/KotestBreadcrumbsCollector.kt
@@ -1,4 +1,3 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package io.kotest.plugin.intellij.breadcrumbs
 
 import com.intellij.codeInsight.breadcrumbs.FileBreadcrumbsCollector
