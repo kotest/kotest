6154c9dc34ff718742316df404965ae705f9bce6
Alex Kuznetsov <Alex.Cue.987@gmail.com>
2026-03-30 12:07:58 -0500

    use-powerSetIndexes-from-common-lib (#5822) (#5823)
    use the function from common lib, not the copy-pasted implementation

--- kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/string/Utils.kt
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/matchers/string/Utils.kt
@@ -44,26 +44,3 @@ sealed interface BestFitForSubstringsInOrderOutcome {
    data class Ineligible(val reason: String) : BestFitForSubstringsInOrderOutcome
    object TimedOut : BestFitForSubstringsInOrderOutcome
 }
-
-
-
-//TODO: import this function from common module when that PR is merged
-
-internal fun powerSetIndexes(size: Int): Sequence<List<Int>> = sequence {
-   require(size > 0) { "Size should be positive, was: $size"}
-   val elementsIncluded = MutableList(size) { true }
-   val allIndexes = (0 until size).toList()
-   yield(allIndexes)
-   while(elementsIncluded.any { it }) {
-      for (index in 0 until size) {
-         if (elementsIncluded[index]) {
-            elementsIncluded[index] = false
-            yield(allIndexes.filterIndexed { i, _ -> elementsIncluded[i] })
-            break
-         } else {
-            elementsIncluded[index] = true
-         }
-      }
-   }
-}
-
