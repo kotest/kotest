cf8e369daaa859b8e8221626abdcc8e28b4714ab
sksamuel <sam@sksamuel.com>
2026-04-06 00:53:10 +0100

    Update RNG seed logic and downgrade rgxgen dependency to 2.0

--- kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/stringsjvm.kt
+++ kotest-property/src/jvmMain/kotlin/io/kotest/property/arbitrary/stringsjvm.kt
@@ -20,9 +20,7 @@ fun Arb.Companion.stringPattern(pattern: String): Arb<String> = object : Arb<Str
    override fun sample(rs: RandomSource): Sample<String> = sampleStringPattern(rs)
 
    private fun sampleStringPattern(rs: RandomSource): Sample<String> = synchronized(this) {
-      val javaRandom = Random()
-      javaRandom.setSeed(rs.random.nextLong())
-      val value = rgxgen.generate(javaRandom)
+      val value = rgxgen.generate(Random(rs.random.nextLong()))
       Sample(value)
    }
 }
