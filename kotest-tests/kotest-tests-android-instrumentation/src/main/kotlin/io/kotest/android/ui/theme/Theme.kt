f250f4e6c36aff5ce9675cc04982a9a280c70ec0
sksamuel <sam@sksamuel.com>
2026-04-07 11:20:58 -0500

    Migrate Android source files and tests to correct `main` and `androidTest` source sets

--- kotest-tests/kotest-tests-android-instrumentation/src/main/kotlin/io/kotest/android/ui/theme/Theme.kt
+++ kotest-tests/kotest-tests-android-instrumentation/src/main/kotlin/io/kotest/android/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.androidsampleapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
