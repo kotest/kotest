5c898b4fbd3c9830972d03b4d67af9005c01f759
Alfonso Ristorato <73172255+alfonsoristorato@users.noreply.github.com>
2026-08-11 10:06:01 +0100

    spec tags cache (#6206)
    <!-- 
    If this PR updates documentation, please update all relevant versions of
    the docs, see:
    https://github.com/kotest/kotest/tree/master/documentation/versioned_docs
    The documentation at
    https://github.com/kotest/kotest/tree/master/documentation/docs is the
    documentation for the next minor or major version _TO BE RELEASED_
    -->

--- kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/tags/tags.kt
+++ kotest-framework/kotest-framework-engine/src/jvmMain/kotlin/io/kotest/engine/tags/tags.kt
@@ -6,25 +6,32 @@ import io.kotest.core.annotation.Tags
 import io.kotest.common.reflection.IncludingAnnotations
 import io.kotest.common.reflection.IncludingSuperclasses
 import io.kotest.common.reflection.reflection
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.reflect.KClass
 import kotlin.reflect.full.declaredMemberProperties
+
+// tags() is called on every test/container enabled-check and every display-name format, i.e. once
+// per test case. But a spec (class) tag will not change cross tests within it, hence the cache.
+private val tagsCache = ConcurrentHashMap<Pair<KClass<*>, Boolean>, Set<Tag>>()
 
 /**
  * Returns the tags specified on the given class (and all it's supertypes) from the @[Tags] annotation if present.
  */
 actual fun KClass<*>.tags(tagInheritance: Boolean): Set<Tag> {
-   val meta = setOf(IncludingAnnotations) + if (tagInheritance) setOf(IncludingSuperclasses) else emptySet()
-   val annotations: List<Annotation> = reflection.annotations(this, meta)
-   val tags = annotations.flatMap { a ->
-      if (a is Tags) {
-         a.values.map { NamedTag(it) }
-      } else {
-         a.annotationClass
-            .declaredMemberProperties
-            .map { it.call(a) }
-            .filterIsInstance<Tags>()
-            .flatMap { tags -> tags.values.map { NamedTag(it) } }
+   return tagsCache.getOrPut(this to tagInheritance) {
+      val meta = setOf(IncludingAnnotations) + if (tagInheritance) setOf(IncludingSuperclasses) else emptySet()
+      val annotations: List<Annotation> = reflection.annotations(this, meta)
+      val tags = annotations.flatMap { a ->
+         if (a is Tags) {
+            a.values.map { NamedTag(it) }
+         } else {
+            a.annotationClass
+               .declaredMemberProperties
+               .map { it.call(a) }
+               .filterIsInstance<Tags>()
+               .flatMap { tags -> tags.values.map { NamedTag(it) } }
+         }
       }
+      tags.toSet()
    }
-   return tags.toSet()
 }
